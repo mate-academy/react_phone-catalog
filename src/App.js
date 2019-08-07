9f3954e03a86baa9fc43e1e1b6cc7b815de0cd52
Yevgeniy <zleonenkov@gmail.com>
2019-08-07 11:30:02 +0300

    1-3 tasks was implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
 import React from 'react';
 import './App.css';
+import {
+  HashRouter,
+  Route,
+  NavLink,
+  Switch,
+} from 'react-router-dom';
 
-const App = () => (
-  <div className="App">
-    <h1>Phone catalog</h1>
-  </div>
-)
+import HomePage from './components/HomePage';
+import PhonesPage from './components/PhonesPage';
+import NotFoundPage from './components/NotFoundPage';
+
+class App extends React.Component {
+  state = {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <HashRouter>
+        <div className="App">
+
+          <nav>
+            <ul className="nav">
+
+              <li>
+                <NavLink
+                  className="navlink"
+                  to="/"
+                  exact
+                >
+                  HomePage
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  className="navlink"
+                  to="/phones/"
+                >
+                  PhonesPage
+                </NavLink>
+              </li>
+
+            </ul>
+          </nav>
+
+          <Switch>
+
+            <Route
+              path="/"
+              exact
+              component={HomePage}
+            />
+
+            <Route
+              path="/phones/:phoneId?"
+              exact
+              component={PhonesPage}
+            />
+
+            <Route
+              path="*"
+              exact
+              component={NotFoundPage}
+            />
+
+          </Switch>
+
+        </div>
+      </HashRouter>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/PhonesPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PhonesPage = () => (
+  <div>
+    <h1>Phones page</h1>
+  </div>
+);
+
+export default PhonesPage;
