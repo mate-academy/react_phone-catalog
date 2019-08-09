8febb6836ed89f0d15d57ef56106bc3001320e6a
h2ash <hun22ash@gmail.com>
2019-08-09 18:39:15 +0300

    11th step is implemented

--- src/components/PhoneDetails.jsx
+++ src/components/PhoneDetails.jsx
@@ -1,42 +1,66 @@
 import React from 'react'
 
-const PhoneDetails = ({id, phone, details}) => {
-  console.log(details);
+class PhoneDetails extends React.Component {
+  state = {
+    currentImg: `/${this.props.phone.imageUrl}`,
+  }
 
-  return (
-    <div>
-      <section
-        className='currentPhone__top-block'
-      >
-        <img 
-          className='currentPhone__top-block-current-img'
-          src={`/${phone.imageUrl}`}
-          alt="phone_photo"
-        />
-        <h1 className='currentPhone__top-block-title'>
-          {phone.name}
-        </h1>
-        <div
-          className='currentPhone__top-block-snippet'
+  componentDidMount = () => {
+    const currentImage = `/${this.props.phone.imageUrl}`;
+
+    this.setState({
+      currentImg: currentImage,
+    })
+  }
+
+  chooseCurrentImg = (event) => {
+    const { src } = event.target;
+
+    this.setState({
+      currentImg: src,
+    })
+  }
+
+  render() {
+    const { id, phone, details } = this.props;
+    const { currentImg } = this.state;
+
+    return (
+      <div>
+        <section
+          className='currentPhone__top-block'
         >
-          {phone.snippet}
-        </div>
-        <ul className='currentPhone__top-block-all-imgs'>
-          {
-            details.images.map(image => 
-              <img 
-                className='currentPhone__top-block-all-imgs-item'
-                src={`/${image}`} 
-                alt=""
-              />
-            )
-          }
-        </ul>
-      </section>
-      <section>
-      </section>
-    </div>
-  )
+          <img
+            className='currentPhone__top-block-current-img'
+            src={currentImg}
+            alt="phone_photo"
+          />
+          <h1 className='currentPhone__top-block-title'>
+            {phone.name}
+          </h1>
+          <div
+            className='currentPhone__top-block-snippet'
+          >
+            {phone.snippet}
+          </div>
+          <ul className='currentPhone__top-block-all-imgs'>
+            {
+              details.images.map(image =>
+                <img
+                  onClick={this.chooseCurrentImg}
+                  className='currentPhone__top-block-all-imgs-item'
+                  src={`/${image}`}
+                  alt=""
+                />
+              )
+            }
+          </ul>
+        </section>
+        <section>
+        </section>
+      </div>
+    )
+  }
 }
 
 export default PhoneDetails
