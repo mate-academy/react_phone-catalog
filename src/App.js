17b69059f48abf6aa1812552b859a39d5892efac
Nastia Krat <nastyakpi1995@gmail.com>
2019-08-09 16:27:41 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -11,31 +11,69 @@ const HomePage = () => (
   </div>
 );
 
-const App = () => (
-  <div className="App">
-    <nav className="nav__main_container">
-      <NavLink
-        to="/"
-        exact
-        className="page__home phone__position"
-        activeClassName="phoneClassActive"
-      >
+const getPhones = async() => {
+  const url = 'https://mate-academy.github.io/phone-catalogue-static';
+  const response = await fetch(
+    `${url}/api/phones.json`
+  );
+
+  const currentContent = await response.json();
+
+  return currentContent;
+};
+
+class App extends React.Component {
+  state = {
+    phones: [],
+  }
+
+  async componentDidMount() {
+    const phones = await getPhones();
+
+    this.setState(prevState => ({
+      phones,
+    }));
+  }
+
+  render() {
+    const { phones } = this.state;
+
+    return (
+      <div className="App">
+        <nav className="nav__main_container">
+          <NavLink
+            to="/"
+            exact
+            className="page__home phone__position"
+            activeClassName="phoneClassActive"
+          >
           Home
-      </NavLink>
-      <NavLink
-        to="/phones"
-        className="Phones__page"
-        activeClassName="phoneClassActive"
-      >
+          </NavLink>
+          <NavLink
+            to="/phones"
+            className="Phones__page"
+            activeClassName="phoneClassActive"
+          >
         PhonesPage
-      </NavLink>
-    </nav>
-    <Switch>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/phones" component={PhonesPage} />
-      <Route path="*" exact component={NotFoundPage} />
-    </Switch>
-  </div>
-);
+          </NavLink>
+        </nav>
+        <Switch>
+
+          <Route path="/" exact component={HomePage} />
+          <Route
+            path="/phones/:id?"
+            render={({ match }) => (
+              <PhonesPage
+                phones={phones}
+                match={match.params.id}
+              />
+            )}
+          />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/PhonesPage.js
+++ src/PhonesPage.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Loading from './Loading';
 
-const Tabs = ({ tabs, tabId }) => (
-  <div
-    className="container"
-  >
-    <h1>Phones</h1>
-  </div>
+const PhonesPage = ({ phones }) => (
+  phones.length === 0 ? <Loading />
+    : (
+      <div className="card" data-qa="card">
+        <img
+          src={phones.map(img => img.imageUrl)}
+          alt="iMac"
+          className="card__img"
+        />
+        <div className="card__text">{phones.map(phone => phone.name)}</div>
+        <div>Код товара: 195434</div>
+
+        <div className="card__line">
+          <div className="start__container">
+            <div className="star start-active" />
+            <div className="star start-active" />
+            <div className="star start-active" />
+            <div className="star start-active" />
+            <div className="star" />
+          </div>
+
+          <div className="card__reviev">Отзывов: 5</div>
+        </div>
+
+        <div className="card__block-price">
+          <div className="card__price">Цена:</div>
+          <div className="card__price-number">69 999 грн</div>
+        </div>
+      </div>
+    )
 );
 
-Tabs.propTypes = {
-  tabs: PropTypes.isRequired,
-  tabId: PropTypes.string.isRequired,
+PhonesPage.propTypes = {
+  phones: PropTypes.shape(
+    PropTypes.array,
+    PropTypes.string,
+    PropTypes.object,
+  ).isRequired,
 };
 
-export default Tabs;
+export default PhonesPage;
