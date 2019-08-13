369ab69156393f84a4ac028cc5c448e6a5d5ba5a
AnnaKuvarina <anya.kuvarina@gmail.com>
2019-08-13 14:35:39 +0300

    not found page

--- src/NotFoundPage.js
+++ src/NotFoundPage.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import './styles/homePage.css';
 
-const NotFoundPage = ({ match }) => {
-  if (match.path !== '/') {
-    return (
-      <h2 className="notfound-title">Error! Page url uncorrected</h2>
-    );
-  }
-};
+const NotFoundPage = () => (
+  <h2 className="notfound-title">Error! Page url uncorrected</h2>
+);
 
 export default NotFoundPage;
