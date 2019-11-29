7cc9bf03fe2e39f8a4dde5bf8e3c799f8cdd101d
h2ash <hun22ash@gmail.com>
2019-11-29 03:31:04 +0200

    localStorage for basket was implemented

--- src/App.js
+++ src/App.js
@@ -1,72 +1,100 @@
-import React from 'react';
-import './styles/app.scss';
-import {
-  Route,
-  Switch,
-} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import LoaderPageOfPhones from './pages/LoaderPageOfPhones';
-import Page404 from './pages/Page404';
-import LoaderDetailsForOnePhone from './pages/LoaderDetailsForOnePhone';
-import BasketPage from './pages/BasketPage';
-import { BASE_URL } from './components/constants';
+import React from "react";
+import "./styles/app.scss";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import LoaderPageOfPhones from "./pages/LoaderPageOfPhones";
+import Page404 from "./pages/Page404";
+import LoaderDetailsForOnePhone from "./pages/LoaderDetailsForOnePhone";
+import BasketPage from "./pages/BasketPage";
+import { BASE_URL } from "./components/constants";
 
 class App extends React.Component {
   state = {
-    phones: '',
+    phones: "",
     isLoading: false,
     isLoaded: false,
-    itemsAtBasket: [],
+    itemsAtBasket: []
   };
+
+  // БУДЕМ ИСПОЛЬЗОВАТЬ НА ПРАКТИКЕ?
+  componentDidMount() {
+    const itemsFromBasketInLocal = JSON.parse(
+      localStorage.getItem("itemsFromBasketInLocal")
+    );
+
+    if (itemsFromBasketInLocal !== null) {
+      this.setState({
+        itemsAtBasket: itemsFromBasketInLocal
+      });
+    }
+  }
 
   basketManager = (id, operation) => {
-    const currentIndex = this.state.itemsAtBasket
-      .findIndex(element => element.id === id);
+    const currentIndex = this.state.itemsAtBasket.findIndex(
+      element => element.id === id
+    );
 
-    this.setState((prevState) => {
-      let changedArray = [...prevState.itemsAtBasket];
+    this.setState(
+      prevState => {
+        let changedArray = [...prevState.itemsAtBasket];
 
-      switch (operation) {
-        case 'increase':
-          return changedArray[currentIndex].quantity += 1;
-        case 'decrease':
-          changedArray[currentIndex].quantity === 1
-            ? changedArray = changedArray.filter(obj => obj.id !== id)
-            : changedArray[currentIndex].quantity -= 1;
-          break;
-        case 'remove':
-          changedArray = changedArray.filter(obj => obj.id !== id);
-          break;
-        default:
-          console.log("Данный случай отстуствует среди данных условий");
+        switch (operation) {
+          case "increase":
+            return (changedArray[currentIndex].quantity += 1);
+          case "decrease":
+            changedArray[currentIndex].quantity === 1
+              ? (changedArray = changedArray.filter(obj => obj.id !== id))
+              : (changedArray[currentIndex].quantity -= 1);
+            break;
+          case "remove":
+            changedArray = changedArray.filter(obj => obj.id !== id);
+            break;
+          default:
+            console.log("Данный случай отстуствует среди данных условий");
+        }
+
+        return {
+          itemsAtBasket: changedArray
+        };
+      },
+      () => {
+        localStorage.setItem(
+          "itemsFromBasketInLocal",
+          JSON.stringify(this.state.itemsAtBasket)
+        );
       }
-
-      return {
-        itemsAtBasket: changedArray,
-      };
-    });
+    );
   };
 
-  addItemToBasket = (itemToAdd) => {
-    const currentIndex = this.state.itemsAtBasket
-      .findIndex(element => element.id === itemToAdd.id);
+  addItemToBasket = itemToAdd => {
+    const currentIndex = this.state.itemsAtBasket.findIndex(
+      element => element.id === itemToAdd.id
+    );
 
     if (currentIndex < 0) {
       const requiredItem = { ...itemToAdd };
 
       requiredItem.quantity = 1;
 
-      this.setState(prevState => ({
-        itemsAtBasket: [...prevState.itemsAtBasket, requiredItem],
-      }));
-    };
+      this.setState(
+        prevState => ({
+          itemsAtBasket: [...prevState.itemsAtBasket, requiredItem]
+        }),
+        () => {
+          localStorage.setItem(
+            "itemsFromBasketInLocal",
+            JSON.stringify(this.state.itemsAtBasket)
+          );
+        }
+      );
+    }
   };
 
-  loadDataPhones = async() => {
+  loadDataPhones = async () => {
     this.setState({
       isLoaded: false,
-      isLoading: true,
+      isLoading: true
     });
 
     const responsePhones = await fetch(`${BASE_URL}/api/phones.json`);
@@ -75,28 +103,20 @@ class App extends React.Component {
     this.setState({
       phones,
       isLoading: false,
-      isLoaded: true,
+      isLoaded: true
     });
   };
 
   render() {
-    const {
-      phones, isLoading, isLoaded, itemsAtBasket,
-    } = this.state;
+    const { phones, isLoading, isLoaded, itemsAtBasket } = this.state;
 
     return (
       <>
-        <Navbar
-          itemsAtBasket={itemsAtBasket}
-        />
+        <Navbar itemsAtBasket={itemsAtBasket} />
 
         <main>
           <Switch>
-            <Route 
-              path="/"
-              exact
-              component={HomePage}
-            />
+            <Route path="/" exact component={HomePage} />
             <Route
               path="/phones"
               exact
@@ -131,9 +151,7 @@ class App extends React.Component {
                 />
               )}
             />
-            <Route 
-              component={Page404} 
-            />
+            <Route component={Page404} />
           </Switch>
         </main>
       </>
