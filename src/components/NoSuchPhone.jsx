8f98abf436d56dd248e1a019b4f26589fd22bce1
h2ash <hun22ash@gmail.com>
2019-12-09 22:46:26 +0200

    page404 + CSS were implemented, NoSuchPhone component + CSS were refactored

--- src/components/NoSuchPhone.jsx
+++ src/components/NoSuchPhone.jsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom'
 
 const NoSuchPhone = () => (
   <>
-    <h1 className="loader-details-page__heading">No any data for this phone</h1>
+    <h1 className="no-such-phone__heading">No any data for this phone</h1>
     <img
-      className="loader-details-page__img"
+      className="no-such-phone__img"
       src="https://i.kym-cdn.com/entries/icons/original/000/022/146/azazazazazazaz.jpg"
       alt="" />
     <NavLink
--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Page404 = () => (
-  <main className="wrapper__main">Not found page!</main>
+  <main className="wrapper__main page404">
+    <h1 className="page404__heading">404</h1>
+    <p>No such page</p>
+    <img
+      className="page404__img"
+      src="https://image.jimcdn.com/app/cms/image/transf/none/path/s56566d5c71d6dab4/backgroundarea/i35686abb714db1b7/version/1533048399/image.jpg" alt="" />
+  </main>
 );
 
 export default Page404;
