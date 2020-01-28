e0ab8c86b0f5da31eed2a21cf24c1298956c3e1b
mgrinko <glukym@gmail.com>
2020-01-28 09:54:57 +0200

    Static files were added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import './App.css';
+import Catalog from './Catalog';
+import Details from './Details';
 
-const App = () => <h1>Phone catalog</h1>;
+const App = () => (
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-md-2">
+        <section>
+          <h2>Filter</h2>
+
+          <label>
+            <div>Search:</div>
+            <input />
+          </label>
+
+          <label>
+            <div>Sort by:</div>
+            <select>
+              <option value="name">Alphabetical</option>
+              <option value="age">Newest</option>
+            </select>
+          </label>
+        </section>
+
+        <section>
+          <h2>Shopping Cart</h2>
+          <ul>
+            <li>Phone 1</li>
+            <li>Phone 2</li>
+            <li>Phone 3</li>
+          </ul>
+        </section>
+      </div>
+
+      <div className="col-md-10">
+        <Details />
+        <Catalog />
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
