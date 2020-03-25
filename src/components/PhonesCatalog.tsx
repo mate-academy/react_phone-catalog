e4d0e6c389de317822dd43edc1eacfc344d04a98
masha <mariafil2107@gmail.com>
2020-03-25 16:14:25 +0200

    implemented sort and filter

--- src/components/PhonesCatalog.tsx
+++ src/components/PhonesCatalog.tsx
@@ -1,9 +1,22 @@
-import React, { useState, useEffect, FC } from 'react';
+import React, { useState, useEffect, FC, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { PHONES_URL } from '../api/constants';
 import { Phone } from './Phone';
 
-export const PhonesCatalog: FC = () => {
+interface Props {
+  filter: string;
+  sort: string;
+}
+
+const filterPhones = (filter: string, phones: Phone[]): Phone[] => {
+  const filterToLowerCase = filter.toLowerCase();
+
+  return phones
+    .filter(phone => phone.name.toLocaleLowerCase().includes(filterToLowerCase)
+    || phone.snippet.toLocaleLowerCase().includes(filterToLowerCase));
+};
+
+export const PhonesCatalog: FC<Props> = ({ filter, sort }) => {
   const [phones, setPhones] = useState<Phone[]>([]);
 
   useEffect(() => {
@@ -11,9 +24,34 @@ export const PhonesCatalog: FC = () => {
       .then(async data => setPhones(await data.json()));
   }, []);
 
+  useEffect(() => {
+    switch (sort) {
+      case 'age': {
+        setPhones([...phones].sort((a, b) => a.age - b.age));
+        break;
+      }
+
+      case 'name': {
+        setPhones([...phones].sort((a, b) => a.name.localeCompare(b.name)));
+        break;
+      }
+
+      default:
+        setPhones(phones);
+    }
+  }, [sort]);
+
+  const phonesToShow = useMemo(() => {
+    if (!phones.length) {
+      return [];
+    }
+
+    return filterPhones(filter, phones);
+  }, [filter, phones]);
+
   return (
     <ul className="phones__list">
-      {phones.map(phone => (
+      {phonesToShow.map(phone => (
         <li className="phones__item" key={phone.id}>
           <Link className="link" to={`/phones/${phone.id}`}>
             <Phone {...phone} />
--- src/components/PhonesPage.tsx
+++ src/components/PhonesPage.tsx
@@ -1,13 +1,39 @@
-import React, { FC } from 'react';
+import React, { FC, useState, ChangeEvent } from 'react';
 import { PhonesCatalog } from './PhonesCatalog';
 
 export const PhonesPage: FC = () => {
+  const [filter, setFilter] = useState('');
+  const [sort, setSort] = useState('age');
+
+  const handleFilter = (e: ChangeEvent<HTMLInputElement>): void => {
+    setFilter(e.target.value);
+  };
+
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSort(e.target.value);
+  };
+
   return (
     <div className="phones">
-      <h1>
-        Phones page
-      </h1>
-      <PhonesCatalog />
+      <div className="settings">
+        <input
+          type="text"
+          value={filter}
+          onChange={handleFilter}
+          className="settings__search"
+          placeholder="Search..."
+        />
+        <select
+          defaultValue="disabled"
+          onChange={(e) => handleSelect(e)}
+          className="settings__select"
+        >
+          <option value="disabled" disabled>Sort by</option>
+          <option value="age">Date</option>
+          <option value="name">Name</option>
+        </select>
+      </div>
+      <PhonesCatalog filter={filter} sort={sort} />
     </div>
   );
 };
