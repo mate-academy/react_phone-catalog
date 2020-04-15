b4e6b354a51a4f4f24c7e7c4fdae02e7b508b35e
Anna <anna.lyrynenko@gmail.com>
2020-04-15 10:47:26 +0300

    ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,35 @@
 /// <reference types="react-scripts" />
+interface Phone {
+  id: string;
+  name: string;
+  phoneId: string;
+  priceRegular: number;
+  priceDiscount: number;
+  screen: string;
+  capacity: string;
+  color: string;
+  ram: string;
+  year: number;
+  image: string;
+}
+
+interface Details {
+  id: string;
+  namespaceId: string;
+  name: string;
+  capacityAvailable: string[];
+  capacity: string;
+  priceRegular: number;
+  priceDiscount: number;
+  colorsAvailable: string[];
+  color: string;
+  images: string[];
+  description: Description[];
+  screen: string;
+  resolution: string;
+  processor: string;
+  ram: string;
+  camera: string;
+  zoom: string;
+  cell: string[];
+}
