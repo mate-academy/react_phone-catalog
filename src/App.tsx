dcf5fba0a6bf344c5164081c6ead50155b84a1e0
Kira Marks <serafita22@gmail.com>
2020-05-22 18:29:48 +0300

    App refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Footer } from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div className="App">
       <h1>React Phone Catalog</h1>
-      <Footer />
     </div>
   );
 };
