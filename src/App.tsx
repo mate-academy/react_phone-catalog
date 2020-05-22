9d65424290b93e5980e6ca00930b1ecb01c43139
Oleg <fasfas7971@gmail.com>
2020-05-22 16:01:00 +0300

    getData function was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.scss';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { getPhones, getPhoneById } from './helpers/getPhones';
 
 export const App = () => {
+  const [phonesProduct, setPhonesProduct] = useState<PhoneCatalog[]>([]);
+  const [phonesDetails, setPhonesDetails] = useState<PhoneDetail[]>([]);
+  const [loadError, setLoadError] = useState(false);
+
+  console.log(phonesProduct);
+  console.log(phonesDetails);
+  console.log(loadError);
+
+  useEffect(() => {
+    getPhones()
+      .then(phones => {
+        setPhonesProduct(phones);
+        return getPhoneById(phones);
+      })
+      .then(details => setPhonesDetails(details))
+      .catch(() => setLoadError(true))
+  }, []);
+
   return (
     <>
       <Header />
