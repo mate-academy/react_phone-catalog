f11cb08b6b9ff10dbf3eed565ce3d2273a51ab8e
Yevhenii <DrozhzhynY>
2020-05-25 21:26:46 +0300

    some fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import { Home } from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 
 const App: React.FC = () => (
@@ -9,7 +10,7 @@ const App: React.FC = () => (
     <Header />
     <main>
       <Switch>
-        <Route path="/home" component={() => <h1>Home</h1>} />
+        <Route path="/home" component={Home} />
         <Route path="/phones" component={() => <h1>phones</h1>} />
         <Route path="/tablets" component={() => <h1>tablets</h1>} />
         <Route path="/accessories" component={() => <h1>accessories</h1>} />
