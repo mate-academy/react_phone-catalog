b7b1b3efc031b3b244a82722ec5bae3770b69f68
Oleg <fasfas7971@gmail.com>
2020-05-25 10:26:46 +0300

    fix width

--- src/components/ShopCategory/ShopCategory.tsx
+++ src/components/ShopCategory/ShopCategory.tsx
@@ -7,48 +7,45 @@ interface Props {
   cards: Good[];
 }
 
-export const ShopCategory: React.FC<Props> = ({ cards }) => {
-  const imageWidth = 368;
-  const imageGap = 16;
-  const imageCount = 3;
-
-  return (
-    <div
-      className="Category"
-      style={{
-        width: `${imageWidth * imageCount + (imageGap * (imageCount - 1))}px`,
-      }}
-    >
-      <h2 className="Category__Title">
-        Shop by category
+export const ShopCategory: React.FC<Props> = ({ cards }) => (
+  <div
+    className="Category"
+  >
+    <h2 className="Category__Title">
+      Shop by category
         </h2>
-      <div className="Category__Container">
-        <ul className="Category__List">
-          {sectionsLinks.map(link => (
-            <li className="Category__Item" key={link.name}>
-              <Link to={link.url} className="Category__Link">
-                <div className="Category__ImageContainer">
-                  <img
-                    src={link.imgUrl}
-                    alt={link.title}
-                    className="Category__Image"
-                  />
-                </div>
+    <div className="Category__Container">
+      <ul className="Category__List">
+        {sectionsLinks.map(link => (
+          <li
+            className="Category__Item"
+            key={link.name}
+          >
+            <Link to={link.url} className="Category__Link">
+              <div
+                className="Category__ImageContainer"
+              >
+                <img
+                  src={link.imgUrl}
+                  alt={link.title}
+                  className="Category__Image"
+                />
+              </div>
 
-                <div className="Category__Info">
-                  <h3 className="Category__InfoType Category__Paragraph">
-                    {link.title}
-                  </h3>
-                  <p className="Category__InfoCount Category__Paragraph">
-                    {cards.filter(card => card.type === link.type).length}
-                    {' models'}
-                  </p>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+              <div className="Category__Info">
+                <h3 className="Category__InfoType Category__Paragraph">
+                  {link.title}
+                </h3>
+                <p className="Category__InfoCount Category__Paragraph">
+                  {cards.filter(card => card.type === link.type).length}
+                  {' models'}
+                </p>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
-};
+  </div>
+);
+
