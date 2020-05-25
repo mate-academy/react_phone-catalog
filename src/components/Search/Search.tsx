29ff2a24cb1c73df663f134ed4aafb25f2b5c146
Oleksii Mykhailov <alem.coding@gmail.com>
2020-05-25 13:31:51 +0300

    Fix clear btn

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import cn from 'classnames';
 
@@ -10,13 +10,27 @@ export const Search = () => {
   const searchParams = new URLSearchParams(location.search);
   const query = searchParams.get('query') || '';
 
+  const myRef = useRef<HTMLInputElement>(null);
+
   const updateQuery = useCallback(
     (actualQuery: string): void => {
       searchParams.set('query', actualQuery);
+      searchParams.set('page', '1');
       history.push({ search: searchParams.toString() });
     },
     [searchParams, history],
   );
+
+  const clearInput = () => {
+    searchParams.set('query', '');
+    history.push({ search: searchParams.toString() });
+  };
+
+  useEffect(() => {
+    if (query === '') {
+      myRef.current!.focus();
+    }
+  }, [query]);
 
   const handleQueryUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
@@ -32,12 +46,16 @@ export const Search = () => {
           className="Search__Input"
           value={query}
           placeholder="Search in phones..."
+          ref={myRef}
           onChange={handleQueryUpdate}
         />
-        <button type="button" className={cn({
-          'Search__Button': true,
-          'Search__Button--clear': false,
-        })} />
+        <button
+          className={cn({
+            Search__Button: true,
+            'Search__Button--clear': query.length > 0,
+          })}
+          onClick={clearInput}
+        />
       </label>
     </form>
   );
