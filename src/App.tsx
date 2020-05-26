8be82675a5885b21305c0d7b601fa77b10f0118c
T Z <kazakova.tanya@gmail.com>
2020-05-26 21:11:29 +0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.scss';
 import { Header } from './components/Header';
+import { NotFoundPage } from './components/NotFoundPage';
 
-const App = () => (
-  <div className="App">
-    <Header />
-  </div>
-);
+
+const Home = () => <h2>Home2</h2>;
+const Footer = () => <h2>Footer</h2>;
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={Home}
+        />
+        <Redirect from="/home" to="/" />
+        <Route component={NotFoundPage} />
+      </Switch>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
