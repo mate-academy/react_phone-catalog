f3b238e4c7d9dfd3a6fd29e91daf5855df941d21
Yevhenii <DrozhzhynY>
2020-05-26 16:07:36 +0300

    added shopByCategory and fonts

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ProductsSlider from '../ProductsSlider/ProductsSlider';
 import MainSlider from '../MainSlider/MainSlider';
+import { ShopByCategory } from '../ShopByCategory/ShopByCategory';
+import ProductsSlider from '../ProductsSlider/ProductsSlider';
 
 export const Home = () => (
   <>
     <MainSlider />
     <ProductsSlider />
+    <ShopByCategory />
   </>
 );
---
+++ src/components/ShopByCategory/ShopByCategory.tsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './ShopByCategory.scss';
+
+import { getProducts } from '../../helpers/api';
+
+interface Slide {
+  imageUrl: string;
+  name: string;
+  snippet: string;
+  price: number;
+  discount: number;
+  screen: string;
+  capacity: string;
+  ram: string;
+  id: string;
+  type: string;
+}
+
+
+export const ShopByCategory = () => {
+  const [phones, setPhones] = useState<Slide[]>([]);
+  const [tablets, setTablets] = useState<Slide[]>([]);
+  const [accessories, setAccessories] = useState<Slide[]>([]);
+
+  useEffect(() => {
+    getProducts().then(data => setPhones(data.filter((product: Slide) => product.type === 'phone')));
+  }, []);
+
+  useEffect(() => {
+    getProducts().then(data => setTablets(data.filter((product: Slide) => product.type === 'tablet')));
+  }, []);
+
+  useEffect(() => {
+    getProducts().then(data => setAccessories(data
+      .filter((product: Slide) => (product.type !== 'tablet' && product.type !== 'phone'))));
+  }, []);
+
+  return (
+    <div className="wrap-category">
+      <h2 className="category__title">Shop by category</h2>
+      <div className="category">
+        <div className="wrap-inner">
+          <Link to="/phones">
+            <div className="category__phones">
+              <img
+                src="./img/category/category-phones.png"
+                alt="category__phones"
+                className="category__img"
+              />
+            </div>
+            <h3 className="category__subtitle">Mobile phones</h3>
+            <p className="category__sum-models">
+              {phones.length}
+              {' '}
+              models
+            </p>
+          </Link>
+        </div>
+        <div className="wrap-inner">
+          <Link to="/tablets">
+            <div className="category__tablets">
+              <img
+                src="./img/category/category-tablets.png"
+                alt="category__phones"
+                className="category__img"
+              />
+            </div>
+            <h3 className="category__subtitle">Tablets</h3>
+            <p className="category__sum-models">
+              {tablets.length}
+              {' '}
+              models
+            </p>
+          </Link>
+        </div>
+        <div className="wrap-inner">
+          <Link to="/accessories">
+            <div className="category__accessories">
+              <img
+                src="./img/category/category-accessories.png"
+                alt="category__phones"
+                className="category__img"
+              />
+            </div>
+            <h3 className="category__subtitle">Accessories</h3>
+            <p className="category__sum-models">
+              {accessories.length}
+              {' '}
+              models
+            </p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
