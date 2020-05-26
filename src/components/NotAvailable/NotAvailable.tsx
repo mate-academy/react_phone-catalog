49e7c8b78d136641b4d1690b7b10ae7d793f22dd
Oleg <fasfas7971@gmail.com>
2020-05-26 14:53:27 +0300

    accessories was created

---
+++ src/components/NotAvailable/NotAvailable.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotAvailable.scss';
+
+export const NotAvailable = () => (
+  <div className="NotAvailable">
+    <img
+      src="images/notAvailable.jpg"
+      alt="Goods not found"
+      className="NotAvailable__Image"
+    />
+  </div>
+)
