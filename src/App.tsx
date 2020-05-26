bbeaefb2f36a2c59f0aadd9f42e80ff2cee3abce
Igor Goncharenko <qoa11a.merry@gmail.com>
2020-05-26 16:09:06 +0300

    Footer realisation. "Back to top" is not functional yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { Footer } from './components/Footer/Footer';
 import './App.scss';
 
 const App = () => (
   <div className="App">
     <h1>React Phone Catalog</h1>
+    <Footer />
   </div>
 );
 
