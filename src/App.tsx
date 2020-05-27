70cfa356eb759f01199141ec72c500bc92603fda
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-05-27 14:39:05 +0300

    routing was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-
+import {
+  Route, Switch, Redirect,
+} from 'react-router-dom';
+import { Header } from './components/Header';
+import { PhonesPage } from './pages/PhonesPage';
+import { HomePage } from './pages/HomePage';
+import { TabletsPage } from './pages/TabletsPage';
+import { AccessoriesPage } from './pages/AccessoriesPage';
+import { Footer } from './components/Footer';
 import './App.scss';
 
 const App = () => (
-  <div className="App">
-    <h1>Start</h1>
-    <p>Hello Vlad</p>
-  </div>
+  <>
+    <Header />
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Redirect from="/home" to="/" />
+      <Route path="/phones" component={PhonesPage} />
+      <Route path="/tablets" component={TabletsPage} />
+      <Route path="/accessories" component={AccessoriesPage} />
+      <h1>Page not found</h1>
+    </Switch>
+    <Footer />
+  </>
 );
 
 export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <h1>Footer</h1>
+  );
+};
