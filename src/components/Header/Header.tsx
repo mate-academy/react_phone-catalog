7d3bc5c0deb101573d2a8bd9a56f5441db33372d
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-05-28 10:35:09 +0300

    search was updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,18 +6,41 @@ import './Header.scss';
 export const Header = () => {
   return (
     <header className="header">
-      <Link to="/">
-        <img src="./LOGO.svg" alt="logo" />
-      </Link>
-      <Nav />
-      <Route path="/:all">
-        <input
-          className="header__input"
-          type="text"
-        />
-      </Route>
-      <Link to="/favorites">FAV</Link>
-      <Link to="/card">CARD</Link>
+      <div className="header__left">
+        <Link to="/">
+          <img src="./LOGO.svg" alt="logo" />
+        </Link>
+        <Nav />
+      </div>
+      <div className="header__right">
+        <Route path="/:all">
+          <div className="search header__search">
+            <img src="./img/search.svg" alt="heart_icon" className="search__icon" />
+            <input
+              className="header__input"
+              type="text"
+              placeholder="Search in phones..."
+            />
+
+          </div>
+        </Route>
+        <Link to="/favorites">
+          <button
+            type="button"
+            className="header__button"
+          >
+            <img src="./img/heart.svg" alt="heart_icon" className="" />
+          </button>
+        </Link>
+        <Link to="/card">
+          <button
+            type="button"
+            className="header__button"
+          >
+            <img src="./img/shopingBag.svg" alt="shopping_icon" className="" />
+          </button>
+        </Link>
+      </div>
 
     </header>
   );
