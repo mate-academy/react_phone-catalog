90dd8da30e4c5c5ca7f92ca7fb2eeae595562c17
Tetiana <ishchuktr@gmail.com>
2020-05-29 15:19:22 +0300

    solution

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Slider } from '../../components/Slider';
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <h1>homePage</h1>
+      <Slider />
+    </div>
+
+  );
+};
