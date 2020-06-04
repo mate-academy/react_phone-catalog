ef4a979d0fcede74d83fbf05670ee4f6e4615995
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-06-04 19:28:50 +0300

    redux was implemented

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store';
 
 ReactDOM.render(
-  <HashRouter><App /></HashRouter>,
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
   document.getElementById('root'),
 );
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -19,3 +19,53 @@ type Slider = {
   id: number;
   imgUrl: string;
 };
+
+interface Product {
+  additionalFeatures: string;
+  android: {
+    os: string;
+    ui: string;
+  };
+  availability: Array;
+  battery: {
+    standbyTime: string;
+    talkTime: string;
+    type: string;
+  };
+  camera: {
+    features: Array;
+    primary: string;
+  };
+  connectivity: {
+    bluetooth: string;
+    cell: string;
+    gps: boolean;
+    infrared: boolean;
+    wifi: string;
+  };
+  description: string;
+  display: {
+    screenResolution: string;
+    screenSize: string;
+    touchScreen: boolean;
+  };
+  hardware: {
+    accelerometer: boolean;
+    audioJack: string;
+    cpu: string;
+    fmRadio: boolean;
+    physicalKeyboard: boolean;
+    usb: string;
+  };
+  id: string;
+  images: Array;
+  name: string;
+  sizeAndWeight: {
+    dimensions: Array;
+    weight: string;
+  };
+  storage: {
+    flash: string;
+    ram: string;
+  };
+}
