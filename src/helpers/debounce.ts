3e7b199cf767631854d9e1922d8b37f4d53ec4e7
Oly Timoshenko <o.b.timoshenko@gmail.com>
2020-06-10 10:54:42 +0300

    added infinity, dots

--- src/helpers/debounce.ts
+++ src/helpers/debounce.ts
@@ -1,7 +1,6 @@
 let timerId: ReturnType<typeof setTimeout>;
 
 export const debounce = (f: (value: string) => void, delay: number) => {
-
   return (value: string) => {
     clearTimeout(timerId);
     timerId = setTimeout(() => f(value), delay);
