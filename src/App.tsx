a97061ec67340f606e3cff5dd556aced2f79dde5
Andrey Faychak <faychak333@gmail.com>
2020-06-15 12:17:47 +0300

    q

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.scss';
 import Header from './components/Header/Header';
-import HomePage from "./components/homePage/homePage";
+import HomePage from './components/homePage/homePage';
 
 const App = () => (
   <div className="App">
