822dc9b78e7fa69a492de78c8d29b4a0223125b3
Yarik1993 <makarov1603@icloud.com>
2020-06-15 12:14:42 +0300

    add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './App.scss';
 import Header from './components/Header/Header';
-import HomePage from "./components/homePage/homePage";
+import HomePage from './components/homePage/homePage';
+import Footer from './components/Footer/Footer';
 
 const App = () => (
   <div className="App">
     <Header />
     <HomePage />
+    <Footer />
   </div>
 );
 
