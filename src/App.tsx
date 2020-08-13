fbc12fad80c1af06fadfb66c3cf1512ccd3ea98f
tyooma <flying.alien.godzilla@gmail.com>
2020-08-14 02:34:02 +0300

    slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 import './App.scss';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Home />
   </div>
 );
 
