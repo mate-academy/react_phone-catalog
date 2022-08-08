6a2ebb1b03ff0f41fcb3d6f073ed882a839afaf2
Roman Kovalchuk <roma220702@gmail.com>
2022-08-08 10:00:30 +0300

    Solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
