d389932fce0d48ced0d494403d27993a22b33780
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-09-05 18:22:56 +0300

    test-1

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -148,7 +148,7 @@ export const Carousel:React.FC<Props> = ({ imgs }) => {
         >
           <img
             src="./img/Vector (Stroke).svg"
-            alt="Next"
+            alt="Nextjuiojuiu"
             className="carousel__mirrored"
           />
         </button>
