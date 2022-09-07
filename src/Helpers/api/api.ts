7a3c316b8a31c22a58fb052b76dabb387958a49f
Anastasiia Sydorchuk <nastia.sydorchuk@gmail.com>
2022-09-07 19:30:30 +0100

    fixes

---
+++ src/Helpers/api/api.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line
+export const API = 'https://mate-academy.github.io/react_phone-catalog/api';
+
+export const BASE_URL = 'https://mate-academy.github.io/react_phone-catalog';
