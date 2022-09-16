7c5e49f2c1cdab34132bb85dda9ac0e1645a8fb6
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-09-16 22:42:29 +0300

    test ghpages

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { store } from './app/store';
 import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root'),
 );
