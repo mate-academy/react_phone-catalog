7b10e049beada19058b7e80d60efa0c4e538d2b0
mhrynko <glukym@gmail.com>
2022-09-21 15:47:44 +0300

    Updates

--- static/js/main.509d6b4b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact_phone-catalog"]=this["webpackJsonpreact_phone-catalog"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var a=n(0),c=n.n(a),o=n(2),r=n.n(o),l=(n(8),function(){return c.a.createElement("div",{className:"App"},c.a.createElement("h1",null,"React Phone Catalog"))});r.a.render(c.a.createElement(l,null),document.getElementById("root"))}},[[3,1,2]]]);
-//# sourceMappingURL=main.509d6b4b.chunk.js.map
