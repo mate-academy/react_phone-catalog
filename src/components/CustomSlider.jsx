ecfcb2789308831dcb6f9df7736d3ba9ee06e675
Ksenia Didukh <k.tsviatko@gmail.com>
2022-11-12 12:49:27 +0200

    SVG fixed

--- src/components/CustomSlider.jsx
+++ src/components/CustomSlider.jsx
@@ -24,9 +24,7 @@ export const CustomSlider = ({ products, settings, title }) => {
           }
         }}
       >
-        <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fillRule="evenodd" clipRule="evenodd" d="M0.528636 0.528606C0.788986 0.268256 1.2111 0.268256 1.47145 0.528606L5.47145 4.52861C5.73179 4.78896 5.73179 5.21107 5.47145 5.47141L1.47145 9.47141C1.2111 9.73176 0.788986 9.73176 0.528636 9.47141C0.268287 9.21107 0.268287 8.78896 0.528636 8.52861L4.05723 5.00001L0.528636 1.47141C0.268287 1.21107 0.268287 0.788955 0.528636 0.528606Z" fill="#b4bdc3" />
-        </svg>
+        <img src="img/svg/arrow-right.svg" alt="Arrow right" />
       </div>
     );
   };
@@ -41,9 +39,7 @@ export const CustomSlider = ({ products, settings, title }) => {
           }
         }}
       >
-        <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fillRule="evenodd" clipRule="evenodd" d="M5.47136 0.528606C5.21101 0.268256 4.7889 0.268256 4.52855 0.528606L0.528555 4.52861C0.268205 4.78896 0.268205 5.21107 0.528555 5.47141L4.52855 9.47141C4.7889 9.73176 5.21101 9.73176 5.47136 9.47141C5.73171 9.21107 5.73171 8.78896 5.47136 8.52861L1.94277 5.00001L5.47136 1.47141C5.73171 1.21107 5.73171 0.788955 5.47136 0.528606Z" fill="#B4BDC4" />
-        </svg>
+        <img src="img/svg/arrow-left-light-grey.svg" alt="Arrow left" />
       </div>
     );
   };
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -45,9 +45,7 @@ export const Pagination: React.FC<Props> = ({
               setActiveItem(activeItem + 1);
             }}
           >
-            <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" clipRule="evenodd" d="M5.47136 0.528514C5.21101 0.268165 4.7889 0.268165 4.52855 0.528514L0.528555 4.52851C0.268205 4.78886 0.268205 5.21097 0.528555 5.47132L4.52855 9.47132C4.7889 9.73167 5.21101 9.73167 5.47136 9.47132C5.73171 9.21097 5.73171 8.78886 5.47136 8.52851L1.94277 4.99992L5.47136 1.47132C5.73171 1.21097 5.73171 0.788864 5.47136 0.528514Z" fill="#313237" />
-            </svg>
+            <img src="img/svg/arrow-left-black.svg" alt="Arrow left" />
 
           </button>
         </li>
@@ -90,9 +88,7 @@ export const Pagination: React.FC<Props> = ({
               setActiveItem(activeItem + 1);
             }}
           >
-            <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" clipRule="evenodd" d="M0.528636 0.528514C0.788986 0.268165 1.2111 0.268165 1.47145 0.528514L5.47145 4.52851C5.73179 4.78886 5.73179 5.21097 5.47145 5.47132L1.47145 9.47132C1.2111 9.73167 0.788986 9.73167 0.528636 9.47132C0.268287 9.21097 0.268287 8.78886 0.528636 8.52851L4.05723 4.99992L0.528636 1.47132C0.268287 1.21097 0.268287 0.788864 0.528636 0.528514Z" fill="#313237" />
-            </svg>
+            <img src="img/svg/arrow-right-black.svg" alt="Arrow right" />
           </button>
         </li>
       </ul>
