e6006058e7a77b840bbfd422b1909f2863d18c04
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-12-15 20:28:21 +0200

    bug

--- src/components/UI/ToCardButton/ToCardButton.tsx
+++ src/components/UI/ToCardButton/ToCardButton.tsx
@@ -13,41 +13,59 @@ type Props = {
 
 export const ToCardButton: FC<Props> = ({ width, height, currentProduct }) => {
   const [cartItems, setCartItems] = useLocalStorage<CartItem[]>('cart', []);
-  // const [isAdded, setIsAdded] = useState(cartItems
-  //   .some(cartItem => cartItem.product.id === currentProduct.id));
-  const isAdded = false;
 
   const styles = {
     width,
     height,
   };
 
-  const addToCart = () => {
-    const newCartItem: CartItem = {
-      id: currentProduct.id,
-      quantity: 1,
-      product: currentProduct,
-    };
+  const handleClick = () => {
+    const isAdded = cartItems
+      .some(cartItem => cartItem.product.id === currentProduct.id);
 
-    setCartItems([...cartItems, newCartItem]);
+    if (!isAdded) {
+      const newCartItem: CartItem = {
+        id: currentProduct.id,
+        quantity: 1,
+        product: currentProduct,
+      };
+
+      setCartItems([...cartItems, newCartItem]);
+    } else {
+      setCartItems([...cartItems
+        .filter(cartItem => cartItem.product.id !== currentProduct.id)]);
+    }
   };
 
-  const removeFromCart = () => {
-    setCartItems([...cartItems
-      .filter(cartItem => cartItem.product.id !== currentProduct.id)]);
-  };
+  // const addToCart = () => {
+  //   const newCartItem: CartItem = {
+  //     id: currentProduct.id,
+  //     quantity: 1,
+  //     product: currentProduct,
+  //   };
+  //
+  //   setCartItems((currentItems) => {
+  //     console.log(currentItems)
+  //     localStorage
+  //       .setItem('cart', JSON.stringify([...currentItems, newCartItem]));
+  //
+  //     return [...currentItems, newCartItem];
+  //   });
+  // };
 
   return (
     <button
       type="button"
       className={classNames(
         'to-card-button',
-        { 'to-card-button--added': isAdded },
+        // { 'to-card-button--added': isAdded },
       )}
       style={styles}
-      onClick={isAdded ? removeFromCart : addToCart}
+      onClick={handleClick}
     >
-      {isAdded ? 'Added to cart' : 'Add to cart'}
+      Added to cart
+
+      {/* {isAdded ? 'Added to cart' : 'Add to cart'} */}
     </button>
   );
 };
