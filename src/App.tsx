54fc1a38da218109c07fb2e90f51614175b01e57
Oleh Savchuk <sav4uk.oleg@gmail.com>
2023-02-07 00:15:24 +0200

    done/first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/App.scss';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { ContextProvider } from './components/ContextProvider';
+import { Main } from './components/Main';
 
-const App: React.FC = () => (
-  <div className="App">
-    <Header />
-  </div>
-);
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <ContextProvider>
+        <div className="App">
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      </ContextProvider>
+    </BrowserRouter>
+  );
+};
 
 export default App;
