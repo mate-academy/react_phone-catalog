905ba4cae6380bafb22cf65d6034a6ea38f78f62
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2023-02-18 18:27:39 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
-import './App.scss';
+/* eslint-disable max-len */
+import { FC, useEffect, useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { getAllProducts } from './api/api';
+import 'bulma';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './styles/main.scss';
 
-const App = () => (
-  <div className="App">
-    <h1>React Phone Catalog</h1>
-  </div>
-);
+import { Home } from './components/Home';
+import { Product } from './types/Product';
+import { ProductsList } from './components/ProductsList';
+import { PageNotFound } from './components/PageNotFound';
+
+const App: FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const loadProducts = async () => {
+    try {
+      const data = await getAllProducts();
+
+      setProducts(data);
+    } catch {
+      Promise.reject(new Error('error'));
+    }
+  };
+
+  const phonesList = products.filter(product => product.type === 'phone');
+  const tabletList = products.filter(product => product.type === 'tablet');
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home products={products} />}>
+        <Route path="/home" element={<Navigate to="/" />} />
+      </Route>
+      <Route path="/phones" element={<ProductsList products={phonesList} title="Mobile phones" />} />
+      <Route path="/tablets" element={<ProductsList products={tabletList} title="Tablets" />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
+
+  );
+};
 
 export default App;
