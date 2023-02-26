0d186301867ad110b69871674301ca0a25f30ea3
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2023-02-26 14:47:18 +0200

    new fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,10 @@ import { NavLink } from 'react-router-dom';
 import { Logo } from './Logo';
 import { Navbar } from './Navbar';
 import '../styles/header.scss';
-import { CartProduct } from '../types/CartProduct';
 
 export const Header: FC = () => {
-  const fav = JSON.parse(localStorage.getItem('favorites') || '').map((item: string) => item);
-  const cart = JSON.parse(localStorage.getItem('carts') || '').filter((item: CartProduct) => item.id);
+  const fav = JSON.parse(localStorage.getItem('favorites') || '');
+  const cart = JSON.parse(localStorage.getItem('carts') || '');
 
   return (
     <header className="header">
