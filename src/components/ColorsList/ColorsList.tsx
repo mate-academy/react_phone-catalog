3ff8e7d416711bb5e953ca842229fa60d206dcd5
Panianchuk_M <panianchuk@gmail.com>
2023-03-15 20:17:19 +0200

    change select color

--- src/components/ColorsList/ColorsList.tsx
+++ src/components/ColorsList/ColorsList.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { DetailsProduct } from '../../types/DetailsProduct';
+import { colors, ColorsType } from '../../types/Colors';
 import './colors-list.scss';
 
 type Props = {
@@ -15,18 +16,17 @@ export const ColorsList: FC<Props> = ({ product, pathname }) => {
       {product.colorsAvailable.map((color) => {
         const isSelected = product.color === color;
         const path = pathname.replace(product.color, color);
+        const bgcColor = colors[color as keyof ColorsType];
 
         return (
           <Link
             to={{ pathname: `${path}` }}
-            aria-label="colors-list"
             title={color}
             className={classNames('colors-list', {
               'colors-list--active': isSelected,
             })}
-            type="button"
             key={color}
-            style={{ background: `${color}` }}
+            style={{ background: `${bgcColor}` }}
           />
         );
       })}
