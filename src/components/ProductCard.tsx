6eea8b70534bbbecd1873286d211ab75fd765c1b
Serhii Yunnikov <yunnikov@icloud.com>
2023-04-17 20:38:09 +0300

    path

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -21,7 +21,7 @@ export const ProductCard: React.FC<Props> = ({ phone }) => {
     favouritesList,
     setFavouritesList,
   } = useContext(ProductsContext);
-  const imagePath = `../assets/${phone.image}`;
+  const imagePath = `src/assets/${phone.image}`;
 
   const handleCartButton
   = (event: React.MouseEvent<HTMLButtonElement>) => {
