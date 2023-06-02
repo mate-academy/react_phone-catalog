b0ce200d287d904d2ff9812b86b4518479f67a50
Zibi95 <sylwester.sob95@gmail.com>
2023-06-02 14:36:18 +0200

    Add HomePage route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+
 import { Navbar } from './components/Navbar/Navbar';
 import { Footer } from './components/Footer/Footer';
+import { HomePage } from './routes/HomePage/HomePage';
 import './reset.scss';
 import './App.scss';
 
 const App = () => (
   <div className="App">
-    <header className="header">
+    <header className="App__header">
       <Navbar />
     </header>
+
+    <main className="App__main">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </main>
 
     <Footer />
   </div>
