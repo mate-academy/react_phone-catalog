830ab7dc33edcd5563ba11d8e36edf271be9ca8c
Zibi95 <sylwester.sob95@gmail.com>
2023-06-02 11:07:20 +0200

    Navbar HTML and CSS adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Navbar } from './components/Navbar/Navbar';
+import { Footer } from './components/Footer/Footer';
 import './reset.scss';
 import './App.scss';
 
 const App = () => (
   <div className="App">
-    <Navbar />
+    <header className="header">
+      <Navbar />
+    </header>
+
+    <Footer />
   </div>
 );
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
-/* eslint-disable max-len */
 import { NavLink } from 'react-router-dom';
 
-import logo from '../../assets/Navbar/logo.svg';
+import logo from '../../assets/logo.svg';
 import favorite from '../../assets/Navbar/heart.svg';
 import cart from '../../assets/Navbar/cart.svg';
 import './Navbar.scss';
@@ -9,52 +8,52 @@ import './Navbar.scss';
 const isLinkActive = ({ isActive }: { isActive: boolean }) => `nav__link${isActive ? ' nav__link--active' : ''}`;
 
 export const Navbar = () => (
-  <header>
-    <nav className="nav">
-      <img src={logo} alt="Site logo" className="nav__logo" />
+  <nav className="nav" id="nav">
+    <ul className="nav__list">
+      <li className="nav__item">
+        <img src={logo} alt="Site logo" className="nav__logo" />
+      </li>
 
-      <ul className="nav__list">
-        <li>
-          <NavLink className={isLinkActive} to="/">
-            Home
-          </NavLink>
-        </li>
+      <li className="nav__item">
+        <NavLink className={isLinkActive} to="/">
+          Home
+        </NavLink>
+      </li>
 
-        <li>
-          <NavLink className={isLinkActive} to="/phones">
-            Phones
-          </NavLink>
-        </li>
+      <li className="nav__item">
+        <NavLink className={isLinkActive} to="/phones">
+          Phones
+        </NavLink>
+      </li>
 
-        <li>
-          <NavLink className={isLinkActive} to="/tablets">
-            Tablets
-          </NavLink>
-        </li>
+      <li className="nav__item">
+        <NavLink className={isLinkActive} to="/tablets">
+          Tablets
+        </NavLink>
+      </li>
 
-        <li>
-          <NavLink className={isLinkActive} to="/accessories">
-            Accessories
-          </NavLink>
-        </li>
-      </ul>
+      <li className="nav__item">
+        <NavLink className={isLinkActive} to="/accessories">
+          Accessories
+        </NavLink>
+      </li>
+    </ul>
 
-      <ul className="nav__controls">
-        <li className="nav__item">
-          <NavLink className={isLinkActive} to="/favorites">
-            <img
-              className="nav__icon"
-              src={favorite}
-              alt="Favorite icon button"
-            />
-          </NavLink>
-        </li>
-        <li className="nav__item">
-          <NavLink className={isLinkActive} to="/cart">
-            <img className="nav__icon" src={cart} alt="Cart icon button" />
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
+    <ul className="nav__controls">
+      <li className="nav__item">
+        <NavLink title="Favorites" className={isLinkActive} to="/favorites">
+          <img
+            className="nav__icon"
+            src={favorite}
+            alt="Favorite icon button"
+          />
+        </NavLink>
+      </li>
+      <li className="nav__item">
+        <NavLink title="Cart" className={isLinkActive} to="/cart">
+          <img className="nav__icon" src={cart} alt="Cart icon button" />
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
 );
