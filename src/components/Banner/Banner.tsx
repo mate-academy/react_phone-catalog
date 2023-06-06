758a54c9dc2a323eef9173dbcf262727c9e1eb05
Zibi95 <sylwester.sob95@gmail.com>
2023-06-06 13:54:42 +0200

    Implement auto sliding, Implement indicator

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 import { ProductSlider } from '../ProductSlider/ProductSlider';
 
-import leftArrow from '../../assets/banner/l_arrow.svg';
-import rightArrow from '../../assets/banner/r_arrow.svg';
+import leftArrow from '../../assets/l_arrow.svg';
+import rightArrow from '../../assets/r_arrow.svg';
 import img1 from '../../assets/images/banner/banner-phones.png';
 import img2 from '../../assets/images/banner/banner-accessories.png';
 import img3 from '../../assets/images/banner/banner-tablets.png';
@@ -13,38 +14,66 @@ import './Banner.scss';
 const bannerImages = [img1, img2, img3];
 
 export const Banner = () => {
-  const [page, setState] = useState(1);
+  const [page, setPage] = useState(1);
+
+  const handleClick = (operation: 1 | -1) => {
+    setPage(prevPage => {
+      if (operation === 1) {
+        return prevPage + 1 > 2 ? 0 : prevPage + 1;
+      }
+
+      return prevPage - 1 < 0 ? 2 : prevPage - 1;
+    });
+  };
+
+  useEffect(() => {
+    const intId = setInterval(() => handleClick(1), 5000);
+
+    return () => clearInterval(intId);
+  }, []);
 
   return (
     <section className="banner">
-      <button
-        type="button"
-        className="banner__button"
-        onClick={() => setState(page - 1 < 0 ? 2 : page - 1)}
-      >
-        <img src={leftArrow} alt="Banners left arrow button" />
-      </button>
-
-      <div className="banner__slider">
-        <ProductSlider page={page}>
-          {bannerImages.map(image => (
-            <img
-              key={image}
-              className="banner__image"
-              src={image}
-              alt="Banner element"
-            />
-          ))}
-        </ProductSlider>
+      <div className="banner__wrapper">
+        <button
+          type="button"
+          className="banner__button"
+          onClick={() => handleClick(-1)}
+        >
+          <img src={leftArrow} alt="Banners left arrow button" />
+        </button>
+        <div className="banner__slider">
+          <ProductSlider page={page}>
+            {bannerImages.map((image) => (
+              <img
+                key={image}
+                className="banner__image"
+                src={image}
+                alt="Banner element"
+              />
+            ))}
+          </ProductSlider>
+        </div>
+        <button
+          type="button"
+          className="banner__button"
+          onClick={() => handleClick(1)}
+        >
+          <img src={rightArrow} alt="Banners left arrow button" />
+        </button>
       </div>
 
-      <button
-        type="button"
-        className="banner__button"
-        onClick={() => setState(page + 1 > 2 ? 0 : page + 1)}
-      >
-        <img src={rightArrow} alt="Banners left arrow button" />
-      </button>
+      <div className="banner__indicator-container">
+        {bannerImages.map((img, i) => (
+          <div
+            key={img}
+            className={classNames('banner__indicator', {
+              'banner__indicator--active': page === i,
+            })}
+          />
+        ))}
+      </div>
+
     </section>
   );
 };
