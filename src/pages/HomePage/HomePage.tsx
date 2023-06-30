ae82bbf605f0f201edcb13a9db6a632d25003711
Vlad <bojko.vladislav16@gmail.com>
2023-06-30 23:57:20 +0300

    testing deployment

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -9,9 +9,13 @@ import { Product } from '../../types/product';
 import { BASE_URL, fetcher } from '../../api/productsApi';
 
 const imagesForBanner = [
-  '../public/_new/img/banner-phones.png',
-  './public/_new/img/banner-tablets.png',
+  './public/_new/img/banner-phones.png',
+  '/_new/img/banner-tablets.png',
+  '_new/img/banner-accessories.png',
   './_new/img/banner-accessories.png',
+  '../_new/img/banner-accessories.png',
+  '../public/_new/img/banner-accessories.png',
+  'public/_new/img/banner-accessories.png',
 ];
 
 export const HomePage = () => {
