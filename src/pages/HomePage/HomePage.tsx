a47c85894729c18e8f2df9778a00bdd73d4c2397
Vlad <bojko.vladislav16@gmail.com>
2023-06-30 16:51:47 +0300

    Image path has been changed (testing deploying)

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -9,7 +9,7 @@ import { Product } from '../../types/product';
 import { BASE_URL, fetcher } from '../../api/productsApi';
 
 const imagesForBanner = [
-  '/public/_new/img/banner-phones.png',
+  '../_new/img/banner-phones.png',
   '/public/_new/img/banner-tablets.png',
   '/public/_new/img/banner-accessories.png',
 ];
