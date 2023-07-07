0a45c3353467b9d2cd41b94b526aeff6718fc201
Vlad <bojko.vladislav16@gmail.com>
2023-07-07 10:49:03 +0300

    HashRouter has been set instead of BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
 import { store } from './app/store';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root'),
 );
