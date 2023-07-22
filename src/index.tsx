5fb38a41faeb46b6137dde00cd40bf9d33cfc543
Anton Vasyliev <116097498+vasyliev-anton@users.noreply.github.com>
2023-07-22 23:35:02 +0300

    HashRoute

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,17 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 import { persistor, store } from './Reducer/store';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
