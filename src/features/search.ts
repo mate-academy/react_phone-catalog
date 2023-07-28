8e9edfa4d7f46f7909f492e63df3750e8ced0d1d
Kateryna Shylina <katya.dhilina@gmail.com>
2023-07-28 19:05:43 +0300

    some fixes and added files

---
+++ src/features/search.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  query: '',
+  results: [],
+};
+
+const searchSlice = createSlice({
+  name: 'search',
+  initialState,
+  reducers: {
+    setQuery: (state, action) => {
+      state.query = action.payload;
+    },
+    setResults: (state, action) => {
+      state.results = action.payload;
+    },
+  },
+});
+
+export const { setQuery, setResults } = searchSlice.actions;
+
+export default searchSlice.reducer;
