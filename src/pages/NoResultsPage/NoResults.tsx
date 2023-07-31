3e8367a10d3369d0158e3c506e5051ca9576472d
Danylo Pasko <danil.pasco@gmail.com>
2023-07-31 21:47:50 +0300

    hotfix2

--- src/pages/NoResultsPage/NoResults.tsx
+++ src/pages/NoResultsPage/NoResults.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import './NoResults.scss';
 import noResults from '../../Icons/noResults.png';
 
@@ -12,7 +13,7 @@ export const NoResults = () => {
 
       <p className="error__desc">
         Maybe you want to go back to
-        <a href="/" className="error__link"> Home page </a>
+        <a href="https://danylopasko.github.io/react_phone-catalog/#/" className="error__link"> Home page </a>
         ?
       </p>
     </div>
