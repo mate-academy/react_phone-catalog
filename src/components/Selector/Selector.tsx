96fd234f2641002eac48a9acc2705719ecc064ad
Eugene Kulish <genekulish13@gmail.com>
2023-08-14 20:27:02 +0300

    fix

--- src/components/Selector/Selector.tsx
+++ src/components/Selector/Selector.tsx
@@ -47,9 +47,9 @@ export const Selector: React.FC<Props> = ({
 
         <img
           src={areOptionsOpen ? (
-            './img/img/icons/chevron-arrow-up.svg'
+            './img/icons/chevron-arrow-up.svg'
           ) : (
-            './img/img/icons/chevron-arrow-down.svg'
+            './img/icons/chevron-arrow-down.svg'
           )}
           alt="open options"
           className="products-list__icon"
