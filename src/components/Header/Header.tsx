0a902e55cebdf28b6a503eda679ff1110183da75
Skrudj <ssinilov@gmail.com>
2023-08-30 19:11:12 +0300

    implement NavBarItem component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,78 @@
-export const Header: React.FC = () => (
-  <header className="header">
-    <nav className="header__navigation nav">
-      <a href="home" className="nav__link">
-        <img
-          src="/static/media/logo.2d8d3e42.svg"
-          alt="logo"
-          className="nav__logo"
-        />
-      </a>
+import logo from '../../images/logo.svg';
+import { NavBarItem } from '../NavBarItem';
 
-      <ul className="nav__list">
-        <li className="nav__item nav__item--is-active">
-          <a href="aaa" className="nav__link">Home</a>
-        </li>
-        <li className="nav__item">
-          <a href="aaa" className="nav__link">Phones</a>
-        </li>
-        <li className="nav__item">
-          <a href="aaa" className="nav__link">Tablets</a>
-        </li>
-        <li className="nav__item">
-          <a href="aaa" className="nav__link">Accessories</a>
-        </li>
-      </ul>
-    </nav>
+export const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <nav className="header__navigation nav">
+        <a href="home" className="logo__link">
+          <img
+            src={logo}
+            alt="logo"
+            className="nav__logo"
+          />
+        </a>
 
-    <div className="header__actions">
-      <a href="fav" className="header__fav">some fav</a>
+        <ul className="nav__list">
+          <li className="nav__item">
+            <NavBarItem
+              to="home"
+              type="text"
+              className="nav__link"
+            >
+              Home
+            </NavBarItem>
+          </li>
+          <li className="nav__item">
+            <NavBarItem
+              to="phones"
+              type="text"
+              className="nav__link"
+            >
+              Phones
+            </NavBarItem>
+          </li>
+          <li className="nav__item">
+            <NavBarItem
+              to="tablets"
+              type="text"
+              className="nav__link"
+            >
+              Tablets
+            </NavBarItem>
+          </li>
+          <li className="nav__item">
+            <NavBarItem
+              to="accessories"
+              type="text"
+              className="nav__link"
+            >
+              Accessories
+            </NavBarItem>
+          </li>
+          {/* <li className="nav__item">
+            <a href="aaa" className="nav__link nav__link--is-active">Home</a>
+          </li>
+          <li className="nav__item">
+            <a href="aaa" className="nav__link">Phones</a>
+          </li>
+          <li className="nav__item">
+            <a href="aaa" className="nav__link">Tablets</a>
+          </li>
+          <li className="nav__item">
+            <a href="aaa" className="nav__link">Accessories</a>
+          </li> */}
+        </ul>
+      </nav>
 
-      <a href="cart" className="header__cart">some cart</a>
-    </div>
-  </header>
-);
+      <div className="header__actions">
+        <NavBarItem to="fav" type="fav">1</NavBarItem>
+        <NavBarItem to="cart" type="cart">2</NavBarItem>
+
+        {/* <a href="fav" className="header__fav">some fav</a>
+
+        <a href="cart" className="header__cart">some cart</a> */}
+      </div>
+    </header>
+  );
+};
---
+++ src/components/NavBarItem/NavBarItem.tsx
@@ -0,0 +1,23 @@
+import { NavLink, NavLinkProps } from 'react-router-dom';
+import classNames from 'classnames';
+
+type Props = NavLinkProps & {
+  type: 'text' | 'fav' | 'cart';
+};
+
+export const NavBarItem: React.FC<Props> = ({
+  type,
+  children,
+  ...props
+}) => {
+  return (
+    <NavLink
+      {...props}
+      className={({ isActive }) => classNames('nav-item', `nav-item-${type}`, {
+        'is-active': isActive,
+      })}
+    >
+      {children}
+    </NavLink>
+  );
+};
