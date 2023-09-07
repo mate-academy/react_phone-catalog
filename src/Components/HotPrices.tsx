562fa20eaf7afe15f6f503cc36765eb43404e8b4
AndriiYelieva <andrew.yelieva@gmail.com>
2023-09-07 09:56:28 +0300

    just save 3

--- src/Components/HotPrices.tsx
+++ src/Components/HotPrices.tsx
@@ -1,16 +1,30 @@
+/* eslint-disable no-console */
+/* eslint-disable no-plusplus */
 import React, { useEffect, useState } from 'react';
+// import Slider from 'react-slick';
 import '../style/main.scss';
+import classNames from 'classnames';
 import { ProductCard } from './ProductCard';
 import { client } from '../utils/fetchClient';
 import { Phone } from '../Type/Phone';
 
+let buttonBack = 0;
+let buttonNext = 4;
+
 export const HotPrices: React.FC = () => {
   const [phones, setPhones] = useState<Phone[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [errorMessage, setErrorMessage] = useState(false);
+  const [back, setBack] = useState(buttonBack);
+  const [next, setNext] = useState(buttonNext);
+  const [, setLoading] = useState(true);
+  const [, setErrorMessage] = useState(false);
 
-  // eslint-disable-next-line max-len
-  const dsfs = 'https://mate-academy.github.io/react_phone-catalog/_new/products.json';
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  // };
 
   useEffect(() => {
     client.get<Phone[]>('/_new/products.json')
@@ -18,10 +32,12 @@ export const HotPrices: React.FC = () => {
       .catch(setErrorMessage)
       .finally(() => setLoading(false));
   }, []);
+  const hotPhones = phones.filter(phone => phone.fullPrice > 1200)
+    .sort((a, b) => b.fullPrice - a.fullPrice);
 
-  console.log(phones);
-  console.log(loading);
-  console.log(errorMessage);
+  const preperaPhones = hotPhones.slice(back, next);
+
+  console.log(preperaPhones);
 
   return (
     <div className="container--hot">
@@ -32,22 +48,40 @@ export const HotPrices: React.FC = () => {
           <button
             type="button"
             aria-label="Mute volume"
-            className="button button__left"
+            className={classNames('button button__left', {
+              'button__left--active': buttonBack > 0,
+            })}
+            disabled={buttonBack === 0}
+            onClick={() => {
+              setNext(buttonNext--);
+              setBack(buttonBack--);
+            }}
           />
           <button
             type="button"
             aria-label="Mute volume"
-            className="button button__right"
+            className={classNames('button button__right', {
+              'button__right--active': buttonNext < hotPhones.length,
+            })}
+            disabled={buttonNext === hotPhones.length}
+            onClick={() => {
+              setNext(buttonNext++);
+              setBack(buttonBack++);
+            }}
           />
         </div>
       </div>
 
-      {phones.map(phone => (
-        <ProductCard
-          key={phone.id}
-          phone={phone}
-        />
-      ))}
+      <div className="product">
+        {/* <Slider {...settings}> */}
+        {preperaPhones.map(phone => (
+          <ProductCard
+            key={phone.id}
+            phone={phone}
+          />
+        ))}
+        {/* </Slider> */}
+      </div>
 
     </div>
   );
--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -7,60 +7,56 @@ type Props = {
   phone: Phone;
 };
 
+const URL = 'https://mate-academy.github.io/react_phone-catalog/_new/';
+
 export const ProductCard: React.FC<Props> = ({ phone }) => {
   return (
-    <div className="product">
-      <div className="product__card">
-        <div className="product__photo">
-          <img
-            className="product__photo--img"
-            src="../../public/_new/img/phones/apple-iphone-7/black/00.jpg"
-            // src={`../../public/_new/${phone.image}`}
-            // src={AppleIphoneXs}
-            // {'../img/phones/apple-iphone-7/black/00.jpg'}
-            alt={phone.phoneId}
-          />
+    <div className="product__card">
+      <div className="product__photo">
+        <img
+          className="product__photo--img"
+          src={`${URL}${phone.image}`}
+          alt={phone.phoneId}
+        />
+      </div>
+
+      <div className="product__description">
+        <p className="product__text--body">
+          {phone.name}
+        </p>
+
+        <div className="product__price">
+          <h2 className="product__price--current">{`$${phone.price}`}</h2>
+          <h2 className="product__price--discount">{`$${phone.fullPrice}`}</h2>
         </div>
 
-        <div className="product__description">
-          <p className="product__text--body">
-            {/* Apple iPhone Xs 64GB Silver (iMT9G2FS/A) */}
-            {phone.name}
-          </p>
+        <div className="product__screen">
+          <p className="product__screen--name">Screen</p>
+          <p className="product__screen--value">{phone.screen}</p>
+        </div>
 
-          <div className="product__price">
-            <h2 className="product__price--current">{`$${phone.price}`}</h2>
-            <h2 className="product__price--discount">{`$${phone.fullPrice}`}</h2>
-          </div>
+        <div className="product__capacity">
+          <p className="product__capacity--name">Capacity</p>
+          <p className="product__capacity--value">{phone.capacity}</p>
+        </div>
 
-          <div className="product__screen">
-            <p className="product__screen--name">Screen</p>
-            <p className="product__screen--value">{phone.screen}</p>
-          </div>
+        <div className="product__ram">
+          <p className="product__ram--name">RAM</p>
+          <p className="product__ram--value">{phone.ram}</p>
+        </div>
 
-          <div className="product__capacity">
-            <p className="product__capacity--name">Capacity</p>
-            <p className="product__capacity--value">{phone.capacity}</p>
-          </div>
-
-          <div className="product__ram">
-            <p className="product__ram--name">RAM</p>
-            <p className="product__ram--value">{phone.ram}</p>
-          </div>
-
-          <div className="product__button">
-            <button
-              type="button"
-              className="product__button--add"
-            >
-              Add to cart
-            </button>
-            <button
-              type="button"
-              aria-label="Mute volume"
-              className="product__button--favourites"
-            />
-          </div>
+        <div className="product__button">
+          <button
+            type="button"
+            className="product__button--add"
+          >
+            Add to cart
+          </button>
+          <button
+            type="button"
+            aria-label="Mute volume"
+            className="product__button--favourites"
+          />
         </div>
       </div>
     </div>
