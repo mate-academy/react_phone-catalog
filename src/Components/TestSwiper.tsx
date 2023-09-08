5514f506ac928bf4d6bdd4f75060b1163c4d0af5
AndriiYelieva <andrew.yelieva@gmail.com>
2023-09-08 19:17:21 +0300

    for mentor

--- src/Components/TestSwiper.tsx
+++ src/Components/TestSwiper.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable react/button-has-type */
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
-import { Navigation, Pagination, EffectFade } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {
+  Navigation, Pagination, EffectFade, Autoplay,
+} from 'swiper';
 
-// Import Swiper styles
 import 'swiper/swiper.scss'; // core Swiper
 import 'swiper/modules/navigation/navigation.scss'; // Navigation module
 import 'swiper/modules/pagination/pagination.scss'; // Pagination module
@@ -11,44 +11,49 @@ import bannerPhones from '../images/banner/banner-phones.png';
 import bannerTablets from '../images/banner/banner-tablets.png';
 import bannerAccessories from '../images/banner/banner-accessories.png';
 
-export const TestSwiper = () => {
-  const swiper = useSwiper();
+const IMAGES = [
+  { id: 0, url: bannerPhones },
+  { id: 1, url: bannerTablets },
+  { id: 2, url: bannerAccessories },
+];
 
+SwiperCore.use([EffectFade, Navigation, Pagination, Autoplay]);
+
+export const TestSwiper = () => {
   return (
-    <Swiper
-      spaceBetween={30}
-      effect="fade"
-      // navigation={{
-      //   nextEl: '.review-swiper-button-next',
-      //   prevEl: '.review-swiper-button-prev',
-      // }}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[EffectFade, Navigation, Pagination]}
-      className="mySwiper"
-    >
+    <div className="container--slider">
       <button
+        type="button"
         className="arrow arrow__left review-swiper-button-prev"
         aria-label="Mute volume"
-        onClick={() => swiper.slideNext()}
       />
-
-      <SwiperSlide>
-        <img src={bannerPhones} alt="Banner" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={bannerTablets} alt="Banner Iphones" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={bannerAccessories} alt="Banner Iphones" />
-      </SwiperSlide>
+      <Swiper
+        navigation={{
+          nextEl: '.review-swiper-button-next',
+          prevEl: '.review-swiper-button-prev',
+        }}
+        autoplay={{
+          delay: 3500,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        loop
+        modules={[EffectFade, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        {IMAGES.map(img => (
+          <SwiperSlide key={img.id}>
+            <img src={img.url} alt="Banner" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <button
-        className="arrow arrow__left review-swiper-button-next"
+        type="button"
+        className="arrow arrow__right review-swiper-button-next"
         aria-label="Mute volume"
-        onClick={() => swiper.slidePrev()}
       />
-    </Swiper>
+    </div>
   );
 };
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,14 @@
 import { NewModels } from '../Components/NewModels';
 import { HotPrices } from '../Components/HotPrices';
 import { Categories } from '../Components/Categories';
-import { Slider } from '../Components/Slider';
+// import { Slider } from '../Components/Slider';
 import { TestSwiper } from '../Components/TestSwiper';
 
 export const HomePage = () => {
   return (
     <main>
 
-      <Slider />
+      {/* <Slider /> */}
 
       <TestSwiper />
 
