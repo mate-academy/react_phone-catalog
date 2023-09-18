5d2ee9bcfe9bc4a8071106610b2d75ae14ffbcc5
Samir Mamedov <samirmamedoviphone@gmail.com>
2023-09-18 16:45:10 +0300

    Fixing images import

--- src/pages/productdetails/ProductDetails.tsx
+++ src/pages/productdetails/ProductDetails.tsx
@@ -264,7 +264,7 @@ export const ProductDetails: FC = () => {
                   key={image}
                 >
                   <img
-                    src={`/new/${image}`}
+                    src={`new/${image}`}
                     alt={product.name}
                     className="simage"
                   />
@@ -275,7 +275,7 @@ export const ProductDetails: FC = () => {
 
             <div className="details__xlimage_container">
               <img
-                src={`/new/${selectedImage}`}
+                src={`new/${selectedImage}`}
                 alt="img"
                 className="xlimage"
               />
