09481e5fce7b24df0a62eca3fa9712ecee701386
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-09-19 15:45:13 +0300

    fixed

--- src/helpers/HTTPClient.ts
+++ src/helpers/HTTPClient.ts
@@ -1,6 +1,6 @@
 // const BASE_URL = 'http://localhost:3000/_new';
-// const BASE_URL = 'https://vlkzmn.github.io/react_phone-catalog/_new';
-const BASE_URL = 'https://mate-academy.github.io/react_phone-catalog/_new';
+const BASE_URL = 'https://vlkzmn.github.io/react_phone-catalog/_new';
+// const BASE_URL = 'https://mate-academy.github.io/react_phone-catalog/_new';
 
 export function getData<T>(url: string): Promise<T> {
   return fetch(BASE_URL + url)
