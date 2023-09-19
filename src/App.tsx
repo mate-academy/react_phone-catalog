c8716e298b65e159a3e7bb138e1a56a9f135c4d5
Alla Gutsul <allagutsul1990@gmail.com>
2023-09-19 11:58:10 +0300

    add img

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import './App.scss';
 
 const App = () => (
   <div className="App">
     <h1>React Phone Catalog</h1>
+    <img src="_new/img/banner-phones.png" alt="" />
   </div>
 );
 
