e43a3944ccc6741d35ba77d82414beffada6871d
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-09-19 14:53:16 +0300

    fixed

--- src/helpers/HTTPClient.ts
+++ src/helpers/HTTPClient.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000/_new';
+const BASE_URL = '/vlkzmn.github.io/react_phone-catalog/_new';
 
 export function getData<T>(url: string): Promise<T> {
   return fetch(BASE_URL + url)
