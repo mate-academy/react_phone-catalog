d5d8157ed952e2880c9f5354568ddd7d6e40f632
david.shamiiev <david.shamiiev@rad4m.com>
2023-09-22 18:56:36 +0300

    minor fixes

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -101,193 +101,192 @@ export const ProductDetailsPage = () => {
   }, [products, phone]);
 
   return (
-    <>
+    <Wrapper>
       {isLoading
         ? (<Loader />)
         : (
           phone && (
             <div className="product-details">
-              <Wrapper>
-                <div className="product-details__path-container">
-                  <BreadCrumbs />
-                </div>
 
-                <div className="product-details__back-container">
-                  <BackButton />
-                </div>
+              <div className="product-details__path-container">
+                <BreadCrumbs />
+              </div>
 
-                <h1
-                  className="product-details__title"
-                >
-                  {phone.name}
-                </h1>
+              <div className="product-details__back-container">
+                <BackButton />
+              </div>
 
-                <div className="product-details__upper-block">
-                  <ProductImage images={phone.images} />
+              <h1
+                className="product-details__title"
+              >
+                {phone.name}
+              </h1>
 
-                  <div className="product-details__upper-right">
+              <div className="product-details__upper-block">
+                <ProductImage images={phone.images} />
 
-                    <Colors
-                      colors={phone.colorsAvailable}
-                      nameSpaceId={phone.namespaceId}
-                      capacity={phone.capacity}
-                      currColor={phone.color}
-                    />
+                <div className="product-details__upper-right">
 
-                    <AvailableCapacity
-                      capacities={phone.capacityAvailable}
-                      currCapacity={phone.capacity}
-                      nameSpaceId={phone.namespaceId}
-                      color={phone.color}
-                    />
-
-                    <div className="product-details__price-block">
-                      <p className="product-details__price-discount">{`$${phone?.priceDiscount}`}</p>
-                      <p className="product-details__price-regular">{`$${phone?.priceRegular}`}</p>
-                    </div>
-
-                    <div className="product-details__btns-block">
-                      <Button
-                        buttonType={ButtonType.CartLarge}
-                        existInCart={productIsInCart}
-                        onClickHandler={addItemToCart}
-                        disabled={productIsInCart}
-                      />
-
-                      <Like
-                        size={LikeSize.Large}
-                        onClickHandler={handleLike}
-                        liked={existInFavourites}
-                      />
-                    </div>
-
-                    <div className="product-details__specs">
-                      <p>Screen</p>
-                      <p
-                        className="product-card__specs--right"
-                      >
-                        {phone.screen}
-                      </p>
-                      <p>Resolution</p>
-                      <p
-                        className="product-card__specs--right"
-                      >
-                        {phone?.resolution}
-                      </p>
-                      <p>Proccessor</p>
-                      <p
-                        className="product-card__specs--right"
-                      >
-                        {phone.processor}
-                      </p>
-                      <p>RAM</p>
-                      <p className="product-card__specs--right">{phone.ram}</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="product-details__lower-block">
-                  <div className="product-details__about">
-
-                    <h2 className="product-details__about-heading">about</h2>
-
-                    <h3
-                      className="product-details__about-title"
-                    >
-                      {phone.description[0].title}
-                    </h3>
-
-                    <p
-                      className="product-details__about-text"
-                    >
-                      {phone.description[0].text}
-                    </p>
-
-                    <h3
-                      className="product-details__about-title"
-                    >
-                      {phone.description[1].title}
-                    </h3>
-
-                    <p
-                      className="product-details__about-text"
-                    >
-                      {phone.description[1].text}
-                    </p>
-
-                    <h3
-                      className="product-details__about-title"
-                    >
-                      {phone.description[2].title}
-                    </h3>
-
-                    <p
-                      className="product-details__about-text"
-                    >
-                      {phone.description[2].text}
-                    </p>
-                  </div>
-
-                  <div>
-                    <h2
-                      className="product-details__about-heading"
-                    >
-                      tech specs
-                    </h2>
-
-                    <div className="product-details__tech-specs">
-                      <p>Screen</p>
-                      <p
-                        className="product-card__specs--right"
-                      >
-                        {phone.screen}
-                      </p>
-                      <p>Resolution</p>
-                      <p
-                        className="product-card__specs--right"
-                      >
-                        {phone.resolution}
-                      </p>
-                      <p>Proccessor</p>
-                      <p
-                        className="product-card__specs--right"
-                      >
-                        {phone.processor}
-                      </p>
-                      <p>RAM</p>
-                      <p className="product-card__specs--right">{phone.ram}</p>
-                      <p>Built in memory</p>
-                      <p
-                        className="product-card__specs--right"
-                      >
-                        {phone.capacity}
-                      </p>
-                      <p>Camera</p>
-                      <p
-                        className="product-card__specs--right"
-                      >
-                        {phone.camera}
-                      </p>
-                      <p>Zoom</p>
-                      <p className="product-card__specs--right">{phone.zoom}</p>
-                      <p>Cell</p>
-                      <p className="product-card__specs--right">
-                        {phone.cell.join(', ')}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="product-details__also-like-container">
-                  <ProductsSlider
-                    title="you may also like"
-                    products={youMayAlsoLikeProducts}
+                  <Colors
+                    colors={phone.colorsAvailable}
+                    nameSpaceId={phone.namespaceId}
+                    capacity={phone.capacity}
+                    currColor={phone.color}
                   />
+
+                  <AvailableCapacity
+                    capacities={phone.capacityAvailable}
+                    currCapacity={phone.capacity}
+                    nameSpaceId={phone.namespaceId}
+                    color={phone.color}
+                  />
+
+                  <div className="product-details__price-block">
+                    <p className="product-details__price-discount">{`$${phone?.priceDiscount}`}</p>
+                    <p className="product-details__price-regular">{`$${phone?.priceRegular}`}</p>
+                  </div>
+
+                  <div className="product-details__btns-block">
+                    <Button
+                      buttonType={ButtonType.CartLarge}
+                      existInCart={productIsInCart}
+                      onClickHandler={addItemToCart}
+                      disabled={productIsInCart}
+                    />
+
+                    <Like
+                      size={LikeSize.Large}
+                      onClickHandler={handleLike}
+                      liked={existInFavourites}
+                    />
+                  </div>
+
+                  <div className="product-details__specs">
+                    <p>Screen</p>
+                    <p
+                      className="product-card__specs--right"
+                    >
+                      {phone.screen}
+                    </p>
+                    <p>Resolution</p>
+                    <p
+                      className="product-card__specs--right"
+                    >
+                      {phone?.resolution}
+                    </p>
+                    <p>Proccessor</p>
+                    <p
+                      className="product-card__specs--right"
+                    >
+                      {phone.processor}
+                    </p>
+                    <p>RAM</p>
+                    <p className="product-card__specs--right">{phone.ram}</p>
+                  </div>
                 </div>
-              </Wrapper>
+              </div>
+
+              <div className="product-details__lower-block">
+                <div className="product-details__about">
+
+                  <h2 className="product-details__about-heading">about</h2>
+
+                  <h3
+                    className="product-details__about-title"
+                  >
+                    {phone.description[0].title}
+                  </h3>
+
+                  <p
+                    className="product-details__about-text"
+                  >
+                    {phone.description[0].text}
+                  </p>
+
+                  <h3
+                    className="product-details__about-title"
+                  >
+                    {phone.description[1].title}
+                  </h3>
+
+                  <p
+                    className="product-details__about-text"
+                  >
+                    {phone.description[1].text}
+                  </p>
+
+                  <h3
+                    className="product-details__about-title"
+                  >
+                    {phone.description[2].title}
+                  </h3>
+
+                  <p
+                    className="product-details__about-text"
+                  >
+                    {phone.description[2].text}
+                  </p>
+                </div>
+
+                <div>
+                  <h2
+                    className="product-details__about-heading"
+                  >
+                    tech specs
+                  </h2>
+
+                  <div className="product-details__tech-specs">
+                    <p>Screen</p>
+                    <p
+                      className="product-card__specs--right"
+                    >
+                      {phone.screen}
+                    </p>
+                    <p>Resolution</p>
+                    <p
+                      className="product-card__specs--right"
+                    >
+                      {phone.resolution}
+                    </p>
+                    <p>Proccessor</p>
+                    <p
+                      className="product-card__specs--right"
+                    >
+                      {phone.processor}
+                    </p>
+                    <p>RAM</p>
+                    <p className="product-card__specs--right">{phone.ram}</p>
+                    <p>Built in memory</p>
+                    <p
+                      className="product-card__specs--right"
+                    >
+                      {phone.capacity}
+                    </p>
+                    <p>Camera</p>
+                    <p
+                      className="product-card__specs--right"
+                    >
+                      {phone.camera}
+                    </p>
+                    <p>Zoom</p>
+                    <p className="product-card__specs--right">{phone.zoom}</p>
+                    <p>Cell</p>
+                    <p className="product-card__specs--right">
+                      {phone.cell.join(', ')}
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="product-details__also-like-container">
+                <ProductsSlider
+                  title="you may also like"
+                  products={youMayAlsoLikeProducts}
+                />
+              </div>
             </div>
           )
         )}
-    </>
+    </Wrapper>
   );
 };
