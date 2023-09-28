7ceba5c438f77f6007ac64da0a8c310c7e54a75a
Antonio S <soi4.toxa@ukr.net>
2023-09-28 17:19:23 +0300

    delete-some-comments

--- src/components/SearchLink.tsx
+++ src/components/SearchLink.tsx
@@ -6,21 +6,18 @@ type Props = Omit<LinkProps, 'to'> & {
 };
 
 export const SearchLink: React.FC<Props> = ({
-  children, // this is the content between the open and closing tags
-  params, // the params to be updated in the `search`
-  ...props // all usual Link props like `className`, `style` and `id`
+  children,
+  params,
+  ...props
 }) => {
   const [searchParams] = useSearchParams();
 
   return (
     <Link
-      // to={{ search: getSearchWith(searchParams, { query: 'sdf' }) }}
-      // to={{ search: getSearchWith(searchParams, { query: null }) }}
-      // to={{ search: getSearchWith(searchParams, { centuries: ['16', '18'] }) }}
       to={{
         search: getSearchWith(searchParams, params),
       }}
-      {...props} // copy all the other props
+      {...props}
     >
       {children}
     </Link>
