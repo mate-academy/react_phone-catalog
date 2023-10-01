b631764929edef4cc3d8189bc9cbaea5129a557e
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2023-10-01 13:34:24 +0300

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import './App.scss';
+import {
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import './styles/App.scss';
 
-const App = () => (
-  <div className="App">
-    <h1>React Phone Catalog</h1>
-  </div>
-);
+import { ProductProvider } from './ProductContext';
 
-export default App;
+import { Layout } from './components/Layout';
+import { HomePage } from './pages/HomePage';
+import { CatalogPage } from './pages/CatalogPage';
+import { FavoritesPage } from './pages/FavoritesPage';
+import { CartPage } from './pages/CartPage';
+
+import { ErrorPage } from './pages/ErrorPage';
+
+export const App = () => {
+  return (
+    <div data-cy="app">
+      <ProductProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="home" element={<Navigate to="/" replace />} />
+            <Route path="favourites" element={<FavoritesPage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="catalog">
+              <Route path=":product" element={<CatalogPage />} />
+              <Route path=":product" element={<CatalogPage />} />
+              <Route path=":product" element={<CatalogPage />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </ProductProvider>
+    </div>
+  );
+};
