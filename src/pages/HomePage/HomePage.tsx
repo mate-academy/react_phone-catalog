a87ffa987fc9c8dfe9ab14098c4270a971b52298
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-10-05 13:13:01 +0300

    Home page layout was implement

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -6,6 +6,7 @@ import { calculateDiscount } from '../../helpers/calculateDiscount';
 import { ProductSlider } from '../../components/ProductSlider/ProductSlider';
 import './HomePage.scss';
 import { getProductCount } from '../../helpers/getProductCount';
+import { Carousel } from '../../components/Carousel/Carousel';
 
 export const HomePage = () => {
   const { data: products = [], isLoading } = useGetProductsQuery();
@@ -44,8 +45,8 @@ export const HomePage = () => {
 
       {!isLoading && (
         <>
-          <h1 className="HomePage__carousel-container">
-            Home page
+          <h1 className="HomePage__carousel-container section">
+            <Carousel />
           </h1>
 
           <section className="section">
@@ -59,25 +60,23 @@ export const HomePage = () => {
           </section>
 
           <section className="HomePage__shop-by-category section">
-            <h2 className="section__title HomePage__shop-by-category--title">
+            <h2 className="section__title HomePage__shop-by-category__title">
               Shop by category
             </h2>
 
             <ul
-              className="HomePage__shop-by-category--container"
+              className="HomePage__shop-by-category__container"
               data-cy="categoryLinksContainer"
             >
-              <li className="HomePage__shop-by-category--category">
+              <li>
                 <Link
                   to="/phones"
-                  className="HomePage__shop-by-category--link"
+                  className="HomePage__shop-by-category-link"
                 >
-                  <div className="HomePage__shop-by-category--photo">
-                    <img
-                      src="_new/img/category-phones.png"
-                      alt="phones category"
-                    />
-                  </div>
+                  <img
+                    src="_new/img/category-phones.png"
+                    alt="phones"
+                  />
                   <h3>
                     Mobile phones
                   </h3>
@@ -87,11 +86,14 @@ export const HomePage = () => {
                 </p>
               </li>
 
-              <li className="HomePage__shop-by-category--category">
-                <Link to="/tablets">
+              <li>
+                <Link
+                  to="/tablets"
+                  className="HomePage__shop-by-category-link"
+                >
                   <img
                     src="_new/img/category-tablets.png"
-                    alt="tablets category"
+                    alt="tablets"
                   />
                   <h3>
                     Tablets
@@ -102,11 +104,14 @@ export const HomePage = () => {
                 </p>
               </li>
 
-              <li className="HomePage__shop-by-category--category">
-                <Link to="/accessories">
+              <li>
+                <Link
+                  to="/accessories"
+                  className="HomePage__shop-by-category-link"
+                >
                   <img
                     src="_new/img/category-accessories.png"
-                    alt="accessories category"
+                    alt="accessories"
                   />
                   <h3>
                     Accessories
