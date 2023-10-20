5bb3a8c0047df28269a38f1192de7210de2724f6
Viktoriia Tkachyk <viko66666vs@gmail.com>
2023-10-20 19:48:34 +0300

    fix mesage

--- src/components/NoSearchResults/NoSearchResults.tsx
+++ src/components/NoSearchResults/NoSearchResults.tsx
@@ -4,7 +4,7 @@ export const NoSearchResults = () => (
   <main className="main">
     <section className="main__no-results">
       <h1 className="main__title main__title--development">
-        We are sorry, but this feature is not implemented yet
+        Phone was not found
       </h1>
       <img
         src={noresult}
