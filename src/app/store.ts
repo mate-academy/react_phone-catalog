71525749d57c0696b402bc7020ca441ec80acf1d
Yuriy Titov <yuranus@gmail.com>
2023-11-17 13:30:08 +0200

    feat: add store

---
+++ src/app/store.ts
@@ -0,0 +1,21 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+// eslint-disable-next-line import/no-cycle
+import productsReducer from '../features/productsSlice';
+
+export const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
+/* eslint-disable @typescript-eslint/indent */
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+/* eslint-enable @typescript-eslint/indent */
