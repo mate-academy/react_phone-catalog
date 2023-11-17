b1b6577e0367d63b04d1dc9962190a4a962d7e41
Yuriy Titov <yuranus@gmail.com>
2023-11-17 10:16:24 +0200

    ref: add HomePage route

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,11 +5,14 @@ import {
 } from 'react-router-dom';
 
 import App from './App';
+import { HomePage } from './pages/HomePage';
 
 export const Root = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+      </Route>
     </Routes>
   </Router>
 );
