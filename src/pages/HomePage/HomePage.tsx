4473f4dd6ec50f8d141a46741b3e7bef478163e0
Yuriy Titov <yuranus@gmail.com>
2023-11-17 13:46:43 +0200

    ref: add fetch products from API

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { fetchProducts } from '../../features/productsSlice';
+
 export const HomePage = () => {
+  const dispatch = useAppDispatch();
+  const { products } = useAppSelector(state => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
+  // eslint-disable-next-line no-console
+  console.log(products);
+
   return (
     <h1>Home page</h1>
   );
