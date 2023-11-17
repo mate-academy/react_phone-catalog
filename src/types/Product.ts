908b38f442c4e5330082bff1e1d59cf7f44a63d3
Yuriy Titov <yuranus@gmail.com>
2023-11-17 13:12:24 +0200

    feat: add Product

---
+++ src/types/Product.ts
@@ -0,0 +1,13 @@
+export type Product = {
+  age: number;
+  capacity: string;
+  discount: number;
+  id: string;
+  imageUrl: string;
+  name: string;
+  price: number;
+  ram: string;
+  screen: string;
+  snippet: string;
+  type: string;
+};
