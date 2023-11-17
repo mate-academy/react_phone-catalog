b40b28b50007030596b5615fedb2a0eee3196137
Yuriy Titov <yuranus@gmail.com>
2023-11-17 13:14:28 +0200

    feat: add fetch client

---
+++ src/utils/fetchClient.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+// eslint-disable-next-line max-len
+const BASE_URL = 'https://mate-academy.github.io/react_phone-catalog/api/products';
+
+// a promise resolved after a given delay
+function wait(delay: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, delay);
+  });
+}
+
+// To have autocompletion and avoid mistypes
+type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+
+function request<T>(
+  url: string,
+  method: RequestMethod = 'GET',
+  data: any = null, // we can send any data to the server
+): Promise<T> {
+  const options: RequestInit = { method };
+
+  if (data) {
+    // We add body and Content-Type only for the requests with data
+    options.body = JSON.stringify(data);
+    options.headers = {
+      'Content-Type': 'application/json; charset=UTF-8',
+    };
+  }
+
+  // for a demo purpose we emulate a delay to see if Loaders work
+  return wait(300)
+    .then(() => fetch(BASE_URL + url, options))
+    .then(response => response.json());
+}
+
+export const client = {
+  get: <T>(url: string) => request<T>(url),
+  post: <T>(url: string, data: any) => request<T>(url, 'POST', data),
+  patch: <T>(url: string, data: any) => request<T>(url, 'PATCH', data),
+  delete: (url: string) => request(url, 'DELETE'),
+};
