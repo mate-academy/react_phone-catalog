271a04f89b796c996df5da7b3ca8076d04309692
Vo7kov <jegor.volkov.a@gmail.com>
2023-11-18 23:40:24 +0100

    final. Added pay now page

--- src/pages/PaynowPage/PaynowPage.tsx
+++ src/pages/PaynowPage/PaynowPage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useForm } from 'react-hook-form';
+
 import { useAppDispatch } from '../../store/hooks';
 import * as goodsActions from '../../store/reducers/goodsSlice';
 
-import './PaynowPage.scss';
 import { MainButton } from '../../components/Buttons/MainButton/MainButton';
+
+import './PaynowPage.scss';
 
 type Props = {
   setIsPaynowOpened: React.Dispatch<React.SetStateAction<boolean>>,
@@ -17,21 +20,153 @@ export const PaynowPage: React.FC<Props> = React.memo(({
 }) => {
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = () => {
+    setIsPaynowOpened(false);
+    setIsSuccessPaynow(true);
+    dispatch(goodsActions.clear());
+  };
 
   return (
     <form
       action="#"
-      onSubmit={(event) => event.preventDefault()}
+      className="payNow"
+      method="GET"
+      onSubmit={handleSubmit(onSubmit)}
     >
+      <div className="payNow__items">
+        <label className="payNow__item">
+          {errors?.cardNumber && (
+            <p className="payNow__item-error">
+              {errors.cardNumber.message || t('unknownError')}
+            </p>
+          )}
+
+          <input
+            className="payNow__item-input"
+            type="text"
+            placeholder={t('cardNumberPlaceholder')}
+            {...register(
+              'cardNumber',
+              {
+                required: t('cardNumberRequiredError'),
+                pattern: {
+                  value: /^(\d{4}-){3}\d{4}$|^(\d{4} ){3}\d{4}$|^\d{16}$/,
+                  message: t('cardNumberPatternError'),
+                },
+              },
+            )}
+          />
+        </label>
+
+        <label className="payNow__item">
+          {errors?.nameOnCard && (
+            <p className="payNow__item-error">
+              {errors.nameOnCard.message || t('unknownError')}
+            </p>
+          )}
+
+          <input
+            className="payNow__item-input"
+            type="text"
+            placeholder={t('nameOnCardPlaceholder')}
+            {...register(
+              'nameOnCard',
+              {
+                required: t('nameOnCardRequiredError'),
+                pattern: {
+                  value: /^[a-zA-Zа-яА-ЯіїєґІЇЄҐšžõäöüŠŽÕÄÖÜ]+$/u,
+                  message: t('nameOnCardPatternError'),
+                },
+              },
+            )}
+          />
+        </label>
+
+        <label className="payNow__item">
+          {errors?.cvc && (
+            <p className="payNow__item-error">
+              {errors.cvc.message || t('unknownError')}
+            </p>
+          )}
+
+          <input
+            className="payNow__item-input"
+            type="text"
+            placeholder={t('cvcPlaceholder')}
+            {...register(
+              'cvc',
+              {
+                required: t('cvcRequiredError'),
+                pattern: {
+                  value: /^\d{3}$/,
+                  message: t('cvcPatternError'),
+                },
+              },
+            )}
+          />
+        </label>
+
+        <div className="payNow__items-extra">
+          <label className="payNow__item">
+            {errors?.month && (
+              <p className="payNow__item-error">
+                {errors.month.message || t('unknownError')}
+              </p>
+            )}
+
+            <input
+              className="payNow__item-input"
+              type="text"
+              placeholder={t('monthPlaceholder')}
+              {...register(
+                'month',
+                {
+                  required: t('monthRequiredError'),
+                  pattern: {
+                    value: /^[a-zA-Zа-яА-ЯіїєґІЇЄҐšžõäöüŠŽÕÄÖÜ]+$/u,
+                    message: t('monthPatternError'),
+                  },
+                },
+              )}
+            />
+          </label>
+
+          <label className="payNow__item">
+            {errors?.year && (
+              <p className="payNow__item-error">
+                {errors.year.message || t('unknownError')}
+              </p>
+            )}
+
+            <input
+              className="payNow__item-input"
+              type="text"
+              placeholder={t('yearPlaceholder')}
+              {...register(
+                'year',
+                {
+                  required: t('yearRequiredError'),
+                  pattern: {
+                    value: /^\d{4}$/,
+                    message: t('yearPatternError'),
+                  },
+                },
+              )}
+            />
+          </label>
+        </div>
+      </div>
+
       <MainButton
         text={t('payNow')}
-        className="paynow__submit-button"
+        className="payNow__submit-button"
         submit
-        onClick={() => {
-          setIsPaynowOpened(false);
-          setIsSuccessPaynow(true);
-          dispatch(goodsActions.clear());
-        }}
       />
     </form>
   );
