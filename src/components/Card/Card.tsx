1a96b6073e07dd35d9fbe61cd20cc32ce6d3e0bd
IvanVaverchak <ivanvaverchak@2000gmail.com>
2023-11-22 23:26:02 +0200

    add task solution

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -81,7 +81,7 @@ export const Card: React.FC<Props> = ({
         }}
       >
         <img
-          src={`_new/${image}`}
+          src={`${process.env.PUBLIC_URL}/_new/${image}`}
           alt="Phone"
           className="product-card__phone-img"
         />
