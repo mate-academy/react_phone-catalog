1a0e1b6d29588b09ec45fe6ea6dbdfe2d6a41b6e
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2023-11-24 21:53:27 +0200

    make getHotPriceProducts method

--- src/components/ProductsSlider/ProductsSlider.tsx
+++ src/components/ProductsSlider/ProductsSlider.tsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from 'react';
+import { Phone } from '../../Types/Phone';
+import { useProducts } from '../../helpers/CatalogContext/CatalogContext';
+import { client } from '../../helpers/utils/fetchData';
+
+function getHotPriceProducts(phonesWithDiscount: Phone[], type: string) {
+  const preparedPhones = [...phonesWithDiscount];
+
+  preparedPhones.filter(phone => phone.category === type);
+
+  preparedPhones.sort((a, b) => {
+    return (b.fullPrice - b.price) - (a.fullPrice - a.price);
+  });
+
+  return preparedPhones;
+}
+
+enum Categories {
+  Phones = 'phones',
+}
+
 export const ProductsSlider: React.FC = () => {
+  const { phones, setPhones } = useProducts();
+  const [sorted, setSorted] = useState<Phone[]>([]);
+
+  useEffect(() => {
+    setSorted(getHotPriceProducts(phones, Categories.Phones));
+  }, [phones]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await client.fetchPhones();
+
+        setPhones(data);
+      } catch (error) {
+        throw new Error();
+      }
+    };
+
+    fetchData();
+  }, [setPhones]);
+
   return (
-    <h1>Products Slider</h1>
+    <>
+      <h1>Products Slider</h1>
+      <p>{sorted.length}</p>
+    </>
   );
 };
--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -1,43 +1,28 @@
-import React, { useEffect } from 'react';
-import { Phone } from '../../Types/Phone';
+import React from 'react';
 import { ProductsSlider } from '../../components/ProductsSlider/ProductsSlider';
-import { useProducts } from '../../helpers/CatalogContext/CatalogContext';
-
-// eslint-disable-next-line
-const BASE_URL = 'https://mate-academy.github.io/react_phone-catalog/_new/products.json';
-
-function getData<T>(url: string): Promise<T> {
-  return fetch(BASE_URL + url)
-    .then(response => response.json());
-}
-
-const getPhones = () => getData<Phone[]>('');
+// import { useProducts } from '../../helpers/CatalogContext/CatalogContext';
 
 export const PhonesPage: React.FC = () => {
-  const { phones, setPhones } = useProducts();
+  // const { phones, setPhones } = useProducts();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getPhones();
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const data = await getPhones();
 
-        setPhones(data);
-      } catch (error) {
-        throw new Error();
-      }
-    };
+  //       setPhones(data);
+  //     } catch (error) {
+  //       throw new Error();
+  //     }
+  //   };
 
-    fetchData();
-  }, [setPhones]);
-
-  // setTimeout(() => {
-  //   console.log(phones);
-  // }, 1000);
+  //   fetchData();
+  // }, [setPhones]);
 
   return (
     <>
       <h1>Phones Page</h1>
-      <p>{`phones arr length is: ${phones.length}`}</p>
+      {/* <p>{`phones arr length is: ${phones.length}`}</p> */}
 
       <ProductsSlider />
 
