c05ffcacbbec01083ac3eeba1df977845e1ea53f
Yuriy Titov <yuranus@gmail.com>
2023-11-27 15:00:28 +0200

    feat: add getSearchWith

---
+++ src/utils/getSearchWith.ts
@@ -0,0 +1,45 @@
+export type SearchParams = {
+  [key: string]: string | string[] | null,
+};
+
+/**
+ * This function prepares a correct search string
+ * from a given currentParams and paramsToUpdate.
+ */
+export function getSearchWith(
+  currentParams: URLSearchParams,
+  paramsToUpdate: SearchParams, // it's our custom type
+): string {
+  // copy currentParams by creating new object from a string
+  const newParams = new URLSearchParams(
+    currentParams.toString(),
+  );
+
+  // Here is the example of paramsToUpdate
+  // {
+  //   sex: 'm',                ['sex', 'm']
+  //   order: null,             ['order', null]
+  //   centuries: ['16', '19'], ['centuries', ['16', '19']]
+  // }
+  //
+  // - params with the `null` value are deleted;
+  // - string value is set to given param key;
+  // - array of strings adds several params with the same key;
+
+  Object.entries(paramsToUpdate)
+    .forEach(([key, value]) => {
+      if (value === null) {
+        newParams.delete(key);
+      } else if (Array.isArray(value)) {
+        newParams.delete(key);
+
+        value.forEach(part => {
+          newParams.append(key, part);
+        });
+      } else {
+        newParams.set(key, value);
+      }
+    });
+
+  return newParams.toString();
+}
