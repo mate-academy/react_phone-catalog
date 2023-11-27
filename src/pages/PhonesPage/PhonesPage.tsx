fda50a200f271dada050dd2690a5b2fe4ee299ff
Yuriy Titov <yuranus@gmail.com>
2023-11-27 09:38:20 +0200

    feat: add getPhones

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useMemo } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { fetchProducts } from '../../features/productsSlice';
+import { ProductType } from '../../types/ProductType';
+
 import './PhonesPage.scss';
 
 export const PhonesPage = () => {
+  const dispatch = useAppDispatch();
+  const { products } = useAppSelector(state => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
+  const getPhones = useMemo(() => {
+    return products
+      .filter(product => product.type === ProductType.PHONE);
+  }, [products]);
+
+  // eslint-disable-next-line no-console
+  console.log(getPhones);
+
   return (
     <h1>Mobile phones</h1>
   );
