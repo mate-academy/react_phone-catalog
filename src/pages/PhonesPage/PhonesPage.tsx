097225f5b1cb82d4e387dd062f04e8f506d15259
Yuriy Titov <yuranus@gmail.com>
2023-11-27 10:26:31 +0200

    feat: add Product list block

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { fetchProducts } from '../../features/productsSlice';
+import { ProductList } from '../../components/ProductList';
 import { ProductType } from '../../types/ProductType';
 
 import './PhonesPage.scss';
@@ -18,10 +19,11 @@ export const PhonesPage = () => {
       .filter(product => product.type === ProductType.PHONE);
   }, [products]);
 
-  // eslint-disable-next-line no-console
-  console.log(getPhones);
-
   return (
-    <h1>Mobile phones</h1>
+    <>
+      <h1>Mobile phones</h1>
+
+      <ProductList products={getPhones} />
+    </>
   );
 };
