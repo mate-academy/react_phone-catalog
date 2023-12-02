ce1941d098e7115dec4040d52f29518583701910
Yuriy Titov <yuranus@gmail.com>
2023-12-02 13:39:03 +0200

    feat: add getPageNumbers

---
+++ src/utils/getPageNumbers.ts
@@ -0,0 +1,9 @@
+export function getPageNumbers(from: number, to: number): number[] {
+  const numbers = [];
+
+  for (let n = from; n <= to; n += 1) {
+    numbers.push(n);
+  }
+
+  return numbers;
+}
