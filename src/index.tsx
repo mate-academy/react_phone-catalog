6a11e2d56780608551ddeb4d1fcc820399ab11bf
Nikita <Nehrim24@gmail.com>
2023-12-04 19:12:26 +0200

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { CartFavoritesProvider } from './providers/CartFavoritesProvider';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root') as HTMLElement;
+
+const root = (ReactDOM as any).createRoot(rootElement);
+
+root.render(
   <Router>
     <CartFavoritesProvider>
       <App />
     </CartFavoritesProvider>
   </Router>,
-  document.getElementById('root'),
 );
