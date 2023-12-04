b54a2afc6b1abd40bc1e1f5df3a0ac5f012e00b6
Victor Hrygorash <viktor12041991@i.ua>
2023-12-04 14:29:35 +0200

    try2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
-import { Footer } from './components/Footer/Footer';
+import { Footer } from './components/Footer';
 import './App.scss';
 
 const App: React.FC = () => (
