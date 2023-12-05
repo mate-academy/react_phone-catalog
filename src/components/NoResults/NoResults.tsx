fb93175adaae38bb6403fa13e96608030e6e6fbf
Yuriy Titov <yuranus@gmail.com>
2023-12-05 11:33:21 +0200

    feat: add NoResults

---
+++ src/components/NoResults/NoResults.tsx
@@ -0,0 +1,20 @@
+import { ProductType } from '../../types/ProductType';
+import './NoResults.scss';
+
+type Props = {
+  productType: ProductType
+};
+
+export const NoResults: React.FC<Props> = ({ productType }) => {
+  const categoryTitle = {
+    [ProductType.PHONE]: 'Mobile phones',
+    [ProductType.TABLET]: 'Tablets',
+    [ProductType.ACCESSORY]: 'Accessories',
+  };
+
+  return (
+    <section className="Page-NoResults NoResults">
+      {`${categoryTitle[productType]} not found`}
+    </section>
+  );
+};
