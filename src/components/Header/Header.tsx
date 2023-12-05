be00b148d514172345476f6181be91c69b5e86dd
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-12-05 18:51:48 +0200

    header favorites was added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,85 @@
 import classNames from 'classnames';
 import './Header.scss';
 
-import { Link, NavLink } from 'react-router-dom';
-// eslint-disable-next-line
-// @ts-ignore
-import Logo from '../../Images/Icons/logo.svg';
+import { NavLink, Link, useLocation } from 'react-router-dom';
+import { Search } from '../Search/Search';
 
 export const Header = () => {
+  const isActiveTab = ({ isActive }: { isActive: boolean }) => classNames(
+    'nav__link', { nav__active: isActive },
+  );
+
+  const location = useLocation();
+  const { pathname } = location;
+  const isPathnameHome = pathname === '/';
+
   return (
     <header className="header">
-      <h1>Start</h1>
-      <div className="header__nav">
-        <div className="header__logo">
-          <Link
-            to="/"
-          >
-            <img
-              src={Logo}
-              alt="logo"
-            />
-          </Link>
-        </div>
-
-        <NavLink
-          className={classNames('header__nav-link')}
+      <div className="container">
+        <Link
           to="/"
+          className="header__logo"
+        />
+
+        <nav className="nav">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <NavLink
+                to="/"
+                className={isActiveTab}
+              >
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav__item">
+              <NavLink
+                to="/phones"
+                className={isActiveTab}
+              >
+                Phones
+              </NavLink>
+            </li>
+
+            <li className="nav__item">
+              <NavLink
+                to="/tablets"
+                className={isActiveTab}
+              >
+                Tablets
+              </NavLink>
+            </li>
+
+            <li className="nav__item">
+              <NavLink
+                to="/accessories"
+                className={isActiveTab}
+              >
+                Accessories
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        {!isPathnameHome && (
+          <div className="header__icon-search">
+            <Search />
+          </div>
+        )}
+
+        <NavLink 
+          to="favorites"
+          className={({ isActive }: { isActive: boolean }) => classNames(
+            'header__favorites', 
+            'nav__link', 
+            { nav__active: isActive },
+          )}
         >
-          home
+          {/* {favourites.length > 0 && (
+            <div className="header__icon-count">
+              {favourites.length}
+            </div>
+          )} */}
         </NavLink>
 
       </div>
