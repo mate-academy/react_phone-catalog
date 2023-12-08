5e09daec10cfb650111a2080a8bb26185fa23938
Yuriy Titov <yuranus@gmail.com>
2023-12-08 09:55:03 +0200

    ref: remove getProduct

--- src/api/product.ts
+++ src/api/product.ts
@@ -4,7 +4,3 @@ import { client } from '../utils/fetchClient';
 export const getProducts = () => {
   return client.get<Product[]>('.json');
 };
-
-export const getProduct = (id: string) => {
-  return client.get<Product>(`/${id}.json`);
-};
