d27dc8964a270a62c768d58111611bc245e58aa1
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-12-18 14:15:47 +0200

    update

--- src/Components/details/details.tsx
+++ src/Components/details/details.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import './details.scss';
 import YouMay from '../Youmayalsolike/youmay';
 import {
