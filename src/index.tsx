8033724f24afc924fa20c1835bf6cbfd005dfbda
Iryna Konovalova <iconovalova19@gmail.com>
2024-01-11 15:23:04 +0100

    create store

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,16 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
 import { App } from './App';
+import { store } from './app/store';
+
+const Root = () => (
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+);
 
 createRoot(document.getElementById('root') as HTMLElement)
-  .render(<App />);
+  .render(<Root />);
