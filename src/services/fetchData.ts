4ae76e88646d1ce76b39b37be7f7f856194531d8
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-01-14 12:51:32 +0100

    implement MainContext

--- src/services/fetchData.ts
+++
@@ -1,7 +0,0 @@
-const BASE_URL = 'https://mate-academy.github.io/react_phone-catalog/_new';
-
-export const fetchData = () => {
-  return fetch(`${BASE_URL}/products.json`).then((response) => {
-    return response.json();
-  });
-};
