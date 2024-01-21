e1e5e5672b0d2083aefb3e3275f118a411a52379
Dmytro Torop <young-z@i.ua>
2024-01-21 11:05:05 +0200

    init: start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.scss';
+import { Header } from './components/Header';
 
 export const App = () => (
   <div className="App">
+    <Header />
     <h1>React Phone Catalog</h1>
   </div>
 );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+  return (
+    <h1>Header</h1>
+  );
+};
