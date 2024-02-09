0c8a12154484db6ca35d348252a03dfa9ae4d632
Dmytro Levchenko <levchenkod91@gmail.com>
2024-02-09 15:55:34 +0200

    fix cart page

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { ButtonBack } from '../../components/ButtonBack';
 import { CartItems } from '../../components/CartItems';
 import { Checkout } from '../../components/Checkout';
 import './CartPage.scss';
+import { GlobalContext } from '../../GlobalContext';
 
 export const CartPage = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const { cartItems } = useContext(GlobalContext);
 
   return (
     <div className="CartPage">
@@ -13,24 +15,33 @@ export const CartPage = () => {
         <ButtonBack />
       </div>
 
-      <h1 className="CartPage__title">
-        Cart
-      </h1>
+      {cartItems.length ? (
+        <>
 
-      <div className="CartPage__list">
-        <CartItems />
-      </div>
+          <h1 className="CartPage__title">
+            Cart
+          </h1>
 
-      <div className="CartPage__checkout">
-        <Checkout onSubmit={setIsVisible} />
-      </div>
+          <div className="CartPage__list">
+            <CartItems />
+          </div>
 
-      {isVisible && (
-        <div className="CartPage__checkout-answer">
-          <h2 className="CartPage__checkout-answer--text">
-            Sorry this feature not yet implemented
-          </h2>
-        </div>
+          <div className="CartPage__checkout">
+            <Checkout onSubmit={setIsVisible} />
+          </div>
+
+          {isVisible && (
+            <div className="CartPage__checkout-answer">
+              <h2 className="CartPage__checkout-answer--text">
+                Sorry this feature not yet implemented
+              </h2>
+            </div>
+          )}
+        </>
+      ) : (
+        <h1 className="CartPage__title CartPage__title--empty">
+          Your cart is empty
+        </h1>
       )}
     </div>
   );
