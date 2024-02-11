cfccfb0237bc2c2b39d5c06b290ee5ece55aed92
Yurii Shkrobut <georgebest2409@gmail.com>
2024-02-11 23:02:39 +0200

    Dropdowns

--- src/components/Dropdowns.tsx
+++ src/components/Dropdowns.tsx
@@ -7,10 +7,17 @@ export const Dropdowns: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const sort = searchParams.get('sort') || '';
+  const perPage = searchParams.get('perPage') || '';
 
   const handleSortByChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSearchParams(
       getSearchWith(searchParams, { sort: e.target.value }),
+    );
+  };
+
+  const handlePerPageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSearchParams(
+      getSearchWith(searchParams, { perPage: e.target.value }),
     );
   };
 
@@ -30,6 +37,27 @@ export const Dropdowns: React.FC = () => {
           <option value="name">Alphabetically</option>
           <option value="price">Cheapest</option>
         </select>
+
+        <div className="dropdown__arrow"></div>
+      </div>
+
+      <div className="dropdown">
+        <p className="dropdown__title">
+          Items on page
+        </p>
+
+        <select
+          value={perPage}
+          className="dropdown__select"
+          onChange={handlePerPageChange}
+        >
+          <option value="All">All</option>
+          <option value="4">4</option>
+          <option value="8">8</option>
+          <option value="16">16</option>
+        </select>
+
+        <div className="dropdown__arrow"></div>
       </div>
     </section>
   );
