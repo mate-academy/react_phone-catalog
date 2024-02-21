6e4ae5f58d2436a56b06bb1c0653e85b5bf28fc8
Anastasiia Em <ema199625@gmail.com>
2024-02-21 17:05:35 +0100

    Refactoring

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ export const Logo = () => {
   return (
     <Link to="/" className="logo">
       <img
-        src={`${process.env.PUBLIC_URL}/images/logo.svg`}
+        src="/images/logo.svg"
         alt="LOGO"
         className="logo__img"
       />
