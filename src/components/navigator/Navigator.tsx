126e977ec7412a3db418a00add6bfb9fe1aa63e9
haduigon <haduigon@gmail.com>
2024-02-22 17:10:19 +0200

    new commit

--- src/components/navigator/Navigator.tsx
+++ src/components/navigator/Navigator.tsx
@@ -2,9 +2,24 @@ type Props = {
   path: string,
 };
 
+// function capitalize(path: string) {
+//   const parts = path.split('/');
+//   const newarr = parts.map(part => {
+//     const letter = part.substring(1);
+//     const firstLetter = part.charAt(0);
+//     const result = firstLetter.toLocaleUpperCase() + letter;
+
+//     return result;
+//   });
+
+//   return newarr;
+// }
+
 export const Navigator: React.FC<Props> = ({ path }) => {
-  // const parts = path.split('/');
-  // const letter = parts[1].slice(1);
+  const parts = path.split('/');
+  const letter = parts[1].substring(1);
+  const firstLetter = parts[1].charAt(0);
+  const result = firstLetter.toLocaleUpperCase() + letter;
 
   return (
     <div className="navigation-block">
@@ -18,7 +33,7 @@ export const Navigator: React.FC<Props> = ({ path }) => {
       <div>
         <img src="./img/icons/arrowRight.svg" alt="img" />
       </div>
-      <div>{path}</div>
+      <div>{result}</div>
     </div>
   );
 };
