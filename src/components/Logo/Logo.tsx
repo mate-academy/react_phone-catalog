232f0bf4e232d6edf67d6cce9947d3a7fec8f405
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-03-25 21:16:52 +0100

    changed footer main logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,13 @@
 import { NavLink } from 'react-router-dom';
 import './logo.scss';
+import { scrollToTop } from '../../helpers/scrollToTop';
 
 export const Logo = () => {
   return (
-    <NavLink to="/" className="logo icon" />
+    <NavLink
+      to="/"
+      className="logo icon"
+      onClick={scrollToTop}
+    />
   );
 };
