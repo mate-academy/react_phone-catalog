983507c0945df13a0f92c0fccfeaec0814826739
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2024-04-13 13:47:01 +0300

    Implement Loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,17 @@
+import { twMerge } from 'tailwind-merge';
+
+interface Props {
+  className?: string;
+}
+
+export const Loader: React.FC<Props> = ({ className = '' }) => {
+  return (
+    <div
+      className={twMerge(
+        `h-16 w-16 animate-spin rounded-full border-8
+        border-dashed border-secondary`,
+        className,
+      )}
+    ></div>
+  );
+};
