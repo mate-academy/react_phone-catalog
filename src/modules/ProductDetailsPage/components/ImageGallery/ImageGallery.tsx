2b7d536bb6256ac1dbb24f978293051352930098
OleksiiNesteruk <nesteruk.oleksiy@gmail.com>
2024-04-16 15:50:42 +0100

    Added effects to the image gallery

--- src/modules/ProductDetailsPage/components/ImageGallery/ImageGallery.tsx
+++ src/modules/ProductDetailsPage/components/ImageGallery/ImageGallery.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './ImageGallery.module.scss';
 import classNames from 'classnames';
 import { BASE_URL } from '../../../../utils/const';
@@ -13,6 +13,15 @@ export const ImageGallery: React.FC<ImageGalleryProps> = ({
   productName,
 }) => {
   const [selectedImage, setSelectedImage] = useState(images[0]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const index = images.indexOf(selectedImage);
+
+    if (index !== activeIndex) {
+      setActiveIndex(index);
+    }
+  }, [selectedImage, images, activeIndex]);
 
   return (
     <div className={styles.imageGallery}>
@@ -31,19 +40,26 @@ export const ImageGallery: React.FC<ImageGalleryProps> = ({
       </div>
 
       <div className={styles.mainImageContainer}>
-        <img
-          src={`${BASE_URL}/${selectedImage}`}
-          alt={productName}
-          className={styles.mainImage}
-        />
+        {images.map((image, index) => (
+          <img
+            key={image}
+            src={`${BASE_URL}/${image}`}
+            alt={productName}
+            className={classNames(styles.mainImage, {
+              [styles.mainImageActive]: index === activeIndex,
+            })}
+          />
+        ))}
       </div>
       <div className={styles.thumbnailContainerMobile}>
         {images.map(image => (
           <img
             key={image}
             src={`${BASE_URL}/${image}`}
-            alt={`Thumbnail of ${productName}`}
-            className={styles.thumbnail}
+            alt={`${productName}`}
+            className={classNames(styles.thumbnail, {
+              [styles.selectedThumbnail]: image === selectedImage,
+            })}
             onClick={() => setSelectedImage(image)}
           />
         ))}
