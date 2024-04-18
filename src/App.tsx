ba21bb6aac14e1a6a15541607f5cf34897d5e51b
Roman S <romanuch03@gmail.com>
2024-04-18 11:50:37 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useContext } from 'react';
 import './App.scss';
+import { Outlet } from 'react-router-dom';
+import { StateStore } from './store/StoreContext';
+import { Header } from './components/Header/Header';
+import { Loader } from './components/Loader';
+import { ErrorMessage } from './components/ErrorMessage/ErrorMessage';
+import { Footer } from './components/Footer/Footer';
 
-export const App = () => (
-  <div className="App">
-    <h1>Product Catalog</h1>
-  </div>
-);
+const App = () => {
+  const { isError, isLoading } = useContext(StateStore);
+
+  return (
+    <div className="App">
+      <Header />
+
+      <div className="container">
+        {isLoading && <Loader />}
+
+        {!isLoading && (isError ? <ErrorMessage /> : <Outlet />)}
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,5 @@
+import './NotFoundPage.scss';
+
+export const NotFoundPage = () => {
+  return <h1 className="title">Page not found</h1>;
+};
