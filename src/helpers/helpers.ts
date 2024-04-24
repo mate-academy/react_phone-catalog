b2f7d4c06e7cb5349dfc682b82ce1b3ce339b35e
Vadim <volandemortuwka@gmail.com>
2024-04-24 16:12:23 +0300

    first save

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -2,17 +2,17 @@ import { getProduct } from '../api/fetchClient';
 import { Product } from '../types/Product';
 
 export const getAllProudct = async () => {
-  return getProduct<Product[]>('api/products.json');
+  return getProduct<Product[]>('/api/products.json');
 };
 
 export const getNewProduct = async () => {
-  const response = await getProduct<Product[]>('api/products.json');
+  const response = await getProduct<Product[]>('/api/products.json');
 
   return response.sort((a, b) => b.year - a.year);
 };
 
 export const getHotProduct = async () => {
-  const response = await getProduct<Product[]>('api/products.json');
+  const response = await getProduct<Product[]>('/api/products.json');
 
   return response.sort(
     (a, b) => b.fullPrice - a.fullPrice - (a.fullPrice - a.price),
