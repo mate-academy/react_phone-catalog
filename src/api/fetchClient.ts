c496b1ddd5cacb108c0929460bdda43ba00525d7
Vadim <volandemortuwka@gmail.com>
2024-04-24 16:10:23 +0300

    first save

--- src/api/fetchClient.ts
+++ src/api/fetchClient.ts
@@ -1,4 +1,5 @@
-const BASE_URL = 'https://github.com/mate-academy/react_phone-catalog';
+const BASE_URL =
+  'https://github.com/mate-academy/react_phone-catalog/blob/master/public';
 
 export const getProduct = async <T>(url: string): Promise<T> => {
   const response = await fetch(BASE_URL + url);
