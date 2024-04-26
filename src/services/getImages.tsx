12b05b09037138f760a92636e7d9f3ae5e3e427a
Ivan <fanmetalheavy@gmail.com>
2024-04-26 14:56:10 +0300

    commit before try set webpack

--- src/services/getImages.tsx
+++ src/services/getImages.tsx
@@ -1,7 +1,5 @@
-import { BASE_URL, handleResponse } from '../api';
 import { WIDTH_DEVICES } from '../modules/constants/WIDTH_DEVICES';
 import { imgs, imgsMobile } from '../modules/shared/imgsForDevices';
-import { Picture } from '../types/Picture';
 
 export function getImages(windowSize: number) {
   if (windowSize <= WIDTH_DEVICES.mobile) {
@@ -9,22 +7,4 @@ export function getImages(windowSize: number) {
   }
 
   return imgs.slice();
-}
-
-export async function getImagesFetch(windowSize: number) {
-  let images: Picture[] = [];
-
-  try {
-    const response = await fetch(`${BASE_URL}slider.json`).then(handleResponse);
-
-    images = await response.json();
-  } catch {
-    return;
-  }
-
-  if (windowSize <= WIDTH_DEVICES.mobile) {
-    images.slice(0, 3);
-  }
-
-  images.slice(3);
 }
