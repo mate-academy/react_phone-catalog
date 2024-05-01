d4d4dcdbeeae48abef953044cea93c712352a54c
Jane Shavrukova <eva.shavrukova@gmail.com>
2024-05-01 11:35:43 -0700

    Add banner slider

--- src/modules/HomePage/components/Slider/PicturesSlider.tsx
+++ src/modules/HomePage/components/Slider/PicturesSlider.tsx
@@ -7,41 +7,45 @@ import styles from './PicturesSlider.module.scss';
 
 export const PicturesSlider = () => {
   return (
-    <div className={styles['swiper-container']}>
-      <div className={styles['swiper-button-prev']}>
+    <div className={styles.container}>
+      <div className={styles.swiperButtonNext}>
         <img src="/img/icons/arrow-back.svg" alt="arrow-back" />
       </div>
 
       <Swiper
-        className={styles.swiper}
         modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+        className={styles.swiper}
         spaceBetween={0}
         slidesPerView={1}
-        // autoplay={{ delay: 5000 }}
-        navigation={{
-          prevEl: `.${styles['swiper-button-prev']}`,
-          nextEl: `.${styles['swiper-button-prev']}`,
-        }}
+        autoplay={{ delay: 5000 }}
         loop={true}
+        navigation={{
+          prevEl: `.${styles.swiperButtonPrev}`,
+          nextEl: `.${styles.swiperButtonNext}`,
+        }}
         pagination={{
           clickable: true,
-          bulletClass: styles['swiper-pagination-bullet'],
-          bulletActiveClass: styles['swiper-pagination-bullet-active'],
+          type: 'bullets',
+          el: `.${styles.swiperPagination}`,
           bulletElement: 'div',
+          bulletClass: styles.swiperPaginationBullet,
+          bulletActiveClass: styles.swiperPaginationBulletActive,
         }}
       >
-        <SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}>
           <img src="img/banner/banner-phones.jpg" alt="phone" />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}>
           <img src="img/banner/banner-accessories.jpg" alt="accessories" />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className={styles.swiperSlide}>
           <img src="img/banner/banner-tablets.jpg" alt="tablet" />
         </SwiperSlide>
       </Swiper>
 
-      <div className={styles['swiper-button-next']}>
+      <div className={styles.swiperPagination}></div>
+
+      <div className={styles.swiperButtonNext}>
         <img src="/img/icons/arrow-forward.svg" alt="arrow-forward" />
       </div>
     </div>
