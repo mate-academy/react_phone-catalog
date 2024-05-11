cf35fd92a54eb7058b2687b8fbacd97afdfaffe8
Vlad Buldenko <vladbuldenko@gmail.com>
2024-05-11 14:36:38 +0300

    Header component init

--- src/components/NavButtons/NavButtons.tsx
+++ src/components/NavButtons/NavButtons.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './NavButtons.scss';
 
 export const NavButtons = () => {
   const [theme, toggleTheme] = useState('light');
@@ -17,7 +18,7 @@ export const NavButtons = () => {
         // state={{ from: location.pathname }}
         className="nav__button"
       >
-        <div className="icon icon--favourites">
+        <div className="icon icon--favourite">
           <div className="counter">{1}</div>
         </div>
       </Link>
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom';
 import { getLinkClass } from '../../utils/utils';
 import { NavBarLinks } from '../../types/NavbarLinks';
 import React from 'react';
+import './Navigation.scss';
 
 type Props = {
   className?: string;
