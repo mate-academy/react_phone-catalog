919e5e5ef47b319e81d2bfa96bdd04848b3d8580
vova <hohiop09@gmail.com>
2024-05-12 14:30:01 +0000

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import './Footer.scss';
+import logo from './../../img/Icons/Logo.png';
+import arrowUp from './../../img/Icons/arrow-up-black.png';
 
 const Footer = () => {
-  return <footer className="footer">asdasd</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__wrapper">
+        <div className="footer__logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <nav className="footer__menu">
+          <ul className="footer__menu--wrapper">
+            <li className="footer__menu--item">
+              <a href="">GITHUB</a>
+            </li>
+            <li className="footer__menu--item">
+              <a href="">CONTACTS</a>
+            </li>
+            <li className="footer__menu--item">
+              <a href="">RIGHTS</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="footer__toTop">
+          <div className="footer__toTop--title">Back to top</div>
+          <div className="footer__toTop--button">
+            <img src={arrowUp} alt="arrowUp" className="footer__toTop--logo" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
