7fd4eabea227b2ea247dc7fe49bd0db77cc88fb7
Olaf <olaf.chuszno@gmail.com>
2024-05-12 22:20:26 +0200

    Display 2 cards on product slider

--- src/pages/HomePage/components/ProductSlider/ProductSlider.tsx
+++ src/pages/HomePage/components/ProductSlider/ProductSlider.tsx
@@ -7,10 +7,14 @@ import './ProductSlider.scss';
 
 export const ProductSlider: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [activeSlide, setActiveSlide] = useState<number>(0);
+
+  const currentProduct = products[activeSlide];
+  const nextProduct = activeSlide < 7 ? products[activeSlide + 1] : products[0];
 
   useEffect(() => {
     getProducts().then((productsFromApi: Product[]) =>
-      setProducts(productsFromApi),
+      setProducts(productsFromApi.reverse().slice(0, 8)),
     );
   }, []);
 
@@ -25,16 +29,35 @@ export const ProductSlider: React.FC = () => {
         <div className="slider-top product-slider__top">
           <h2 className="slider-top__title">Brand new models</h2>
           <nav className="slider-top__nav">
-            <button className="arrow-button">
+            <button
+              className="arrow-button"
+              onClick={() =>
+                setActiveSlide((currentSlide: number) => {
+                  return currentSlide > 0 ? currentSlide - 1 : 7;
+                })
+              }
+            >
               <img src="./icons/arrow-left.svg" alt="left arrow icon" />
             </button>
-            <button className="arrow-button">
+            <button
+              className="arrow-button"
+              onClick={() =>
+                setActiveSlide((currentSlide: number) => {
+                  return currentSlide < 7 ? currentSlide + 1 : 0;
+                })
+              }
+            >
               <img src="./icons/arrow-right.svg" alt="right arrow icon" />
             </button>
           </nav>
         </div>
-        <div className="product-slider__cards">
-          <ProductCard product={products[0]} />
+        <div className="cards product-slider__cards">
+          <span className="cards__container">
+            <ProductCard product={currentProduct} />
+          </span>
+          <span className="cards__container">
+            <ProductCard product={nextProduct} />
+          </span>
         </div>
       </section>
     )
