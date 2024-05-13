4d3ee5d52dc7dba85bc19d4c5a7f2faad85bfc22
Olaf <olaf.chuszno@gmail.com>
2024-05-13 16:23:05 +0200

    Include base url in fetch

--- src/helpers/getProducts.ts
+++ src/helpers/getProducts.ts
@@ -1,12 +1,12 @@
 import { Product } from '../types/Product';
 import { ProductCategories } from '../types/ProductCategories';
 
+const BASE_URL = 'https://olafchuszno.github.io/react_phone-catalog';
+
 export const getProductsByCategory: (
   category: ProductCategories,
 ) => Promise<Product[]> = (category: ProductCategories) => {
-  return fetch(`/api/${category}.json`)
+  return fetch(BASE_URL + `/api/${category}.json`)
     .then(response => response.json())
-    .then(parsed => {
-      return parsed;
-    });
+    .then(parsed => parsed);
 };
