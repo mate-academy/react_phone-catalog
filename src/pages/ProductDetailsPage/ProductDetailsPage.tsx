625c2fcf10cf787132c53d7b173372b111420029
Olaf <olaf.chuszno@gmail.com>
2024-05-16 17:55:24 +0200

    Create breadcrumbs for product details

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -5,6 +5,7 @@ import { DetailedProduct } from '../../types/DetailedProduct';
 import { Product } from '../../types/ProductCard';
 import { ProductCategories } from '../../types/ProductCategories';
 import { GetDetailedProducts } from '../../helpers/GetDetailedProducts';
+import { Link, useParams } from 'react-router-dom';
 
 export const ProductDetailsPage: React.FC = () => {
   const [foundProduct, setFoundProduct] = useState<Product | undefined>(
@@ -13,17 +14,15 @@ export const ProductDetailsPage: React.FC = () => {
   const [displayedProduct, setDisplayedProduct] =
     useState<DetailedProduct | null>(null);
 
-  const PRODUCT_ID_FROM_URL = 'apple-iphone-7-32gb-black';
+  const { productId } = useParams();
 
   useEffect(() => {
     getAllProducts().then((products: Product[]) =>
       setFoundProduct(
-        products.find(
-          (product: Product) => product.itemId === PRODUCT_ID_FROM_URL,
-        ),
+        products.find((product: Product) => product.itemId === productId),
       ),
     );
-  }, []);
+  }, [productId]);
 
   // Get product from the Api
   useEffect(() => {
@@ -34,25 +33,82 @@ export const ProductDetailsPage: React.FC = () => {
           setDisplayedProduct(
             productsFromApi.find(
               (detailedProduct: DetailedProduct) =>
-                detailedProduct.id === PRODUCT_ID_FROM_URL,
+                detailedProduct.id === productId,
             ) ?? null,
           );
         },
       );
     }
-  }, [foundProduct]);
+  }, [foundProduct, productId]);
+
+  if (!displayedProduct) {
+    return <p>Product doesn&apos;t exist</p>;
+  }
+
+  const {
+    category,
+    // id,
+    // namespaceId,
+    name,
+    // capacityAvailable,
+    // capacity,
+    // priceRegular,
+    // priceDiscount,
+    // colorsAvailable,
+    // color,
+    // images,
+    // description,
+  } = displayedProduct;
 
   return (
     <>
       {displayedProduct && (
         <main className="product-details">
-          ProductDetailsPage
-          <section>
-            <p>Top</p>
+          <section className="product-details__top ">
+            <nav className="breadcrumbs">
+              <div className="breadcrumbs__wrapper">
+                <img
+                  className="breadcrumbs__home"
+                  src="./icons/home.svg"
+                  alt="home icon"
+                />
+                <img
+                  className="breadcrumbs__next"
+                  src="./icons/arrow-right-disabled.svg"
+                  alt="right arrow image"
+                />
+                <Link to={`/${category}`} className="breadcrumbs__category">
+                  {category.charAt(0).toUpperCase() + category.slice(1)}
+                </Link>
+
+                <img
+                  className="breadcrumbs__next"
+                  src="./icons/arrow-right-disabled.svg"
+                  alt="right arrow image"
+                />
+
+                <p className="breadcrumbs__product-name">{name}</p>
+              </div>
+
+              <button
+                onClick={history.back}
+                className="back-link breadcrumbs__back-link"
+              >
+                <div className="back-link__image-wrapper">
+                  <img
+                    className="back-link__image breadcrumbs__back-link"
+                    src="./icons/arrow-left.svg"
+                    alt="left arrow icon"
+                  />
+                </div>
+
+                <p className="back-link__text">Back</p>
+              </button>
+            </nav>
           </section>
           <section>
-            product name: {displayedProduct.name}
-            product priceRegular: {displayedProduct.priceRegular}
+            {/* product name: {displayedProduct.name}
+            product priceRegular: {displayedProduct.priceRegular} */}
           </section>
         </main>
       )}
