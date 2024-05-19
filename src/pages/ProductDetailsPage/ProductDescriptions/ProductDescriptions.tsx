37022ddd206832f35fb9ff33c866e5abbdbc6a43
Olaf <olaf.chuszno@gmail.com>
2024-05-19 11:23:58 +0200

    Style product descriptions

--- src/pages/ProductDetailsPage/ProductDescriptions/ProductDescriptions.tsx
+++ src/pages/ProductDetailsPage/ProductDescriptions/ProductDescriptions.tsx
@@ -9,31 +9,31 @@ type Props = {
 export const ProductDescriptions: React.FC<Props> = ({ descriptions }) => {
   return (
     <section className="product-descriptions">
-      <h3 className="product-descriptions__title title--3">About</h3>
+      <div className="product-descriptions__top">
+        <h3 className="product-descriptions__title title--3">About</h3>
 
-      <hr />
+        <hr className="product-descriptions__divider" />
+      </div>
 
-      <ul className="descriptions-list product-descriptions__list">
-        {descriptions.map((pair: DescriptionPair) => (
-          <li
-            key={pair.title}
-            className="description-pair descriptions-list__element"
-          >
-            <h4 className="description-pair__title title--4">{pair.title}</h4>
-            <div className="description-pair__info-wrapper">
-              {pair.text.map((description: string, index) => (
-                <p
-                  // Using an index here since it wouldn't make sense for the key to be a 200 char string
-                  key={index}
-                  className="description-pair__info body-text--14"
-                >
-                  {description}
-                </p>
-              ))}
-            </div>
-          </li>
-        ))}
-      </ul>
+      {descriptions.map((pair: DescriptionPair) => (
+        <div
+          key={pair.title}
+          className="description-pair product-descriptions__description-pair"
+        >
+          <h4 className="description-pair__title title--4">{pair.title}</h4>
+          <div className="description-pair__info-wrapper">
+            {pair.text.map((description: string, index) => (
+              <p
+                // Using an index here since it wouldn't make sense for the key to be a 200 char string
+                key={index}
+                className="description-pair__info body-text--14"
+              >
+                {description}
+              </p>
+            ))}
+          </div>
+        </div>
+      ))}
     </section>
   );
 };
