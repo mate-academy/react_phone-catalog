7e801b923eb0f8ef199e4c4bb14f03a12539ac66
Iryna Konovalova <iconovalova19@gmail.com>
2024-05-21 15:12:27 +0200

    change banner and categories images

--- src/libs/components/CategoryCard/CategoryCard.tsx
+++ src/libs/components/CategoryCard/CategoryCard.tsx
@@ -24,27 +24,15 @@ export const CategoryCard: React.FC<Props> = ({
     .length;
 
   return (
-    <Link
-      to={pass}
-      className="category-card"
-      data-cy="categoryLinksContainer"
-    >
+    <Link to={pass} className="category-card" data-cy="categoryLinksContainer">
       <img
-        src={`./img/categories/${pass}-category.jpg`}
+        src={`./img/category-${pass}.webp`}
         alt={title}
         className="category-card__photo"
       />
       <div className="category-card__text-container">
-        <h3
-          className="category-card__title"
-        >
-          {title}
-        </h3>
-        <p
-          className="category-card__models-count"
-        >
-          {`${modelsCount} models`}
-        </p>
+        <h3 className="category-card__title">{title}</h3>
+        <p className="category-card__models-count">{`${modelsCount} models`}</p>
       </div>
     </Link>
   );
