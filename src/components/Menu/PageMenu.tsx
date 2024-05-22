2d9f1564415a2c178df738c59ded5a193a89b3a6
Ihor Prodan <prodanigor0@gmail.com>
2024-05-22 08:42:04 +0200

    test

--- src/components/Menu/PageMenu.tsx
+++ src/components/Menu/PageMenu.tsx
@@ -8,6 +8,7 @@ import shopsIconDark from './Pictures/shopDark.png';
 import { useAppSelector } from '../../Hooks/hooks';
 import { Header } from '../Header/Header';
 import { Theme } from '../../Helpers/theme';
+import { motion } from 'framer-motion';
 
 export const PageMenu: React.FC = () => {
   const items = useAppSelector(state => state.cartAndFavorits.favorites);
@@ -23,113 +24,129 @@ export const PageMenu: React.FC = () => {
     ].join(' ');
   };
 
+  const animationVariants = {
+    initial: { opacity: 0, x: -100 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: 100 },
+  };
+
   return (
     <>
-      <Header />
-      <section
-        className={
-          theme === Theme.light
-            ? styles.PageMenuSection
-            : styles.PageMenuSectionDark
-        }
+      <motion.div
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={animationVariants}
+        transition={{ duration: 0.5 }}
       >
-        <div className={styles.pageMenuNav}>
-          <nav className={styles.nav}>
-            <ul className={styles.navigation}>
-              <li className={styles.navigation__li}>
-                <NavLink
-                  to={{ pathname: '/', search: '' }}
-                  className={getLinkClass}
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className={styles.navigation__li}>
-                <NavLink
-                  to={{ pathname: '/phones', search: '' }}
-                  className={getLinkClass}
-                >
-                  Phones
-                </NavLink>
-              </li>
-              <li className={styles.navigation__li}>
-                <NavLink
-                  to={{ pathname: '/tablets', search: '' }}
-                  className={getLinkClass}
-                >
-                  Tablets
-                </NavLink>
-              </li>
-              <li className={styles.navigation__li}>
-                <NavLink
-                  to={{ pathname: '/accessories', search: '' }}
-                  className={getLinkClass}
-                >
-                  Accessories
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div
+        <Header />
+        <section
           className={
             theme === Theme.light
-              ? styles.buttonsContainer
-              : styles.buttonsContainerDark
+              ? styles.PageMenuSection
+              : styles.PageMenuSectionDark
           }
         >
-          <NavLink
-            to={'/favorites'}
+          <div className={styles.pageMenuNav}>
+            <nav className={styles.nav}>
+              <ul className={styles.navigation}>
+                <li className={styles.navigation__li}>
+                  <NavLink
+                    to={{ pathname: '/', search: '' }}
+                    className={getLinkClass}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className={styles.navigation__li}>
+                  <NavLink
+                    to={{ pathname: '/phones', search: '' }}
+                    className={getLinkClass}
+                  >
+                    Phones
+                  </NavLink>
+                </li>
+                <li className={styles.navigation__li}>
+                  <NavLink
+                    to={{ pathname: '/tablets', search: '' }}
+                    className={getLinkClass}
+                  >
+                    Tablets
+                  </NavLink>
+                </li>
+                <li className={styles.navigation__li}>
+                  <NavLink
+                    to={{ pathname: '/accessories', search: '' }}
+                    className={getLinkClass}
+                  >
+                    Accessories
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div
             className={
               theme === Theme.light
-                ? styles.buttonsLink
-                : styles.buttonsLinkDark
+                ? styles.buttonsContainer
+                : styles.buttonsContainerDark
             }
           >
-            <div
+            <NavLink
+              to={'/favorites'}
               className={
                 theme === Theme.light
-                  ? styles.menuButtons
-                  : styles.menuButtonsDark
+                  ? styles.buttonsLink
+                  : styles.buttonsLinkDark
               }
             >
-              {items.length > 0 && (
-                <div className={styles.favoriteCounts}>{items.length}</div>
-              )}
-              <img
-                className={styles.pageButtonIcon}
-                src={theme === Theme.light ? FavoritesHeardIcon : FavoritesDark}
-                alt="Favorites"
-              />
-            </div>
-          </NavLink>
-          <NavLink
-            to={'/cart'}
-            className={
-              theme === Theme.light
-                ? styles.buttonsLink
-                : styles.buttonsLinkDark
-            }
-          >
-            <div
+              <div
+                className={
+                  theme === Theme.light
+                    ? styles.menuButtons
+                    : styles.menuButtonsDark
+                }
+              >
+                {items.length > 0 && (
+                  <div className={styles.favoriteCounts}>{items.length}</div>
+                )}
+                <img
+                  className={styles.pageButtonIcon}
+                  src={
+                    theme === Theme.light ? FavoritesHeardIcon : FavoritesDark
+                  }
+                  alt="Favorites"
+                />
+              </div>
+            </NavLink>
+            <NavLink
+              to={'/cart'}
               className={
                 theme === Theme.light
-                  ? styles.menuButtons
-                  : styles.menuButtonsDark
+                  ? styles.buttonsLink
+                  : styles.buttonsLinkDark
               }
             >
-              {cart.length > 0 && (
-                <div className={styles.favoriteCounts}>{cart.length}</div>
-              )}
-              <img
-                className={styles.pageButtonIcon}
-                src={theme === Theme.light ? shopsIcon : shopsIconDark}
-                alt="Shopping"
-              />
-            </div>
-          </NavLink>
-        </div>
-      </section>
+              <div
+                className={
+                  theme === Theme.light
+                    ? styles.menuButtons
+                    : styles.menuButtonsDark
+                }
+              >
+                {cart.length > 0 && (
+                  <div className={styles.favoriteCounts}>{cart.length}</div>
+                )}
+                <img
+                  className={styles.pageButtonIcon}
+                  src={theme === Theme.light ? shopsIcon : shopsIconDark}
+                  alt="Shopping"
+                />
+              </div>
+            </NavLink>
+          </div>
+        </section>
+      </motion.div>
     </>
   );
 };
