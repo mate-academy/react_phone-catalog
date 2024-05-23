7419c4bf7cbcb38a83dc9cb0b4f607c1cb642b32
Olaf <olaf.chuszno@gmail.com>
2024-05-23 11:48:24 +0200

    Style Favorites page

--- src/pages/FavoritesPage/components/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/components/FavoritesPage/FavoritesPage.tsx
@@ -6,6 +6,10 @@ import './FavoritesPage.scss';
 export const FavoritesPage: React.FC = () => {
   const { favoriteProducts } = useContext(FavoritesContext);
 
+  const favoritesCounterText =
+    `${favoriteProducts.length} item` +
+    (favoriteProducts.length > 1 ? 's' : '');
+
   return (
     <main className="product-page">
       <section className="product-page__top">
@@ -24,13 +28,17 @@ export const FavoritesPage: React.FC = () => {
         </nav>
         <div className="product-page__main-info">
           <h1 className="product-page__title">Favorites</h1>
-          <p className="product-page__models-count">95 items TODO</p>
+          <p className="product-page__models-count">{favoritesCounterText}</p>
         </div>
       </section>
 
-      <section className="favorites-products product-page__favorites">
-        <ProductsList products={favoriteProducts} />
-      </section>
+      {!favoriteProducts.length ? (
+        <p className="body-text--14 product-page__empty">No favorites yet.</p>
+      ) : (
+        <section className="favorites-products product-page__favorites">
+          <ProductsList products={favoriteProducts} />
+        </section>
+      )}
     </main>
   );
 };
