deed4c96f3ee50feeee000575ff0bc67a323d8b6
Andriy Sak <andreyysak17@gmail.com>
2024-05-23 10:25:55 +0300

    error

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
+import logo from '../../icons/Logo.svg';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Link to="/" className={styles}>
-        <img src="" alt="" />
+      <Link to="/" className={styles.logo}>
+        <img src={logo} alt="" />
       </Link>
       <h1 className={styles.title}>Hello</h1>
     </header>
