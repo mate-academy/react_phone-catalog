73a5b3edbe38fbd5d0a650e652365696ba3d2754
Olaf <olaf.chuszno@gmail.com>
2024-05-23 11:48:51 +0200

    Remove temp link from home page

--- src/pages/HomePage/components/Home/Home.tsx
+++ src/pages/HomePage/components/Home/Home.tsx
@@ -1,14 +1,12 @@
 import { PicturesSlider } from '../PicturesSlider';
 import { ProductSlider } from '../ProductSlider';
+import { Categories } from '../Categories';
 import './Home.scss';
 import '../../../../styles/main.scss';
-import { Categories } from '../Categories';
-import { Link } from 'react-router-dom';
 
 export const Home: React.FC = () => {
   return (
     <main className="main">
-      <Link to="/cart">Cart</Link>
       <section className="section main__section">
         <div className="title-container">
           <h1 className="title title--1 title-container__title">
