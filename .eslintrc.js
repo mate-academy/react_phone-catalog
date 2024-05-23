fdf62663cc7b0076396ea0c1d7bae57983731c47
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-05-23 20:25:17 +0200

    initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,24 @@
 module.exports = {
-  extends: [
-    '@mate-academy/eslint-config-react-typescript',
-    'plugin:cypress/recommended',
-  ],
-  rules: {},
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 };
