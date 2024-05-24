28d39c5dcea409baba1b08ca4014967dd6a48da9
Roman Tarasov <rtarasov1k90@gmail.com>
2024-05-24 17:39:30 +0300

    State is deleted

--- src/modules/HomePage/components/PicturesSlider/PicturesSlider.tsx
+++ src/modules/HomePage/components/PicturesSlider/PicturesSlider.tsx
@@ -3,48 +3,39 @@ import styles from './PicturesSlider.module.scss';
 
 const IMAGE_WIDTH = 1040;
 const IMAGE_NUM = 5;
-const TRANSITION_DURATION = 250;
-const BUTTON_LOCK_DURATION = TRANSITION_DURATION * 2;
+const TRANSITION_DURATION = 300;
 
 export const PicturesSlider = () => {
   const [offset, setOffset] = useState(-1040);
-  const [transitionDuration, setTransitionDuration] =
-    useState(TRANSITION_DURATION);
-  const [isButtonLocked, setIsButtonLocked] = useState(false);
-
-  useEffect(() => {
-    if (transitionDuration === 0) {
-      setTimeout(() => {
-        setTransitionDuration(TRANSITION_DURATION);
-      }, TRANSITION_DURATION);
-    }
-  }, [transitionDuration]);
+  // const [transitionDuration, setTransitionDuration] =  useState(TRANSITION_DURATION);
+  //
+  // useEffect(() => {
+  //   if (transitionDuration === 0) {
+  //     setTimeout(() => {
+  //       setTransitionDuration(TRANSITION_DURATION);
+  //     }, TRANSITION_DURATION);
+  //   }
+  // }, [transitionDuration]);
 
   useEffect(() => {
     if (offset < -IMAGE_WIDTH * (IMAGE_NUM - 2)) {
       setTimeout(() => {
-        setTransitionDuration(0);
+        // setTransitionDuration(0);
         setOffset(-IMAGE_WIDTH);
-      }, TRANSITION_DURATION);
+      }, 300);
     }
   }, [offset]);
 
   useEffect(() => {
     if (offset === 0) {
       setTimeout(() => {
-        setTransitionDuration(0);
+        // setTransitionDuration(0);
         setOffset(-IMAGE_WIDTH * (IMAGE_NUM - 2));
       }, TRANSITION_DURATION);
     }
   }, [offset]);
 
   const handleRightBtn = () => {
-    // if (isButtonLocked) {
-    //   return;
-    // }
-
-    // setIsButtonLocked(true);
-    // setTimeout(() => setIsButtonLocked(false), BUTTON_LOCK_DURATION);
     setOffset(currentOffset => {
       const newOffset = currentOffset - IMAGE_WIDTH;
 
@@ -53,17 +44,11 @@ export const PicturesSlider = () => {
   };
 
   const handleLeftBtn = () => {
-    if (isButtonLocked) {
-      return;
-    }
-
-    setIsButtonLocked(true);
     setOffset(currentOffset => {
       const newOffset = currentOffset + IMAGE_WIDTH;
 
       return newOffset;
     });
-    setTimeout(() => setIsButtonLocked(false), BUTTON_LOCK_DURATION);
   };
 
   return (
@@ -71,14 +56,15 @@ export const PicturesSlider = () => {
       <button
         className={`${styles.btn}, ${styles.btnLeft}`}
         onClick={handleLeftBtn}
-        disabled={isButtonLocked}
       ></button>
       <div className={styles.window}>
         <div
           className={`${styles.imgContainer}`}
           style={{
-            transitionDuration: `${transitionDuration}ms`,
             transform: `translateX(${offset}px)`,
+            ...(offset < -IMAGE_WIDTH * (IMAGE_NUM - 2) && {
+              transition: 'none',
+            }),
           }}
         >
           <div className={`${styles.sliderImg}, ${styles.sliderImg3}`}>
