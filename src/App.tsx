7638f3df563d05ece99fbce20dce39f584df9cf4
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-05-25 17:54:56 +0200

    problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
 import './App.scss';
+import { Header } from './components/header';
 
 export const App = () => (
   <div className="App">
+    <Header />
+
     <h1>Product Catalog</h1>
   </div>
 );
