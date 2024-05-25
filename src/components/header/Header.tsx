3467ec31005ac5f582c1181754e55a994dca8351
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-05-25 18:07:04 +0200

    try

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.scss';
 
 export const Header: React.FC = () => {
   return (
-    <header className={styles.header}>
+    <><header className={styles.header}>
       <div className={styles[`header__container--1`]}>
         <img src=".\img\svg\Nice Gadgets_logo.svg" className="logo" />
 
@@ -22,17 +22,17 @@ export const Header: React.FC = () => {
           <img
             src=".\img\svg\fav_icon.svg"
             alt="Fav_logo"
-            className="logo__image"
-          />
+            className="logo__image" />
         </a>
         <a href="#cart" className={styles[`header__cart`]}>
           <img
             src="./img/svg/Shopping_bag_Cart.svg"
             alt="Cart_logo"
-            className="logo__image"
-          />
+            className="logo__image" />
         </a>
       </div>
-    </header>
+    </header><main>
+        <p className='bg-green-600'>siema to ja</p>
+      </main></>
   );
 };
