f95b6e4436966cdfbaf9865c320e9eb6d9768937
vova <hohiop09@gmail.com>
2024-05-27 19:34:42 +0000

    created homePage

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,49 +1,54 @@
 import React from 'react';
 import './HomeStyle.scss';
-import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 // import required modules
-import { Pagination } from 'swiper/modules';
 import CardSlider from '../CardSlider/CardSlider';
-import Categories from './Categories/Categories';
+import CategoryCard from './CategoryCard/CategoryCard';
+import MainSwiper from './MainSwiper/MainSwiper';
 
 const HomePage = () => {
   return (
     <div className="home">
       <div className="home__wrapper">
-        <h1 className="home__title container ">
-          Welcome to Nice Gadgets store!
-        </h1>
-        <div className="home__slider container">
-          <Swiper
-            spaceBetween={30}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Pagination]}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <img src="icons/swiper.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="icons/swiper.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="icons/swiper.jpg" alt="" />
-            </SwiperSlide>
-          </Swiper>
+        <div className="home__title--wrapper container">
+          <h1 className="home__title">Welcome to Nice Gadgets store!</h1>
+        </div>
+
+        <div className="home-slider__main container">
+          <MainSwiper />
         </div>
         <div className="home__slider-card">
-          <CardSlider title={'title'} />
+          <CardSlider title={'Brand new models'} />
         </div>
-        <Categories />
+        <div className="home__categories--sections categories container">
+          <h2 className="categories__title">Shop by category</h2>
+          <div className="categories__wrapper">
+            <CategoryCard
+              title="Mobile phones"
+              subTitle=""
+              imgUrl="img/caregory-phones-main.png"
+              UrlTo="/"
+            />
+            <CategoryCard
+              title="Tablets"
+              subTitle=""
+              imgUrl="img/category-tablets-main.png"
+              UrlTo="/"
+            />
+            <CategoryCard
+              title="Accessories"
+              subTitle=""
+              imgUrl="img/category-accessories-main.png"
+              UrlTo="/"
+            />
+          </div>
+        </div>
         <div className="home__second-slider-card">
-          <CardSlider title={'title 2'} />
+          <CardSlider title={'Hot prices'} />
         </div>
       </div>
     </div>
---
+++ src/components/HomePage/MainSwiper/MainSwiper.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { v4 as uuidv4 } from 'uuid';
+import './MainSwiperStyle.scss';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+
+const MainSwiper = () => {
+  const prevButtonId = `prev-${uuidv4()}`;
+  const nextButtonId = `next-${uuidv4()}`;
+
+  return (
+    <>
+      <button className="home-slider__prev" id={prevButtonId}>
+        <img
+          src="icons/arrow-up-black.png"
+          alt="arrow"
+          className="home-slider__prev--img"
+        />
+      </button>
+      <Swiper
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={{
+          nextEl: `#${nextButtonId}`,
+          prevEl: `#${prevButtonId}`,
+        }}
+        modules={[Pagination, Navigation, Autoplay]}
+        className="mySwiper"
+        autoplay={{
+          delay: 3000, // Delay between slides in milliseconds (3000ms = 3s)
+          disableOnInteraction: false, // Continue autoplay after user interactions
+        }}
+        loop={true}
+      >
+        <SwiperSlide>
+          <img src="icons/swiper.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="icons/swiper.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="icons/swiper.jpg" alt="" />
+        </SwiperSlide>
+      </Swiper>
+      <button id={nextButtonId} className="home-slider__next">
+        <img
+          src="icons/arrow-up-black.png"
+          alt="arrow"
+          className="home-slider__next--img"
+        />
+      </button>
+    </>
+  );
+};
+
+export default MainSwiper;
