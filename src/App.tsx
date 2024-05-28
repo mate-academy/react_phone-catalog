d501b5770bb129239a55d343b8272b6e929eb3fe
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-05-28 20:40:11 +0200

    Loader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.scss';
+import {Loader} from './components/Loader'
 
 export const App = () => (
   <div className="App">
     <h1>This webiste is under construction...</h1>
+      <Loader />
   </div>
 );
