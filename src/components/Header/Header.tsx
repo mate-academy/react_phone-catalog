58c0452f22d7181608995bcc7e7036dccc27262b
Olaf <olaf.chuszno@gmail.com>
2024-05-28 16:53:07 +0200

    Extract showing menu logic to a custom hook

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,21 @@
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import useOpenMenu from '../../hooks/useOpenMenu';
 import './Header.scss';
 import '../../styles/main.scss';
 
 export const Header: React.FC = () => {
-  const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
+  const [isMenuOpen, hideMenu, showMenu] = useOpenMenu();
 
   useEffect(() => {
-    if (isMenuVisible) {
+    if (isMenuOpen) {
       (document.querySelector('body') as HTMLBodyElement).style.overflow =
         'hidden';
     } else {
       (document.querySelector('body') as HTMLBodyElement).style.overflow =
         'scroll';
     }
-  }, [isMenuVisible]);
-
-  const handleCloseMenu = () => {
-    setIsMenuVisible(false);
-  };
+  }, [isMenuOpen]);
 
   return (
     <>
@@ -29,7 +26,7 @@ export const Header: React.FC = () => {
           </Link>
 
           <div
-            onClick={() => setIsMenuVisible(true)}
+            onClick={showMenu}
             className="header-nav__item header-nav__burger"
           >
             <img src="./icons/burger.svg" alt="Open menu button" />
@@ -37,7 +34,7 @@ export const Header: React.FC = () => {
         </nav>
       </header>
 
-      {isMenuVisible && (
+      {isMenuOpen && (
         <section className="menu">
           <div className="menu-top menu__top">
             <div className="menu-top__strip-wrapper">
@@ -45,15 +42,12 @@ export const Header: React.FC = () => {
                 <Link
                   className="menu-top-strip__logo"
                   to="/"
-                  onClick={handleCloseMenu}
+                  onClick={hideMenu}
                 >
                   <img src="./icons/logo.svg" alt="nice gadgets logo" />
                 </Link>
 
-                <button
-                  onClick={handleCloseMenu}
-                  className="menu-top-strip__close"
-                >
+                <button onClick={hideMenu} className="menu-top-strip__close">
                   <img src="./icons/close-16.svg" alt="close icon" />
                 </button>
               </nav>
@@ -63,28 +57,28 @@ export const Header: React.FC = () => {
               <Link
                 className="menu-category-nav__link"
                 to="/"
-                onClick={handleCloseMenu}
+                onClick={hideMenu}
               >
                 Home
               </Link>
               <Link
                 className="menu-category-nav__link"
                 to="/phones"
-                onClick={handleCloseMenu}
+                onClick={hideMenu}
               >
                 Phones
               </Link>
               <Link
                 className="menu-category-nav__link"
                 to="tablets"
-                onClick={handleCloseMenu}
+                onClick={hideMenu}
               >
                 Tablets
               </Link>
               <Link
                 className="menu-category-nav__link"
                 to="accessories"
-                onClick={handleCloseMenu}
+                onClick={hideMenu}
               >
                 Accessories
               </Link>
@@ -96,14 +90,14 @@ export const Header: React.FC = () => {
             <Link
               to={'/favorites'}
               className="menu-bottom__wishlist menu-bottom__button"
-              onClick={handleCloseMenu}
+              onClick={hideMenu}
             >
               <img src="./icons/heart-black.svg" alt="heart icon" />
             </Link>
             <Link
               to={'/cart'}
               className="menu-bottom__cart menu-bottom__button"
-              onClick={handleCloseMenu}
+              onClick={hideMenu}
             >
               <img src="./icons/cart.svg" alt="cart icon" />
             </Link>
