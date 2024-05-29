17467be0e653ce6eb8e38529b51761d0c374487d
Olaf <olaf.chuszno@gmail.com>
2024-05-29 15:47:10 +0200

    Allow changing pictures in slider by swipe

--- src/pages/HomePage/components/PicturesSlider/PicturesSlider.tsx
+++ src/pages/HomePage/components/PicturesSlider/PicturesSlider.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useEffect, useState } from 'react';
+import { SetStateAction, useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
 import { Tabs } from '../../../../types/Tabs';
 import './PicturesSlider.scss';
@@ -10,19 +10,68 @@ export const PicturesSlider = () => {
 
   const bannerClass = cn('banner', `banner--${activeTab}`);
 
+  const moveToNextSlide = () => {
+    setActiveTab((currentTab: Tabs) => {
+      return currentTab === 3 ? 1 : ((currentTab + 1) as Tabs);
+    });
+  };
+
+  const moveToPreviousSlide = () => {
+    setActiveTab((currentTab: Tabs) => {
+      return currentTab === 1 ? 3 : ((currentTab - 1) as Tabs);
+    });
+  };
+
   useEffect(() => {
     setInterval(() => {
-      setActiveTab((currentTab: Tabs) => {
-        return currentTab === 3 ? 1 : ((currentTab + 1) as Tabs);
-      });
+      moveToNextSlide();
     }, 2000);
   }, []);
 
   const slideList = [1, 2, 3];
 
+  // ---------------
+
+  const slider = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const sliderCopy = slider;
+
+    let touchstartX = 0;
+    let touchendX = 0;
+
+    function checkDirection() {
+      if (touchendX < touchstartX) {
+        moveToNextSlide();
+      }
+
+      if (touchendX > touchstartX) {
+        moveToPreviousSlide();
+      }
+    }
+
+    const handle1 = (e: TouchEvent) => {
+      touchstartX = e.changedTouches[0].screenX;
+    };
+
+    slider.current?.addEventListener('touchstart', handle1);
+
+    const handle2 = (e: TouchEvent) => {
+      touchendX = e.changedTouches[0].screenX;
+      checkDirection();
+    };
+
+    slider.current?.addEventListener('touchend', handle2);
+
+    return () => {
+      sliderCopy.current?.removeEventListener('touchstart', handle1);
+      sliderCopy.current?.removeEventListener('touchend', handle2);
+    };
+  }, []);
+
   return (
     <div className="slider">
-      <div className={bannerClass}></div>
+      <div ref={slider} className={bannerClass}></div>
 
       <div className="tabs slider__tabs">
         {slideList.map((slide: number) => (
