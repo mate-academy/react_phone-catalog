9bdc7f779ac139fdf4c6585b807111d44e121cba
mrsvolodya <vovamursky@gmail.com>
2024-06-02 21:32:47 +0300

    on-touch

--- src/components/Header/AsideMenu/Slider/Slider.tsx
+++ src/components/Header/AsideMenu/Slider/Slider.tsx
@@ -42,7 +42,11 @@ export const Slider: React.FC<Props> = ({ count }) => {
         style={{ transform: `translateX( -${count * width}px)` }}
         onMouseEnter={() => setAutoPlay(true)}
         onMouseLeave={() => setAutoPlay(false)}
-        onDragStart={() => setWidth(prev => prev + 1)}
+        onDragStart={e => {
+          if (e.clientX) {
+            setWidth(prev => prev + 1);
+          }
+        }}
         // onDragEnd={e => console.log('End', e.clientX)}
       >
         {!isMobile ? (
