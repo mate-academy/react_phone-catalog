413a05264d3e1631f7439f90bfc8e3345b1dcf2c
volodymyr <volodiayurovych@gmail.com>
2024-06-04 17:19:53 +0200

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.scss';
+import { Header } from './modules/Header/components/Header';
 
 export const App = () => (
   <div className="App">
     <h1>Product Catalog</h1>
+    <Header />
   </div>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+    </Routes>
+  </Router>,
+);
