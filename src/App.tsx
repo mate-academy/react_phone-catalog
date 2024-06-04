939aa588746617ebc4bcdbf9c9d12d0db421f92c
ordila <ordila99@gmail.com>
2024-06-04 11:51:46 +0300

    add general routes into project

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import { useState } from 'react';
+import { AppRouter } from './router';
+
 import './App.scss';
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-  const [value, setValue] = useState(3);
-
-  const increment = () => {
-    setCount(prev => prev + value);
-  };
-
-  const decrement = () => {
-    setCount(prev => prev - value);
-  };
-
   return (
     <div className="App">
-      <h1>Product Catalog</h1>
-      <h2>{count}</h2>
-      <input
-        type="number"
-        min={1}
-        max={100}
-        value={value}
-        onChange={e => setValue(+e.target.value)}
-      />
-      <div className="buttons">
-        <button onClick={increment}>+</button>
-        <button onClick={decrement}>-</button>
-      </div>
+      <AppRouter />
     </div>
   );
 };
