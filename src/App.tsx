bc5129980e221102f0c119ca97a50258cba7e99f
Ivan <tixehack@gmail.com>
2024-06-05 12:00:21 +0300

    feat: adding a basic style structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.scss';
 
 export const App = () => (
-  <div className="App">
+  <div className="App AppWidth">
     <h1>Product Catalog</h1>
   </div>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client';
+import '../src/shared/global/globalStyles/globalStyles.scss';
+
 import { App } from './App';
 
 createRoot(document.getElementById('root') as HTMLElement).render(<App />);
