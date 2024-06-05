6609920b2974277d1742978d4072c3f45030712c
marchAlbion <marchAlbion@gmail.com>
2024-06-05 10:14:15 +0300

    Add Product Card and Card Item

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,32 @@
+import styles from './ProductCard.module.scss';
+import { ProductParams } from './ProductParams/ProductParams';
+const titlePlaceholder = 'Apple iPhone Xs 64GB Silver (iMT9G2FS/A)';
+
+import imagePlaceholder from './img.png';
+
+const params = {
+  screen: '5.8” OLED',
+  capacity: '64 GB',
+  ram: '4 GB',
+};
+
+export const ProductCard = () => {
+  return (
+    <article className={styles.card}>
+      <img className={styles.card__image} src={imagePlaceholder} />
+      <h1 className={styles.card__title}>{titlePlaceholder}</h1>
+      <div>
+        <p className={styles.card__price}>
+          <span className={styles.card__price__actual}>$799</span>
+          <div className={styles.card__price__withoutDiscount}>$899</div>
+        </p>
+      </div>
+
+      <ProductParams phoneParams={params} />
+      <div className={styles.buttonsPlaceholder}>
+        <div className={styles.buttonsPlaceholder__primary}>Add to cart</div>
+        <div className={styles.buttonsPlaceholder__secondary}>♡</div>
+      </div>
+    </article>
+  );
+};
