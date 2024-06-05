c30bcf1b5052c1376b798082dab04801b65e8d7e
Oleksii Dunchyk <oleksiidunchik@gmail.com>
2024-06-05 15:20:24 +0300

    added size props

--- src/UI/Button/Button.tsx
+++ src/UI/Button/Button.tsx
@@ -5,8 +5,21 @@ import styles from './Button.module.scss';
 import { makeColorDarker } from '../../utils/makeColorDarker';
 import ArrowLeft from '../../assets/icons/ArrowLeftBold.svg';
 
+type Size = {
+  width?: number;
+  height: number;
+};
+
+interface CustomCSSProperties extends React.CSSProperties {
+  '--btn-width': string;
+  '--btn-height': string;
+  '--radio-color'?: string;
+  '--radio-hover-color'?: string;
+}
+
 type ButtonProps = {
   type: 'number' | 'radio' | 'icon' | 'back' | 'primary';
+  size: Size;
   state?: 'selected' | 'disabled';
   color?: string;
   onClick?: () => void;
@@ -16,6 +29,7 @@ type ButtonProps = {
 const Button: React.FC<ButtonProps> = ({
   type,
   state,
+  size,
   color,
   onClick,
   children,
@@ -25,13 +39,16 @@ const Button: React.FC<ButtonProps> = ({
   });
 
   const darkerColor = color ? makeColorDarker(color, 15) : undefined;
-  const style =
-    type === 'radio' && color
-      ? ({
+  const style: CustomCSSProperties = {
+    '--btn-width': `${size.width}px`,
+    '--btn-height': `${size.height}px`,
+    ...(type === 'radio' && color
+      ? {
           '--radio-color': color,
           '--radio-hover-color': darkerColor,
-        } as React.CSSProperties)
-      : {};
+        }
+      : {}),
+  };
 
   return (
     <button className={btnClass} onClick={onClick} style={style}>
