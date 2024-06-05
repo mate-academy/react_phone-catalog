725cb6e635e02045fd54f632dff67a68ece3a861
Ivan Sumskyi <ivensmsk@gmail.com>
2024-06-05 18:08:02 +0300

    fix adaptive

--- src/modules/shared/Pagination/Pagination.tsx
+++ src/modules/shared/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import cn from 'classnames';
+import React from 'react';
 import Button from '../../../UI/Buttons/Button';
 import s from './Pagination.module.css';
 
@@ -15,15 +15,19 @@ export const Pagination: React.FC<Props> = ({
   currentPage,
   onPageChange,
 }) => {
+  if (total === 0 || perPage === 0) {
+    return null; // or some other handling for zero values
+  }
+
   const totalPages = Math.ceil(total / perPage);
-  let startPage = Math.max(currentPage - 4, 1);
-  let endPage = Math.min(currentPage + 5, totalPages);
+  let startPage = Math.max(currentPage - 1, 1);
+  let endPage = Math.min(startPage + 3, totalPages);
 
   if (endPage - startPage < 9) {
     if (currentPage < 5) {
-      endPage = Math.min(startPage + 9, totalPages);
+      endPage = Math.min(startPage + 4, totalPages);
     } else {
-      startPage = Math.max(endPage - 9, 1);
+      startPage = Math.max(endPage - 4, 1);
     }
   }
 
@@ -46,18 +50,13 @@ export const Pagination: React.FC<Props> = ({
 
   return (
     <ul className={s.pagination}>
-      <li
-        onClick={handlePrevPage}
-        className={cn('page-item', {
-          disabled: currentPage === 1,
-        })}
-      >
+      <li onClick={handlePrevPage}>
         <Button
           variant="pagination"
           size={[32, 32]}
           disabled={currentPage === 1}
         >
-          <img src="img/icons/arrow-left-dark-icon.svg" alt="" />
+          <img src="img/icons/arrow-left-dark-icon.svg" alt="Previous Page" />
         </Button>
       </li>
 
@@ -73,18 +72,13 @@ export const Pagination: React.FC<Props> = ({
         </li>
       ))}
 
-      <li
-        onClick={handleNextPage}
-        className={cn('page-item', {
-          disabled: currentPage === totalPages,
-        })}
-      >
+      <li onClick={handleNextPage}>
         <Button
           variant="pagination"
           size={[32, 32]}
           disabled={currentPage === totalPages}
         >
-          <img src="img/icons/arrow-right-dark-icon.svg" alt="" />
+          <img src="img/icons/arrow-right-dark-icon.svg" alt="Next Page" />
         </Button>
       </li>
     </ul>
