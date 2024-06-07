3b4bcabce83d6a9eb9dd2a2eba6fc4afdeacb6f0
Alina Ovod <alina.ipatova98@gmail.com>
2024-06-07 15:28:13 +0300

    test

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import './HomePage.scss';
-import { BrandNew } from './components/BrandNew';
+// import { BrandNew } from './components/BrandNew';
 import { Categories } from './components/Categories';
-import { HotPrices } from './components/HotPrices';
+// import { HotPrices } from './components/HotPrices';
 
 import { PicturesSlider } from './components/PicturesSlider';
-import { Product } from '../../types/Product';
-import { getAllProducts } from '../../services/products';
+// import { Product } from '../../types/Product';
+// import { getAllProducts } from '../../services/products';
 
 export const HomePage = () => {
-  const [allProducts, setAllProducts] = useState<Product[]>([]);
+  // const [allProducts, setAllProducts] = useState<Product[]>([]);
 
-  useEffect(() => {
-    getAllProducts().then(setAllProducts);
-  }, []);
+  // useEffect(() => {
+  //   getAllProducts().then(setAllProducts);
+  // }, []);
 
   return (
     <div className="home-page">
@@ -23,9 +23,9 @@ export const HomePage = () => {
       </p>
       <div className="home-page__content">
         <PicturesSlider />
-        <BrandNew products={allProducts} />
+        {/* <BrandNew products={allProducts} /> */}
         <Categories />
-        <HotPrices products={allProducts} />
+        {/* <HotPrices products={allProducts} /> */}
       </div>
     </div>
   );
