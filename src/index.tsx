f6ee001de9356446498e309c0f8f3076ced9ea40
Valentyn Sobol <valiksobol14@gmail.com>
2024-06-10 16:46:37 +0300

    delete redux root

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 
-const container = document.getElementById('root') as HTMLElement;
-const root = createRoot(container);
-
-const Root = () => (
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>
-);
-
-root.render(<Root />);
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
