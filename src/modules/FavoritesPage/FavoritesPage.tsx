db0b8169007cde9e7f79e39c3bb50a2db87a1547
Dmytro Yakymchuk <dimayakim2000@gmail.com>
2024-06-10 12:25:40 +0300

    starting favorites page

--- src/modules/FavoritesPage/FavoritesPage.tsx
+++ src/modules/FavoritesPage/FavoritesPage.tsx
@@ -1,5 +1,22 @@
+import Heading from '../../UI/Heading/Heading';
+import s from './FavoritesPage.module.css';
+import { useProductStore } from '../../store/store';
+import ProductsList from '../shared/ProductsList/ProductsList';
+import { Breadcrumbs } from '../shared/Breadcrumbs';
+
 const FavoritesPage = () => {
-  return <div>FavoritesPage</div>;
+  const favorites = useProductStore(state => state.favorites);
+  return (
+    <div className="container">
+       <Breadcrumbs />
+      <Heading as="h1" className={s.title}>Favorites</Heading>
+      <p className={s.categoryModels}>{`${favorites.length} models`}</p>
+      <ProductsList
+       products={favorites}
+      //  isChangingPage={isChangingPage}
+     />
+    </div>
+  );
 };
 
 export default FavoritesPage;
