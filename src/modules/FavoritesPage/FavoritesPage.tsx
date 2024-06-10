67db9e5048e0ac046ddec1b183cd8d9dcbb0a9f6
Dmytro Yakymchuk <dimayakim2000@gmail.com>
2024-06-10 13:28:53 +0300

    add favorite page

--- src/modules/FavoritesPage/FavoritesPage.tsx
+++ src/modules/FavoritesPage/FavoritesPage.tsx
@@ -3,18 +3,35 @@ import s from './FavoritesPage.module.css';
 import { useProductStore } from '../../store/store';
 import ProductsList from '../shared/ProductsList/ProductsList';
 import { Breadcrumbs } from '../shared/Breadcrumbs';
+import { useEffect, useState } from 'react';
 
 const FavoritesPage = () => {
+  const [isChangingPage, setIsChangingPage] = useState(false);
+
   const favorites = useProductStore(state => state.favorites);
+
+  const handleLoadCard = () => {
+    setIsChangingPage(true);
+    setTimeout(() => {
+      setIsChangingPage(false);
+    }, 800)
+  }
+
+  useEffect(() => {
+    handleLoadCard();
+  }, []);
+
   return (
-    <div className="container">
+    <div className={s.content} >
+        <div className="container">
        <Breadcrumbs />
       <Heading as="h1" className={s.title}>Favorites</Heading>
-      <p className={s.categoryModels}>{`${favorites.length} models`}</p>
+      <p className={s.quantity}>{`${favorites.length} models`}</p>
       <ProductsList
        products={favorites}
-      //  isChangingPage={isChangingPage}
+       isChangingPage={isChangingPage}
      />
+    </div>
     </div>
   );
 };
