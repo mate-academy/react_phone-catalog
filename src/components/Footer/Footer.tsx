881c6022127c7a01158c09a623c4d8a42705ca5d
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-06-13 20:33:08 +0300

    done footer landing

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from 'react';
+import './Footer.scss';
+import { Link } from 'react-router-dom';
 
 export const Footer: React.FC = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <div>Footer: React.FC</div>
+    <footer className="footer container">
+      <Link to="/" className="icon icon--footer-logo">
+        <img src="nav/logo.svg" alt="logo" />
+      </Link>
+
+      <ul className="footer__ul">
+        <li className="footer__list">
+          <Link to="home" className="footer__link">
+            GitHub
+          </Link>
+        </li>
+        <li className="footer__list">
+          <Link to="phones" className="footer__link">
+            Contacts
+          </Link>
+        </li>
+        <li className="footer__list">
+          <Link to="tablets" className="footer__link">
+            Rights
+          </Link>
+        </li>
+      </ul>
+
+      <div className="footer__top">
+        <span className="footer__text">Back to top</span>
+        <Link to="/" className="icon icon--chevron" onClick={scrollToTop}>
+          <img src="nav/chevron (arrow top).svg" alt="chevron (arrow top)" />
+        </Link>
+      </div>
+    </footer>
   );
 };
