8983f9d448e3b0115335988da07a2dc23b066e37
vova <hohiop09@gmail.com>
2024-06-18 15:24:21 +0300

    created Routing

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,6 +6,7 @@ import GadgetsPage from '../GadgetsPage/GadgetsPage';
 import { PageType } from 'src/types/PageType';
 import CartPage from '../CartPage/CartPage';
 import './MainStyle.scss';
+import CardPage from '../CardPage/CardPage';
 
 register();
 
@@ -13,21 +14,25 @@ const Main = () => {
   return (
     <main className="main">
       <Routes>
-        <Route path="/" element={<HomePage />} index />
-        <Route path="phones" element={<GadgetsPage type={PageType.Phones} />} />
-        <Route
-          path="tablets"
-          element={<GadgetsPage type={PageType.Tablets} />}
-        />
-        <Route
-          path="accessories"
-          element={<GadgetsPage type={PageType.Accessories} />}
-        />
-        <Route
-          path="favoutire"
-          element={<GadgetsPage type={PageType.Favourite} />}
-        />
-        <Route path="cart" element={<CartPage />} />
+        <Route path="/" element={<HomePage />}>
+          <Route path=':id' element={<CardPage/>}/>
+        </Route>
+        <Route path={PageType.Phones} element={<GadgetsPage type={PageType.Phones} />} />
+        <Route path={PageType.Phones} element={<GadgetsPage type={PageType.Phones} />}>
+          <Route path=":id" element={<CardPage />} />
+        </Route>
+        <Route path={PageType.Tablets} element={<GadgetsPage type={PageType.Tablets} />}>
+          <Route path=":id" element={<CardPage />} />
+        </Route>
+        <Route path={PageType.Accessories} element={<GadgetsPage type={PageType.Accessories} />}>
+          <Route path=":id" element={<CardPage />} />
+        </Route>
+        <Route path={PageType.Favourite} element={<GadgetsPage type={PageType.Favourite} />}>
+          <Route path=":id" element={<CardPage />} />
+        </Route>
+        <Route path="Cart" element={<CartPage />} />
+        <Route path='Card' element={<CardPage />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </main>
   );
