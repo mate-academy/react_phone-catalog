b94dc2892f11b3d858162ba2017f03501934b6a4
mrsvolodya <vovamursky@gmail.com>
2024-06-20 17:15:20 +0300

    add background

--- src/store/ThemeProvider.tsx
+++ src/store/ThemeProvider.tsx
@@ -15,11 +15,11 @@ const ThemeProvider: React.FC<Props> = ({ children }) => {
 
   useEffect(() => {
     if (theme) {
-      document.documentElement.classList.remove('light');
-      document.documentElement.classList.add('dark');
+      document.body.classList.remove('light');
+      document.body.classList.add('dark');
     } else {
-      document.documentElement.classList.remove('dark');
-      document.documentElement.classList.add('light');
+      document.body.classList.remove('dark');
+      document.body.classList.add('light');
     }
   }, [theme]);
 
