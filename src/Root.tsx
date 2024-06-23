3d584e79f073594663d4ba4dadf926b6f82b1fca
Ivan <tixehack@gmail.com>
2024-06-23 20:31:57 +0300

    feat: implementation of the 404 page

--- src/Root.tsx
+++ src/Root.tsx
@@ -6,44 +6,49 @@ import { CatalogPage } from './pages/CatalogPage/CatalogPage';
 import { ProductPage } from './pages/ProductPage';
 import { FavouritePage } from './pages/FavouritePage';
 import { BasketPage } from './pages/BasketPage';
+import { SkeletonTheme } from 'react-loading-skeleton';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 export const Root = () => {
   return (
-    <ContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<HomePage />} />
+    <SkeletonTheme baseColor="#e2e6e9" highlightColor="#b4bdc3">
+      <ContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<HomePage />} />
 
-            <Route path="phones">
-              <Route index element={<CatalogPage type="phones" />} />
-              <Route
-                path=":productId"
-                element={<ProductPage type="phones" />}
-              />
+              <Route path="phones">
+                <Route index element={<CatalogPage type="phones" />} />
+                <Route
+                  path=":productId"
+                  element={<ProductPage type="phones" />}
+                />
+              </Route>
+
+              <Route path="tablets">
+                <Route index element={<CatalogPage type="tablets" />} />
+                <Route
+                  path=":productId"
+                  element={<ProductPage type="tablets" />}
+                />
+              </Route>
+
+              <Route path="accessories">
+                <Route index element={<CatalogPage type="accessories" />} />
+                <Route
+                  path=":productId"
+                  element={<ProductPage type="accessories" />}
+                />
+              </Route>
+
+              <Route path="favourites" element={<FavouritePage />} />
+              <Route path="basket" element={<BasketPage />} />
+              <Route path="*" element={<NotFoundPage />} />
             </Route>
-
-            <Route path="tablets">
-              <Route index element={<CatalogPage type="tablets" />} />
-              <Route
-                path=":productId"
-                element={<ProductPage type="tablets" />}
-              />
-            </Route>
-
-            <Route path="accessories">
-              <Route index element={<CatalogPage type="accessories" />} />
-              <Route
-                path=":productId"
-                element={<ProductPage type="accessories" />}
-              />
-            </Route>
-
-            <Route path="favourites" element={<FavouritePage />} />
-            <Route path="basket" element={<BasketPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ContextProvider>
+          </Routes>
+        </BrowserRouter>
+      </ContextProvider>
+    </SkeletonTheme>
   );
 };
