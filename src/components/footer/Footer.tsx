2a0c4f85f17285452576b2bed1bb5febf9342aae
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-06-27 21:11:50 +0200

    add hover effect

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,14 +4,29 @@ import Styles from './Footer.module.scss';
 export const Footer: React.FC = () => {
   return (
     <div className={Styles['footer']}>
-      <p className={Styles['footer__logo']} >
-        <img className={Styles['footer__logo__img']} src="./img/svg/Nice_Gadgets_logo.svg" alt="logo" />
+      <p className={Styles['footer__logo']}>
+        <img
+          className={Styles['footer__logo__img']}
+          src="./img/svg/Nice_Gadgets_logo.svg"
+          alt="logo"
+        />
       </p>
 
       <div className={Styles['footer__links']}>
-        <a className={Styles['footer__links__item']} href="#git">Github</a>
-        <a className={Styles['footer__links__item']} href="#con">Contacts</a>
-        <a className={Styles['footer__links__item']} href="#rights">rights</a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className={Styles['footer__links__item']}
+          href="https://github.com/mate-academy/react_phone-catalog"
+        >
+          Github
+        </a>
+        <a className={Styles['footer__links__item']} href="#con">
+          Contacts
+        </a>
+        <a className={Styles['footer__links__item']} href="#rights">
+          rights
+        </a>
       </div>
     </div>
   );
