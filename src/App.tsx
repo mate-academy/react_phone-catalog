94a019851e378f6393cd02f24bddca7f85eb9862
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-08 15:46:12 +0200

    add Product Slider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.module.scss';
 import { Loader } from './components/Loader/Loader';
 import { ProductCard } from './components/ProductCard/ProductCard';
+import { ProductSlider } from './components/ProductSlider';
 
 export const App: React.FC = () => (
   <div className="App">
     <h1>This webiste is under construction...</h1>
     <Loader />
+    <ProductSlider />
     <ProductCard />
   </div>
 );
---
+++ src/components/ProductSlider/ProductSlider.tsx
@@ -0,0 +1,35 @@
+import styles from './ProductSlider.module.scss';
+import ChevronIcon from './img/ChevronIcon.module.svg';
+
+export const ProductSlider: React.FC = () => {
+  return (
+    <div className={styles.ProductSlider}>
+      <div className={styles.topWrapper}>
+        <div className={styles.buttons}>
+          <button>
+            <img
+              src={ChevronIcon}
+              alt="scroll"
+              className={styles.iconPrev}
+            />
+          </button>
+
+          <div className={styles.container}>
+            <div className={styles.sliderWrapper}>
+              <img src="./img/banner-accessories.png" />
+            </div>
+          </div>
+
+          <button>
+            <img
+              src={ChevronIcon}
+              alt="scroll"
+              className={styles.iconNext}
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
