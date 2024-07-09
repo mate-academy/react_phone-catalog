d08fb4724db01ff8c11f8326574bb4821a966b13
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-09 21:31:51 +0200

    add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Link, NavLink } from 'react-router-dom';
+import styles from './Footer.module.scss';
+import LogoIcon from '../../img/icons/LogoIcon.svg'
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className={styles.wrapper}>
+      <Link
+        to="/"
+        className={styles.logoLink}
+      >
+        <img
+          src={LogoIcon}
+        />
+      </Link>
+      <nav className={styles.nav}>
+        <NavLink
+          to="https://github.com/meljaszuk"
+          target="_blank"
+          className={styles.item}
+        >
+          GitHub
+        </NavLink>
+
+        <NavLink
+          to="https://github.com/meljaszuk"
+          target="_blank"
+          className={styles.item}
+        >
+          Contact
+        </NavLink>
+
+        <NavLink
+          to="https://github.com/meljaszuk"
+          target="_blank"
+          className={styles.item}
+        >
+          Rights
+        </NavLink>
+      </nav>
+    </footer>
+  )
+}
