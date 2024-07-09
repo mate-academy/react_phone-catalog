2699a4c12238c90c721b01e13c8020d4feba4d2c
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-09 21:33:00 +0200

    add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,55 @@
+import { NavLink, Link } from "react-router-dom";
+import styles from './Header.module.scss';
+import LogoIcon from '../../img/icons/LogoIcon.svg'
+
+
+export const Header: React.FC = () => (
+  <header className={styles.header}>
+    <Link
+      to="/"
+      className={styles.logoLink}
+    >
+      <img
+        src={LogoIcon}
+        alt="Nice Gadgets Logo"
+        className={styles.logo}
+      />
+    </Link>
+
+    <div className={styles.container}>
+      <nav className={styles.nav}>
+
+        <NavLink
+          to="/"
+          className={styles.navItem}
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/phones"
+          className={styles.navItem}
+        >
+          Phones
+        </NavLink>
+
+        <NavLink
+          to="/tablets"
+          className={styles.navItem}
+        >
+          Tablets
+        </NavLink>
+
+        <NavLink
+          to="/accessories"
+          className={styles.navItem}
+        >
+          Accessories
+        </NavLink>
+      </nav>
+    </div>
+  </header>
+  );
+
+
+
