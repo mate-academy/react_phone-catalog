1f73fe9b84a552da14cbf4c4bf7a5297223d419e
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-10 12:42:22 +0200

    add arro buttons to Pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import styles from './Pagination.module.scss'
+import ChevronIcon from '../../img/icons/ChevronIcon.svg'
 
 export const Pagination: React.FC = () => {
   return (
     <ul className={styles.pagination}>
+      <button
+        className={styles.button}
+      >
+        <img
+          src={ChevronIcon}
+          alt="Previous page"
+        />
+      </button>
 
+      <button
+        className={styles.button}
+      >
+        <img
+          src={ChevronIcon}
+          alt="Next page"
+        />
+      </button>
     </ul>
   )
 }
