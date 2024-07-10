44f5af9acc09ab038f946cba06ac825b15900ec1
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-10 11:59:42 +0200

    add Pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Pagination.module.scss'
+
+export const Pagination: React.FC = () => {
+  return (
+    <ul className={styles.pagination}>
+
+    </ul>
+  )
+}
