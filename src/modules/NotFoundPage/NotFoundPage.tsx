1b0d477c951d75e1571215d5fba3405fe35f0248
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-14 23:03:41 +0200

    add NotFoundPage  module

---
+++ src/modules/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+/* import { useLocation } from 'react-router-dom'; */
+/* import { ProductList } from '../../components/ProductList'; */
+
+export const NotFoundPage: React.FC = () => {
+
+    return (
+
+      <div>
+      Not found page
+
+
+      </div>
+    );
+};
