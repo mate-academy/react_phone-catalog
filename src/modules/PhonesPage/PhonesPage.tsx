44549f0eba58b5a9bc48f51cea5932ecc5dfb98f
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-14 23:07:59 +0200

    add PhonesPage

---
+++ src/modules/PhonesPage/PhonesPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+/* import { useLocation } from 'react-router-dom'; */
+/* import { ProductList } from '../../components/ProductList'; */
+import {Header} from '../../components/Header';
+
+export const PhonesPage: React.FC = () => {
+
+    return (
+
+      <div>
+      <Header />
+      Phones - test
+  {/*     <ProductList /> */}
+
+      </div>
+    );
+};
