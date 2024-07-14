7a859c092e6b3674837ab3fbdf6253c214e4a92a
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-14 23:27:46 +0200

    fill CartPage to test

---
+++ src/modules/CartPage/CartPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+/* import { useLocation } from 'react-router-dom'; */
+/* import { ProductList } from '../../components/ProductList'; */
+import {Header} from '../../components/Header';
+export const CartPage: React.FC = () => {
+
+    return (
+
+      <div>
+        <Header />
+      Cart - test
+  {/*     <ProductList /> */}
+
+      </div>
+    );
+};
