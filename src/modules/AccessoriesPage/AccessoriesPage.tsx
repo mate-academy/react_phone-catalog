ffbfad285894394cced4189e9429fa246ccac0a5
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-14 23:02:38 +0200

    add Accessories module

---
+++ src/modules/AccessoriesPage/AccessoriesPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+/* import { useLocation } from 'react-router-dom'; */
+/* import { ProductList } from '../../components/ProductList'; */
+import {Header} from '../../components/Header';
+export const AccessoriesPage: React.FC = () => {
+
+    return (
+
+      <div>
+        <Header />
+      Accessories - test
+  {/*     <ProductList /> */}
+
+      </div>
+    );
+};
