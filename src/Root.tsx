7d6483fdc8f247414034d31cf208abbab62a58f8
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-14 23:02:12 +0200

    add routes to Root

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,25 @@
 import { App } from './App';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import React from 'react';
+import { AccessoriesPage } from './modules/AccessoriesPage';
+import { PhonesPage } from './modules/PhonesPage';
+import { TabletsPage } from './modules/TabletsPage';
+import { HomePage } from './modules/HomePage';
+/* import { NotFoundPage } from './modules/NotFoundPage;' */
+
 
 export const Root = () => {
   return (
     <Router>
-      <React.Fragment /> {/* // USED TO AVOID LINTIG ISSUES !!!!! */}
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
+      <div>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/home" component={HomePage} />
+          <Route path="/phones" component={PhonesPage} />
+          <Route path="/tablets" component={TabletsPage} />
+          <Route path="/accessories" component={AccessoriesPage} />
+          {/* <Route path="*" component={NotFoundPage} /> */}
+        </Switch>
+      </div>
     </Router>
   );
 };
