86bb8d4c1f555a4c0980c660516f3d740db25e25
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-14 23:04:01 +0200

    add TabletsPage module

---
+++ src/modules/TabletsPage/TabletsPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+/* import { useLocation } from 'react-router-dom'; */
+/* import { ProductList } from '../../components/ProductList'; */
+import {Header} from '../../components/Header';
+
+export const TabletsPage: React.FC = () => {
+
+    return (
+
+      <div>
+        <Header />
+      Tablets - test
+  {/*     <ProductList /> */}
+
+      </div>
+    );
+};
