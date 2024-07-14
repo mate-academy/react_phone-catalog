f6b3090aea2c383c146637fdb52fdc0fd05c3718
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-14 23:03:13 +0200

    add HomePage module

---
+++ src/modules/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+/* import { useLocation } from 'react-router-dom'; */
+/* import { ProductList } from '../../components/ProductList'; */
+
+export const HomePage: React.FC = () => {
+
+    return (
+
+      <div>
+      Home Page - test
+  {/*     <ProductList /> */}
+
+      </div>
+    );
+};
