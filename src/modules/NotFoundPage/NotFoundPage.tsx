37a953aa8bae62d62a647a294723677d7949404c
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-14 23:28:30 +0200

    fill NotFoundPage to test

--- src/modules/NotFoundPage/NotFoundPage.tsx
+++ src/modules/NotFoundPage/NotFoundPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 /* import { useLocation } from 'react-router-dom'; */
 /* import { ProductList } from '../../components/ProductList'; */
+import {Header} from '../../components/Header';
 
 export const NotFoundPage: React.FC = () => {
 
     return (
 
       <div>
+        <Header />
       Not found page
 
 
