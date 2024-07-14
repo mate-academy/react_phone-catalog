976491cd6ef3d4d90d3706d27db6218eedcc65e6
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-14 21:49:39 +0200

    develop staic ProductList

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
-/* import styles from './ProductList.module.scss'; */
+import styles from './ProductList.module.scss';
+import { Breadcrumbs } from '../Breadcrumbs';
 
 export const ProductList: React.FC = () => {
   return (
-    <div>
+    <div className={styles.ProductsPage}>
+      <div className={styles.topContainer}>
+        <Breadcrumbs />
+        <h1 className={styles.title}>
+          Title
+        </h1>
+        <p className={styles.count}>
+          XX
+        </p>
+      </div>
+
+      <p className={styles.noResult}>
+        There are no category matching your query.
+      </p>
+
+      <div className={styles.sortBy}>
+        PLACEHOLDER: DROPDOWN
+      </div>
 
     </div>
   )
