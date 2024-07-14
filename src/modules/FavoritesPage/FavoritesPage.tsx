a38378ebf5d010c768a40e66de8e82ab0f068beb
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-07-14 23:28:06 +0200

    fill FavoritesPage to test

---
+++ src/modules/FavoritesPage/FavoritesPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+/* import { useLocation } from 'react-router-dom'; */
+/* import { ProductList } from '../../components/ProductList'; */
+import {Header} from '../../components/Header';
+export const FavoritesPage: React.FC = () => {
+
+    return (
+
+      <div>
+        <Header />
+      Favs - test
+  {/*     <ProductList /> */}
+
+      </div>
+    );
+};
