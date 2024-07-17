f2ae8ea42799467f409faab0c429052f475ab313
Rostyslav Sobchyshyn <rosta2604@icloud.com>
2024-07-17 20:09:16 +0100

    Header is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import './App.scss';
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './main.scss';
+import { Header } from './components/Header';
 
-export const App = () => (
-  <div className="App">
-    <h1>Product Catalog</h1>
-  </div>
-);
+export const App: React.FC = () => {
+  return (
+    <div className="app">
+      <Header />
+
+      <div className="main">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<h1>home</h1>} />
+            <Route path="/home" element={<Navigate to={'/'} replace />} />
+
+            <Route path="*" element={<h1>Page not found</h1>} />
+          </Routes>
+        </div>
+      </div>
+
+      {/* <Footer /> */}
+    </div>
+  );
+};
