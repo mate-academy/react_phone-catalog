06fda272fbc58ab71f023a37d8d03a32ec8bdcd7
Roman Kulchytskyi-Dashynych <kdrs0812@gmail.com>
2024-08-02 18:52:11 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './App.scss';
 
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { HomePage } from './pages/HomePage';
 
 export const App = () => (
   <div className="App">
     <Header />
+    <HomePage />
     <Footer />
   </div>
 );
