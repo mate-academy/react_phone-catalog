40b9786ead341e634fca82d920bfb97174a52d35
Volodymyr Kolisnichenko <vk-workshop@vovalogic.local>
2024-08-08 14:27:08 +0200

    skelton

--- src/components/shopByCategory/ShopByCategory.tsx
+++ src/components/shopByCategory/ShopByCategory.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './ShopByCategory.scss';
 import { Link } from 'react-router-dom';
 import { Product } from '../../types/Product';
+import { ProductsContext } from '../../context/ProductsContext';
 
 type Props = {
   products: Product[];
 };
 
 export const ShopByCategory: React.FC<Props> = ({ products }) => {
+  const context = useContext(ProductsContext);
+  const { isLoading } = context;
+
   const getGoods = (good: string) => products.filter(p => p.category === good);
 
   return (
@@ -18,19 +22,27 @@ export const ShopByCategory: React.FC<Props> = ({ products }) => {
         <Link className="shop_by_category--link" to="/phones">
           <div className="shop_by_category-bg-gold phone-gold" />
           <h3 className="shop_by_category__subtitle">Mobile phones</h3>
-          <p className="shop_by_category__models-number">{`${getGoods('phones').length} models`}</p>
+          <p className="shop_by_category__models-number">
+            {isLoading ? 'Loading...' : `${getGoods('phones').length} models`}
+          </p>
         </Link>
 
         <Link className="shop_by_category--link" to="/tablets">
           <div className="shop_by_category-bg-grey tablet-grey" />
           <h3 className="shop_by_category__subtitle">Tablets</h3>
-          <p className="shop_by_category__models-number">{`${getGoods('tablets').length} models`}</p>
+          <p className="shop_by_category__models-number">
+            {isLoading ? 'Loading...' : `${getGoods('tablets').length} models`}
+          </p>
         </Link>
 
         <Link className="shop_by_category--link" to="/accessories">
           <div className="shop_by_category-bg-red phones-red" />
           <h3 className="shop_by_category__subtitle">Accessories</h3>
-          <p className="shop_by_category__models-number">{`${getGoods('accessories').length} models`}</p>
+          <p className="shop_by_category__models-number">
+            {isLoading
+              ? 'Loading...'
+              : `${getGoods('accessories').length} models`}
+          </p>
         </Link>
       </div>
     </section>
--- src/components/skeleton/DetailsSkeleton.tsx
+++ src/components/skeleton/DetailsSkeleton.tsx
@@ -2,46 +2,24 @@ import './DetailsSkeleton.scss';
 
 export const DetailsSkeleton = () => {
   return (
-    <div className="elems">
-      <div className="elem__image-container skeleton" />
-      <p className="elem__title skeleton" />
-
-      <p className="elem__title-bottom skeleton" />
-
-      <div className="elem__price-group">
-        <p className="elem__price skeleton" />
-        <p className="elem__price-right skeleton" />
-      </div>
-
-      <div className="elem__divider skeleton" />
-
-      <div className="elem__text-group">
-        <p className="elem__text skeleton" />
-        <p className="elem__text-right skeleton" />
-      </div>
-
-      <div className="elem__text-group">
-        <p className="elem__text skeleton" />
-        <p className="elem__text-right skeleton" />
-      </div>
-
-      <div className="elem__text-group">
-        <p className="elem__text skeleton" />
-        <p className="elem__text-right skeleton" />
-      </div>
-
-      <div className="elem__actions">
-        <button
-          type="button"
-          aria-label="Loading button"
-          className="elem__button elem__button-left skeleton"
-        />
-        <button
-          type="button"
-          aria-label="Loading button"
-          className="elem__button elem__button-right skeleton"
-        />
-      </div>
+    <div className="parent">
+      <div className="div1 skeleton"> </div>
+      <div className="div2 skeleton"> </div>
+      <div className="div3 skeleton"> </div>
+      <div className="div4 skeleton"> </div>
+      <div className="div5 skeleton"> </div>
+      <div className="div6 skeleton"> </div>
+      <div className="div7 skeleton"> </div>
+      <div className="div8 skeleton"> </div>
+      <div className="div9 skeleton"> </div>
+      <div className="div10 skeleton"> </div>
+      <div className="div11 skeleton"> </div>
+      <div className="div12 skeleton"> </div>
+      <div className="div13 skeleton"> </div>
+      <div className="div14 skeleton"> </div>
+      <div className="div15 skeleton"> </div>
+      <div className="div16 skeleton"> </div>
+      <div className="div17 skeleton"> </div>
     </div>
   );
 };
