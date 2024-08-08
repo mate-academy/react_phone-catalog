c28f67d7602a71f03550e89f41a6c47d162db513
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-08 22:32:36 +0200

    remove ProductList from App

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,23 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Breadcrumbs } from './components/Breadcrumbs';
 import { Pagination } from './components/Pagination';
+/* import { ProductList } from './components/ProductList'; */
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>This website is under construction...</h1>
-    <Header />
-    <Breadcrumbs />
-    <ProductSlider />
-    <ProductCard />
-    <Pagination />
-    <Footer />
-    <Loader />
-  </div>
-);
+
+export const App: React.FC = () => {
+
+
+  return (
+    <div className="App">
+      <h1>This website is under construction...</h1>
+      <Header />
+      <Breadcrumbs />
+      <ProductSlider />
+      <ProductCard productId="apple-iphone-7-32gb-rosegold" />
+      {/* <ProductList /> */}
+      <Pagination />
+      <Footer />
+      <Loader />
+    </div>
+  );
+};
