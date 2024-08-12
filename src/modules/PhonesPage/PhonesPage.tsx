75594134e519fc20166e440cb104677d1c1c300b
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-12 20:11:37 +0200

    update import links for ProductList

--- src/modules/PhonesPage/PhonesPage.tsx
+++ src/modules/PhonesPage/PhonesPage.tsx
@@ -1,7 +1,7 @@
 /* import { useLocation } from 'react-router-dom'; */
-/* import { ProductList } from '../../components/ProductList'; */
+import { ProductList } from '../../components/ProductList';
 import {Header} from '../../components/Header';
-import { ProductList } from '../ProductDetailsPage/ProductDetailsPage';
+
 
 
 
