c0a975b0d6c620402b97a552dbd5e21c23ade065
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-12 20:10:08 +0200

    remove ProductList from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.module.scss';
 import React from 'react';
 import { Loader } from './components/Loader/Loader';
-import { ProductCard } from './components/ProductCard/ProductCard';
 import { ProductSlider } from './components/ProductSlider';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Breadcrumbs } from './components/Breadcrumbs';
 import { Pagination } from './components/Pagination';
-import { ProductList } from './components/ProductList';
 
 
 export const App: React.FC = () => {
@@ -19,8 +17,6 @@ export const App: React.FC = () => {
       <Header />
       <Breadcrumbs />
       <ProductSlider />
-      <ProductCard productId="apple-iphone-7-32gb-rosegold" />
-      <ProductList />
       <Pagination />
       <Footer />
       <Loader />
