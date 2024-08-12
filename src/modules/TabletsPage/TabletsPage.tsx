79db9c804fef240a0bf2420794eb7fde001dcba5
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-12 20:11:08 +0200

    add ProductList to tablets

--- src/modules/TabletsPage/TabletsPage.tsx
+++ src/modules/TabletsPage/TabletsPage.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
 /* import { useLocation } from 'react-router-dom'; */
-/* import { ProductList } from '../../components/ProductList'; */
+import { ProductList } from '../../components/ProductList';
 import {Header} from '../../components/Header';
 
 export const TabletsPage: React.FC = () => {
 
-    return (
+   return (
 
       <div>
         <Header />
-      Tablets - test
-  {/*     <ProductList /> */}
-
+        <ProductList />
       </div>
     );
 };
