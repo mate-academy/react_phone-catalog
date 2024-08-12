f07858b74b92042849bff1f2a5e93f0706ecd535
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-12 20:46:52 +0200

    add product type

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,4 +1,4 @@
-export interface Product {
+export interface ProductPhone {
   id: string;
   category: string;
   phoneId: string;
@@ -12,4 +12,29 @@ export interface Product {
   ram: string;
   year: number;
   images: string;
+}
+
+export interface ProductTablet {
+  id: string;
+  category: string;
+  namespaceId: string; // Changed from phoneId to namespaceId
+  name: string;
+  capacityAvailable: string[]; // Added this field
+  capacity: string;
+  priceRegular: number;
+  priceDiscount: number;
+  colorsAvailable: string[]; // Added this field
+  color: string;
+  images: string[]; // Changed from string to an array of strings
+  description: Array<{
+    title: string;
+    text: string[];
+  }>; // Added this field to match the description structure
+  screen: string;
+  resolution: string; // Added this field
+  processor: string; // Added this field
+  ram: string;
+  camera: string; // Changed this field
+  zoom: string; // Added this field
+  cell: string[]; // Added this field
 }
