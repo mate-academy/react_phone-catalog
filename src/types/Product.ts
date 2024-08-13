78741c9048a572832720ec98326baf64bc9cf28f
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-13 11:15:17 +0200

    add Accessory interface def

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -38,3 +38,26 @@ export interface ProductTablet {
   zoom: string; // Added this field
   cell: string[]; // Added this field
 }
+
+export interface ProductAccessory {
+  id: string;
+  category: string;
+  namespaceId: string;
+  name: string;
+  capacityAvailable: string[];
+  capacity: string;
+  priceRegular: number;
+  priceDiscount: number;
+  colorsAvailable: string[];
+  color: string;
+  images: string[];
+  description: Array<{
+    title: string;
+    text: string[];
+  }>;
+  screen: string;
+  resolution: string;
+  processor: string;
+  ram: string;
+  cell: string[];
+}
