8f95647fac8dfb836931ba907b4b8df3cfbde187
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-15 19:49:00 +0200

    add missing import react line

--- src/modules/PhonesPage/PhonesPage.tsx
+++ src/modules/PhonesPage/PhonesPage.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ProductList } from '../../components/ProductList';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
