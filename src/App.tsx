e7bcf7b162f3ff036c8b0a37d9c5ad0a4d013b40
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-15 19:48:27 +0200

    add category ThumbNails

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.module.scss';
-
 import React, { useState, useEffect } from 'react';
-
 import { Loader } from './components/Loader/Loader';
 import { BannerSlider } from './components/BannerSlider';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { UnderConstruction } from './components/UnderConstruction';
 import { ProductSlider } from './components/ProductSlider';
+import { Thumbnails } from './components/Thumbnails';
 
 export const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -26,6 +25,7 @@ export const App: React.FC = () => {
           <Header />
           <BannerSlider />
           <ProductSlider title="Brand new models" count={10}/>
+          <Thumbnails />
           <ProductSlider title="Hot prices" count={10}/>
           <Footer />
         </div>
