82a5ab10fd1fa1c48f1041c4f6d156af218a958b
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-15 19:49:19 +0200

    add category ThumbNail components

---
+++ src/components/Thumbnails/Thumbnails.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState, useMemo} from 'react';
+import { Link } from 'react-router-dom'
+import styles from './Thumbnails.module.scss'
+
+type Product = {
+  id: string;
+  category: string
+}
+
+
+
+export const Thumbnails: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([])
+  const categories = ['Phones','Tablets','Accessories']
+
+  useEffect(() => {
+
+    const fetchProductData = async () => {
+      try {
+        const response = await fetch(`https://meljaszuk.github.io/react_phone-catalog/api/products.json`);
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching product data:', error);
+      }
+    };
+
+    fetchProductData();
+
+  }, []);
+
+
+
+    const memoizedCounts = useMemo(() => {
+      let categoryCounts = {
+        Phones:0,
+        Tablets:0,
+        Accessories:0
+      }
+      for(const product of products) {
+        if (product.category === categories[0].toLowerCase()) {
+          categoryCounts['Phones']++
+        }
+        if (product.category === categories[1].toLowerCase()) {
+          categoryCounts['Tablets']++
+        }
+        if (product.category === categories[2].toLowerCase()) {
+          categoryCounts['Accessories']++
+        }
+      }
+      return categoryCounts
+    }, [products]);
+
+
+  return (
+    <div className={styles.categories}>
+      <h1 className={styles.title}>Shop by category</h1>
+
+      <div className={styles.categoryContainer}>
+      {categories.map((category) => (
+        <div className={styles.category} key={category}>
+          <Link to="/" className={styles.link}>
+            <img
+              src={`./img/category-${category}.png`}
+              className={styles.image}
+            />
+
+            <h2 className={styles.category__title}>
+            {category} {memoizedCounts[category as keyof typeof memoizedCounts]} items
+            </h2>
+
+            <span className={styles.category__subtitle}>
+              <span className={styles.category__subtitle}>
+              </span>
+            </span>
+          </Link>
+        </div>
+      ))}
+      </div>
+    </div>
+  )
+}
