55f649417b855e439e7e5d88f34e5e9b99fa4986
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-17 23:33:04 +0200

    add dynamic product details to TechSpec

--- src/modules/ProductDetailsPage/components/TechSpecs/TechSpecs.tsx
+++ src/modules/ProductDetailsPage/components/TechSpecs/TechSpecs.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './TechSpecs.module.scss';
-/* import { useAppContext } from '../../../../context/AppContext'; */
+import { useAppContext } from '../../../../context/AppContext';
 
 /* type TechSpecsProps = {
   productDetails: ProductDetails;
 }; */
 
 export const TechSpecs: React.FC = () => {
+  const { clickedProduct } = useAppContext();
+  if (clickedProduct !== undefined) {
   return (
     <div className={styles.section }>
       <section className={styles.techSpecsSection}>
@@ -16,48 +18,59 @@ export const TechSpecs: React.FC = () => {
         <ul className={styles.specsList}>
           <li className={styles.specs}>
             <strong className={styles.specsKey}>Screen </strong>
-            <span className={styles.specsValue}>XXX</span>
+            <span className={styles.specsValue}>{`${clickedProduct.screen}`}</span>
           </li>
 
           <li className={styles.specs}>
             <strong className={styles.specsKey}>Resolution </strong>
-            <span className={styles.specsValue}>XXX</span>
+            <span className={styles.specsValue}>{`${clickedProduct.resolution}`}</span>
           </li>
 
           <li className={styles.specs}>
             <strong className={styles.specsKey}>Processor</strong>
-            <span className={styles.specsValue}>XXXX</span>
+            <span className={styles.specsValue}>{`${clickedProduct.processor}`}</span>
           </li>
 
           <li className={styles.specs}>
             <strong className={styles.specsKey}>Ram</strong>
-            <span className={styles.specsValue}>XXX</span>
+            <span className={styles.specsValue}>{`${clickedProduct.ram}`}</span>
           </li>
 
           <li className={styles.specs}>
             <strong className={styles.specsKey}>Built in memory</strong>
-            <span className={styles.specsValue}>XXX</span>
+            <span className={styles.specsValue}>{`${clickedProduct.capacity}`}</span>
           </li>
 
+          {'camera' in clickedProduct && (
             <li className={styles.specs}>
               <strong className={styles.specsKey}>Camera</strong>
-              <span className={styles.specsValue}>XXX</span>
+              <span className={styles.specsValue}>{`${clickedProduct.camera}`}</span>
             </li>
+                )
+              }
 
-
+            {'zoom' in clickedProduct && (
             <li className={styles.specs}>
-              <strong className={styles.specsKey}>Zoom</strong>
-              <span className={styles.specsValue}>XXXX</span>
-            </li>
-
-          <li className={styles.specs}>
-            <strong className={styles.specsKey}>Cell</strong>
-            <span className={styles.specsValue}>
-              xxx
-            </span>
+            <strong className={styles.specsKey}>Zoom</strong>
+            <span className={styles.specsValue}>{`${clickedProduct.zoom}`}</span>
           </li>
+            )
+            }
+
+            {'ceil' in clickedProduct && (
+      <li className={styles.specs}>
+      <strong className={styles.specsKey}>Cell</strong>
+      <span className={styles.specsValue}>
+      {`${clickedProduct.ceil}`}
+      </span>
+    </li>
+            )
+
+            }
+
         </ul>
       </section>
     </div>
   );
-};
+} else return <div/>
+}
