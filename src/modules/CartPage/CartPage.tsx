19060aee9fb347e834c9d77752448c2250204cc9
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-22 01:53:00 +0200

    add styles and content

--- src/modules/CartPage/CartPage.tsx
+++ src/modules/CartPage/CartPage.tsx
@@ -1,19 +1,73 @@
 import React from 'react';
 import { Header } from '../../components/Header';
+import { Breadcrumbs } from '../../components/Breadcrumbs';
 import { useLocation } from 'react-router-dom';
 import { PreviousPage } from '../../components/PreviousPage';
+import styles from './CartPage.module.scss';
+import chevronIcon from '../../img/icons/ChevronIcon.svg';
+import { useAppContext } from '../../context/AppContext';
+import { CartItem } from './CartItem/CartItem';
+import { Link } from 'react-router-dom';
 
 export const CartPage: React.FC = () => {
   const category = useLocation().pathname.slice(1)
   console.log('PAGE CLICKED',category)
-    return (
+  const {handleNotReady, previousCurrentPage} = useAppContext();
+  return (
+    <div>
+<Header />
+    <PreviousPage category={category}/>
 
-      <div>
-        <PreviousPage category={category}/>
-        <Header />
-        <center>
-          In progress
-        </center>
+    <div className={styles.cartPage}>
+    <Breadcrumbs category={category}/>
+      <div className={styles.topContainer}>
+        <div className={styles.topLeft}>
+          <button className={styles.goBackButton}>
+            <img
+              src={chevronIcon}
+              alt="home"
+              className={styles.chevronIcon}
+            />
+            <div className={styles.goBackText}>
+            <Link to={`/${previousCurrentPage[0]}`}>
+              <div className={styles.label}>Back</div>
+              </Link>
+            </div>
+          </button>
+        </div>
+        <h1 className={styles.title}>Cart</h1>
       </div>
+
+        <div className={styles.emptyContainer}>
+          <img src="img/cart-is-empty.png" className={styles.image}/>
+        </div>
+
+        <div className={styles.container}>
+          <div className={styles.cartItems}>
+
+              <CartItem />
+
+          </div>
+          <div className={styles.bottomContainer}>
+            <div className={styles.checkout}>
+              <h2 className={styles.totalPrice}>$XXX</h2>
+              <p
+                className={styles.totalItems}
+              >"Total for xxx item xxx"</p>
+              <div className={styles.divider}></div>
+              <button
+                className={styles.checkoutButton}
+                onClick={handleNotReady}
+              >
+                Checkout
+              </button>
+            </div>
+          </div>
+        </div>
+
+    </div>
+    </div>
+
+
     );
 };
