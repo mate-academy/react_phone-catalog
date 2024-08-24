af906ab906dcdea01a000685a6f1fa7820648def
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-24 10:22:57 +0200

    temproary disable sorting

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import styles from './Sort.module.scss';
 import { useAppContext } from '../../context/AppContext';
 
-type SortMethodTypes = "newest" | "alpha" | "price";
+/* type SortMethodTypes = "newest" | "alpha" | "price"; */
 
 export const Sort: React.FC = () => {
-  const {sortMethod, setSortMethod} = useAppContext();
+  const {sortMethod, /* setSortMethod, */ handleNotReady} = useAppContext();
 
-  const handleMethodChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+/*   const handleMethodChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value as SortMethodTypes;
     setSortMethod(value);
-  }
+  } */
 
   return (
     <div>
       <div className={styles.label}>Sort by</div>
-        <select onChange={handleMethodChange} className={styles.select} defaultValue={sortMethod}>
+        <select onChange={handleNotReady} className={styles.select} defaultValue={sortMethod}>
           <option value="newest">Newest</option>
           <option value="alpha">Alphabethically</option>
           <option value="price">Cheapest</option>
