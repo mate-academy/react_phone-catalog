30c18b5d97862867eb6328b9801d09122e15ec83
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-25 22:38:33 +0200

    add LimitedProduct type

--- src BACKUP/types/Product.ts
+++ src BACKUP/types/Product.ts
@@ -70,3 +70,18 @@ export interface ProductAccessory {
 }
 
 export type Product =  ProductPhone | ProductTablet | ProductAccessory;
+
+export interface LimitedProduct {
+  id: number,
+  category: string,
+  itemId: string,
+  name: string,
+  fullPrice: number,
+  price: number,
+  screen: string,
+  capacity: string,
+  color: string,
+  ram: string,
+  year: number,
+  image: string
+}
