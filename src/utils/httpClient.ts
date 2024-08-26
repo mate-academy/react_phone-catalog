15b4f5ee36496366d274426a88d64808a6a8aaa3
SrTrace <levashov04sergey@gmail.com>
2024-08-26 18:36:28 +0300

    try to change base url for api

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,8 +1,10 @@
-// const BASE_URL = 'https://github.com/SrTrace/react_phone-catalog/tree/srtrace/react_phone-catalog-solution/src/api';
-const TEMP_BASE_URL = 'http://localhost:3000/api';
+const BASE_URL =
+  // eslint-disable-next-line
+  'https://github.com/SrTrace/react_phone-catalog/blob/srtrace/react_phone-catalog-solution/public/api/';
+// const TEMP_BASE_URL = 'http://localhost:3000/api';
 
 export function getData<T>(url: string): Promise<T> {
-  return fetch(TEMP_BASE_URL + url).then(response => {
+  return fetch(BASE_URL + url).then(response => {
     if (!response.ok) {
       throw new Error(`${response.status} ${response.text}`);
     }
