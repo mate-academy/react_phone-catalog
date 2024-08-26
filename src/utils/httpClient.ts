3782e383feb1e11603f718532f7c5c5e68ee31db
SrTrace <levashov04sergey@gmail.com>
2024-08-26 18:50:12 +0300

    try to fix path to api

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,7 +1,8 @@
-const BASE_URL =
-  // eslint-disable-next-line
-  'https://github.com/SrTrace/react_phone-catalog/blob/srtrace/react_phone-catalog-solution/public/api/';
+// const BASE_URL =
+//   // eslint-disable-next-line
+//   'https://github.com/SrTrace/react_phone-catalog/blob/srtrace/react_phone-catalog-solution/public/api/';
 // const TEMP_BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = '..//api';
 
 export function getData<T>(url: string): Promise<T> {
   return fetch(BASE_URL + url).then(response => {
