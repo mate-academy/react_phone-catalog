3d020b600cd057c4536fb5a028c1ca5be11c208d
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-04 12:11:52 +0200

    create GoBack component

---
+++ src/components/GoBack/GoBack.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import styles from './GoBack.module.scss';
+
+
+export const GoBack: React.FC = () => {
+
+
+  const history = useHistory();
+
+
+  const handleGoBack = () => {
+    history.goBack();
+  };
+
+  return (
+    <div className={styles.goBackText}>
+      <div className={styles.label} onClick={handleGoBack}>Back</div>
+    </div>
+  );
+}
