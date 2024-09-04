a3439c9defc866f2bb73f426debf46eb29d21052
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-04 23:24:35 +0200

    delete temporary folder

--- bugs/src/components/UnderConstruction/UnderConstruction.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const UnderConstruction: React.FC = () => {
-  return (
-    <h1>
-      This webiste is under construction. Some FEATURES and STYLES HAVE NOT BEEN IMPLEMENTED.
-    </h1>
-  )
-}
