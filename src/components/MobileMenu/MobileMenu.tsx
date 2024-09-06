932512d2d8fea230f61ac7a2b5ba06b507f8434a
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-06 21:46:37 +0200

    add mobile sliding menu

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -1,101 +1,94 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './MobileMenu.module.scss';
 import favoritesIconSrc from '../../img/icons/FavoritesIcon.svg';
 import { useAppContext } from '../../context/AppContext';
-/* import favoritesIconSrcDT from '../../img/icons/FavoritesIcon--DarkTheme.svg'; */
+import favoritesIconSrcDT from '../../img/icons/FavoritesIcon--DarkTheme.svg';
 import cartIconSrc from '../../img/icons/CartIcon.svg';
-/* import cartIconSrcDT from '../../img/icons/CartIcon--DarkTheme.svg'; */
+import cartIconSrcDT from '../../img/icons/CartIcon--DarkTheme.svg';
+import { Theme } from '../Theme';
 
 export const MobileMenu: React.FC = () => {
-  const { isMobMenuOpen } = useAppContext();
+  const { setIsMobMenuOpen, theme } = useAppContext();
+  const handleMenuStatus = () => {
+    setIsMobMenuOpen(false);
+  };
 
-  useEffect(() => {
-    console.log(isMobMenuOpen)
-  }, [isMobMenuOpen])
   return (
     <div className={styles.topWrapper}>
-  <div className={styles.menuOverlay}
-      /* className={classNames(styles.menuOverlay, {
-        [styles.show]: isMenuOpen,
-      })} */
-      /* aria-expanded={isMenuOpen} */
-    >
-      <nav className={styles.nav} role="navigation">
-        <NavLink
-          to="/"
-          /* onClick={toggleIsMenuOpen} */
-/*           className={({ isActive }) =>
-            classNames(styles.item, { [styles.isActive]: isActive })
-          } */
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/phones"
-          /* onClick={toggleIsMenuOpen} */
-         /*  className={({ isActive }) =>
-            classNames(styles.item, { [styles.isActive]: isActive })
-          } */
-        >
-          Phones
-        </NavLink>
-        <NavLink
-          to="/tablets"
-          /* onClick={toggleIsMenuOpen}
-          className={({ isActive }) =>
-            classNames(styles.item, { [styles.isActive]: isActive })
-          } */
-        >
-          Tablets
-        </NavLink>
-        <NavLink
-          to="/accessories"
-          /* onClick={toggleIsMenuOpen}
-          className={({ isActive }) =>
-            classNames(styles.item, { [styles.isActive]: isActive })
-          } */
-        >
-          Accessories
-        </NavLink>
-      </nav>
-      <div className={styles.actions}>
-        <NavLink
-          to="/favorites"
-          /* onClick={toggleIsMenuOpen}
-          className={({ isActive }) =>
-            classNames(styles.action, { [styles.isActive]: isActive })
-          } */
-        >
-          <div className={styles.actionIcon}>
-            <img src={favoritesIconSrc} alt="Favorites" />
-            {/* {favorites.length > 0 && (
-              <span className={styles.count}>
-                <p className={styles.countText}>{favorites.length}</p>
-              </span>
-            )} */}
+      <div className={styles.menuOverlay}>
+        <div className={styles.linkWrapper}>
+          <nav className={styles.nav} role="navigation">
+            <NavLink
+              to="/"
+              exact
+              className={styles.item}
+              onClick={handleMenuStatus}
+              activeClassName={styles.isActive}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/phones"
+              className={styles.item}
+              onClick={handleMenuStatus}
+              activeClassName={styles.isActive}
+            >
+              Phones
+            </NavLink>
+            <NavLink
+              to="/tablets"
+              className={styles.item}
+              onClick={handleMenuStatus}
+              activeClassName={styles.isActive}
+            >
+              Tablets
+            </NavLink>
+            <NavLink
+              to="/accessories"
+              className={styles.item}
+              onClick={handleMenuStatus}
+              activeClassName={styles.isActive}
+            >
+              Accessories
+            </NavLink>
+          </nav>
+        </div>
+
+        <div className={styles.actions}>
+          <NavLink
+            to="/favorites"
+            className={styles.action}
+            onClick={handleMenuStatus}
+            activeClassName={styles.isActive}
+          >
+            <div className={styles.actionIcon}>
+              <img
+                src={`${theme === 'dark' ? favoritesIconSrcDT : favoritesIconSrc}`}
+                alt="Favorites"
+              />
+            </div>
+          </NavLink>
+          <NavLink
+            to="/cart"
+            className={styles.action}
+            onClick={handleMenuStatus}
+            activeClassName={styles.isActive}
+          >
+            <div className={styles.actionIcon}>
+              <img
+                src={`${theme === 'dark' ? cartIconSrcDT : cartIconSrc}`}
+                alt="Cart"
+                className={styles.icon}
+              />
+            </div>
+          </NavLink>
+
+          <div className={styles.action}>
+            <Theme />
           </div>
-        </NavLink>
-        <NavLink
-          to="/cart"
-          /* onClick={toggleIsMenuOpen}
-          className={({ isActive }) =>
-            classNames(styles.action, { [styles.isActive]: isActive })
-          } */
-        >
-          <div className={styles.actionIcon}>
-            <img src={cartIconSrc} alt="Cart" className={styles.icon} />
-           {/*  {cart.length > 0 && (
-              <span className={styles.count}>
-                <p className={styles.countText}>{cart.length}</p>
-              </span>
-            )} */}
-          </div>
-        </NavLink>
+        </div>
       </div>
     </div>
-
-    </div>
-
   );
 };
