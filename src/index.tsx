90acdd159ffac280fcf99f9caba05d691e820867
Galers <galosaandrew@gmail.com>
2024-09-07 12:05:22 +0300

    changed the build, add vite, update node -v 20

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { App } from '@components/app/App';
+import { store } from '@store/store';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
