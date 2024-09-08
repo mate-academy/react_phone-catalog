2d8d745972bfa2b06359b056897d52d5dcc62280
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-08 22:42:50 +0200

    export fetching to separte file

---
+++ src/utils/fetchProducts.ts
@@ -0,0 +1,27 @@
+import { LimitedProduct } from '../types/Product';
+
+export const fetchProducts = async (category: string, sortMethod: string): Promise<LimitedProduct[]> => {
+  try {
+    const response = await fetch('https://meljaszuk.github.io/react_phone-catalog/api/products.json');
+    const data: LimitedProduct[] = await response.json();
+
+    const filteredData = data.filter((item) => item.category === category);
+
+    switch (sortMethod) {
+      case 'alpha':
+        filteredData.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case 'price':
+        filteredData.sort((a, b) => a.price - b.price);
+        break;
+      case 'newest':
+        filteredData.sort((a, b) => b.year - a.year);
+        break;
+    }
+
+    return filteredData;
+  } catch (error) {
+    console.error('Error fetching product data:', error);
+    return [];
+  }
+};
