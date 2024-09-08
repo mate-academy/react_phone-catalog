e7a5984402bb750527142c15ee2a63e92a62bcbe
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-09-08 20:19:50 +0200

    limit visibility of pagination buttons to max 5

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Pagination.module.scss';
 import chevronIcon from '../../img/icons/ChevronIcon.svg';
 import chevronIconDT from '../../img/icons/ChevronIcon--DarkTheme.svg';
@@ -17,29 +17,28 @@ export const Pagination: React.FC<PaginationProps> = ({
   displayedPage,
 }) => {
   const arrayOfPageButtons = Array.from({ length: numberOfPages }, (_, i) => i + 1);
-  const [offset, setOffset] = useState(0);
-  const [mvmtBalance, setMvmtBalance] = useState(0);
+  const [visiblePages, setVisiblePages] = useState<number[]>([1,5]);
   const { theme } = useAppContext();
 
-  const moveLeft = () => {
-      if (mvmtBalance > 0) {
-        setOffset(offset + 42)
-        setMvmtBalance(mvmtBalance-1)
-      }
-  }
-
-  const moveRight = () => {
-    if (mvmtBalance < numberOfPages - 5)  {
-      setOffset(offset - 42)
-      setMvmtBalance(mvmtBalance+1)
+  useEffect(() => {
+    if (numberOfPages <= 5) {
+      setVisiblePages([1,numberOfPages]);
     }
-  }
+  }, [numberOfPages]);
+
+
+  const move = (mvm: 1 | -1) => {
+    if (displayedPage + mvm !== 0 && displayedPage + mvm !== numberOfPages + 1) {
+      handleDisplayedPage(displayedPage + mvm);
+      setVisiblePages([visiblePages[0] + mvm, visiblePages[1] + mvm])
+    }
+  };
 
   return (
     <ul className={styles.pagination}>
       <button
-        className={`${styles.button} ${mvmtBalance === 0 ? styles.disabled : ""}`}
-        onClick={moveLeft}
+        className={`${styles.button}`}
+        onClick={() => move(-1)}
       >
         <img
           src={`${theme === 'dark' ? chevronIconDT : chevronIcon}`}
@@ -48,24 +47,26 @@ export const Pagination: React.FC<PaginationProps> = ({
       </button>
 
       <div className={styles.topWrapper}>
-        <div className={styles.buttonWrapper} style={{ transform: `translateX(${offset}px)` }}>
+        <div className={styles.buttonWrapper}>
             {arrayOfPageButtons.map(pageButton => (
-            <button
-              key={pageButton}
-              className={classNames(styles.button, {
-                [styles.active]: pageButton === displayedPage,
-              })}
-              onClick={() => handleDisplayedPage(pageButton)}
-            >
-              {pageButton}
-            </button>
+            <div className={`${pageButton >= visiblePages[0] && pageButton <= visiblePages[1] ? "" : styles.hidden}`}>
+              <button
+                key={pageButton}
+                className={classNames(styles.button, {
+                  [styles.active]: pageButton === displayedPage,
+                })}
+                onClick={() => handleDisplayedPage(pageButton)}
+              >
+                {pageButton}
+              </button>
+            </div>
           ))}
         </div>
       </div>
 
       <button
-        className={`${styles.button} ${mvmtBalance === numberOfPages - 5 ? styles.disabled : ""}`}
-        onClick={moveRight}
+        className={`${styles.button}`}
+        onClick={() => move(1)}
       >
         <img
           src={`${theme === 'dark' ? chevronIconDT : chevronIcon}`}
