2d9fa915e83229f581e1c8ad7f26eedc3cc174e0
Vladyslav Korobka <vladseipa@gmail.com>
2024-09-13 13:34:46 +0300

    add hash in Router for GitHub Pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { HashRouter as Router } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from './App';
 import { AppProvider } from './AppContext';
 
