58b37848a1f758d9a628537f685a77fa9a63bd05
Vladyslav Korobka <vladseipa@gmail.com>
2024-09-13 17:51:25 +0300

    changing ProductAPI

--- src/api/ProductsAPI.ts
+++ src/api/ProductsAPI.ts
@@ -4,13 +4,19 @@ const BASE_URL = './api';
 const PRODUCTS = 'products.json';
 
 export const getProducts = async (): Promise<Product[]> => {
-  return fetch(`${BASE_URL}/${PRODUCTS}`).then(response => response.json());
+  return fetch(`${BASE_URL}/${PRODUCTS}`)
+    .then(response => response.json())
+    .catch(() => {
+      throw new Error('Error: getting products');
+    });
 };
 
 export const getProductDetails = async (
   productURL: string,
 ): Promise<ProductDetail[]> => {
   return fetch(`${BASE_URL}/${productURL}.json`).then(response =>
-    response.json(),
+    response.json().catch(() => {
+      throw new Error('Error: getting product details');
+    }),
   );
 };
