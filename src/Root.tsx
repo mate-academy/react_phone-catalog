cc7f65f3fe43bab8b74c61bae5586a095ba2e19f
Nadiia Vlasenko <vlasenkonadia19@gmail.com>
2024-09-20 20:32:47 +0200

    added hash router

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { PhonesPage } from './pages/PhonesPage';
 import { TabletsPage } from './pages/TabletsPage';
 import { AccessoriesPage } from './pages/AccessoriesPage';
