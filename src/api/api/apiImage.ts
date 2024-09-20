28c63370e6414c01d5a8152d11cd07750c57b2d7
Nadiia Vlasenko <vlasenkonadia19@gmail.com>
2024-09-20 20:35:35 +0200

    changed img

--- src/api/api/apiImage.ts
+++ src/api/api/apiImage.ts
@@ -1,5 +1,10 @@
+// export const images = [
+//   '/img/Banner.svg',
+//   '/img/Banner.svg',
+//   '/img/Banner.svg',
+// ];
 export const images = [
-  '/img/Banner.svg',
-  '/img/Banner.svg',
-  '/img/Banner.svg',
+  `${process.env.PUBLIC_URL}/img/Banner.svg`,
+  `${process.env.PUBLIC_URL}/img/Banner.svg`,
+  `${process.env.PUBLIC_URL}/img/Banner.svg`,
 ];
