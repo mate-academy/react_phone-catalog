c316137bb0452c76b38d0960acdf8e8f7fa28d9f
Den <clixqqk@gmail.com>
2024-09-29 15:50:37 +0300

    fdsf

---
+++ src/variables.ts
@@ -0,0 +1,18 @@
+export const COLORS = {
+  black: '#0F0F11',
+  gold: '#F4BA47',
+  silver: '#C0C0C0',
+  spacegray: '#A9A9A9',
+  coral: '#FF7F50',
+  white: '#FFFFFF',
+  midnightgreen: '#003030',
+  yellow: '#FFFF00',
+  purple: '#800080',
+  green: '#008000',
+  red: '#FF0000',
+  rosegold: '#B76E79',
+  midnight: '#191970',
+  graphite: '#7F7F7F',
+  sierrablue: '#A8B3C8',
+};
+
