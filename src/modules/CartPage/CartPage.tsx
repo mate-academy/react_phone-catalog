9af4d95cb6243a51ec63a3ce5c94c431094e933f
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2024-09-29 22:38:47 +0300

    delete dependencies

--- src/modules/CartPage/CartPage.tsx
+++ src/modules/CartPage/CartPage.tsx
@@ -1,5 +1,15 @@
 import { FC } from 'react';
+import { Breadcrumbs } from '../../components/Breadcrumbs';
+import styles from './CartPage.module.scss';
 
 export const CartPage: FC = ({}) => {
-  return <div>CartPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.productTop}>
+        <Breadcrumbs />
+        <h1 className={styles.title}>Cart</h1>
+      </div>
+      <div className={styles.list}></div>
+    </div>
+  );
 };
