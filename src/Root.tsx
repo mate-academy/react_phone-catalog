e0423125b46d83b5f2d4b098cfab7c65f9692b55
Ivan <ivankp2000@gmail.com>
2024-10-01 22:51:42 +0100

    changed to HashRouter

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import { App } from './App';
 import { HomePage } from './pages/HomePage';
 import { PhonePage } from './pages/PhonePage';
