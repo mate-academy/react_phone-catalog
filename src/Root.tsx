f9c3d4238e86bebbdd92f9f913d8191d438cffb7
Ivan <ivankp2000@gmail.com>
2024-10-07 14:07:31 +0100

    package json update

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import { App } from './App';
 import { HomePage } from './pages/HomePage';
 import { PhonePage } from './pages/PhonePage';
@@ -11,18 +16,18 @@ import { NotFoundPage } from './pages/NotFoundPage';
 import './styles/index.scss';
 
 export const Root = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<HomePage />} />
-        <Route path="home" element={<Navigate to="/" replace />} />
-        <Route path="phones" element={<PhonePage />} />
-        <Route path="tablets" element={<TabletPage />} />
-        <Route path="accessories" element={<AccessoriesPage />} />
-        <Route path="favorites" element={<FavoritesPage />} />
-        <Route path="cart" element={<CartPage />} />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  </Router>
+<Router>
+  <Routes>
+    <Route path="/" element={<App />}>
+      <Route index element={<HomePage />} />
+      <Route path="home" element={<Navigate to="/" replace />} />
+      <Route path="phones" element={<PhonePage />} />
+      <Route path="tablets" element={<TabletPage />} />
+      <Route path="accessories" element={<AccessoriesPage />} />
+      <Route path="favorites" element={<FavoritesPage />} />
+      <Route path="cart" element={<CartPage />} />
+    </Route>
+    <Route path="*" element={<NotFoundPage />} />
+  </Routes>
+</Router>
 );
