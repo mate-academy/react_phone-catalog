05216be44c11c4e9b040fb164db7063e446c6e22
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2024-10-21 11:28:22 +0200

    check img

--- src/components/Favourites/Favourites.tsx
+++ src/components/Favourites/Favourites.tsx
@@ -38,11 +38,14 @@ export const Favourites: React.FC<Props> = ({ models }) => {
             <div className={styles.favourites__main}>
               <div className={styles.favourites__breadcrumbs}>
                 <Link className={styles['favourites__breadcrumbs-link']} to="/">
-                  <img src={`${BASE_URL}/src/img/products/home.svg`} alt="home" />
+                  <img
+                    src={`../public/img/products/home.svg`}
+                    alt="home"
+                  />
                 </Link>
                 <img
                   className={styles['favourites__breadcrumbs-link']}
-                  src={`${BASE_URL}/src/img/products/arrow.svg`}
+                  src={`..public/img/products/arrow.svg`}
                   alt="arrow"
                 />
                 <Link
