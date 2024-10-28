459fea039b17d07b61f3b6eb51a9a6e07bb79713
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-10-28 05:03:49 +0300

    fix hero slider imgs path

--- src/constants/imagesHero.ts
+++ src/constants/imagesHero.ts
@@ -1,17 +1,17 @@
 export const imagesHero = [
   {
     id: 'iPad Air',
-    srcSmall: './src/assets/images/ipad_air.png',
-    srcLarge: './src/assets/images/ipad_air_large.png',
+    srcSmall: './src/assets/images/ipad-air.png',
+    srcLarge: './src/assets/images/ipad-air-large.png',
   },
   {
     id: 'iPhone 16 Pro Max',
-    srcSmall: './src/assets/images/iPhone_16_Pro_Max.png',
-    srcLarge: 'src/assets/images/iPhone_16_Pro_Max_large.png',
+    srcSmall: './src/assets/images/iphone-16-pro-max.png',
+    srcLarge: './src/assets/images/iphone-16-pro-max-large.png',
   },
   {
     id: 'Apple watch 10',
-    srcSmall: './src/assets/images/apple_watch.png',
-    srcLarge: './src/assets/images/apple_watch_large.png',
+    srcSmall: './src/assets/images/apple-watch.png',
+    srcLarge: './src/assets/images/apple-watch-large.png',
   },
 ];
