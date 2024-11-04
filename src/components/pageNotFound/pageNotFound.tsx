e8bc0fd542a244a8f749c3d7366b3a93fadc564d
vanvalera <115824927+vanvalera@users.noreply.github.com>
2024-11-04 22:48:09 +0200

    123

--- src/components/pageNotFound/pageNotFound.tsx
+++ src/components/pageNotFound/pageNotFound.tsx
@@ -5,7 +5,7 @@ import styles from './pageNotFound.module.scss';
 export const NotFound: React.FC = () => {
   return (
     <div className={styles.page}>
-      <h2 className={styles.page_title}>404: Page Not Found</h2>;
+      <h2 className={styles.page_title}>404: Page Not Found</h2>
       <img src={image} alt="page not found" className={styles.page_img} />
     </div>
   );
--- src/components/productNotFound/productNotFound.tsx
+++ src/components/productNotFound/productNotFound.tsx
@@ -5,7 +5,7 @@ import styles from './productNotFound.module.scss';
 export const ProductNotFound: React.FC = () => {
   return (
     <div className={styles.page}>
-      <h2 className={styles.page_title}>Product Not Found</h2>;
+      <h2 className={styles.page_title}>Product Not Found</h2>
       <img src={image} alt="page not found" className={styles.page_img} />
     </div>
   );
