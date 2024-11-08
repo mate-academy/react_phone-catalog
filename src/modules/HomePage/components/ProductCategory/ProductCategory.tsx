eb326852250abbb2459d550a616a38d1b7f704db
Jane Shavrukova <eva.shavrukova@gmail.com>
2024-11-07 19:15:55 -0800

    Add category section on Home page

--- src/modules/HomePage/components/ProductCategory/ProductCategory.tsx
+++ src/modules/HomePage/components/ProductCategory/ProductCategory.tsx
@@ -1,9 +1,33 @@
 import { Product } from 'modules/shared/types/Product';
+import styles from './ProductCategory.module.scss';
+import { Link } from 'react-router-dom';
 
 type Props = {
   products: Product[];
+  image: string;
+  title: string;
+  link: string;
 };
 
-export const ProductCategory: React.FC<Props> = ({ products }) => {
-  return <div>`Category ${products.length}`</div>;
+export const ProductCategory: React.FC<Props> = ({
+  products,
+  image,
+  title,
+  link,
+}) => {
+  return (
+    <Link className={styles.link} to={link}>
+      <div className={styles.categoryContainer}>
+        <div className={styles.imageWrapper}>
+          <img className={styles.image} src={image} alt={title} />
+        </div>
+        <div className={styles.categoryInfo}>
+          <span className={styles.categoryName}>{title}</span>
+          <span className={styles.categoryAmount}>
+            ${products.length} models
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
 };
--- src/modules/HomePage/components/ProductsCategory/ProductsCategory.tsx
+++ src/modules/HomePage/components/ProductsCategory/ProductsCategory.tsx
@@ -12,10 +12,25 @@ export const ProductsCategory = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Shop by category</h2>
-      <div className={styles.productContainer}>
-        <ProductCategory products={data.phones || []} />
-        <ProductCategory products={data.tablets || []} />
-        <ProductCategory products={data.accessories || []} />
+      <div className={styles.productsContainer}>
+        <ProductCategory
+          products={data.phones || []}
+          image="img/category-phones.png"
+          title="Mobile phones"
+          link="/phones"
+        />
+        <ProductCategory
+          products={data.tablets || []}
+          image="img/category-tablets.png"
+          title="Tablets"
+          link="/tablets"
+        />
+        <ProductCategory
+          products={data.accessories || []}
+          image="img/category-accessories.png"
+          title="Accessories"
+          link="/accessories"
+        />
       </div>
     </div>
   );
