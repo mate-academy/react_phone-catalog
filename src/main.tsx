b57df5c5dbaf704ae6e7840c76cfba0e7f1ac7e8
Marina Avramenko <marina.avramenko123@gmail.com>
2024-11-09 22:10:53 +0200

    add task solution

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { store } from './app/store';
 import { App } from './App';
@@ -9,9 +9,9 @@ import { App } from './App';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 );
