57793862742b231a50795a5f792ac692c20cc803
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-11-18 03:26:14 +0300

    add more padding to emty cart/favorites

--- src/modules/FavoritesPage/FavoritesPage.tsx
+++ src/modules/FavoritesPage/FavoritesPage.tsx
@@ -4,18 +4,23 @@ import { useContext } from 'react';
 import { FavouriteContext } from '../../ContextProvider';
 import { ProductCard } from '../../components/ProductCard';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
 
 export const FavoritesPage = () => {
   const { favouriteProducts } = useContext(FavouriteContext);
+
+  const numOfProductsTitle = !favouriteProducts.length
+    ? 'No products'
+    : favouriteProducts.length === 1
+      ? '1 model'
+      : `${favouriteProducts.length} models`;
 
   return (
     <section className={styles.favoritesPageWrapper}>
       <Breadcrumbs productList={favouriteProducts} />
       <div className={styles.favoritesPageContainer}>
         <h1 className={styles.title}>Favorites</h1>
-        <p className={styles.categoryNumModels}>
-          {favouriteProducts.length} models
-        </p>
+        <p className={styles.categoryNumModels}>{numOfProductsTitle}</p>
         <div className={styles.mainPartContainer}>
           {favouriteProducts.length ? (
             <div className={styles.productsContainer}>
@@ -29,7 +34,7 @@ export const FavoritesPage = () => {
                 Your favorites will appear here.
                 Start&nbsp;adding&nbsp;some&nbsp;items&nbsp;you&nbsp;like!
               </p>
-              <Link to={'/'} className="ctaBtn">
+              <Link to={'/'} className={classNames('ctaBtn', styles.ctaBtn)}>
                 Find Your Favorites
               </Link>
               <div className={styles.emptyFavImg}></div>
