53439f392c92b72b8f1b071ef652be365df32e0b
Ann Busyhina <070196ann@gmail.com>
2024-11-26 14:03:51 +0200

    added slider

---
+++ src/components/MainSlider/MainSlider.tsx
@@ -0,0 +1,71 @@
+import slider1_mobile from '../../assets/img/slider/slider-1-mobile.png';
+import slider1_tablet from '../../assets/img/slider/slider-1-tablet.png';
+import slider2 from '../../assets/img/slider/slider-2.png';
+import slider3 from '../../assets/img/slider/slider-3.png';
+import style from './MainSlider.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Pagination, Autoplay, Navigation, Thumbs } from 'swiper/modules';
+import 'swiper/scss';
+import 'swiper/scss/pagination';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+export const MainSlider = () => {
+  const [isActiveNavigation, setIsActiveNavigation] = useState(false);
+
+  useEffect(() => {
+    const updateSwiperSettings = () => {
+      if (window.innerWidth > 639) {
+        setIsActiveNavigation(true);
+      } else {
+        setIsActiveNavigation(false);
+      }
+    };
+
+    updateSwiperSettings();
+    window.addEventListener('resize', updateSwiperSettings);
+
+    return () => window.removeEventListener('resize', updateSwiperSettings);
+  }, []);
+
+  return (
+    <Swiper
+      modules={[Pagination, Autoplay, Navigation, Thumbs]}
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 5000 }}
+      navigation={
+        isActiveNavigation && {
+          nextEl: `.${style.swiperButtonNext}`,
+          prevEl: `.${style.swiperButtonPrev}`,
+        }
+      }
+      loop={true}
+      centeredSlides={true}
+      spaceBetween={20}
+    >
+      <SwiperSlide>
+        <img
+          src={isActiveNavigation ? slider1_tablet : slider1_mobile}
+          className={style.img}
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className={style.img__container}>
+          <img src={slider2} className={style.img} />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slider3} className={style.img} />
+      </SwiperSlide>
+      <div slot="container-end" className={style.paginationContainer} />
+
+      {isActiveNavigation && (
+        <>
+          <div className={`${style.swiperButtonNext} swiper-button-next`} />
+          <div className={`${style.swiperButtonPrev} swiper-button-prev`} />
+        </>
+      )}
+    </Swiper>
+  );
+};
--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,10 +1,13 @@
+import { MainSlider } from '../../components/MainSlider/MainSlider';
 import style from './HomePage.module.scss';
 
 export const HomePage = () => (
   <div className={style.homePage}>
-    <div className={style.header}>
+    <div className={style.homePage__header}>
       <h1 style={{ visibility: 'hidden' }}>Product Catalog</h1>
-      <h2 className={style.title}>Welcome to Nice Gadgets store!</h2>
+      <h2 className={style.homePage__title}>Welcome to Nice Gadgets store!</h2>
+
+      <MainSlider />
     </div>
   </div>
 );
