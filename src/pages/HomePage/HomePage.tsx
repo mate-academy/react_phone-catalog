d029debb542bc1a039f234cddcfac36127bb642e
Andrii <kalenskuja@gmail.com>
2024-12-04 12:58:57 +0100

    Add models section on home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,28 +1,53 @@
+import { ProductCard } from '../../components/ProductCard';
 import './HomePage.scss';
 
 export const HomePage = () => {
   return (
     <div className="home-page">
-      <h1>Welcome to Nice Gadgets store!</h1>
-      <div className="home-page__banner">
-        <div className="home-page__banner-container">
-          <div className="home-page__banner-button">
-            <img src="/icons/arrow_left.svg" alt="Arrow left" />
+      <h1 className="home-page__title">Welcome to Nice Gadgets store!</h1>
+
+      <main className="home-page__container">
+        <section className="home-page__banner">
+          <div className="home-page__banner-container">
+            <div className="button--arrow">
+              <img src="/icons/arrow_left.svg" alt="Arrow left" />
+            </div>
+
+            <img src="/home_banner.png" alt="Banner" />
+
+            <div className="button--arrow">
+              <img src="/icons/arrow_right.svg" alt="Arrow right" />
+            </div>
           </div>
 
-          <img src="/home_banner.png" alt="Banner" />
-
-          <div className="home-page__banner-button">
-            <img src="/icons/arrow_right.svg" alt="Arrow right" />
+          <div className="home-page__banner-pages">
+            <div className="home-page__banner-page"></div>
+            <div className="home-page__banner-page"></div>
+            <div className="home-page__banner-page"></div>
           </div>
-        </div>
+        </section>
 
-        <div className="home-page__banner-pages">
-          <div className="home-page__banner-page"></div>
-          <div className="home-page__banner-page"></div>
-          <div className="home-page__banner-page"></div>
-        </div>
-      </div>
+        <section className="home-page__models home-page__section">
+          <div className="home-page__section-top">
+            <h2 className="home-page__section-title">Brand new models</h2>
+
+            <div className="home-page__models-buttons">
+              <div className="button--arrow">
+                <img src="/icons/arrow_left.svg" alt="Arrow left" />
+              </div>
+              <div className="button--arrow">
+                <img src="/icons/arrow_right.svg" alt="Arrow right" />
+              </div>
+            </div>
+          </div>
+
+          <div className="home-page__models-container">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
