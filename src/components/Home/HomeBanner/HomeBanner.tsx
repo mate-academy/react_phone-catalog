786ae337b839122920b9f2ba8cfbf5ccc7b5bcd7
Rusłan Karaniewski <karane88vka@gmail.com>
2024-12-08 12:34:42 +0100

    Home done and almoust full page

--- src/components/Home/HomeBanner/HomeBanner.tsx
+++ src/components/Home/HomeBanner/HomeBanner.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import { useSwipeable } from "react-swipeable";
 import ArrowLeft from "../../../assets/icons/ArrowLeft";
--- src/components/Product/Product.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const Product = (props: Props) => {
-  return <div>Product</div>;
-};
-
-export default Product;
