aea88a99dc55d1473a6897247d634fa6d290ee42
Andrii <kalenskuja@gmail.com>
2024-12-12 15:05:33 +0100

    Implement favourites page layout

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { App } from './App';
 import { HomePage } from './pages/HomePage';
 import { Catalog } from './pages/Catalog';
 import { Product } from './pages/Product';
+import { Favourites } from './pages/Favourites/Favourites';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Route index element={<HomePage />} />
         <Route path="catalog" element={<Catalog />} />
         <Route path="product" element={<Product />} />
+        <Route path="favourites" element={<Favourites />} />
       </Route>
     </Routes>
   </BrowserRouter>,
---
+++ src/pages/Favourites/Favourites.tsx
@@ -0,0 +1,23 @@
+import { Breadcrumbs } from '../../components/Breadcrumbs';
+import { ProductCard } from '../../components/ProductCard';
+import './Favourites.scss';
+
+export const Favourites = () => {
+  return (
+    <div className="favourites">
+      <Breadcrumbs paths={['Favourites']} />
+
+      <h1 className="favourites__title">Favourites</h1>
+      <p className="favourites__subtitle body-text">5 items</p>
+
+      <div className="favourites__container">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </div>
+  );
+};
