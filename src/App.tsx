199728c2621536b10597145ba68728df2939d700
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-12-14 17:20:47 +0200

    add app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  HomePage,
+  ProductsPage,
+  ProductDetailsPage,
+  FavouritesPage,
+  CartPage,
+  NotFoundPage,
+} from './pages';
+import { Layout } from './components';
+import { AppRoute } from './enums';
 import './App.scss';
+import { Preview } from './_preview/Preview';
 
 export const App = () => (
-  <div className="App">
-    <h1>Product Catalog</h1>
-  </div>
+  <Router>
+    <h1 className="visually-hidden">Product Catalog</h1>
+    <Routes>
+      <Route path={AppRoute.HOME} element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path={AppRoute.CATEGORY} element={<ProductsPage />} />
+        <Route
+          path={AppRoute.PRODUCT_DETAILS}
+          element={<ProductDetailsPage />}
+        />
+        <Route path={AppRoute.CART} element={<CartPage />} />
+        <Route path={AppRoute.FAVOURITES} element={<FavouritesPage />} />
+        <Route path={AppRoute.NOT_FOUND} element={<NotFoundPage />} />
+        <Route path="/preview" element={<Preview />} />
+      </Route>
+    </Routes>
+  </Router>
 );
