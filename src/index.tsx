6f4303aecfa6b797f8f27ea49395599ad4712d28
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-12-14 17:55:26 +0200

    update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { StateProvider } from './state/provider';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StateProvider>
+    <App />
+  </StateProvider>,
+);
