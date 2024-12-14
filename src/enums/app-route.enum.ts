29004597bafbc5fa79c3052fb9f8254d5b9e5f22
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-12-14 17:35:10 +0200

    add enums

---
+++ src/enums/app-route.enum.ts
@@ -0,0 +1,11 @@
+export enum AppRoute {
+  HOME = '/',
+  CATEGORY = '/:category',
+  PRODUCT_DETAILS = '/:category/:productId',
+  PHONES = '/phones',
+  TABLETS = '/tablets',
+  ACCESSORIES = '/accessories',
+  CART = '/cart',
+  FAVOURITES = '/favourites',
+  NOT_FOUND = '*',
+}
---
+++ src/enums/index.ts
@@ -0,0 +1,7 @@
+export { ActionTypes } from './action-types.enum';
+export { Categories } from './category.enum';
+export { Pages } from './pages.enum';
+export { AppRoute } from './app-route.enum';
+export { SortOptions } from './sort-options.enum';
+export { PerPageOptions } from './per-page-options.enum';
+export { ApiRoutes } from './api-routes.enum';
