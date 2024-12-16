d15ba6cac32dc05d2a6495c78e813f71a4240ec7
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-12-16 16:27:57 +0200

    update not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,21 +1,14 @@
-export const NotFoundPage = () => {
+import React from 'react';
+import './NotFoundPage.scss';
+
+export const NotFoundPage: React.FC = () => {
   return (
-    <div
-      className="page-not-found"
-      style={{
-        marginTop: '40px',
-        height: '60vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <h2>Page not found</h2>
+    <div className="page-not-found">
+      <h2 className="typography__h2">Page not found</h2>
       <img
         src="/img/page-not-found.png"
         alt="Page Not Found"
-        style={{ height: '100%', width: '100%', objectFit: 'contain' }}
+        className="page-not-found__image"
       />
     </div>
   );
