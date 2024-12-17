e7e62a2220c954f6a4708b8099dd02eba3e50ed7
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-12-17 19:15:10 +0200

    update new models component

--- src/pages/HomePage/components/NewModels/NewModels.tsx
+++ src/pages/HomePage/components/NewModels/NewModels.tsx
@@ -12,12 +12,18 @@ export const NewModels: React.FC<Props> = ({ className }) => {
   const { state } = useStateContext();
   const sortByYear = (a: Product, b: Product) => b.year - a.year;
 
+  const latestYear = Math.max(...state.products.map(product => product.year));
+
+  const latestProducts = state.products.filter(
+    product => product.year === latestYear,
+  );
+
   return (
     <section className={classNames(className, 'new-models')}>
       <ProductsSlider
         title="Brand New Models"
         sortFunction={sortByYear}
-        slides={state.products}
+        slides={latestProducts}
       />
     </section>
   );
