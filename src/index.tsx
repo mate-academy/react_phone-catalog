9e0a0b065797d5e21cf2c41cb70a72628e442039
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-12-19 11:26:25 +0100

    piwik

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 import { HashRouter as Router } from 'react-router-dom';
 import { AppContext } from './appContext/AppContext';
+import PiwikPro from '@piwikpro/react-piwik-pro';
+
+PiwikPro.initialize('f4be222a-cf5f-4dcb-bf11-34b62c9f571a', 'https://patryk.piwik.pro');
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <Router>
-    <AppContext>
-      <App />
-    </AppContext>
-  </Router>,
+  <React.StrictMode>
+    <Router>
+      <AppContext>
+        <App />
+      </AppContext>
+    </Router>
+  </React.StrictMode>,
 );
