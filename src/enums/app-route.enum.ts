df4f6ebc470d8d13f3c81bcf073e289f76961991
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-12-20 15:56:33 +0200

    update app routes enum

--- src/enums/app-route.enum.ts
+++ src/enums/app-route.enum.ts
@@ -1,11 +1,12 @@
-export enum AppRoute {
-  HOME = '/',
-  CATEGORY = '/:category',
-  PRODUCT_DETAILS = '/:category/:productId',
-  PHONES = '/phones',
-  TABLETS = '/tablets',
-  ACCESSORIES = '/accessories',
-  CART = '/cart',
-  FAVOURITES = '/favourites',
-  NOT_FOUND = '*',
-}
+export const AppRoute = {
+  HOME: '/',
+  CATEGORY: '/:category',
+  PRODUCT_DETAILS: (category = ':category', productId = ':productId'): string =>
+    `/${category}/${productId}`,
+  PHONES: '/phones',
+  TABLETS: '/tablets',
+  ACCESSORIES: '/accessories',
+  CART: '/cart',
+  FAVOURITES: '/favourites',
+  NOT_FOUND: '*',
+} as const;
