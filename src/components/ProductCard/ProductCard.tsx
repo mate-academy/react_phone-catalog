856961ace3ca63f310e1aa51d5647e7723a6b248
Andrii <kalenskuja@gmail.com>
2024-12-29 10:52:19 +0100

    Remove unused useEffect import

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import { ProductType } from '../../types/ProductType';
