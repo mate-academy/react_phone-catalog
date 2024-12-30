ab8f3142b96387020f7822db49dadafd4fc2febf
Ann Busyhina <070196ann@gmail.com>
2024-12-30 22:26:20 +0200

    added favoutitesPage

--- src/modules/FavouritesPage/FavouritesPage.tsx
+++ src/modules/FavouritesPage/FavouritesPage.tsx
@@ -1,3 +1,40 @@
+import { useAppDispatch, useAppSelector } from '../../app/hook';
+import { Breadcrumbs } from '../../components/Breadcrumbs';
+import { ProductCart } from '../../components/ProductCart';
+import { init } from '../../features/products';
+import style from './FavouritesPage.module.scss';
+import { useEffect } from 'react';
+
 export const FavouritesPage = () => {
-  return <h1>Favourites Page</h1>;
+  const dispatch = useAppDispatch();
+  const favourites = useAppSelector(state => state.favourites.products);
+  const favouritesCount = favourites.length;
+  const itemText = favouritesCount === 1 ? 'item' : 'items';
+
+  const products = useAppSelector(state => state.products.items);
+
+  const favouritesProducts = products.filter(product =>
+    favourites.includes(product.itemId),
+  );
+
+  useEffect(() => {
+    if (products.length === 0) {
+      dispatch(init());
+    }
+  }, [dispatch, products]);
+
+  return (
+    <div className={style.favourites}>
+      <Breadcrumbs />
+      <div className={style.favourites__title}>Favourites</div>
+      <div className={style.favourites__countItems}>
+        {favouritesCount} {itemText}
+      </div>
+      <div className={style.favourites__cards}>
+        {favouritesProducts.map(product => (
+          <ProductCart key={product.itemId} product={product} discount={true} />
+        ))}
+      </div>
+    </div>
+  );
 };
