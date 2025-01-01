a6ea381cf34e1116e88f291ad4cf407737b4f33d
an-marryKyslenko <olih.a@icloud.com>
2025-01-01 16:37:26 +0100

    delete icon fonts

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { App } from './App';
 import { HashRouter } from 'react-router-dom';
 
 import './modules/shared/styles/null.scss';
-import './modules/shared/styles/fonts/icons.scss';
 import { ProductsProvider } from './modules/shared/context/productsContext';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
