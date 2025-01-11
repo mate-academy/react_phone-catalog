b45e9ae76a6c229825a8c211552e595fcc6ef884
NazarHaida <nazar.haida.knm.2020@lpnu.ua>
2025-01-11 22:30:11 +0200

    add task solution

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react_phone-catalog/', // Set the repository name as the base path
+  resolve: {
+    alias: {
+      'react-redux': require.resolve('react-redux'), // Explicitly resolve react-redux if needed
+    },
+  },
 })
