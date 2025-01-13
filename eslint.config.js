9ece1d551935c7243d94a29e5c610d8fffd8bb42
Iryna Mariiko <IrynaMM497@gmail.com>
2025-01-13 19:22:55 +0200

    fixed the project setting

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,37 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import prettier from "eslint-config-prettier";
-import react from "eslint-plugin-react";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      prettier, // This is a config, so it goes under "extends"
-    ],
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      react, // We register the "eslint-plugin-react" plugin
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      ...react.configs.recommended.rules, // This way we use recommended set of rules from "eslint-plugin-react" plugin
-
-      "react/react-in-jsx-scope": "off", // This rule is included in "recommended", but since React 17 it is not needed!
-
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      ...react.configs.recommended.rules,
     },
   },
-);
+  {
+    ignores: [
+      '**/.eslintrc.cjs',
+      '**/.stylelintrc.js',
+      'cypress/*',
+      '**/cypress.config.ts',
+    ],
+  },
+];
