7b744f86d6822ee02954cd6dc9302818c28ed4bf
Anna Agerone <olesnevych12@gmail.com>
2025-01-18 06:36:34 -0600

    update category link

--- src/services/getProductDetails.ts
+++ src/services/getProductDetails.ts
@@ -4,5 +4,5 @@ import { ProductDetails } from '../types/ProductDetails';
 export function getProductsDetails(
   category: string,
 ): Promise<ProductDetails[]> {
-  return getData<ProductDetails[]>(`${category}.json`);
+  return getData<ProductDetails[]>(`/${category}.json`);
 }
