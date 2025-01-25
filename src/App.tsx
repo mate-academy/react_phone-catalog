80874689d3eed464e1fc06a353e2e327f9141862
Vitor <vitorhugovieirabraga@gmail.com>
2025-01-25 19:42:18 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 
 import './assets/scss/variables.scss';
 import './App.scss';
