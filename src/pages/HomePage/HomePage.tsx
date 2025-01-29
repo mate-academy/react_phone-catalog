fd050f5772e27c252f1d6fd584008f25e1c174ca
Andrii <kalenskuja@gmail.com>
2025-01-29 10:38:20 +0100

    Add Footer links hove effect, implement 3d image hover animation for categories

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { getProducts } from '../../api/api';
 import { ProductSlider } from '../../components/ProductSlider';
@@ -46,9 +46,52 @@ const CATEGORIES = [
   },
 ];
 
+const normalize = (number: number) => {
+  return Math.min(Math.max(number, -1), 1);
+};
+
+const calculateBrightness = (x: number, y: number) => {
+  const normalizedX = normalize(x);
+  const normalizedY = normalize(y);
+
+  const baseBrightness = 1;
+  const brightnessRange = 0.4;
+
+  const brightness =
+    baseBrightness + (-normalizedY * 0.5 + normalizedX * 0.2) * brightnessRange;
+
+  return Math.min(Math.max(brightness, 0.6), 1.05);
+};
+
+const calculateImageStyles = (
+  image: HTMLImageElement,
+  mouseX: number,
+  mouseY: number,
+) => {
+  const category = image.parentElement;
+
+  if (!category) {
+    return;
+  }
+
+  const categoryRect = category.getBoundingClientRect();
+
+  const middleX = (categoryRect.left + categoryRect.right) / 2;
+  const middleY = (categoryRect.top + categoryRect.bottom) / 2;
+
+  const posX = normalize((mouseX - middleX) / 150);
+  const posY = -normalize((mouseY - middleY) / 150);
+
+  const targetRotation = `${posY} ${posX} 0 ${Math.max(Math.abs(posX), Math.abs(posY)) * 10}deg`;
+  const targetBrightness = `brightness(${calculateBrightness(posX, -posY)})`;
+
+  return { rotation: targetRotation, brightness: targetBrightness };
+};
+
 export const HomePage = () => {
   const [newestProducts, setNewestProducts] = useState<ProductType[]>([]);
   const [hotProducts, setHotProducts] = useState<ProductType[]>([]);
+  const imageRef = useRef<HTMLImageElement>();
 
   const fetchProducts = async () => {
     const newestProducts = await getProducts();
@@ -56,6 +99,106 @@ export const HomePage = () => {
 
     setNewestProducts(newestProducts.products);
     setHotProducts(hotProducts.products);
+  };
+
+  const handleMouseEnter = (
+    event: React.MouseEvent<HTMLImageElement, MouseEvent>,
+  ) => {
+    imageRef.current = event.currentTarget;
+
+    if (!event.currentTarget) {
+      return;
+    }
+
+    const styles = calculateImageStyles(
+      event.currentTarget,
+      event.clientX,
+      event.clientY,
+    );
+
+    if (!styles) {
+      return;
+    }
+
+    const imageKeyframes: Partial<CSSStyleDeclaration>[] = [
+      {
+        rotate: 'none',
+        filter: 'none',
+      },
+      {
+        rotate: styles.rotation,
+        filter: styles.brightness,
+      },
+    ];
+
+    const imageTiming: KeyframeAnimationOptions = {
+      duration: 50,
+      iterations: 1,
+      easing: 'ease-in-out',
+    };
+
+    event.currentTarget.animate(imageKeyframes as Keyframe[], imageTiming);
+  };
+
+  const handleMouseMove = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    const image = imageRef.current;
+
+    if (!image) {
+      return;
+    }
+
+    const styles = calculateImageStyles(image, event.clientX, event.clientY);
+
+    if (!styles) {
+      return;
+    }
+
+    image.style.rotate = styles.rotation;
+    image.style.filter = styles.brightness;
+  };
+
+  const handleMouseLeave = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    const image = event.currentTarget.querySelector('img');
+
+    if (!image) {
+      return;
+    }
+
+    const imageKeyframes: Partial<CSSStyleDeclaration>[] = [
+      {
+        rotate: image.style.rotate,
+        filter: image.style.filter,
+      },
+      {
+        rotate: 'none',
+        filter: 'none',
+      },
+    ];
+
+    const imageTiming: KeyframeAnimationOptions = {
+      duration: 300,
+      iterations: 1,
+      easing: 'ease-in-out',
+    };
+
+    const animation = image.animate(imageKeyframes as Keyframe[], imageTiming);
+
+    imageRef.current = undefined;
+
+    animation.addEventListener('finish', (ev: AnimationPlaybackEvent) => {
+      const effect = (ev.currentTarget as Animation).effect as KeyframeEffect;
+
+      const image = effect?.target as HTMLImageElement;
+
+      if (image) {
+        image.style.rotate = 'none';
+        image.style.filter = 'none';
+      }
+    });
   };
 
   useEffect(() => {
@@ -83,10 +226,13 @@ export const HomePage = () => {
               <Link
                 key={category.title}
                 to={category.link}
+                onMouseMove={handleMouseMove}
+                onMouseLeave={handleMouseLeave}
                 className="home-page__categories-category"
               >
                 <img
                   className="home-page__categories-category-photo"
+                  onMouseEnter={handleMouseEnter}
                   src={category.image}
                   alt="Phones category photo"
                 />
