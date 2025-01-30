b5b04b8f0e484e98900a9324b0c31991fdb7289e
Viktoriia <vkkishko@gmail.com>
2025-01-30 13:30:17 +0200

    draft 2

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/src/utils/globalStyles/global.scss";`
+        additionalData: `@import "/src/utils/globalStyles/global";`
       }
     }
   },
