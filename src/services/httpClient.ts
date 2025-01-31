7d2eb3f652b393a19d875e463e0dc90329524372
unknown <daniilbarilotti@gmail.com>
2025-01-31 20:56:31 +0100

    add task solution

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://daniilbarilotti.github.io/react_phone-catalog/api';
+const BASE_URL = 'https://daniilbarilotti.github.io/react_phone-catalog/';
 
 export function getData<T>(url: string): Promise<T> {
   return fetch(BASE_URL + url).then(response => {
