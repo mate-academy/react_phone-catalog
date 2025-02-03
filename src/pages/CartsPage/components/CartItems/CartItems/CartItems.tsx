e5fd247492e9a7583fc6f90415282dcfec03a719
Iryna Mariiko <IrynaMM497@gmail.com>
2025-02-03 16:44:30 +0200

    fixid 3

--- src/pages/CartsPage/components/CartItems/CartItems/CartItems.tsx
+++ src/pages/CartsPage/components/CartItems/CartItems/CartItems.tsx
@@ -1,5 +1,5 @@
 // CartItems.tsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styles from './CartItems.module.scss';
 import { Icon } from '../../../../../components/Icon';
 import { useProductsContext } from '../../../../../hooks/savedProducts';
