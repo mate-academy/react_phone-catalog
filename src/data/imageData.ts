9f6b3d44dc199075e31a01ee8ad09c670b069cab
Diana Darienko <diana.darienko21@gmail.com>
2025-02-10 16:55:50 +0200

    commit 11

--- src/data/imageData.ts
+++ src/data/imageData.ts
@@ -1,11 +1,11 @@
 export const imagesMobile = [
-  'src/media/img/banner/mobile-banner-phones.png',
-  'src/media/img/banner/mobile-banner-accessories.png',
-  './img/banner/mobile-banner-tablets.png',
+  '/src/media/img/banner/mobile-banner-phones.png',
+  '/src/media/img/banner/mobile-banner-accessories.png',
+  '/src/media/img/banner/mobile-banner-tablets.png'
 ];
 
 export const imagesDesktop = [
-  'src/media/img/banner/banner-phones.png',
-  'src/media/img/banner/banner-accessories.png',
-  'src/media/img/banner/banner-tablets.png',
+  '/src/media/img/banner/banner-phones.png',
+  '/src/media/img/banner/banner-accessories.png',
+  '/src/media/img/banner/banner-tablets.png',
 ];
