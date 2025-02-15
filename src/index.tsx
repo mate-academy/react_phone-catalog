778cec3039de58d974050cfc14bf2dae52b3489e
dima <shymdima74@gmail.com>
2025-02-15 09:17:32 +0200

    add

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,17 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+<<<<<<< HEAD
 
 createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+=======
+import { BrowserRouter } from 'react-router-dom';
+import { ContextProvider } from './utils/contexts';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <ContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ContextProvider>,
+);
+>>>>>>> 2ed609b (add)
