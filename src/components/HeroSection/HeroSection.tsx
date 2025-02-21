f711750065d070072c0bf276268ce1e3b32ea1b8
Yuliia Shevchuk <shevchuuchkaaa@gmail.com>
2025-02-21 15:53:45 +0100

    app h1

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -4,9 +4,9 @@ import styles from './HeroSection.module.scss';
 export const HeroSection = () => {
   return (
     <div className={styles.hero}>
-      <h1 className={`${styles.title} homeTitle`}>
+      <div className={`${styles.title} homeTitle`}>
         Welcome to Nice Gadgets store!
-      </h1>
+      </div>
 
       <PicturesSlider />
     </div>
