3e3c296bd64eb6c352ccdc4a9a76d5ba6fa2c475
Andrii Tkachuk <kovklad@gmail.com>
2025-02-22 10:01:45 +0100

    add links

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ export const Logo = () => (
   <Link to="/" className="logo">
     <div className="logo__box">
       <p>Nice</p>
-      <img src="/img/icons/favicon.png" alt="img logo" className="icon__logo" />
+      <img src="img/icons/favicon.png" alt="img logo" className="icon__logo" />
     </div>
     <p>Gadgets</p>
   </Link>
