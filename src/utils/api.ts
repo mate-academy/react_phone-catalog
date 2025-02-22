6a8aef00a54dbe53465b6c53e3c38b765ce64714
Andrii Tkachuk <kovklad@gmail.com>
2025-02-22 10:24:19 +0100

    add links

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,13 @@
 export const getPhones = () => {
-  return fetch('api/phones.json').then(response => response.json());
+  return fetch('/phones.json').then(response => response.json());
 };
 
 export const getTablets = () => {
-  return fetch('api/tablets.json').then(response => response.json());
+  return fetch('/tablets.json').then(response => response.json());
 };
 
 export const getAccessories = () => {
-  return fetch('api/accessories.json').then(response => response.json());
+  return fetch('/accessories.json').then(response => response.json());
 };
 
 export const getNewModels = () => {
