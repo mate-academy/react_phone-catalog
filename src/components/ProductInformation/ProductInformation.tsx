441d13fdab85aa0930c3b0bb7a95674762691456
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-03-07 10:38:12 +0200

    ProductInformation

--- src/components/ProductInformation/ProductInformation.tsx
+++ src/components/ProductInformation/ProductInformation.tsx
@@ -11,23 +11,48 @@ import { Loader } from '../Loader/Loader';
 export const ProductInformation: React.FC = () => {
   const { productId } = useParams<{ productId: string }>(); // Отримуємо productId з URL
   const [phonesInfo, setPhonesInfo] = useState<ProductDetails[]>([]);
+  const [selectedPhone, setSelectedPhone] = useState<ProductDetails | null>(
+    null,
+  );
   const [loading, setLoading] = useState(true);
   const backNavigate = useNavigate(); // повернення на попередню сторінку
+  const [mainImage, setMainImage] = useState<string>('');
+  const [selecredColor, setSelectedColor] = useState<string | null>(null);
+  const [selectedMemory, setSelectedMemory] = useState<string | null>(null);
 
   useEffect(() => {
     setLoading(true);
     getPhones()
       .then(data => {
         setPhonesInfo(data);
+        const foundPhone = data.find(phone => phone.name === productId);
+
+        if (foundPhone) {
+          setSelectedPhone(foundPhone);
+          setMainImage(foundPhone.image[0]);
+        }
       })
       .finally(() => {
         setLoading(false);
       });
-  }, []);
+  }, [productId]);
 
-  const selectedDetailPhone = phonesInfo.find(
-    phone => phone.name === productId,
-  );
+  const handleChangeColor = (color: string) => {
+    setSelectedColor(color);
+
+    const newPhone = phonesInfo.find(
+      phone => phone.name.includes(productId!) && phone.color === color,
+    );
+
+    if (newPhone) {
+      setSelectedPhone(newPhone);
+      setMainImage(newPhone.images[0]);
+    }
+  };
+
+  const handleChangeMemory = (mamory: string) => {
+    setSelectedMemory(mamory);
+  };
 
   return (
     <main className="productInfo">
@@ -60,17 +85,66 @@ export const ProductInformation: React.FC = () => {
 
       {loading ? (
         <Loader />
-      ) : selectedDetailPhone ? (
-        <div key={selectedDetailPhone.id}>
-          <h2 className="productInfo__title">{selectedDetailPhone.name}</h2>
-          <div className="productInfo__image">
-            {selectedDetailPhone.images.map((image, index) => (
-              <img key={index} src={image} alt={`image_phone_${index}`} />
-            ))}
+      ) : selectedPhone ? (
+        <div key={selectedPhone.id}>
+          <h2 className="productInfo__title">{selectedPhone.name}</h2>
+          <div className="productInfo__wrapperPhone">
+            <div className="Image">
+              {selectedPhone.images.map((image, index) => (
+                <img
+                  className="productInfo__Image"
+                  key={index}
+                  src={image}
+                  alt={`image_phone_${index}`}
+                  onClick={() => setMainImage(image)}
+                />
+              ))}
+            </div>
+
+            <div className="Main__Image">
+              <img
+                src={mainImage}
+                className="productInfo__mainImage"
+                alt="image__main"
+              />
+            </div>
           </div>
-          <div className="productInfo__line"></div>
+
+          <div className="productInfo__wraperdeteils">
+            <div className="productInfo__colors">
+              <h3 className="productInfo__contentTitle">Available colors</h3>
+
+              {selectedPhone.colorsAvailable.map((color, i) => (
+                <button
+                  key={i}
+                  className={`productInfo__color ${selecredColor === color ? 'selected' : ''}`}
+                  onClick={() => handleChangeColor(color)}
+                >
+                  {color}
+                </button>
+              ))}
+            </div>
+            <div className="productInfo__line"></div>
+
+            <div className="productInfo__memory">
+              <h3 className="productInfo__contentTitle">Select capacity</h3>
+              {selectedPhone.capacityAvailable.map((memorys, i) => (
+                <button
+                  key={i}
+                  className={`productInfo__memorys ${selectedMemory === memorys ? 'selected' : ''}`}
+                  onClick={() => handleChangeMemory(memorys)}
+                >
+                  {memorys}
+                </button>
+              ))}
+            </div>
+            <div className="productInfo__line"></div>
+          </div>
+
           <div className="productInfo__paragraph">
-            {selectedDetailPhone.description.map((item, index) => (
+            <h3 className="productInfo__about">About</h3>
+            <div className="productInfo__line"></div>
+            {selectedPhone.description.map((item, index) => (
               <div className="productInfo__content" key={index}>
                 <h2 className="productInfo__itemTitle">{item.title}</h2>
                 {item.text.map((paragraph, i) => (
