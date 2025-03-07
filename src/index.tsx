8f5e3398f025ec330b56911903318dec00da9142
Polina Dabizha <polinkadabi29@gmail.com>
2025-03-07 18:54:26 +0200

    blabla

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
-// import { App } from './App';
 
 import 'bulma/css/bulma.css';
 import { Root } from './Root';
 import { GlobalStateProvider } from './store/GadgetsContext';
-// import '@fortawesome/fontawesome-free/css/all.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <Router>
