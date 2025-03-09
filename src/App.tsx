55f0c20c9d8df8148d93606aad94cb510c70d39c
Dmytro Popovych <dmytro.popovych.dev@gmail.com>
2025-03-09 01:27:13 +0100

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import './App.scss';
+import { Header } from './components/Header';
+import { Aside } from './components/Aside';
+import { Footer } from './components/Footer';
 
-export const App = () => (
-  <div className="App">
-    <h1>Product Catalog</h1>
-  </div>
-);
+export const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
+
+  return (
+    <div className="container">
+      <div className="app">
+        <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+        <Aside isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+
+        <div className="app__container">
+          <main className="app__main">
+            <Outlet />
+          </main>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,67 @@
+import { Link, useLocation } from 'react-router-dom';
+import styles from './Footer.module.scss';
+
+const handleClickTopButton = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
+export const Footer = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        <div className={styles['footer__logo-container']}>
+          <Link
+            to="/"
+            onClick={e => {
+              if (pathname === '/') {
+                e.preventDefault();
+                handleClickTopButton();
+              }
+            }}
+          >
+            <img
+              className={styles.footer__logo}
+              src="/img/logo.svg"
+              alt="Logo"
+            />
+          </Link>
+        </div>
+
+        <nav className={styles.footer__navigation}>
+          <ul className={styles['footer__navigation-list']}>
+            <li className={styles['footer__navigation-item']}>
+              <a
+                href="https://github.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </li>
+            <li className={styles['footer__navigation-item']}>
+              <Link to="/contacts">Contacts</Link>
+            </li>
+            <li className={styles['footer__navigation-item']}>
+              <Link to="/rights">Rights</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <button
+          type="button"
+          onClick={handleClickTopButton}
+          className={styles.footer__button}
+        >
+          <span className={styles['footer__button-text']}>Back to top</span>
+          <img
+            className={styles['footer__button-icon']}
+            src="/icons/chevron-arrow-right.svg"
+            alt="Scroll to top"
+          />
+        </button>
+      </div>
+    </footer>
+  );
+};
