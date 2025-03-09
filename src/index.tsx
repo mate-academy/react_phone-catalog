798e0246f0024a012fbcdfb7c389c31ff3629501
Dmytro Popovych <dmytro.popovych.dev@gmail.com>
2025-03-09 03:50:48 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AppRoutes } from './AppRoutes';
 import { ProductsProvider } from './contexts/ProductsContext';
 import { ItemCardProvider } from './contexts/ItemCardContext';
