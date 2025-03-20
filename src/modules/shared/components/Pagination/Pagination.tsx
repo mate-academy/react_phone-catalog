d7da1037ecd6ecc7155ebba6772c3e6e433e2ef5
Volodymyr Konoplianyi <volodymyr.konoplianyi@gmail.com>
2025-03-20 23:04:03 +0200

    Improve hover effects & smooth page scroll on pagination

--- src/modules/shared/components/Pagination/Pagination.tsx
+++ src/modules/shared/components/Pagination/Pagination.tsx
@@ -8,12 +8,19 @@ type Props = {
 };
 
 export const Pagination: React.FC<Props> = ({ pageCount, currentPage, onChange }) => {
+  const handlePageChange = (event: { selected: number }) => {
+    onChange(event.selected + 1);
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <ReactPaginate
       pageCount={pageCount}
-      onPageChange={event => {
-        onChange(event.selected + 1);
-      }}
+      onPageChange={handlePageChange}
       nextLabel=""
       previousLabel=""
       renderOnZeroPageCount={null}
