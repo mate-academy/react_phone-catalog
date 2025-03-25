9ffa1c904f7910f25f2cda13cd0050dbd9ab1e63
reelow29 <rasevychr@gmail.com>
2025-03-25 16:08:45 +0200

    fix: vite-path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
   base: '/react_phone-catalog/',
-  resolve : {
+  plugins: [react()],
+  resolve: {
     alias: {
-      '@' : path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
