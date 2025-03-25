6601322c9184514c509da9e5c274d015ce377d81
reelow29 <rasevychr@gmail.com>
2025-03-25 14:35:23 +0200

    added vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/react_phone-catalog/',
   resolve : {
     alias: {
       '@' : path.resolve(__dirname, 'src'),
