cd590b9b020df909a9e8f999d77016eec386ab73
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2025-04-01 00:05:28 +0300

    created not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,17 @@
-export const NotFoundPage = () => {
-  return <p>NotFoundPage</p>;
+import React from 'react';
+import styles from './NotFoundPage.module.scss';
+import NotFoundImg from '/img/page-not-found.png';
+
+export const NotFoundPage: React.FC = () => {
+  return (
+    <div className={styles.not_found_page}>
+      <h2 className="secondary-title">Page not Found</h2>
+
+      <img
+        className={styles.not_found_page__image}
+        src={NotFoundImg}
+        alt="404 Error"
+      />
+    </div>
+  );
 };
