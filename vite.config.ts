3a69dac64008c83e97325629416c554a7ad5556d
Mykola Melnichuk <nikolfutdeveloper@gmail.com>
2025-04-02 06:57:27 +0200

    bug fix ProductsSlider

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/react_phone-catalog/',
+  base: '/react_phone-catalog/',
   // ! uncomment to on deploy
 })
