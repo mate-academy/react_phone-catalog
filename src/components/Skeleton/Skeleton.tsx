f7708bf09d9b5c1c475b8047eb72a122976b9a4b
Mykola Melnichuk <nikolfutdeveloper@gmail.com>
2025-04-02 23:01:19 +0200

    bug fixes with paths 4

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -15,37 +15,26 @@ const Skeleton: React.FC<Props> = () => {
         foregroundColor="#ecebeb"
         preserveAspectRatio="xMidYMid meet"
       >
-
         // ! Breadcrumbs
         <rect x="3%" y="1%" rx="3" ry="3" width="10%" height="5%" />
-        
         // ! Title
         <rect x="3%" y="10%" rx="4" ry="4" width="20%" height="8%" />
-        
         // ! Models
         <rect x="3%" y="20%" rx="4" ry="4" width="8%" height="4%" />
-
         // ! Select label #1
         <rect x="3%" y="30%" rx="4" ry="4" width="6%" height="3%" />
-        
         // ! Select input #1
         <rect x="3%" y="34%" rx="4" ry="4" width="15%" height="8%" />
-
         // ! Select label #2
         <rect x="23%" y="30%" rx="4" ry="4" width="6%" height="3%" />
-        
         // ! Select input #2
         <rect x="23%" y="34%" rx="4" ry="4" width="15%" height="8%" />
-        
         // ! Card #1
         <rect x="3%" y="55%" rx="4" ry="4" width="15%" height="40%" />
-        
         // ! Card #2
         <rect x="23%" y="55%" rx="4" ry="4" width="15%" height="40%" />
-        
         // ! Card #3
         <rect x="43%" y="55%" rx="4" ry="4" width="15%" height="40%" />
-        
         // ! Card #4
         <rect x="63%" y="55%" rx="4" ry="4" width="15%" height="40%" />
       </ContentLoader>
