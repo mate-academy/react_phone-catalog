cf5b68f399382f40e6f8dc39d0ce3771547c9c9d
serbin.nick@gmail.com <serbin.nick@gmail.com>
2025-04-09 15:37:53 +0200

    test commit

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
