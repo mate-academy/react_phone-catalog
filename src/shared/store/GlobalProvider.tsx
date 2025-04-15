f659e93c40dc87ea6241876dc0adc31df5727e41
Taras Stanishevskyy <stanishevskyy98@gmail.com>
2025-04-15 19:10:27 +0300

    change GLobal Provider

--- src/shared/store/GlobalProvider.tsx
+++ src/shared/store/GlobalProvider.tsx
@@ -2,19 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { AllProducts } from '../types/AllProducts/AllProducts';
-import { Product } from '../types/Product/Product';
 
 import { getAllProducts } from '../services/apiServices';
 
-type AnyProduct = AllProducts[] | Product[];
-
 interface ProductContextType {
-  data: {
-    [key: string]: AnyProduct;
-  };
+  data: AllProducts[];
   loading: boolean;
   error: string;
-  loadProduct: (category: string) => void;
 }
 
 type Props = {
@@ -22,57 +16,40 @@ type Props = {
 };
 
 export const ProductContext = React.createContext<ProductContextType>({
-  data: {},
+  data: [],
   loading: false,
   error: '',
-  loadProduct: () => {},
 });
 
 export const GlobalProvider: React.FC<Props> = ({ children }) => {
-  const [data, setData] = useState<{ [key: string]: AnyProduct }>({});
+  const [data, setData] = useState<AllProducts[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
   const { pathname } = useLocation();
 
-  const loadProduct = async (category: string) => {
-    setLoading(true);
-    setError('');
-
-    try {
-      let products: AnyProduct = [];
-
-      if (category === '/') {
-        const res = await getAllProducts('/products.json');
-
-        products = res.data;
-        setData(prevData => ({ ...prevData, all: products }));
-      } else {
-        const res = await getAllProducts(`${category}.json`);
-
-        products = res.data;
-
-        setData(prevData => ({ ...prevData, [category]: products }));
-      }
-    } catch {
-      setError('Something get wrong');
-    } finally {
-      setLoading(false);
-    }
-  };
-
   useEffect(() => {
-    const key = pathname === '/' ? 'all' : pathname;
+    const loadProduct = async () => {
+      setLoading(true);
+      setError('');
 
-    if (!data[key]) {
-      loadProduct(pathname);
-    }
-  }, [pathname, data]);
+      try {
+        const response = await getAllProducts('/products.json');
+
+        setData(response.data);
+      } catch {
+        setError('Something get wrong');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadProduct();
+  }, [pathname]);
 
   const value = {
     data,
     loading,
     error,
-    loadProduct,
   };
 
   return (
