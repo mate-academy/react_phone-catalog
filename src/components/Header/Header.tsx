a747ab80b4a669f59707609428ab9ebe8728f7d2
Work <ongrifi@gmail.com>
2025-04-15 05:58:18 +0200

    header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,44 +6,46 @@ export const Header = () => {
       <a className={s.logo}>
         <img src="../../img/icons/logo.svg" alt="logo" />
       </a>
-      <li className={s.nav__list}>
-        <ul className={s.nav__item}>
-          <a href="#" className={s.nav__link}>
-            Home
-          </a>
-        </ul>
-        <ul className={s.nav__item}>
-          <a href="#" className={s.nav__link}>
-            Phones
-          </a>
-        </ul>
-        <ul className={s.nav__item}>
-          <a href="#" className={s.nav__link}>
-            Tablets
-          </a>
-        </ul>
-        <ul className={s.nav__item}>
-          <a href="#" className={s.nav__link}>
-            Label
-          </a>
-        </ul>
-      </li>
-      <div className={s.menu}>
-        <li className={s.controls}>
-          <ul className={s.controls__item}>
-            <a href="">
-              <img src="#" alt="" />
+      <div className={s.nav__flex}>
+        <li className={s.nav__list}>
+          <ul className={s.nav__item}>
+            <a href="#" className={s.nav__link}>
+              Home
             </a>
           </ul>
-          <ul className={s.controls__item}>
-            <a href="">
-              <img src="#" alt="" />
+          <ul className={s.nav__item}>
+            <a href="#" className={s.nav__link}>
+              Phones
+            </a>
+          </ul>
+          <ul className={s.nav__item}>
+            <a href="#" className={s.nav__link}>
+              Tablets
+            </a>
+          </ul>
+          <ul className={s.nav__item}>
+            <a href="#" className={s.nav__link}>
+              Label
             </a>
           </ul>
         </li>
-        <a href="" className={s.menu__colapsed}>
-          <img src="" alt="" />
-        </a>
+        <div className={s.menu}>
+          <li className={s.controls}>
+            <ul className={s.controls__item}>
+              <a href="">
+                <img src="../../img/icons/shoping-bag.svg" alt="" />
+              </a>
+            </ul>
+            <ul className={s.controls__item}>
+              <a href="">
+                <img src="../../img/icons/heard.svg" alt="" />
+              </a>
+            </ul>
+          </li>
+          <a href="" className={s.menu__colapsed}>
+            <img src="" alt="" />
+          </a>
+        </div>
       </div>
     </nav>
   );
