5058d43e905148062da8d6aa4ae3660cd035f136
Vitalii <v.hulaievych@gmail.com>
2025-04-18 20:35:58 +0100

    add path for i18next

--- src/i18n.ts
+++ src/i18n.ts
@@ -7,28 +7,22 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 // have a look at the Quick start guide
 // for passing in lng and translations on init
 
+const isGithubPages = window.location.hostname === 'vitalii120296.github.io';
+
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
   .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'en',
     debug: true,
-
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false, // не потрібно для React
     },
-
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      loadPath: isGithubPages
+        ? '/react_phone-catalog/locales/{{lng}}/{{ns}}.json' // для GitHub Pages
+        : '/locales/{{lng}}/{{ns}}.json', // для локального середовища
     },
   });
 
