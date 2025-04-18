4a50bd41df660e9a359dc8a864b4e1a766a5c974
David Moskalenko <dvdmsk21@gmail.com>
2025-04-18 11:18:08 +0300

    small fixed in modal dialog in checkout

--- src/modules/CartPage/components/ModalCheckout/ModalCheckout.tsx
+++ src/modules/CartPage/components/ModalCheckout/ModalCheckout.tsx
@@ -20,30 +20,33 @@ export const ModalCheckout: React.FC<Props> = ({ onClick }) => {
 
   return (
     <aside className={styles.overley} onClick={onClick}>
-      <div
-        className={styles.confirmation}
-        onClick={(event: React.MouseEvent) => {
-          event.stopPropagation();
-        }}
-      >
-        <p className={styles.confirmation__title}>
-          Checkout is not implemented yet.
-        </p>
-
-        <p className={styles.confirmation__text}>
-          Do you want to clear the Cart?
-        </p>
-
-        <div className={styles.confirmation__control}>
-          <button className={styles.confirmation__clear} onClick={handleClear}>
-            Clear
-          </button>
-          <button
-            className={styles.confirmation__cancel}
-            onClick={handleCancel}
-          >
-            Cancel
-          </button>
+      <div className={styles.overley__wrapper}>
+        <div
+          className={styles.confirmation}
+          onClick={(event: React.MouseEvent) => {
+            event.stopPropagation();
+          }}
+        >
+          <p className={styles.confirmation__title}>
+            Checkout is not implemented yet.
+          </p>
+          <p className={styles.confirmation__text}>
+            Do you want to clear the Cart?
+          </p>
+          <div className={styles.confirmation__control}>
+            <button
+              className={styles.confirmation__clear}
+              onClick={handleClear}
+            >
+              Clear
+            </button>
+            <button
+              className={styles.confirmation__cancel}
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
       </div>
     </aside>
