7112f533a4cddb5810cc41d1844cd7c313eed811
Maksym <makstarasovv1@gmail.com>
2025-04-25 12:33:34 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { GlobalProvider } from './context/GlobalContext';
 export const App = () => {
   return (
     <>
-      <h1 className="visually-hidden">Product Catalog</h1>
       <GlobalProvider>
         <AppRoutes />
+        <h1 className="visually-hidden">Product Catalog</h1>
       </GlobalProvider>
     </>
   );
