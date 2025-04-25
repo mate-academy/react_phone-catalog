5ba1392d44e1f84b0077d128594a03b8adb8d55d
André Leão <andrefelipesl123@gmail.com>
2025-04-25 20:30:40 -0300

    Deploy

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,7 @@
 // export const APP_BASE_PATH =
 //   import.meta.env.VITE_APP_BASE_PATH ||
 //   (import.meta.env.PROD ? '/react_phone-catalog/' : '/');
-export const APP_BASE_PATH =
-  import.meta.env.MODE === 'production' ? '/react_phone-catalog/' : '/';
+export const APP_BASE_PATH = '/';
 
 export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || 'http://localhost:5173/public/api/';
