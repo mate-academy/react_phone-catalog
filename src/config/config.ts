2d48ad5a0544b014f3763dd6626417b8f7bab51f
André Leão <andrefelipesl123@gmail.com>
2025-04-25 17:21:39 -0300

    Deploy

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,10 @@
-export const APP_BASE_PATH = import.meta.env.VITE_APP_BASE_PATH || '/';
+// export const APP_BASE_PATH = import.meta.env.VITE_APP_BASE_PATH || '/';
+// export const API_BASE_URL =
+//   import.meta.env.VITE_API_BASE_URL || 'http://localhost:5173/public/api/';
+export const APP_BASE_PATH = import.meta.env.VITE_APP_BASE_PATH || '';
+
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'http://localhost:5173/public/api/';
-export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || 'img';
+  import.meta.env.VITE_API_BASE_URL || `${APP_BASE_PATH}/api/`;
+
+export const IMAGE_BASE_URL =
+  import.meta.env.VITE_IMAGE_BASE_URL || `${APP_BASE_PATH}/img`;
