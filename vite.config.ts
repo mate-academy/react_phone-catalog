571b4d3dba3da464b89a1bae8d14283e8b34500c
André Leão <andrefelipesl123@gmail.com>
2025-04-25 17:55:22 -0300

    Ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'path';
 
 export default defineConfig(({ mode }) => ({
-  // base: mode === 'production' ? '/react_phone-catalog/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
