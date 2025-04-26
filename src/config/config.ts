ed43bca15331b7a8f3a0a7f59eb317742d30817a
André Leão <andrefelipesl123@gmail.com>
2025-04-26 11:37:21 -0300

    Ajuste

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 export const APP_BASE_PATH = import.meta.env.BASE_URL;
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api/';
+export const API_BASE_URL = `${APP_BASE_PATH}api/`;
