bb72d4826b370ead420a1f815ae4b3f22a2c4bd3
Olya Vidzivashets <olyavidzi97@gmail.com>
2025-04-28 16:32:15 +0300

    added changes

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { store } from "./store/store";
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <Router basename='/react_phone-catalog'>
+      <Router>
         <App />
       </Router>
     </Provider>
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,10 +1,11 @@
 import { IProductCard } from "../interfaces/ProductCard.interface";
 import { IProductDetails } from "../interfaces/ProductDetails.interface";
+export const BASE_URL = 'https://olyavidzi.github.io/react_phone-catalog/';
 
 export const ProductService = {
   async getAll() {
     try {
-      const response = await fetch('/api/products.json');
+      const response = await fetch(`${BASE_URL}/api/products.json`);
 
       if (!response.ok) {
         throw new Error('Failed to fetch products');
@@ -30,7 +31,7 @@ export const ProductService = {
 
   async getByProductIdDetails(productId: string, category: string) {
     try {
-      const response = await fetch(`/api/${category}.json`);
+      const response = await fetch(`${BASE_URL}/api/${category}.json`);
       const products: IProductDetails[] = await response.json();
 
       return products.find(product => product.id === productId);
@@ -42,7 +43,7 @@ export const ProductService = {
 
   async getByProductId(productId: string) {
     try {
-      const response = await fetch(`/api/products.json`);
+      const response = await fetch(`${BASE_URL}/api/products.json`);
       const products: IProductCard[] = await response.json();
 
       return products.find(product => product.itemId === productId);
