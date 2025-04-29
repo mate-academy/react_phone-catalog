6afbe5da8c7dbdcbf78290740aa08572e07b669f
reelow29 <rasevychr@gmail.com>
2025-04-29 13:30:43 +0300

    change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: '/react_phone-catalog/',
+  base: './',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
