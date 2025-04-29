b6e91084658d97606f8a28bc4459cea2b2172348
Taras Stanishevskyy <stanishevskyy98@gmail.com>
2025-04-29 10:52:33 +0300

    add product loader

--- src/modules/CartPage/components/CartProduct/CartProduct.tsx
+++ src/modules/CartPage/components/CartProduct/CartProduct.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+
 import styles from './CartProduct.module.scss';
+
 import { Cart } from '../../../../shared/store/CartProvider';
-import { Loader } from '../../../../shared/components/Loader';
+import { ProductLoader } from '../../../../shared/components/ProductLoader';
 
 type Props = {
   cartProduct: Cart;
@@ -18,71 +20,64 @@ export const CartProduct: React.FC<Props> = ({
 }) => {
   return (
     <div className={styles.cart__item}>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <div className={styles.cart__description}>
-            <button
-              className={styles.cart__itemDeleteBtn}
-              onClick={() => deleteProduct(cartProduct.product.id)}
-            >
-              <img
-                className={styles.cart__itemDeleteImg}
-                src="src/assets/icons/cart-icons/cart-delete-icons.svg"
-                alt="Видалити товар з корзини"
-              />
-            </button>
-            <img
-              className={styles.cart__itemImage}
-              src={cartProduct.product.image}
-              alt="Фото товару"
-            />
+      <div className={styles.cart__description}>
+        <button
+          className={styles.cart__itemDeleteBtn}
+          onClick={() => deleteProduct(cartProduct.product.id)}
+        >
+          <img
+            className={styles.cart__itemDeleteImg}
+            src="src/assets/icons/cart-icons/cart-delete-icons.svg"
+            alt="Видалити товар з корзини"
+          />
+        </button>
+        <img
+          className={styles.cart__itemImage}
+          src={cartProduct.product.image}
+          alt="Фото товару"
+        />
 
-            <p className={styles.cart__itemDescription}>
-              {cartProduct.product.name}
-            </p>
-          </div>
-          <div className={styles.cart__toPay}>
-            <div className={styles.cart__itemControl}>
-              <button
-                className={styles.cart__minusBtn}
-                onClick={() =>
-                  changeQuantity(
-                    cartProduct.product.id,
-                    cartProduct.quantity - 1,
-                  )
-                }
-                disabled={cartProduct.quantity === 1}
-              >
-                <img
-                  className={styles.cart__minusBtnImg}
-                  src={`src/assets/icons/cart-icons/${cartProduct.quantity === 1 ? 'cart-minus-icon.svg' : 'cart-minus-icon-active.svg'}`}
-                  alt=""
-                />
-              </button>
-              <p className={styles.cart__addedItems}>{cartProduct.quantity}</p>
-              <button
-                className={styles.cart__plusBtn}
-                onClick={() =>
-                  changeQuantity(
-                    cartProduct.product.id,
-                    cartProduct.quantity + 1,
-                  )
-                }
-              >
-                <img
-                  className={styles.cart__plusBtnImg}
-                  src="src/assets/icons/cart-icons/cart-plus-icon.svg"
-                  alt=""
-                />
-              </button>
-            </div>
-            <h3 className={styles.cart__price}>
-              {`$${cartProduct.quantity * cartProduct.product.price}`}
-            </h3>
-          </div>
-        </>
+        <p className={styles.cart__itemDescription}>
+          {cartProduct.product.name}
+        </p>
+      </div>
+      <div className={styles.cart__toPay}>
+        <div className={styles.cart__itemControl}>
+          <button
+            className={styles.cart__minusBtn}
+            onClick={() =>
+              changeQuantity(cartProduct.product.id, cartProduct.quantity - 1)
+            }
+            disabled={cartProduct.quantity === 1}
+          >
+            <img
+              className={styles.cart__minusBtnImg}
+              src={`src/assets/icons/cart-icons/${cartProduct.quantity === 1 ? 'cart-minus-icon.svg' : 'cart-minus-icon-active.svg'}`}
+              alt=""
+            />
+          </button>
+          <p className={styles.cart__addedItems}>{cartProduct.quantity}</p>
+          <button
+            className={styles.cart__plusBtn}
+            onClick={() =>
+              changeQuantity(cartProduct.product.id, cartProduct.quantity + 1)
+            }
+          >
+            <img
+              className={styles.cart__plusBtnImg}
+              src="src/assets/icons/cart-icons/cart-plus-icon.svg"
+              alt=""
+            />
+          </button>
+        </div>
+        <h3 className={styles.cart__price}>
+          {`$${cartProduct.quantity * cartProduct.product.price}`}
+        </h3>
+      </div>
+      {isLoading && (
+        <div className={styles.cart__laoder}>
+          <ProductLoader />
+        </div>
       )}
     </div>
   );
