ebdf49483175b61edc236c2058c4211423d08a82
Taras Stanishevskyy <stanishevskyy98@gmail.com>
2025-04-30 20:16:50 +0300

    add active btn for ProductDetails

--- src/modules/ProductDetailsPage/components/ProductOverview/components/ProductOptions/ProductOptions.tsx
+++ src/modules/ProductDetailsPage/components/ProductOverview/components/ProductOptions/ProductOptions.tsx
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import styles from './ProductOptions.module.scss';
 
 import { Product } from '../../../../../../shared/types/Product/Product';
+import {
+  FavoritesDispatchContext,
+  FavoritesStateContext,
+} from '../../../../../../shared/store/FavoritesProvider';
+import { ProductContext } from '../../../../../../shared/store/GlobalProvider';
+import {
+  CartDispatchContext,
+  CartStateContext,
+} from '../../../../../../shared/store/CartProvider';
+// eslint-disable-next-line
+import { AllProducts } from '../../../../../../shared/types/AllProducts/AllProducts';
 
 type Props = {
   product: Product;
 };
 
 export const ProductOptions: React.FC<Props> = ({ product }) => {
+  const { data } = useContext(ProductContext);
+  const favoritesProduct = useContext(FavoritesStateContext);
+  const dispatchFavorites = useContext(FavoritesDispatchContext);
+  const cartsProduct = useContext(CartStateContext);
+  const dispatchCart = useContext(CartDispatchContext);
+
+  const findProduct = data.find(el => el.itemId === product.id);
+
+  const handleFavorites = () => {
+    const isFavorites = favoritesProduct.some(
+      favorite => favorite.id === findProduct?.id,
+    );
+
+    if (isFavorites) {
+      dispatchFavorites({
+        type: 'deleteFavoritesProduct',
+        payload: findProduct?.id as number,
+      });
+    } else {
+      dispatchFavorites({ type: 'addFavoritesProduct', payload: findProduct! });
+    }
+  };
+
+  const isFavorites = favoritesProduct.some(
+    favoriteProduct => favoriteProduct.itemId === product.id,
+  );
+
+  const handleCart = () => {
+    const newCartItem = {
+      id: findProduct?.id as number,
+      quantity: 1,
+      product: findProduct as AllProducts,
+    };
+
+    dispatchCart({ type: 'addCartProduct', payload: newCartItem });
+  };
+
+  const isCart = cartsProduct.some(
+    cartsProducts => cartsProducts.product.itemId === product.id,
+  );
+
   return (
     <section className={styles.productOverview__options}>
       <div className={styles.productOverview__optionsWrapper}>
@@ -57,16 +109,23 @@ export const ProductOptions: React.FC<Props> = ({ product }) => {
         </div>
 
         <div className={styles.productOverview__addToCart}>
-          <button className={styles.productOverview__addButton}>
-            Add to Cart
+          <button
+            className={styles.productOverview__addButton}
+            onClick={() => handleCart()}
+            disabled={isCart}
+          >
+            {isCart ? 'Added to Cart' : 'Add to Cart'}
           </button>
-          <div className={styles.productOverview__iconWrapper}>
+          <button
+            className={styles.productOverview__iconWrapper}
+            onClick={handleFavorites}
+          >
             <img
               className={styles.productOverview__addIcon}
-              src="src/assets/icons/product-details/favorites-icon.svg"
+              src={`src/assets/icons/product-details/${isFavorites ? 'favorites-icon-added.svg' : 'favorites-icon.svg'}`}
               alt="Add to cart"
             />
-          </div>
+          </button>
         </div>
       </div>
 
