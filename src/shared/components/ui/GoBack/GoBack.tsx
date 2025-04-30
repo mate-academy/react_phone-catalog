f68b0c8b52aa1f91d5833ec92c1a214473d74c90
Jane Shavrukova <eva.shavrukova@gmail.com>
2025-04-29 22:21:44 -0700

    Refactor styles

--- src/shared/components/ui/GoBack/GoBack.tsx
+++ src/shared/components/ui/GoBack/GoBack.tsx
@@ -5,10 +5,16 @@ import styles from './GoBack.module.scss';
 export const GoBack: React.FC = () => {
   const navigate = useNavigate();
 
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <div className={styles.goBack} onClick={() => navigate(-1)}>
-      <img src="img/icons/arrow-back-white.svg" alt="arrow-back" />
-      <span>Back</span>
-    </div>
+    <button type="button" className={styles.goBack} onClick={handleGoBack}>
+      <div className={styles.arrow}>
+        <img src="img/icons/arrow-back-white.svg" alt="back" />
+      </div>
+      <span className={styles.text}>Back </span>
+    </button>
   );
 };
