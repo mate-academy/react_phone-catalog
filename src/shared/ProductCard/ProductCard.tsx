398fe34c686e0b62bb893ea432f6122e0a36f187
Work <ongrifi@gmail.com>
2025-05-02 03:18:46 +0200

    product card

--- src/shared/ProductCard/ProductCard.tsx
+++ src/shared/ProductCard/ProductCard.tsx
@@ -8,7 +8,32 @@ type Props = {
 export const ProductCard: React.FC<Props> = ({ product }) => {
   return (
     <div className={s.card}>
-      <h1>{product.name}</h1>
+      <div className={s.picture}>
+        <img className={s.img} src={product.image} alt="Product image" />
+      </div>
+      <span>{product.name}</span>
+
+      <span>{product.price}</span>
+
+      <div>__</div>
+
+      <div>
+        <span>Screen</span>
+        <span>{product.screen}</span>
+      </div>
+      <div>
+        <span>Capacity</span>
+        <span>{product.capacity}</span>
+      </div>
+      <div>
+        <span>Ram</span>
+        <span>{product.ram}</span>
+      </div>
+
+      <div>
+        <button>buy</button>
+        <button>like</button>
+      </div>
     </div>
   );
 };
