9986360f2eda388c260c4c82c2984afb51bb17f8
nex1994 <bilicki1994@gmail.com>
2025-05-17 21:50:38 +0200

    implemented fetching client to get data from api

--- src/modules/PhonesPage/PhonesCatalog/PhonesCatalog.tsx
+++ src/modules/PhonesPage/PhonesCatalog/PhonesCatalog.tsx
@@ -1,14 +1,24 @@
 import styles from './PhonesCatalog.module.scss';
 import { ProductCard } from '../../shared/components/ProductCard';
+import { getProduct } from '../../shared/utils/fetchClient';
+import { useEffect, useState } from 'react';
 
 export const PhoneCatalog = () => {
+  const [phones, setPhones] = useState();
+
+  const loadPhones = getProduct('phones.json');
+
+  useEffect(() => {
+    loadPhones.then(data => setPhones(data));
+  }, [loadPhones]);
+
   return (
     <>
       <div className={styles.catalog}>
         <div className={styles['catalog__bread-crumbs']}>
           <img src="public/icons/Home.svg" alt="home icon" />
           <img src="public/icons/ArrowRight.svg" alt="arrow right icon" />
-          <p className={styles['catalog__bread-crumbs--text']}>Phones</p>
+          <p className={styles['catalog__bread-crumbs--text']}>{phones}</p>
         </div>
         <ProductCard />
         <ProductCard />
