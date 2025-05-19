7d7b722b8223138e329bf153bf004c5c76723520
Mykhailo Forkosh <forkos277@gmail.com>
2025-05-19 16:39:01 +0300

    test

--- src/modules/shared/components/carousel/Carousel.tsx
+++ src/modules/shared/components/carousel/Carousel.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { Children, useEffect, useRef, useState } from 'react';
 import styles from './Carousel.module.scss';
 import { Item } from './item';
