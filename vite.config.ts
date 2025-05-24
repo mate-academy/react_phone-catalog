5b27b703845da18bf0ea921e2e2a29fa67bd7106
Marian <getmoodyup@gmail.com>
2025-05-24 08:18:17 +0200

    config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/react_phone-catalog/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
