04e7ae9861ff6481482ba8022f028b672dd58d85
Nazar <154551346+naz4ik@users.noreply.github.com>
2025-05-28 13:43:09 +0200

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -68,7 +68,7 @@ export const NavBar = () => {
                     `${styles.headerIconFav} ${isActive ? styles.activeLink : ''}`
                   }
                 >
-                  <img src="/icons/favorite.svg" alt="to favorites" className={styles.fav} />
+                  <img src="./icons/favorite.svg" alt="to favorites" className={styles.fav} />
                 </NavLink>
               </div>
               <div className={styles.iconBorder}>
@@ -78,7 +78,7 @@ export const NavBar = () => {
                     `${styles.headerIconCart} ${isActive ? styles.activeLink : ''}`
                   }
                 >
-                  <img src="/icons/cart.svg" alt="to cart" className={styles.carti} />
+                  <img src="./icons/cart.svg" alt="to cart" className={styles.carti} />
                 </NavLink>
               </div>
             </div>
@@ -131,13 +131,13 @@ export const NavBar = () => {
           <div className={styles.menuIcons}>
             <NavLink to="/cart" onClick={() => setIsSidebarOpen(false)}>
               <button className={styles.menuButtonsCart}>
-                <img src="/icons/cart.svg" alt="to cart" className={styles.menuButtonsIcons} />
+                <img src="./icons/cart.svg" alt="to cart" className={styles.menuButtonsIcons} />
               </button>
             </NavLink>
             <NavLink to="/favorites" onClick={() => setIsSidebarOpen(false)}>
               <button className={styles.menuButtonsFav}>
                 <img
-                  src="/icons/favorite.svg"
+                  src="./icons/favorite.svg"
                   alt="to favorites"
                   className={styles.menuButtonsIcons}
                 />
