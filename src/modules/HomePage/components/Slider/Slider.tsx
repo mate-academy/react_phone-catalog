7591c7926baecc93295135b25d4dac6e7fe133e2
nex1994 <bilicki1994@gmail.com>
2025-06-01 07:41:25 +0200

    implemented swipe on slider component for mobile version

--- src/modules/HomePage/components/Slider/Slider.tsx
+++ src/modules/HomePage/components/Slider/Slider.tsx
@@ -2,21 +2,59 @@ import styles from './Slider.module.scss';
 import sliderImage1 from '../../../../../public/img/banner-accessories.png';
 import sliderImage2 from '../../../../../public/img/banner-phones.png';
 import sliderImage3 from '../../../../../public/img/banner-tablets.png';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 const images = [sliderImage1, sliderImage2, sliderImage3];
 
 export const Slider = () => {
-  const [imageIndex] = useState(2);
+  const [imageIndex, setImageindex] = useState(2);
+  let swipeStartPosition = 0;
+  let swipeDistance = 0;
+
+  const previousImage = useCallback(() => {
+    setImageindex(index => {
+      if (index === 0) {
+        return images.length - 1;
+      }
+
+      return index - 1;
+    });
+  }, []);
+
+  const nextImage = useCallback(() => {
+    setImageindex(index => {
+      if (index === images.length - 1) {
+        return 0;
+      }
+
+      return index + 1;
+    });
+  }, []);
 
   return (
     <>
       <div className={styles.slider}>
-        <button className={styles.slider__buttons}>
+        <button onClick={previousImage} className={styles.slider__buttons}>
           <img src="public/icons/ArrowLeft.svg" />
         </button>
-        <img className={styles.slider__image} src={images[imageIndex]} />
-        <button className={styles.slider__buttons}>
+        <img
+          onTouchStart={e => {
+            swipeStartPosition = e.changedTouches[0].pageX;
+          }}
+          onTouchMove={e => e.preventDefault()}
+          onTouchEnd={e => {
+            swipeDistance = e.changedTouches[0].pageX - swipeStartPosition;
+            if (swipeDistance > 0) {
+              nextImage();
+            } else {
+              previousImage();
+            }
+          }}
+          loading="lazy"
+          className={styles.slider__image}
+          src={images[imageIndex]}
+        />
+        <button onClick={nextImage} className={styles.slider__buttons}>
           <img src="public/icons/ArrowRight.svg" />
         </button>
       </div>
