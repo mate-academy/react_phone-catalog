f00d4ab4cc68d7f9e6123b9131848a1bc0df8014
nex1994 <bilicki1994@gmail.com>
2025-06-01 09:34:40 +0200

    slider now works properly on mobile version

--- src/modules/HomePage/components/Slider/Slider.tsx
+++ src/modules/HomePage/components/Slider/Slider.tsx
@@ -2,7 +2,10 @@ import styles from './Slider.module.scss';
 import sliderImage1 from '../../../../../public/img/banner-accessories.png';
 import sliderImage2 from '../../../../../public/img/banner-phones.png';
 import sliderImage3 from '../../../../../public/img/banner-tablets.png';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import classNames from 'classnames/bind';
+
+const cn = classNames.bind(styles);
 
 const images = [sliderImage1, sliderImage2, sliderImage3];
 
@@ -31,32 +34,62 @@ export const Slider = () => {
     });
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => nextImage(), 5000);
+
+    return () => clearInterval(interval);
+  }, [nextImage, imageIndex]);
+
   return (
     <>
       <div className={styles.slider}>
-        <button onClick={previousImage} className={styles.slider__buttons}>
-          <img src="public/icons/ArrowLeft.svg" />
-        </button>
-        <img
-          onTouchStart={e => {
-            swipeStartPosition = e.changedTouches[0].pageX;
-          }}
-          onTouchMove={e => e.preventDefault()}
-          onTouchEnd={e => {
-            swipeDistance = e.changedTouches[0].pageX - swipeStartPosition;
-            if (swipeDistance > 0) {
-              nextImage();
-            } else {
-              previousImage();
-            }
-          }}
-          loading="lazy"
-          className={styles.slider__image}
-          src={images[imageIndex]}
-        />
-        <button onClick={nextImage} className={styles.slider__buttons}>
-          <img src="public/icons/ArrowRight.svg" />
-        </button>
+        <div className={styles.slider__container}>
+          <button onClick={previousImage} className={styles.slider__buttons}>
+            <img src="public/icons/ArrowLeft.svg" />
+          </button>
+          <div className={styles['slider__image-container']}>
+            {images.map(image => {
+              return (
+                <img
+                  key={image}
+                  onTouchStart={e => {
+                    swipeStartPosition = e.changedTouches[0].pageX;
+                  }}
+                  onTouchMove={e => e.preventDefault()}
+                  onTouchEnd={e => {
+                    swipeDistance =
+                      e.changedTouches[0].pageX - swipeStartPosition;
+                    if (swipeDistance > 0) {
+                      previousImage();
+                    } else {
+                      nextImage();
+                    }
+                  }}
+                  style={{ translate: `${-100 * imageIndex}%` }}
+                  loading="lazy"
+                  className={styles.slider__image}
+                  src={image}
+                />
+              );
+            })}
+          </div>
+          <button onClick={nextImage} className={styles.slider__buttons}>
+            <img src="public/icons/ArrowRight.svg" />
+          </button>
+        </div>
+        <div className={styles.slider__navigation}>
+          {images.map((_, i) => {
+            return (
+              <span
+                onClick={() => setImageindex(i)}
+                className={cn(styles['slider__navigation-buttons'], {
+                  'slider__navigation-buttons--is-Active': imageIndex === i,
+                })}
+                key={i}
+              ></span>
+            );
+          })}
+        </div>
       </div>
     </>
   );
