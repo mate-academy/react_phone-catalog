177ffbed59e3d1eb38cf0523d9a8430fbd3f7e31
mar kol <example@e.com>
2025-06-02 17:43:13 +0200

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react_phone-catalog/',
+ // base: '/react_phone-catalog/',
   build: {
     outDir: 'dist',
   },
   resolve: {
     alias: {
-      
+
     },
   },
 });
