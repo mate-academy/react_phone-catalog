fc96e83f8e78bba6717fcd9a5ac5d03b5147afd0
Pasha Shchukin <pasha4320692@gmail.com>
2025-06-03 20:19:45 +0300

    solution 2.1

--- src/components/data/slidesData.ts
+++ src/components/data/slidesData.ts
@@ -1,3 +1,4 @@
+import banner from '../../../public/img/Banner.mobil.svg';
 import { SlideType } from '../../types/SlideType';
 
 export const slides: SlideType[] = [
@@ -5,18 +6,18 @@ export const slides: SlideType[] = [
     id: 1,
     title: 'Now available in our store!',
     description: 'Be the first!',
-    image: '/img/Banner.mobil.svg',
+    image: banner,
   },
   {
     id: 2,
     title: 'Grab this week’s hot discounts!',
     description: 'Don’t miss out!',
-    image: '/img/Banner.mobil.svg',
+    image: banner,
   },
   {
     id: 3,
     title: 'Discover brand‑new tech today!',
     description: 'Explore our collection.',
-    image: '/img/Banner.mobil.svg',
+    image: banner,
   },
 ];
