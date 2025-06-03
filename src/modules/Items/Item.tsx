2b63411785ec99a912d884cb363c3be63941a9ee
Work <ongrifi@gmail.com>
2025-06-03 03:59:35 +0200

    item v02

--- src/modules/Items/Item.tsx
+++ src/modules/Items/Item.tsx
@@ -1,9 +1,15 @@
+import { useSearchParams } from 'react-router-dom';
 import s from './Item.module.scss';
+import phones from '../../../public/api/phones.json';
 
 export const Item = () => {
+  const [searchParams] = useSearchParams();
+  const productId = searchParams.get('productId');
+  const product = phones.find(phone => phone.id === productId);
+
   return (
     <div className={s.item}>
-      <p>fdjals;fj;sla</p>
+      <p>{product?.category}</p>
     </div>
   );
 };
