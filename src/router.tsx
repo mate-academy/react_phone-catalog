63057aa4459692d22a1f4d90d41e5211e97232b3
daradanilova <lemma1102@gmail.com>
2025-06-25 14:06:34 +0300

    add task solution

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { HomePage } from './modules/HomePage';
 import { CartPage } from './modules/CartPage';
@@ -7,7 +7,7 @@ import { ProductDetailsPage } from './modules/ProductDetailsPage';
 import { NotFoundPage } from './modules/NotFoundPage';
 import { CategoryPage } from './modules/CategoryPage';
 
-export const router = createBrowserRouter(
+export const router = createHashRouter(
   [
     {
       path: '/',
