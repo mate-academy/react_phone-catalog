82e9cc830266aaa7f31501c0b79173fc5b6f78c9
beliyandr <beliyandr@gmail.com>
2025-06-25 22:14:37 +0300

    432

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -6,6 +6,11 @@ export const Catalog = () => {
   return (
     <section className="catalog">
       <div className="container catalog__container">
+        <div className="catalog__bradcrumbs">
+          <span>&#9813; </span>
+          <span>&#62;</span>
+          <span>home</span>
+        </div>
         <h1 className="catalog__title">Mobile phones</h1>
         <div className="catalog__counter">95 models</div>
         <div className="catalog__dropdowns">
@@ -47,7 +52,14 @@ export const Catalog = () => {
           <ProductCard />
           <ProductCard />
         </div>
-        <div className="catalog__pagination"></div>
+        <div className="catalog__pagination pagination">
+          <span className="pagination__item  pagination__left"> &#60;</span>
+          <span className="pagination__item pagination__item_active"> 1 </span>
+          <span className="pagination__item "> 2 </span>
+          <span className="pagination__item "> 3 </span>
+          <span className="pagination__item "> 4 </span>
+          <span className="pagination__item pagination__rigth"> &#62;</span>
+        </div>
       </div>
     </section>
   );
