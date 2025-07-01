a25d4dfdc6ff810598111162dc8e745c747f4ee3
Oleh Chernov <nineuito@mail.com>
2025-07-01 13:14:11 +0300

    latest

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
+  // base: '/react_phone-catalog/',
   plugins: [react(), svgr()],
   css: {
     preprocessorOptions: {
