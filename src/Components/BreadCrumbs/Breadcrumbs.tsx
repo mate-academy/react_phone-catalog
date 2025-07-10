f61812ea6afa9c6dd9cb41245d3c21a140754378
Oleh <pryge321@gmail.com>
2025-07-10 12:16:24 +0300

    save progress

--- src/Components/BreadCrumbs/Breadcrumbs.tsx
+++ src/Components/BreadCrumbs/Breadcrumbs.tsx
@@ -1,3 +1,49 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import useBreadcrumbs from 'use-react-router-breadcrumbs';
+
+const routes = [
+  {
+    path: '/',
+    breadcrumb: () => (
+      <img
+        src="/img/icons/Home.png"
+        alt="Home"
+        style={{ width: 16, height: 16 }}
+      />
+    ),
+  },
+  { path: '/phones', breadcrumb: 'Phones' },
+];
+
 export const Breadcrumbs = () => {
-  return <h1>Breadcrumbs</h1>;
+  const breadcrumbs = useBreadcrumbs(routes);
+
+  return (
+    <React.Fragment>
+      <div className="breadcrumbs">
+        {breadcrumbs.map(({ breadcrumb, key }, index) => {
+          if (index === breadcrumbs.length - 1) {
+            return (
+              <span key={key} className="breadcrumb breadcrumb--not-active">
+                {breadcrumb}
+              </span>
+            );
+          }
+
+          return (
+            <div key={key} style={{ display: 'flex' }}>
+              <NavLink to={key} className="breadcrumb">
+                {breadcrumb}
+              </NavLink>
+              <img
+                src="/img/icons/SliderRight.png"
+                style={{ width: 16, height: 16 }}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </React.Fragment>
+  );
 };
--- src/Components/PhonesPage/PhonesPage.tsx
+++ src/Components/PhonesPage/PhonesPage.tsx
@@ -1,9 +1,12 @@
+import { Breadcrumbs } from '../BreadCrumbs/Breadcrumbs';
+
 export const PhonesPage = () => {
   return (
     <div className="phones-page">
       <h1>
         Phones Page
         {/* Your favourite phones will appear here! */}
+        <Breadcrumbs />
       </h1>
     </div>
   );
