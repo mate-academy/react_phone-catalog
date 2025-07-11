a79ef861037ab810a59350569ab8e43327f565ad
maksimka-gif <maks.markevych@gmail.com>
2025-07-11 21:21:13 +0300

    add favicon and title, correct links

--- src/shared/components/SideMenu/SideMenu.tsx
+++ src/shared/components/SideMenu/SideMenu.tsx
@@ -5,12 +5,23 @@ import styles from './SideMenu.module.scss';
 import FavoriteIcon from '../../../assets/icons/header-icons/favorites-icon.svg';
 import CartIcon from '../../../assets/icons/header-icons/cart-icon.svg';
 import { getClassName } from '../../utils/classNameActive';
+import { CartStateContext } from '../../store/CartProvider';
+import { FavoritesStateContext } from '../../store/FavoritesProvider';
+import { useContext } from 'react';
 
 type Props = {
   isOpenSide: boolean;
   setIsOpenSide: React.Dispatch<React.SetStateAction<boolean>>;
 };
 export const SideMenu: React.FC<Props> = ({ isOpenSide, setIsOpenSide }) => {
+  const favoritesProduct = useContext(FavoritesStateContext);
+  const cartsProduct = useContext(CartStateContext);
+
+  const tottalCartItems =
+    Array.isArray(cartsProduct) &&
+    cartsProduct.length > 0 &&
+    cartsProduct.reduce((acc, item) => acc + item.quantity, 0);
+
   return (
     <aside
       className={getClassName({
@@ -37,38 +48,56 @@ export const SideMenu: React.FC<Props> = ({ isOpenSide, setIsOpenSide }) => {
             </NavLink>
           </li>
           <li className={styles.menu__item}>
-            <a
-              href="#"
-              className={styles.menu__link}
+            <NavLink
+              to="phones"
+              className={({ isActive }) =>
+                getClassName({
+                  isActive,
+                  baseClass: styles.menu__link,
+                  activeClass: styles.menu__linkActive,
+                })
+              }
               onClick={() => setIsOpenSide(false)}
             >
               Phones
-            </a>
+            </NavLink>
           </li>
           <li className={styles.menu__item}>
-            <a
-              href="#"
-              className={styles.menu__link}
+            <NavLink
+              to="tablets"
+              className={({ isActive }) =>
+                getClassName({
+                  isActive,
+                  baseClass: styles.menu__link,
+                  activeClass: styles.menu__linkActive,
+                })
+              }
               onClick={() => setIsOpenSide(false)}
             >
               Tablets
-            </a>
+            </NavLink>
           </li>
           <li className={styles.menu__item}>
-            <a
-              href="#"
-              className={styles.menu__link}
+            <NavLink
+              to="accessories"
+              className={({ isActive }) =>
+              getClassName({
+                isActive,
+                baseClass: styles.menu__link,
+                activeClass: styles.menu__linkActive,
+              })
+            }
               onClick={() => setIsOpenSide(false)}
             >
               Accessories
-            </a>
+            </NavLink>
           </li>
         </ul>
       </nav>
 
       <div className={styles.menu__icons}>
-        <a
-          href="#"
+        <NavLink
+          to="favorites"
           className={styles.menu__favoritesLink}
           onClick={() => setIsOpenSide(false)}
         >
@@ -77,10 +106,14 @@ export const SideMenu: React.FC<Props> = ({ isOpenSide, setIsOpenSide }) => {
             alt="Відкрити улюблені товари"
             className={styles.menu__favoritesImg}
           />
-          <span className={styles.menu__cart_favorite_number}>23</span>
-        </a>
-        <a
-          href="#"
+         {favoritesProduct.length > 0 && (
+              <span className={styles.menu__cart_favorite_number}>
+                {favoritesProduct.length}
+              </span>
+            )}
+        </NavLink>
+        <NavLink
+          to="cart"
           className={styles.menu__cartLink}
           onClick={() => setIsOpenSide(false)}
         >
@@ -89,8 +122,12 @@ export const SideMenu: React.FC<Props> = ({ isOpenSide, setIsOpenSide }) => {
             alt="Відкрити корзину з товарами"
             className={styles.header__cartImg}
           />
-          <span className={styles.menu__cart_favorite_number}>23</span>
-        </a>
+         {tottalCartItems && (
+              <span className={styles.menu__cart_favorite_number}>
+                {tottalCartItems}
+              </span>
+            )}
+        </NavLink>
       </div>
     </aside>
   );
