851f0fee75ab0169740e35f7d93ef781fdae673f
kateliaskov <58365756+KateLis84@users.noreply.github.com>
2025-07-13 13:39:10 +0300

    change Route

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 );
