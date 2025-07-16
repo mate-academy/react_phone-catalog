2d99020acd25cacafe38aa742745ff1c1f492a5d
DyvakOleksandr <dyvakaleksandr370@gmail.com>
2025-07-16 17:43:44 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.scss';
+import { Outlet } from 'react-router-dom';
+import { Header } from './modules/shared/Header/Header';
+import { Footer } from './modules/shared/Footer/Footer';
 
 export const App = () => (
   <div className="App">
-    <h1>Product Catalog</h1>
+    <h1 hidden>Product Catalog</h1>
+    <Header />
+    <main>
+      <Outlet />
+    </main>
+    <Footer />
   </div>
 );
---
+++ src/app/hook.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import type { RootState, AppDispatch } from './store';
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
