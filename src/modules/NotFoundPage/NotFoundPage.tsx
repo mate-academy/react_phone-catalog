68651c9ac698b3a46e9399526f84ebb1b0e718c3
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-07-25 16:43:04 +0300

    mend

--- src/modules/NotFoundPage/NotFoundPage.tsx
+++ src/modules/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,23 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigate('..');
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [navigate]);
+
+  return (
+    <section className="container">
+      <div className="page-not-found">
+        <h2>Page Not Found</h2>
+        <p>The page you are looking for does not exist.</p>
+      </div>
+    </section>
+  );
 };
