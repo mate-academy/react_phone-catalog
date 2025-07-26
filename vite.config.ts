62ca158ed1c56d23f0c4095e28b58ac9bc5a2696
Yulia Fil <filyulia.yf@gmail.com>
2025-07-26 13:56:42 +0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const mode = process.env.NODE_ENV;
+// : 'development' | "production"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/react_phone-catalog/',
+  base: mode === 'production' ? '/react_phone-catalog/' : undefined,
 });
