706c82a47b32ef9556dbdb0945d1f5c0c6e1d709
Yulia Fil <filyulia.yf@gmail.com>
2025-07-26 10:14:41 +0300

    update BASE_URL

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -1,6 +1,9 @@
 import { Product } from '../types/Product';
 
-const BASE_URL = '/api';
+const mode = import.meta.env.MODE;
+// : 'development' | "production"
+
+const BASE_URL = mode === 'development' ? '/api' : '/react_phone-catalog/api';
 
 export function getData<T>(url: string): Promise<T> {
   return fetch(BASE_URL + url + '.json').then(response => {
