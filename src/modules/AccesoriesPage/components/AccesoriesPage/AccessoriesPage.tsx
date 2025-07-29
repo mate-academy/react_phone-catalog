f5c2e56722e6bc4b8b578676db2755a84c34888e
nex1994 <bilicki1994@gmail.com>
2025-07-29 19:06:44 +0200

    fixed section elements size

--- src/modules/AccesoriesPage/components/AccesoriesPage/AccessoriesPage.tsx
+++ src/modules/AccesoriesPage/components/AccesoriesPage/AccessoriesPage.tsx
@@ -1,8 +1,9 @@
 import { AccessoriesCatalog } from '../AccesoriesCatalog';
+import styles from './AccessoriesPage.module.scss';
 
 export const AccessoriesPage = () => {
   return (
-    <div>
+    <div className={styles.phones}>
       <AccessoriesCatalog />
     </div>
   );
