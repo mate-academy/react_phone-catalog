61add1127cc148b22f5c809d220dafcac219898f
Valentyna Moskvinova <tina.moskwinova@gmail.com>
2025-07-31 15:44:46 +0300

    small changes

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,64 @@
+import { NavLink } from 'react-router-dom';
+import React, { useContext } from 'react';
+import './Sidebar.scss';
+import classNames from 'classnames';
+
+import DarkFav from '../../img/heart.svg';
+import LightFav from '../../img/heartLight.svg';
+import DarkBag from '../../img/bagCart.svg';
+import LightBag from '../../img/bagCartLigth.svg';
+import { ThemeContext } from '../Themes/Themes';
+import { useFavourites } from '../Favourites/FavouritesContext';
+import { useCart } from '../BoughtCard/CartContext';
+
+interface SidebarProps {
+  openMenu: boolean;
+  setOpenMenu: () => void;
+}
+
+export const Sidebar: React.FC<SidebarProps> = ({ openMenu, setOpenMenu }) => {
+  const getLinkActive = ({ isActive }: { isActive: boolean }) =>
+    classNames('navbar-item', { 'active-link': isActive });
+
+  const { theme } = useContext(ThemeContext);
+  const isBasicDark = theme === 'dark';
+  const { favourites } = useFavourites();
+  const { cart } = useCart();
+
+  const totalCount = cart.reduce((acc, item) => {
+    return acc + (item.quantity || 1);
+  }, 0);
+
+  return (
+    <aside className={`sidebar ${openMenu ? 'open' : ''}`}>
+      <div className="sidebar__menu">
+        <nav className="sidebar-brand">
+          {/*check later */}
+          <NavLink className={getLinkActive} to="/" onClick={setOpenMenu}>
+            Home
+          </NavLink>
+
+          <NavLink className={getLinkActive} to="/phones" onClick={setOpenMenu}>
+            Phones
+          </NavLink>
+
+          <NavLink
+            className={getLinkActive}
+            to="/tablets"
+            onClick={setOpenMenu}
+          >
+            Tablets
+          </NavLink>
+
+          <NavLink
+            className={getLinkActive}
+            to="/accessories"
+            onClick={setOpenMenu}
+          >
+            Accessories
+          </NavLink>
+        </nav>
+      </div>
+    </aside>
+  );
+};
