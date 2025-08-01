80eecbc0252862f87204eae413a74fc1c175b5a2
Valentyna Moskvinova <tina.moskwinova@gmail.com>
2025-08-01 12:58:07 +0300

    themes-2

--- src/components/Themes/Themes.tsx
+++ src/components/Themes/Themes.tsx
@@ -1,6 +1,10 @@
 import { createContext } from 'react';
+type ThemeContextType = {
+  theme: string;
+  setTheme: (theme: string) => void;
+};
 
-export const ThemeContext = createContext({
+export const ThemeContext = createContext<ThemeContextType>({
   theme: 'dark',
-  setTheme: (theme: string) => {},
+  setTheme: () => {},
 });
