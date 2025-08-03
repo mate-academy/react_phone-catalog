abbfbad26fe17ff581e1393e2a67b2acd47eb2de
Valentyna Moskvinova <tina.moskwinova@gmail.com>
2025-08-03 12:06:49 +0300

    home

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,88 @@
+import './HomePage.scss';
+import { Swiper } from '../Swiper/Swiper';
+import { Category } from '../Category';
+import { ProductSlider } from '../ProductCard';
+import { useEffect, useState } from 'react';
+import { Product } from '../../types/ProductTipes';
+import { fetchProducts } from '../../utils/api';
+import { GlassyOrbLoader } from '../Loader';
+import { NameSlider } from '../../types/nameslider';
+
+export const HomePage = () => {
+  const [phones, setPhones] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchProducts();
+
+        setPhones(
+          data.filter((product: Product) => product.category === 'phones'),
+        );
+        setError(null);
+        {
+          /*it should be checked later*/
+        }
+      } catch (er: any) {
+        if (er.message === 'No internet connection') {
+          setError('No internet connection. Please, check your network.');
+        } else {
+          setError('Server is not responding. Please, try again later.');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const timeout = setTimeout(fetchData, 500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <main className="main__homepage">
+      {error && (
+        <div className="error__container">
+          <p className="error-message">
+            Something went wrong...
+            <br>Please, check your connection and try again later.</br>
+          </p>
+        </div>
+      )}
+
+      {!error && (
+        <h1 className="page__title">Welcome to Nice Gadgets store!</h1>
+      )}
+
+      {loading && (
+        <div className="loader-container">
+          <GlassyOrbLoader />
+        </div>
+      )}
+
+      {!loading && !error && (
+        <>
+          <Swiper />
+          <div className="homepage__product">
+            <ProductSlider products={phones} title={NameSlider.Brand} />
+          </div>
+          <div className="homepage__category">
+            <div className="category__wrapper">
+              <h2 className="category__title">Shop by category</h2>
+              <Category />
+            </div>
+          </div>
+          <div className="homepage__hotproduct">
+            <ProductSlider
+              products={phones}
+              AdditionalPrice={true}
+              title={NameSlider.Hot}
+            />
+          </div>
+        </>
+      )}
+    </main>
+  );
+};
