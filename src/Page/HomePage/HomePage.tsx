cbc77c79c11dd4b38a1b26c8050d048ab14873ae
Саша <martinecsasa5@gmail.com>
2025-08-05 20:43:58 +0300

    fix12.4

--- src/Page/HomePage/HomePage.tsx
+++ src/Page/HomePage/HomePage.tsx
@@ -4,9 +4,9 @@ import style from './HomePage.module.scss';
 
 export const HomePage = () => {
   const slides = [
-    'img/Logo/baner-header.png',
     '/img/Logo/baner-header.png',
-    './img/Logo/baner-header.png',
+    '/img/Logo/baner-header.png',
+    '/img/Logo/baner-header.png',
   ];
 
   return (
