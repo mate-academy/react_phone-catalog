797a8108a2148234b37365eeb7896265831ba18f
Lirik <melnicukkiril2002@gmail.com>
2025-08-08 22:12:36 +0300

    updated

--- src/Root.tsx
+++ src/Root.tsx
@@ -14,41 +14,11 @@ import { TabletsPage } from './modules/ProductPages/TabletsPage';
 import { CartPage } from './modules/CartPage';
 import { FavoritePage } from './modules/FavoritePage';
 import { PagesType } from './types/PagesType';
-// import { replace } from 'cypress/types/lodash';
 
 export const Root = () => {
   return (
     <Router>
       <Routes>
-        {/* <Route path={`${PagesType.home}`} element={<App />}>
-          <Route index element={<HomePage />} />
-          <Route path="home" element={<Navigate to={PagesType.home} />} />
-          <Route path={`${PagesType.phones}`} element={<PhonesPage />}>
-            <Route
-              path={`${PagesType.phones}/:productId`}
-              element={<ProductDetailsPage />}
-            />
-          </Route>
-          <Route path={`${PagesType.tablets}`} element={<TabletsPage />}>
-            <Route
-              path={`${PagesType.tablets}/:productId`}
-              element={<ProductDetailsPage />}
-            />
-          </Route>
-          <Route
-            path={`${PagesType.accessories}`}
-            element={<AccessoriesPage />}
-          >
-            <Route
-              path={`${PagesType.accessories}/:productId`}
-              element={<ProductDetailsPage />}
-            />
-          </Route>
-
-          <Route path={`${PagesType.cart}`} element={<CartPage />} />
-          <Route path={`${PagesType.favorites}`} element={<FavoritePage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route> */}
         <Route path={`${PagesType.home}`} element={<App />}>
           <Route index element={<HomePage />} />
           <Route path="home" element={<Navigate to={PagesType.home} />} />
@@ -72,10 +42,6 @@ export const Root = () => {
 
           <Route path={PagesType.cart} element={<CartPage />} />
           <Route path={PagesType.favorites} element={<FavoritePage />} />
-
-          {/* <Route path="404" element={<NotFoundPage />} />
-          <Route path="*" element={<Navigate to="/404" replace />} /> */}
-
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
