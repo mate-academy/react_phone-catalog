a0d1127f05d78a5ce74b13e16ee7616632e11a87
Lirik <melnicukkiril2002@gmail.com>
2025-08-09 04:05:28 +0300

    update links

--- src/modules/ProductDetailsPage/components/DetailGallery/DetailGallery.tsx
+++ src/modules/ProductDetailsPage/components/DetailGallery/DetailGallery.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import { useMediaQuery } from 'react-responsive';
-import 'react-image-gallery/styles/css/image-gallery.css';
 import style from './detailGallery.module.scss';
 import cn from 'classnames';
 
@@ -13,9 +12,14 @@ interface Props {
 
 export const DetailGallery: React.FC<Props> = React.memo(({ images }) => {
   const isTabletOrLarger = useMediaQuery({ query: '(min-width: 640px)' });
+
+  // Використовуємо BASE_URL з Vite для коректного формування шляху
+  const baseUrl = import.meta.env.BASE_URL;
+
   const galleryImages = images.map(imgPath => ({
-    original: `/${imgPath}`, // або повний шлях якщо потрібно
-    thumbnail: `/${imgPath}`,
+    // Шляхи тепер формуються з BASE_URL
+    original: `${baseUrl}${imgPath}`,
+    thumbnail: `${baseUrl}${imgPath}`,
   }));
 
   return (
@@ -25,7 +29,6 @@ export const DetailGallery: React.FC<Props> = React.memo(({ images }) => {
       showFullscreenButton={false}
       showNav={false}
       thumbnailPosition={isTabletOrLarger ? 'left' : 'bottom'}
-      // slideOnThumbnailOver={true}
       lazyLoad={true}
       additionalClass={cn(style['detail-gallery'])}
     />
