4c614423c488e50635b8d89bff5553aae643a6a4
Grzegorz <grze.pra@gmail.com>
2025-08-10 21:31:24 +0200

    Initial menu skeleton done. Icons missing

--- src/modules/shared/components/Header/Header.tsx
+++ src/modules/shared/components/Header/Header.tsx
@@ -1,17 +1,17 @@
 import { Menu } from '../Menu/Menu';
-import './Header.scss';
+import styles from './Header.module.scss';
 
 export const Header = () => {
   return (
-    <header className="header">
-      <div className="header__container">
+    <header className={styles.header}>
+      <div className={styles.header__container}>
         <a href="/" aria-label="Link to main page">
-          <svg className="logo-icon">
+          <svg className={styles.logoIcon}>
             <use href="/icons/icons.svg#logo-icon"></use>
           </svg>
         </a>
         <button aria-label="Open and close menu">
-          <svg className="burger-icon">
+          <svg className={styles.burgerIcon}>
             <use href="/icons/icons.svg#burger-icon"></use>
           </svg>
         </button>
--- src/modules/shared/components/Menu/Menu.tsx
+++ src/modules/shared/components/Menu/Menu.tsx
@@ -1,3 +1,64 @@
+import { NavLink } from 'react-router-dom';
+import styles from './Menu.module.scss';
+import classNames from 'classnames';
+
 export const Menu = () => {
-  return <p>MENU</p>;
+  return (
+    <nav className="menu" role="navigation">
+      <ul role="list">
+        <li>
+          {' '}
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              classNames(styles.menu__link, {
+                [styles.menu__linkActive]: isActive,
+              })
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          {' '}
+          <NavLink
+            to="/phones"
+            className={({ isActive }) =>
+              classNames(styles.menu__link, {
+                [styles.menu__linkActive]: isActive,
+              })
+            }
+          >
+            Phones
+          </NavLink>
+        </li>
+        <li>
+          {' '}
+          <NavLink
+            to="/tablets"
+            className={({ isActive }) =>
+              classNames(styles.menu__link, {
+                [styles.menu__linkActive]: isActive,
+              })
+            }
+          >
+            Tablets
+          </NavLink>
+        </li>
+        <li>
+          {' '}
+          <NavLink
+            to="/accesories"
+            className={({ isActive }) =>
+              classNames(styles.menu__link, {
+                [styles.menu__linkActive]: isActive,
+              })
+            }
+          >
+            Accessories
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
