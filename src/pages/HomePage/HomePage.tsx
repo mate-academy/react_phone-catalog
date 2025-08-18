16207b2118e84113a60620311bd4e8d0f3574d5f
Uusuff <90629840+Uusuff@users.noreply.github.com>
2025-08-18 17:52:26 +0200

    replacement in scss modules imports on yus and adding hidden title to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -7,9 +7,12 @@ import styles from './HomePage.module.scss';
 export const HomePage = () => {
   return (
     <div className={styles.home__page}>
-      <h1 className={styles.home__page_title}>
-        Welcome to Nice Gadgets store!
+      <h1 className={styles.home__page_title_visually_hidden}>
+        Product Catalog
       </h1>
+      <h2 className={styles.home__page_title}>
+        Welcome to Nice Gadgets store!
+      </h2>
       <BannerSlider />
       <div className={styles.home__page_new_models_slider}>
         <NewModels />
