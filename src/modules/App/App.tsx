7182fa75991ece425a06730cda38082ee73a6dac
Cirillo Negruzzi <assus.12345nagi@gmail.com>
2025-08-20 02:39:46 +0200

    added changes

--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import { HomePage } from '../HomePage';
