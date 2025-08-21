3978633d1bc7b5aead877184b235bf0973b940cf
Sofiia13 <sophiyastanish@gmail.com>
2025-08-21 22:52:57 +0300

    fix favorites page

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -3,15 +3,17 @@ import { Breadcrumbs } from '../../components/Breadcrumbs';
 import { AllItemsList } from '../../components/AllItemsList';
 import { useFavorites } from '../../context/Favorites/FavoritesContext';
 
+import styles from './FavoritesPage.module.scss';
+
 export const FavoritesPage: React.FC = () => {
   const { favorites } = useFavorites();
 
   return (
     <div>
       <Breadcrumbs />
-      <div className="title">
-        <h1 className="title__name">Favourites</h1>
-        <p className="title__quantity">{favorites.length} models</p>
+      <div className={styles['title']}>
+        <h1 className={styles['title__name']}>Favourites</h1>
+        <p className={styles['title__quantity']}>{favorites.length} models</p>
       </div>
 
       <AllItemsList allItems={favorites} useFilters={false} />
