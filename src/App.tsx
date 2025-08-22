fe05e027bf8e04c8e61c0db10e8b4f43efc00a8c
Viktoriia <viktoriamyhailiak@gmail.com>
2025-08-22 12:19:24 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './App.scss';
 
 export const App = () => (
@@ -5,3 +6,31 @@ export const App = () => (
     <h1>Product Catalog</h1>
   </div>
 );
+=======
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> 5093d5c (Initialize project using Create React App)
