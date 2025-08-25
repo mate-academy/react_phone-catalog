691f33a6b8c34d99de727d1c9e419256df0d142e
Саша <martinecsasa5@gmail.com>
2025-08-25 10:30:28 +0300

    fix3

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react_phone-catalog/',
   plugins: [react(), svgr()],
   css: {
     preprocessorOptions: {
