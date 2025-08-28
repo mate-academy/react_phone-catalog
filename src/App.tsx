0aa2b6e5f894c2209c0131f1c587c2bea94055fe
Bogdan Kalytovskiy <bogdankalytovskkiy@gmail.com>
2025-08-28 15:36:06 +0300

    your messag

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import { Header } from './Components/Header';
 
-export const App = () => (
-  <div className="App">
-    <h1>Product Catalog</h1>
-  </div>
-);
+export const App = () => {
+  return (
+    <main>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<div>Home page</div>} />
+        <Route path="/phones" element={<div>Phones page</div>} />
+        <Route path="/tablets" element={<div>Tablets page</div>} />
+        <Route path="/accessories" element={<div>Accessories page</div>} />
+        <Route path="/cart" element={<div>Phones page</div>} />
+        <Route path="/favorites" element={<div>Phones page</div>} />
+        <Route path="*" element={<div>Phones page</div>} />
+      </Routes>
+    </main>
+  );
+};
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,125 @@
+import { Link, NavLink } from 'react-router-dom';
+import styles from './Header.module.scss';
+import { useState } from 'react';
+
+export const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.containerNav}>
+          <Link to="/" className={styles.logo} />
+          <nav className={styles.nav}>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/phones"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Phones
+            </NavLink>
+            <NavLink
+              to="/tablets"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Tablets
+            </NavLink>
+            <NavLink
+              to="/accessories"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Accessories
+            </NavLink>
+          </nav>
+        </div>
+
+        <div className={styles.actions}>
+          <NavLink
+            to="/favorites"
+            className={`${styles.icon} ${styles.iconLike}`}
+          />
+          <NavLink to="/cart" className={`${styles.icon} ${styles.iconShop}`} />
+          <div
+            className={`${styles.icon} ${styles.iconBurger}`}
+            onClick={() => setIsMenuOpen(true)}
+          />
+        </div>
+      </div>
+
+      <aside className={`${styles.menu} ${isMenuOpen ? styles.open : ''}`}>
+        <div className={styles.menu__top}>
+          <Link to="/" className={styles.logo} />
+          <div className={styles.containerClose}>
+            <div
+              className={`${styles.icon} ${styles.iconClose}`}
+              onClick={() => setIsMenuOpen(false)}
+            />
+          </div>
+        </div>
+
+        <div className={styles.menu__hero}>
+          <ul className={styles.menu__list}>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? styles.active : '')}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/phones"
+                className={({ isActive }) => (isActive ? styles.active : '')}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Phones
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/tablets"
+                className={({ isActive }) => (isActive ? styles.active : '')}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Tablets
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/accessories"
+                className={({ isActive }) => (isActive ? styles.active : '')}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Accessories
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.menu__bottom}>
+          <NavLink
+            to="/favorites"
+            className={({ isActive }) =>
+              `${styles.icon} ${styles.iconLike} ${isActive ? styles.activeIcon : ''}`
+            }
+            onClick={() => setIsMenuOpen(false)}
+          />
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              `${styles.icon} ${styles.iconShop} ${isActive ? styles.activeIcon : ''}`
+            }
+            onClick={() => setIsMenuOpen(false)}
+          />
+        </div>
+      </aside>
+    </header>
+  );
+};
