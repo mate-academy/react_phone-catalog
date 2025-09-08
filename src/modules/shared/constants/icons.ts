5b9e78d51848f418b7304ee54941a0c0c34f879b
YanamiYokico <v.a.nikolojchyk@gmail.com>
2025-09-09 00:23:36 +0300

    icons fix

--- src/modules/shared/constants/icons.ts
+++ src/modules/shared/constants/icons.ts
@@ -6,70 +6,70 @@ type Icon = {
 export const icons: Record<string, Icon> = {
   logo: {
     title: 'Logo icon',
-    path: '/src/modules/shared/icons/Logo.svg',
+    path: './icons/Logo.svg',
   },
   favorites: {
     title: 'Favorites icon',
-    path: '/src/modules/shared/icons/heart.svg',
+    path: './icons/heart.svg',
   },
   favorites_filled: {
     title: 'Favorites icon',
-    path: '/src/modules/shared/icons/heart-filled.svg',
+    path: './icons/heart-filled.svg',
   },
   shopping_cart: {
     title: 'Shopping cart icon',
-    path: '/src/modules/shared/icons/Group 17.svg',
+    path: './icons/Group 17.svg',
   },
   menu: {
     title: 'Menu icon',
-    path: '/src/modules/shared/icons/Menu.svg',
+    path: './icons/Menu.svg',
   },
   close: {
     title: 'Close icon',
-    path: '/src/modules/shared/icons/Menu-close.svg',
+    path: './icons/Menu-close.svg',
   },
   arrow_top: {
     title: 'Back to top',
-    path: '/src/modules/shared/icons/BackToTop.svg',
+    path: './icons/BackToTop.svg',
   },
   arrow_left: {
     title: 'arrow left',
-    path: '/src/modules/shared/icons/arrow-left.svg',
+    path: './icons/arrow-left.svg',
   },
   arrow_right: {
     title: 'arrow left',
-    path: '/src/modules/shared/icons/arrow-right.svg',
+    path: './icons/arrow-right.svg',
   },
   arrow_down: {
     title: 'arrow down',
-    path: '/src/modules/shared/icons/arrow-down.svg',
+    path: './icons/arrow-down.svg',
   },
   arrow_left__disabled: {
     title: 'arrow left disabled',
-    path: '/src/modules/shared/icons/arrow-left-disabled.svg',
+    path: './icons/arrow-left-disabled.svg',
   },
   arrow_right__disabled: {
     title: 'arrow left disabled',
-    path: '/src/modules/shared/icons/arrow-right-disabled.svg',
+    path: './icons/arrow-right-disabled.svg',
   },
   cross: {
     title: 'cross',
-    path: '/src/modules/shared/icons/cross.svg',
+    path: './icons/cross.svg',
   },
   plus: {
     title: 'plus',
-    path: '/src/modules/shared/icons/plus.svg',
+    path: './icons/plus.svg',
   },
   minus: {
     title: 'minus',
-    path: '/src/modules/shared/icons/minus.svg',
+    path: './icons/minus.svg',
   },
   minus_disabled: {
     title: 'minus disabled',
-    path: '/src/modules/shared/icons/minus-disabled.svg',
+    path: './icons/minus-disabled.svg',
   },
   home: {
     title: 'home',
-    path: '/src/modules/shared/icons/Home.svg',
+    path: './icons/Home.svg',
   },
 };
