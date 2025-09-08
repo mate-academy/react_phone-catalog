be75a9afa35c2d2036e66a3076ea2d747cd754ba
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-09-08 23:13:05 +0300

    fix arrow-icon

--- src/modules/CartPage/components/CartHeader/CartHeader.tsx
+++ src/modules/CartPage/components/CartHeader/CartHeader.tsx
@@ -6,7 +6,7 @@ export const CartHeader = () => {
   return (
     <div className={styles.header}>
       <Link to={`/`} className={styles.headerBackLink}>
-        <img className={styles.headerLinkImage} src="/src/assets/icons/arrow-left.svg"/>
+        <img className={styles.headerLinkImage} src="src/assets/icons/arrow-left.svg"/>
         <span className={styles.headerLinkText}>Back</span>
       </Link>
       <h1 className={styles.headerTitle}>
