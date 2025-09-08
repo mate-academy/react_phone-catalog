7677d00c9a100110affdb87f5d647f1a3bed91de
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-09-08 21:22:20 +0300

    fix vite setup for base root

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react_phone-catalog/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
