2775c9a471d85c7f0f7f89a94b4d042f0bc07ac8
Diana <dinamitka2369@gmail.com>
2025-09-14 12:01:00 +0200

    Gadgets Store full

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/fonts/variables.scss';
 import './index.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 import { FavoritesProvider } from './contexts/FavoritesContext';
 import { CartProvider } from './contexts/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <FavoritesProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </FavoritesProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
