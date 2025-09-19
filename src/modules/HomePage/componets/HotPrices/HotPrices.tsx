97131da085067e707d3c6fa0054ec0b633beefc5
annaabramovaa <abramovaana193@gmail.com>
2025-09-19 12:14:20 +0300

    message

--- src/modules/HomePage/componets/HotPrices/HotPrices.tsx
+++ src/modules/HomePage/componets/HotPrices/HotPrices.tsx
@@ -7,7 +7,7 @@ export const HotPrices = () => {
   const [allProducts, setAllProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch('/api/products.json')
+    fetch('/react_phone-catalog/api/products.json')
       .then(res => res.json())
       .then(setAllProducts);
   }, []);
