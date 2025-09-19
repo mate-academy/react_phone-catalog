483ba186e6293edc58434f597e3af24849953806
annaabramovaa <abramovaana193@gmail.com>
2025-09-19 11:44:38 +0300

    Message

--- src/modules/HomePage/componets/ShopByCategory/ShopByCategory.tsx
+++ src/modules/HomePage/componets/ShopByCategory/ShopByCategory.tsx
@@ -8,26 +8,47 @@ export const ShopByCategory = () => {
   return (
     <div className={styles.categories}>
       <h2 className={styles.categories_title}>Shop by category</h2>
-      <div className={styles.categories_wrapper}></div>
-      <NavLink to="./phones" className={styles.categories_photo_container}>
-        <img src={phones} alt="phones" className={styles.categories_photo} />
-      </NavLink>
-      <h4>Mobile phones</h4>
-      <p>95 models</p>
-      <NavLink to="./tablets" className={styles.categories_photo_container}>
-        <img src={tablets} alt="tablets" className={styles.categories_photo} />
-      </NavLink>
-      <h4>Tablets</h4>
-      <p>24 models</p>
-      <NavLink to="./accessories" className={styles.categories_photo_container}>
-        <img
-          src={accessories}
-          alt="accessories"
-          className={styles.categories_photo}
-        />
-      </NavLink>
-      <h4>Accessories</h4>
-      <p>100 models</p>
+
+      <div className={styles.categories_wrapper}>
+        <div className={styles.category}>
+          <NavLink to="./phones" className={styles.categories_photo_container}>
+            <img
+              src={phones}
+              alt="phones"
+              className={styles.categories_photo}
+            />
+          </NavLink>
+          <h4>Mobile phones</h4>
+          <p>95 models</p>
+        </div>
+
+        <div className={styles.category}>
+          <NavLink to="./tablets" className={styles.categories_photo_container}>
+            <img
+              src={tablets}
+              alt="tablets"
+              className={styles.categories_photo}
+            />
+          </NavLink>
+          <h4>Tablets</h4>
+          <p>24 models</p>
+        </div>
+
+        <div className={styles.category}>
+          <NavLink
+            to="./accessories"
+            className={styles.categories_photo_container}
+          >
+            <img
+              src={accessories}
+              alt="accessories"
+              className={styles.categories_photo}
+            />
+          </NavLink>
+          <h4>Accessories</h4>
+          <p>100 models</p>
+        </div>
+      </div>
     </div>
   );
 };
