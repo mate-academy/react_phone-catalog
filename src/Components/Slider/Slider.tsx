42070a7aca85a6a3892f89748c21f4d5b79a3f71
Bogdan Kalytovskiy <bogdankalytovskkiy@gmail.com>
2025-09-19 20:19:30 +0300

    add15

--- src/Components/Slider/Slider.tsx
+++ src/Components/Slider/Slider.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
-
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
 import styles from './Slider.module.scss';
 
 const slidesData = [
@@ -29,19 +27,26 @@ export const Slider: React.FC = () => {
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         navigation
-        pagination={{ clickable: true }}
+        pagination={{
+          clickable: true,
+          el: '.swiper-pagination',
+        }}
         autoplay={{ delay: 5000, disableOnInteraction: false }}
         loop={true}
         spaceBetween={30}
+        className={styles.swiper}
       >
         {slidesData.map(slide => (
-          <SwiperSlide key={slide.id}>
+          <SwiperSlide key={slide.id} className={styles['swiper-slide']}>
             <div className={styles.slide}>
               <img src={slide.img} alt={`Slide ${slide.id}`} />
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className="pagination-container">
+        <div className="swiper-pagination"></div>
+      </div>
     </div>
   );
 };
