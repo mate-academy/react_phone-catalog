c3b2670a4c11424224f7e103b49deebed8466146
YULIIA M <yuliiamartynovych@gmail.com>
2025-09-24 19:23:00 +0200

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/react_phone-catalog/' : '/',
+
+  base: '/react_phone-catalog/',
 });
