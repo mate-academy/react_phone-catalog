6331ab8ef35ce919e94927bacf746e305246e0f4
YULIIA M <yuliiamartynovych@gmail.com>
2025-09-25 17:18:29 +0200

    feat: switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { CartProvider } from './contexts/CartContext';
 import { FavoritesProvider } from './contexts/FavoritesContext';
