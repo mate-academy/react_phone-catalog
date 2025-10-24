ff03d682357798390e51fbe8c08390dce8d013da
Whslv <vladon19977@gmail.com>
2025-10-24 12:44:44 -0400

    adjusted addToCart button

--- src/modules/shared/components/Buttons/AddToCartButton/AddToCartButton.tsx
+++ src/modules/shared/components/Buttons/AddToCartButton/AddToCartButton.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 import styles from '../Button.module.scss';
 import { useAppDispatch, useAppSelector } from '../../../../../store/hooks';
-import { addToCart, selectItemIds } from '../../../../../store/slices/cartSlice';
+import { addToCart, removeFromCart, selectItemIds } from '../../../../../store/slices/cartSlice';
 import { ProductData } from '../../../types/ProductData';
 
 type Props = {
@@ -15,20 +15,32 @@ export const AddToCartButton: React.FC<Props> = ({ product }) => {
 
   const inCart = itemsIdsInCart.includes(product.itemId);
 
-  return (
-    <button
-      disabled={inCart}
-      className={styles.action__button}
-      onClick={() => {
-        dispatch(
-          addToCart({
-            item: product,
-            qty: 1,
-          }),
-        );
-      }}
-    >
-      {!inCart ? 'Add to cart' : 'Added to cart'}
-    </button>
-  );
+  if (!inCart) {
+    return (
+      <button
+        className={styles.action__button}
+        onClick={() => {
+          dispatch(
+            addToCart({
+              item: product,
+              qty: 1,
+            }),
+          );
+        }}
+      >
+        {'Add to cart'}
+      </button>
+    );
+  } else {
+    return (
+      <button
+        className={`${styles.action__button} ${styles.remove}`}
+        onClick={() => {
+          dispatch(removeFromCart({ itemId: product.itemId }));
+        }}
+      >
+        {'Added to cart'}
+      </button>
+    );
+  }
 };
