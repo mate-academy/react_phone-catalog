6232ad0cb6fc7e1d6a2aaea91f38c19c0b8a2bd6
Tim <drdakkadakka@gmail.com>
2025-10-24 20:15:50 +0200

    fix

--- src/serverMock/static/endPoints.ts
+++ src/serverMock/static/endPoints.ts
@@ -1,7 +1,7 @@
 enum ApiEndpoint {
-  PRODUCTS = '/api/products.json',
-  BANNERS = '/api/banners.json',
-  ITEMS = '/api/items.json',
+  PRODUCTS = 'api/products.json',
+  BANNERS = 'api/banners.json',
+  ITEMS = 'api/items.json',
 }
 
 export { ApiEndpoint };
