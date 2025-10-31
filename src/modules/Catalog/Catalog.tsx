c3a79f4b7b81af0e50b75c6ec1837f7883e1b920
Grzegorz <grze.pra@gmail.com>
2025-10-31 12:51:34 +0100

    feat(Catalog): Add Loader and empty state to products list

--- src/modules/Catalog/Catalog.tsx
+++ src/modules/Catalog/Catalog.tsx
@@ -4,6 +4,7 @@ import { DataContext } from '../../context/ContextProvider';
 import { Breadcrumbs } from '../shared/components/Breadcrumbs';
 import scss from './Catalog.module.scss';
 import { ProductsList } from './components';
+import { Loader } from '../shared/components/Loader';
 
 interface Props {
   items: Product[];
@@ -13,14 +14,23 @@ interface Props {
 export const Catalog: React.FC<Props> = ({ items, title }) => {
   const { isLoading } = useContext(DataContext);
 
-  const amount = items.length;
+  const amount = 0;
 
   return (
     <div className={scss.catalog}>
       <Breadcrumbs page={title === 'Mobile phones' ? 'Phones' : title} />
       <h1 className={scss.catalog__title}>{title}</h1>
       <span className={scss.catalog__counter}>{`${amount} models`}</span>
-      {!isLoading && <ProductsList items={items} />}
+
+      {isLoading ? (
+        <Loader />
+      ) : amount > 0 ? (
+        <ProductsList items={items} />
+      ) : (
+        <p className={scss.catalog__empty}>
+          There are no {title.toLowerCase()} yet
+        </p>
+      )}
     </div>
   );
 };
