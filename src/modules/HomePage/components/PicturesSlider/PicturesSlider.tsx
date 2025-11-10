c8cae431807060a0c3e9768b8c9f973c8b8f5189
Grzegorz <grze.pra@gmail.com>
2025-11-10 10:21:58 +0100

    refactor(PictureSlider): Improved the swap handling code

--- src/modules/HomePage/components/PicturesSlider/PicturesSlider.tsx
+++ src/modules/HomePage/components/PicturesSlider/PicturesSlider.tsx
@@ -7,11 +7,9 @@ import { Pictures } from './Pictures';
 export const PicturesSlider = () => {
   const [activeSlide, setActiveSlide] = useState<number>(0);
   const [touchStart, setTouchStart] = useState<number>(0);
-  const [touchEnd, setTouchEnd] = useState<number>(0);
+  const slideCount = slides.length / 2;
 
   useEffect(() => {
-    const slideCount = slides.length / 2;
-
     const timer = setInterval(() => {
       setActiveSlide(prevSlide => {
         return prevSlide === slideCount - 1 ? 0 : prevSlide + 1;
@@ -19,27 +17,24 @@ export const PicturesSlider = () => {
     }, 5000);
 
     return () => clearInterval(timer);
-  }, [activeSlide]);
+  }, [activeSlide, slideCount]);
 
   const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
     setTouchStart(e.targetTouches[0].clientX);
   };
 
-  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
-    setTouchEnd(e.targetTouches[0].clientX);
-  };
+  const handleTouchEnd = (e: React.TouchEvent<HTMLImageElement>) => {
+    const touchEnd = e.changedTouches[0].clientX;
 
-  const handleTouchEnd = () => {
-    if (touchStart - touchEnd > 20) {
-      setActiveSlide(current =>
-        current === slides.length / 2 - 1 ? 0 : current + 1,
-      );
+    const deltaX = touchStart - touchEnd;
+    const SWIPE_THRESHOLD = 50;
+
+    if (deltaX > SWIPE_THRESHOLD) {
+      setActiveSlide(current => (current === slideCount - 1 ? 0 : current + 1));
     }
 
-    if (touchStart - touchEnd < -20) {
-      setActiveSlide(current =>
-        current === 0 ? slides.length / 2 - 1 : current - 1,
-      );
+    if (deltaX < -SWIPE_THRESHOLD) {
+      setActiveSlide(current => (current === 0 ? slideCount - 1 : current - 1));
     }
   };
 
@@ -47,7 +42,6 @@ export const PicturesSlider = () => {
     <article
       className={scss.slider}
       onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
       onTouchEnd={handleTouchEnd}
     >
       <Pictures activeSlide={activeSlide} />
