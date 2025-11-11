c9c3764fb3699b67ccf763de90f073876dc390e2
tvk777 <klimenko.tv7@gmail.com>
2025-11-11 13:19:50 +0200

    add getData and getAllProducts functions

---
+++ src/api/products.ts
@@ -0,0 +1,20 @@
+import { Product } from '../types/Product';
+import { getData } from './clients';
+
+export const getAllProducts = () => getData<Product>('products');
+
+/* usage:
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const products = await getAllProducts();
+
+        console.log(products); // should now log your data
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+*/
---
+++ src/types/Product.ts
@@ -0,0 +1,15 @@
+export type Category = 'phones' | 'tablets' | 'accessories';
+export interface Product {
+  id: number;
+  category: Category;
+  itemId: string;
+  name: string;
+  fullPrice: number;
+  price: number;
+  screen: string;
+  capacity: string;
+  color: string;
+  ram: string;
+  year: number;
+  image: string;
+}
