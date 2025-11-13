3930c438f05393cf2846195fa754952073357281
Mykyta Kalniev <nikitamal4ik@gmail.com>
2025-11-13 19:05:38 +0100

    add navbar;

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { useEffect } from 'react';
+
+//style
 import './App.scss';
 
-export const App = () => (
-  <div className="App">
-    <h1>Product Catalog</h1>
-  </div>
-);
+//react-router
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+//components
+import { Navbar } from './components/Navbar';
+
+//pages
+import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { ProductsPage } from './pages/ProductsPage';
+
+export const App = () => {
+  useEffect(() => {
+    if (!window.location.hash) {
+      window.history.replaceState(null, '', window.location.pathname + '#/');
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Routes>
+        <Route path="/home" element={<Navigate to="/" replace={true} />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products">
+          <Route index element={<ProductsPage />} />
+          <Route path=":productType" element={<ProductsPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
+};
