34ad5613c3faf02c087f83ff07021a1e840e50a8
tvk777 <klimenko.tv7@gmail.com>
2025-11-13 18:31:21 +0200

    add CategoryPage

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,7 +1,29 @@
-import { Product } from '../types/Product';
+import { Category, Product, ProductDetails } from '../types/Product';
 import { getData } from './clients';
 
 export const getAllProducts = () => getData<Product[]>('products');
+
+const getProductsByCategoty = (category: Category) =>
+  getData<ProductDetails[]>(category);
+
+export const getProductList = async (category: Category) => {
+  const list = await getProductsByCategoty(category);
+
+  return list.map(item => ({
+    id: Math.floor(Math.random() * 1000) + 1,
+    category,
+    itemId: item.id,
+    name: item.name,
+    fullPrice: item.priceRegular,
+    price: item.priceDiscount,
+    screen: item.screen,
+    capacity: item.capacity,
+    color: item.color,
+    ram: item.ram,
+    year: 0,
+    image: item.images[0],
+  }));
+};
 
 /* usage:
   useEffect(() => {
--- src/modules/CategoryPage/CategoryPage.tsx
+++ src/modules/CategoryPage/CategoryPage.tsx
@@ -1,9 +1,47 @@
-import { FC } from 'react';
-import { Category } from '../../types/Product';
+/* eslint no-console: [,{ allow: ["warn", "log", "error"] }] */
+import { FC, useEffect, useState } from 'react';
+import { Category, Product } from '../../types/Product';
+import s from './CategoryPage.module.scss';
+import { getProductList } from '../../api/products';
+import { ProductList } from './components/ProductList';
 
 interface Props {
   category: Category;
 }
+
+const categoryTitles = {
+  phones: 'Mobile Phones',
+  tablets: 'Tablets',
+  accessories: 'Accessories',
+};
+
 export const CategoryPage: FC<Props> = ({ category }) => {
-  return <section>{category}</section>;
+  const [products, setProducts] = useState<Product[]>([]);
+  const count = products.length;
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const productList = await getProductList(category);
+
+        setProducts(productList); // should now log your data
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, [category]);
+
+  return (
+    <main>
+      <div className={s.container}>
+        <div>Breadcrumps</div>
+        <h1>{categoryTitles[category]}</h1>
+        <div>{count && count} models</div>
+        <div>Filter</div>
+        <ProductList products={products} />
+      </div>
+    </main>
+  );
 };
