f3453ac96249c0019982e0567d56e5b4677cc7fe
Misha Knysh <knih19@gmail.com>
2025-11-14 22:41:13 +0100

    Force deploy with updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react_phone-catalog/' : '/',
-  plugins: [react()],
-});
-
 // export default defineConfig({
-//   base: '/react_phone-catalog/',
+//   base: process.env.NODE_ENV === 'production' ? '/react_phone-catalog/' : '/',
 //   plugins: [react()],
 // });
+
+export default defineConfig({
+  base: '/react_phone-catalog/',
+  plugins: [react()],
+});
