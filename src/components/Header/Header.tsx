b5bfca06a463df2c04dcfcc5e5b2eccd5f871825
Kateryna Berdnikova <berdekaterinasp@gmail.com>
2025-11-20 14:45:32 +0200

    solutioт2

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,48 +1,110 @@
+import { useState } from 'react';
 import styles from './ Header.module.scss';
 import { Link, NavLink } from 'react-router-dom';
+import { Menu } from '../Menu/Menu';
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <header className={styles.header}>
-      <nav className={styles.header__nav}>
-        <Link to="/" className={styles.header__logo}>
-          <img
-            src="../public/img/logo.svg"
-            alt="Nice Gadgets"
-            className={styles.header__logo__img}
-          />
-        </Link>
+    <>
+      <header className={styles.header}>
+        <nav className={styles.header__nav}>
+          <Link to="/" className={styles.header__logo}>
+            <img
+              src="../public/img/logo.svg"
+              alt="Nice Gadgets"
+              className={styles.header__logo__img}
+            />
+          </Link>
 
-        <ul className={styles.header__nav__list}>
-          <li className={styles.header__nav__item}>
-            <NavLink to="/" className={styles.header__nav__link}>
-              HOME
-            </NavLink>
-          </li>
-          <li className={styles.header__nav__item}>
-            <NavLink to="/phones" className={styles.header__nav__link}>
-              Phones
-            </NavLink>
-          </li>
-          <li className={styles.header__nav__item}>
-            <NavLink to="/tablets" className={styles.header__nav__link}>
-              TABLETS
-            </NavLink>
-          </li>
-          <li className={styles.header__nav__item}>
-            <NavLink to="/accessories" className={styles.header__nav__link}>
-              ACCESSORIES
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+          <ul className={styles.header__nav__list}>
+            <li className={styles.header__nav__item}>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${styles.header__nav__link} ${styles['header__nav__link--active']}`
+                    : styles.header__nav__link
+                }
+              >
+                HOME
+              </NavLink>
+            </li>
+            <li className={styles.header__nav__item}>
+              <NavLink
+                to="/phones"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${styles.header__nav__link} ${styles['header__nav__link--active']}`
+                    : styles.header__nav__link
+                }
+              >
+                Phones
+              </NavLink>
+            </li>
+            <li className={styles.header__nav__item}>
+              <NavLink
+                to="/tablets"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${styles.header__nav__link} ${styles['header__nav__link--active']}`
+                    : styles.header__nav__link
+                }
+              >
+                TABLETS
+              </NavLink>
+            </li>
+            <li className={styles.header__nav__item}>
+              <NavLink
+                to="/accessories"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${styles.header__nav__link} ${styles['header__nav__link--active']}`
+                    : styles.header__nav__link
+                }
+              >
+                ACCESSORIES
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
 
-      <div className={styles.header__buttons}>
-        <NavLink
-          to="/"
-          className={`${styles.header__button} ${styles['header__button--icon-menu']}`}
-        ></NavLink>
-      </div>
-    </header>
+        <div className={styles.header__buttons}>
+          <button
+            className={`${styles.header__button} ${styles['header__button--menu']}`}
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <img
+              src="/public/img/icons/icon-burger-menu.svg"
+              alt="icon-menu"
+              className={styles.header__icon}
+            />
+          </button>
+          <NavLink
+            to="/favorites"
+            className={`${styles.header__button} ${styles['header__button--heart']}`}
+          >
+            <img
+              src="/public/img/icons/icon-heart.svg"
+              alt="icon-heart"
+              className={styles.header__icon}
+            />
+          </NavLink>
+          <NavLink
+            to="/cart"
+            className={`${styles.header__button} ${styles['header__button--bag']}`}
+          >
+            <img
+              src="/public/img/icons/icon-shopping-bag.svg"
+              alt="icon-shopping-bag"
+              className={styles.header__icon}
+            />
+          </NavLink>
+        </div>
+      </header>
+
+      {isMenuOpen && <Menu onClose={() => setIsMenuOpen(false)} />}
+    </>
   );
 };
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,80 @@
+import styles from './Menu.module.scss';
+import { Link, NavLink } from 'react-router-dom';
+
+export const Menu = ({ onClose }: { onClose: () => void }) => {
+  return (
+    <aside className={styles.menu}>
+      <nav className={styles.menu__nav}>
+        <Link to="/" className={styles.menu__nav__logo}>
+          <img
+            src="../public/img/logo.svg"
+            alt="Nice Gadgets"
+            className={styles.menu__nav__logo__img}
+          />
+        </Link>
+
+        <button className={`${styles.menu__nav__button}`} onClick={onClose}>
+          <img
+            src="/public/img/icons/icon-close.svg"
+            alt="icon-close"
+            className={styles.menu__nav__button__icon}
+          />
+        </button>
+      </nav>
+
+      <ul className={styles.menu__list}>
+        <li className={styles.menu__list__item}>
+          <NavLink to="/" className={styles.menu__list__link}>
+            HOME
+          </NavLink>
+        </li>
+        <li className={styles.menu__list__item}>
+          <NavLink to="/phones" className={styles.menu__list__link}>
+            Phones
+          </NavLink>
+        </li>
+        <li className={styles.menu__list__item}>
+          <NavLink to="/tablets" className={styles.menu__list__link}>
+            TABLETS
+          </NavLink>
+        </li>
+        <li className={styles.menu__list__item}>
+          <NavLink to="/accessories" className={styles.menu__list__link}>
+            ACCESSORIES
+          </NavLink>
+        </li>
+      </ul>
+
+      <div className={styles.menu__buttons}>
+        <NavLink
+          to="/favorites"
+          className={({ isActive }) =>
+            isActive
+              ? `${styles.menu__button} ${styles['menu__button--active']}`
+              : styles.menu__button
+          }
+        >
+          <img
+            src="/public/img/icons/icon-heart.svg"
+            alt="icon-heart"
+            className={styles.header__button__icon}
+          />
+        </NavLink>
+        <NavLink
+          to="/cart"
+          className={({ isActive }) =>
+            isActive
+              ? `${styles.menu__button} ${styles['menu__button--active']}`
+              : styles.menu__button
+          }
+        >
+          <img
+            src="/public/img/icons/icon-shopping-bag.svg"
+            alt="icon-shopping-bag"
+            className={styles.header__button__icon}
+          />
+        </NavLink>
+      </div>
+    </aside>
+  );
+};
