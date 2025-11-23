64d438227344889ee2aeedf9ec0ec70288d85f1e
kapidastra <pachasvichak@gmail.com>
2025-11-23 20:57:06 +0100

    fix: update ProductsList mapping to correctly build Product object and pass showOldPrice

--- src/shared/components/ProductList/ProductsList.tsx
+++ src/shared/components/ProductList/ProductsList.tsx
@@ -1,28 +1,19 @@
 import { ProductCard } from '../ProductCard';
 import styles from './ProductsList.module.scss';
 import React from 'react';
-import { UiProduct } from '../ProductsSlider/ProductSlider';
+import { Product } from '../../../types/product';
 
 type Props = {
-  products: UiProduct[];
+  products: Product[];
 };
 
 export const ProductsList: React.FC<Props> = ({ products }) => (
   <div className={styles.list}>
-    {products.map(p => (
+    {products.map(product => (
       <ProductCard
-        key={p.id}
-        product={{
-          id: p.id,
-          title: p.title,
-          img: p.img,
-          price: p.price,
-          oldPrice: p.oldPrice,
-          year: p.year,
-          screen: '',
-          capacity: '',
-          ram: '',
-        }}
+        key={product.id}
+        product={product}
+        showOldPrice={product.fullPrice > product.price}
       />
     ))}
   </div>
