b7ec3f96687a518f4bd024a658e26e2a4f638e87
Pavlo Sich <pachasvichak@gmail.com>
2025-11-25 15:23:33 +0100

    refactor: migrate AccessoriesPage.tsx to useProductsPage hook

---
+++ src/modules/AccessoriesPage/AccessoriesPage.tsx
@@ -0,0 +1,93 @@
+import styles from './AccessoriesPage.module.scss';
+import { ProductsList } from '../../shared/components/ProductList/ProductsList';
+import { Loader } from '../../shared/components/Loader/Loader';
+import { getAccessoriesFromProducts } from '../../services/productsService';
+import { Pagination } from '../../shared/components/Pagination';
+import { useProductsPage } from '../../shared/hooks/useProductPage';
+
+export const AccessoriesPage = () => {
+  const {
+    loading,
+    error,
+
+    sorted,
+    sortBy,
+    setSortBy,
+
+    paginated,
+    itemsPerPage,
+    setItemsPerPage,
+
+    currentPage,
+    totalPages,
+    handlePageChange,
+  } = useProductsPage({ fetchFn: getAccessoriesFromProducts });
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return (
+      <div className={styles.message}>
+        <p>Something went wrong</p>
+        <button onClick={() => window.location.reload()}>Reload</button>
+      </div>
+    );
+  }
+
+  if (!sorted.length) {
+    return <p className={styles.message}>There are no accessories yet</p>;
+  }
+
+  return (
+    <div className={styles.accessoriesPage}>
+      <h1> Accessories </h1>
+
+      <div className={styles.controls}>
+        <div className={styles.selectGroup}>
+          <label htmlFor="sort">Sort by:</label>
+          <select
+            id="sort"
+            value={sortBy}
+            onChange={e => setSortBy(e.target.value)}
+            className={styles.select}
+          >
+            <option value="newest">Newest</option>
+            <option value="alphabetically">Alphabetically</option>
+            <option value="cheapest">Cheapest</option>
+          </select>
+        </div>
+
+        <div className={styles.selectGroup}>
+          <label htmlFor="perPage">Items on page:</label>
+          <select
+            id="perPage"
+            value={itemsPerPage}
+            onChange={e =>
+              setItemsPerPage(
+                e.target.value === 'all' ? 'all' : Number(e.target.value),
+              )
+            }
+            className={styles.select}
+          >
+            <option value="all">All</option>
+            <option value="16">16</option>
+            <option value="8">8</option>
+            <option value="4">4</option>
+          </select>
+        </div>
+      </div>
+
+      <ProductsList products={paginated} />
+
+      {itemsPerPage !== 'all' && totalPages > 1 && (
+        <Pagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      )}
+    </div>
+  );
+};
