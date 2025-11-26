5e26b7ef063b8e6d13e9c47f0938bc3e97fee6bf
Anatoliy Korotenko <tolik.korotenko@21gmail.com>
2025-11-26 14:00:19 +0200

    done2

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Outlet } from 'react-router-dom';
 import './assets/styles/main.scss';
 import { Header } from '@Header';
 import { Footer } from '@GlobalComponents';
+import style from './app.module.scss';
 
 export const App = () => {
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <div className={style.wrapper}>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
     </>
   );
 };
