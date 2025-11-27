4b7eec0228cda3619c8a5a7f1aa25084b8f02330
Grzegorz <grze.pra@gmail.com>
2025-11-27 20:11:04 +0100

    feat(NotFoundPage): Add ButtonBack to NotFoundPage

--- src/modules/NotFoundPage/NotFoundPage.tsx
+++ src/modules/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import scss from './NotFoundPage.module.scss';
+import { ButtonBack } from '../shared/components/ButtonBack';
 
 export const NotFoundPage = () => {
   return (
     <section className={scss.notFoundPage}>
+      <ButtonBack />
       <h1 className={scss.notFoundPage__title}>Page not found</h1>
       <img
         src="/img/page-not-found.png"
