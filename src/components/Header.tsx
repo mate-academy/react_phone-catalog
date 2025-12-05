7e485fa6f36074e27cf2277ed28ef1c58b51cf7e
diego69-foda <diego.bkuhn@gmail.com>
2025-12-05 16:57:29 -0300

    🙏

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+export const Header = () => (
+  <header className="header">
+    <div>
+        <h1>NICE</h1>
+        <img src="./images/xoia.png" className="header__logo" alt="logo" />
+    </div>
+    <h1>GADGETS</h1>
+  </header>
+);
