f3603ea43e03b474c11f6e2384929f94cd9e4b16
TetiankaSh <shh.tetiana@gmail.com>
2025-12-18 13:00:56 +0200

    some basic brand new models scrolling implemented and buttons are done

--- src/components/BrandNewModels/BrandNewModels.tsx
+++ src/components/BrandNewModels/BrandNewModels.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import products from '../../../public/api/products.json';
 import styles from './BrandNewModels.module.scss';
 import FavoritesIcon from './favorites_icon.png';
+import Disabled_left from '../../icons/arrows/Disabled_left.png';
+import Disabled_right from '../../icons/arrows/Disabled_right.png';
+import Active_left from '../../icons/arrows/Active_left.png';
+import Active_right from '../../icons/arrows/Active_right.png';
 
 const sortedByYear = [...products]
   .filter(product => product.capacity === '256GB')
@@ -9,18 +13,59 @@ const sortedByYear = [...products]
   .splice(0, 11);
 
 export const BrandNewModels:React.FC = () => {
+  const VISIBLE_COUNT = 4;
+
+  const [startIndex, setStartIndex] = useState(0);
+  const maxIndex = sortedByYear.length - VISIBLE_COUNT;
+
+  const visibleProducts = sortedByYear.slice(
+    startIndex,
+    startIndex + VISIBLE_COUNT,
+  );
+
+  const handleLeftButton = () => {
+    setStartIndex(prev => prev - 1);
+  };
+
+  const handleRightButton = () => {
+    setStartIndex(prev => prev + 1);
+  };
+
+  const isLeftDisabled = startIndex === 0;
+  const isRightDisabled = startIndex === maxIndex;
+
   return (
     <div className={styles.new_models}>
       <div className={styles.new_models__top_bar}>
         <h3>Brand new models</h3>
         <div className={styles.new_models__top_bar__buttons}>
-          <button>left</button>
-          <button>right</button>
+          <button
+            onClick={handleLeftButton}
+            disabled={isLeftDisabled}
+            className={styles.new_models__top_bar__buttons__button}
+          >
+            {isLeftDisabled ? (
+              <img src={Disabled_left} alt="Previous products" className={styles.new_models__top_bar__buttons__button__img} />
+            ) : (
+                <img src={Active_left} alt="Previous products" className={styles.new_models__top_bar__buttons__button__img} />
+            )}
+          </button>
+          <button
+            onClick={handleRightButton}
+            disabled={isRightDisabled}
+            className={styles.new_models__top_bar__buttons__button}
+          >
+            {isRightDisabled ? (
+              <img src={Disabled_right} alt="Next products" className={styles.new_models__top_bar__buttons__button__img} />
+            ) : (
+                <img src={Active_right} alt="Next products" className={styles.new_models__top_bar__buttons__button__img} />
+            )}
+          </button>
         </div>
       </div>
 
       <div className={styles.new_models__container}>
-        {sortedByYear.map(item => (
+        {visibleProducts.map(item => (
           <div key={item.id} className={styles.new_models__item}>
             <div className={styles.new_models__item__container}>
               <img
