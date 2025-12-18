b1a7a3e8e559c26884ad45ee1c531b8c9c413768
TetiankaSh <shh.tetiana@gmail.com>
2025-12-18 12:19:44 +0200

    added variables for colors and imported it properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.scss';
+import './styles/globals.scss';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { HomePage } from './components/Home/HomePage';
