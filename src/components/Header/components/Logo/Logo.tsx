4fa59a81acb5ddca9ca5c2c0a307b8b5de590f95
annaabramovaa <abramovaana193@gmail.com>
2025-12-22 22:38:52 +0200

    no message

--- src/components/Header/components/Logo/Logo.tsx
+++ src/components/Header/components/Logo/Logo.tsx
@@ -1,10 +1,13 @@
 import logo from '/img/logo/Logo.png';
 import styles from './Logo.module.scss';
+import { Link } from 'react-router-dom';
 
 export const Logo: React.FC = () => {
   return (
-    <div className={styles.logo}>
-      <img src={logo} alt="logo" className={styles.logo_img} />
-    </div>
+    <Link to="/">
+      <div className={styles.logo}>
+        <img src={logo} alt="logo" className={styles.logo_img} />
+      </div>
+    </Link>
   );
 };
