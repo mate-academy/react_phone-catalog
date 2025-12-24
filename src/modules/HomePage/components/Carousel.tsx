a6cd5f9a8e1d95eb69966a19e36bcccedac4fb1c
Anton Donchenko <donch.a080@gmail.com>
2025-12-24 19:37:14 +0200

    feat(Navbar): enhance styling and theme support
    - Update logo dimensions and add padding for better layout.
    - Implement theme-based logo switching between light and dark modes.
    - Replace cart icon with an SVG for better scalability and performance.
    - Adjust global styles for hover background color to improve UI consistency.

--- src/modules/HomePage/components/Carousel.tsx
+++ src/modules/HomePage/components/Carousel.tsx
@@ -62,7 +62,20 @@ export const Carousel: React.FC = () => {
     <section className={styles.carousel}>
       <div className={styles.carousel__body}>
         <button className={styles.carousel__button} onClick={prev}>
-          <img src="img/arrow-left.svg" alt="Previous" />
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M10.4712 3.52864C10.2109 3.26829 9.78878 3.26829 9.52843 3.52864L5.52843 7.52864C5.26808 7.78899 5.26808 8.2111 5.52843 8.47145L9.52843 12.4714C9.78878 12.7318 10.2109 12.7318 10.4712 12.4714C10.7316 12.2111 10.7316 11.789 10.4712 11.5286L6.94265 8.00004L10.4712 4.47145C10.7316 4.2111 10.7316 3.78899 10.4712 3.52864Z"
+              fill="currentColor"
+            />
+          </svg>
         </button>
 
         {/* Це наше "вікно" перегляду */}
@@ -101,7 +114,20 @@ export const Carousel: React.FC = () => {
         </div>
 
         <button className={styles.carousel__button} onClick={next}>
-          <img src="img/arrow-right.svg" alt="Next" />
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M5.52876 3.52864C5.78911 3.26829 6.21122 3.26829 6.47157 3.52864L10.4716 7.52864C10.7319 7.78899 10.7319 8.2111 10.4716 8.47145L6.47157 12.4714C6.21122 12.7318 5.78911 12.7318 5.52876 12.4714C5.26841 12.2111 5.26841 11.789 5.52876 11.5286L9.05735 8.00004L5.52876 4.47145C5.26841 4.2111 5.26841 3.78899 5.52876 3.52864Z"
+              fill="currentColor"
+            />
+          </svg>
         </button>
       </div>
       <CarouselIndicator
