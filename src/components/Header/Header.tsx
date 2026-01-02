e7bb9d8da5d010f6d3274fa37ded44ad81704f80
TetiankaSh <shh.tetiana@gmail.com>
2026-01-02 14:43:49 +0200

    commit for advise on styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,82 +1,116 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
 import Logo from './icons/Logo.png';
 import HeartIcon from './icons/heart-icon.png';
 import CartIcon from './icons/cart-icon.png';
 import BurgerMenu from './icons/burger-menu-icon.png';
+import CloseMenu from '../../icons/remove_icon.png';
 import { useShoppingCart } from '../../context/ShoppingCartContext';
 import { useFavorites } from '../../context/FavoritesContext';
+import { Menu } from './Menu';
 
 export const Header: React.FC = () => {
   const { cartItems } = useShoppingCart();
   const { favItems } = useFavorites();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [isMenuOpen]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 640) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
-    <header className={styles.header}>
-      <div className={styles.header__container}>
-        <div className={styles.header__container__left}>
-          <div className={styles.header__logo}>
-            <NavLink to="/">
-              <img src={Logo} alt="Logo" className={styles.header__logo__img} />
-            </NavLink>
+    <>
+      <header className={styles.header}>
+        <div className={styles.header__container}>
+          <div className={styles.header__container__left}>
+            <div className={styles.header__logo}>
+              <NavLink to="/">
+                <img src={Logo} alt="Logo" className={styles.header__logo__img} />
+              </NavLink>
+            </div>
+
+            <nav className={`${styles.header__nav} ${isMenuOpen ? styles.header__nav_active : ''}`}>
+              <NavLink to="/" className={styles.header__nav__item} onClick={() => setIsMenuOpen(false)}>
+                HOME
+              </NavLink>
+              <NavLink to="/phones" className={styles.header__nav__item} onClick={() => setIsMenuOpen(false)}>
+                PHONES
+              </NavLink>
+              <NavLink to="/tablets" className={styles.header__nav__item} onClick={() => setIsMenuOpen(false)}>
+                TABLETS
+              </NavLink>
+              <NavLink to="/accessories" className={styles.header__nav__item} onClick={() => setIsMenuOpen(false)}>
+                ACCESSORIES
+              </NavLink>
+            </nav>
           </div>
 
-          <nav className={styles.header__nav}>
-            <NavLink to="/" className={styles.header__nav__item}>
-              HOME
-            </NavLink>
+          <div className={styles.header__container__right}>
+            <div className={styles.header__icons__icon}>
+              <NavLink to="/favorites">
+                <div className={styles.header__icons__wrapper}>
+                  <img
+                    src={HeartIcon}
+                    alt="Favorites"
+                    className={styles.header__icons__img}
+                  />
+                  {favItems.length > 0 && (
+                    <div className={styles.header__icons__amount}>
+                      {favItems.length}
+                    </div>
+                  )}
+                </div>
+              </NavLink>
+            </div>
 
-            <NavLink to="/phones" className={styles.header__nav__item}>
-              PHONES
-            </NavLink>
+            <div className={styles.header__icons__icon}>
+              <NavLink to="/cart">
+                <div className={styles.header__icons__wrapper}>
+                  <img
+                    src={CartIcon}
+                    alt="Cart"
+                    className={styles.header__icons__img}
+                  />
+                  {cartItems.length > 0 && (
+                    <div className={styles.header__icons__amount}>
+                      {cartItems.length}
+                    </div>
+                  )}
+                </div>
+              </NavLink>
+            </div>
+          </div>
 
-            <NavLink to="/tablets" className={styles.header__nav__item}>
-              TABLETS
-            </NavLink>
-
-            <NavLink to="/accessories" className={styles.header__nav__item}>
-              ACCESSORIES
-            </NavLink>
-          </nav>
+          <button className={styles.header__burger} onClick={toggleMenu}>
+            <img
+              className={styles.header__burger__img}
+              src={isMenuOpen ? CloseMenu : BurgerMenu}
+              alt="Menu"
+            />
+          </button>
         </div>
+      </header>
 
-        <div className={styles.header__container__right}>
-          <div className={styles.header__icons__icon}>
-            <NavLink to="/favorites">
-              <div className={styles.header__icons__wrapper}>
-                <img
-                  src={HeartIcon}
-                  alt="Favorites"
-                  className={styles.header__icons__img}
-                />
-                {favItems.length > 0 && (
-                  <div className={styles.header__icons__amount}>
-                    {favItems.length}
-                  </div>
-                )}
-              </div>
-            </NavLink>
-          </div>
-
-          <div className={styles.header__icons__icon}>
-            <NavLink to="/cart">
-              <div className={styles.header__icons__wrapper}>
-                <img
-                  src={CartIcon}
-                  alt="Cart"
-                  className={styles.header__icons__img}
-                />
-                {cartItems.length > 0 && (
-                  <div className={styles.header__icons__amount}>
-                    {cartItems.length}
-                  </div>
-                )}
-              </div>
-            </NavLink>
-          </div>
-        </div>
-      </div>
-    </header>
+      {isMenuOpen && <Menu onClose={() => setIsMenuOpen(false)} />}
+    </>
   );
 };
