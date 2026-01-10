f2710be0101705e7c8f104785db48206519e7524
Alina Luchytska <aluchitska@gmail.com>
2026-01-11 00:34:07 +0200

    update and change cart page

--- src/modules/CartPage/CartPage.tsx
+++ src/modules/CartPage/CartPage.tsx
@@ -3,13 +3,17 @@ import './CartPage.module.scss';
 import { Header } from 'shared/Header';
 import { Footer } from 'shared/Footer';
 import { useProducts } from 'src/context/ProductsContext';
-import { ProductCard } from 'shared/ProductCard';
 import styles from './CartPage.module.scss';
 import { useNavigate } from 'react-router';
+import { ProductType } from 'models/product.model';
 
 export const CartPage: React.FC = () => {
-  const { cart } = useProducts();
+  const { cart, toggleCart } = useProducts();
   const navigate = useNavigate();
+
+  const removeProduct = (product: ProductType) => {
+    toggleCart(product);
+  };
 
   return (
     <>
@@ -26,6 +30,7 @@ export const CartPage: React.FC = () => {
           />
           <p className={styles.cartpage__backbutton__text}>Back</p>
         </button>
+        <h1 className={styles.cartpage__title}>Cart</h1>
         {cart.length === 0 ? (
           <div className={styles.cartpage__noproducts}>
             <p className={styles.cartpage__noproducts_p}>
@@ -41,7 +46,25 @@ export const CartPage: React.FC = () => {
           <>
             <div className={styles.cartpage__list}>
               {cart.map(product => (
-                <ProductCard key={product.id} product={product} />
+                <div key={product.id} className={styles.cartpage__list_product}>
+                  <img
+                    src="/public/img/icons/icon-close.png"
+                    alt="image"
+                    className={styles.cartpage__closebut}
+                    onClick={() => removeProduct(product)}
+                  />
+                  <img
+                    src={product.image}
+                    alt="image"
+                    className={styles.cartpage__list_product_img}
+                  />
+                  <h1 className={styles.cartpage__list_product_name}>
+                    {product.name}
+                  </h1>
+                  <h2 className={styles.cartpage__list_product_price}>
+                    ${product.price}
+                  </h2>
+                </div>
               ))}
             </div>
             <div className={styles.cartpage__summary}>
