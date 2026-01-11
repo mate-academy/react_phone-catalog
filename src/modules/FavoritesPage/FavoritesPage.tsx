36a58042cb45e852c773595cca9ac9d1c97821f1
Alina Luchytska <aluchitska@gmail.com>
2026-01-11 19:35:05 +0200

    upgate fav page code and styles

--- src/modules/FavoritesPage/FavoritesPage.tsx
+++ src/modules/FavoritesPage/FavoritesPage.tsx
@@ -1,30 +1,37 @@
 import React from 'react';
-import './FavoritesPage.module.scss';
+import styles from './FavoritesPage.module.scss';
 import { Header } from '../shared/Header';
 import { Footer } from '../shared/Footer';
 import { useProducts } from 'src/context/ProductsContext';
 import { ProductCard } from 'shared/ProductCard';
+import { Breadcrumbs } from 'shared/Breadcrumbs';
 
 export const FavoritesPage: React.FC = () => {
   const { favorites } = useProducts();
 
   return (
-    <div>
+    <>
       <Header />
-      <h1 className="visually-hidden">Favorites Page</h1>
-      <p>Favorites Page content goes here.</p>
-      <img src="./../../img/banner-phones.png" alt="" />
+      <div className={styles.favpage}>
+        <Breadcrumbs category={'Favourites'} productId={''} />
+        <h1 className={styles.favpage__title}>Favourites</h1>
 
-      {favorites.length === 0 ? (
-        <p>No favorite products yet</p>
-      ) : (
-        <div>
-          {favorites.map(product => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
-      )}
+        {favorites.length === 0 ? (
+          <p className={styles.favpage__p}>No favorite products yet</p>
+        ) : (
+          <>
+            <p className={styles.favpage__countmodel_p}>
+              {favorites.length} items
+            </p>
+            <div className={styles.favpage__list}>
+              {favorites.map(product => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+            </div>
+          </>
+        )}
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
