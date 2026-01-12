6a4cd013e9385acb6c8239eb6c49bffca189c133
Sashka <sashkatmshchk@gmail.com>
2026-01-12 19:31:27 +0200

    Fix tests and build configuration

---
+++ build/static/js/488.9c94003b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_phone_catalog=self.webpackChunkreact_phone_catalog||[]).push([[488],{488(e,a,c){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.9c94003b.chunk.js.map
