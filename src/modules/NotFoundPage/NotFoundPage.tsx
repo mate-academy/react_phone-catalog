71eb35144fb7e2e98c10e269522ce56def3b9760
Pavlo <marlana08022022@gmail.com>
2026-01-18 22:48:16 +0200

    add Not Found

--- src/modules/NotFoundPage/NotFoundPage.tsx
+++ src/modules/NotFoundPage/NotFoundPage.tsx
@@ -5,11 +5,11 @@ export const NotFoundPage = () => {
   return (
     <div className={styles.notFound}>
       <h1 className={styles.title}>Page not found</h1>
-
-      <p className={styles.text}>
-        Sorry, the page you are looking for does not exist.
-      </p>
-
+      <img
+        src="img/page-not-found.png"
+        alt="not-found-page"
+        className={styles.image}
+      ></img>
       <Link to="/" className={styles.link}>
         Go to Home page
       </Link>
