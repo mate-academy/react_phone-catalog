fb166cd0d134a60621c3b1bbbc713738b68c9eee
Heorhii Lytvynenko <104064704+heorhii-lytvynenko@users.noreply.github.com>
2026-01-19 22:16:23 +0100

    card update

--- src/organisms/GoodList/ui/GoodCard.tsx
+++ src/organisms/GoodList/ui/GoodCard.tsx
@@ -1,4 +1,7 @@
+import Button from '@/atoms/Button';
 import styles from './GoodCard.module.scss';
+import Heart from '@/assets/icons/heart.svg?react';
+
 
 import { Phone } from '@/types/phone';
 import { FC } from 'react';
@@ -11,9 +14,16 @@ export const GoodCard: FC<Props> = ({ item }) => {
   return (
     <div className={styles.card}>
       <img className={styles.card__img} src={item.images[0]} />
-      <label>{item.name}</label>
+      <div className={styles.card__footer}>
+        <label className={styles.card__name}>{item.name}</label>
 
-      {item.id}
+        <div className={styles.card__footer__controls}>
+          <Button className={styles.button__cart}>Add to cart</Button>
+          <Button className={styles.button__like}>
+            <Heart />
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
