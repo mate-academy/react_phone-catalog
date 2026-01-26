18506f3d0bcfa7f28b55db78b7b702928a791e78
Korshun <dmitriy.kor91@gmail.com>
2026-01-26 18:32:02 +0200

    add task last

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 // import { ProductCard } from './components/ProductCard';
 // import product from '../public/api/products.json';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AppRouter } from './router/AppRouter';
 
 export const App = () => {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
     <div className="app">
 
       <Header />
@@ -20,6 +20,6 @@ export const App = () => {
       <Footer />
 
     </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 };
