a3de3c798c64efb63a561de5b0fb25010b09f581
Alina Luchytska <aluchitska@gmail.com>
2026-01-27 15:59:23 +0200

    fix BrowserRouter basename for github pages

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react_phone-catalog">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
