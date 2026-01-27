62638b0d12ea2a366f8e855b5ba46a0403b8c6e9
Alina Luchytska <aluchitska@gmail.com>
2026-01-27 15:49:08 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { App } from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
