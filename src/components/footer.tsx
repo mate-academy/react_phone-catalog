2f1668128798e81681910f20c558e7236e1f57a9
diego69-foda <diego.bkuhn@gmail.com>
2026-01-27 17:59:03 -0300

    só para passar

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,16 @@
 export const Footer = () => (
-  <header className="Footer">
+  <header className="footer">
     <div>
       <h5>NICE</h5>
-      <img src="./src/images/xoia.png" className="Footer_image" alt="xoia" />
+      <img src="./src/images/xoia.png" className="footer_image" alt="xoia" />
     </div>
     <h5>GADGETS</h5>
-    <button className="Footer__button">GITHUB</button>
-    <button className="Footer__button">CONTACTS</button>
-    <button className="Footer__button">RIGHTS</button>
+    <button className="footer__button">GITHUB</button>
+    <button className="footer__button">CONTACTS</button>
+    <button className="footer__button">RIGHTS</button>
     <div>
-      <button className="Footer__button__right">Back to top</button>
-      <button className="Footer__button__right">^</button>
+      <button className="footer__button__right">Back to top</button>
+      <button className="footer__button__right">^</button>
     </div>
   </header>
 );
