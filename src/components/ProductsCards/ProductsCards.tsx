77812b7047278c2e4a573d5e4fc361268b8bb175
Dmytro Kovtun <kovtundima2910@gmail.com>
2026-01-30 17:41:57 +0200

    fix

--- src/components/ProductsCards/ProductsCards.tsx
+++ src/components/ProductsCards/ProductsCards.tsx
@@ -106,7 +106,9 @@ export const ProductsCards: React.FC<ProductsCardsProps> = ({
 
     const basePath = import.meta.env.BASE_URL || '/';
 
-    fetch(`${basePath}api/products.json`)
+    fetch(
+      `${basePath.endsWith('/') ? basePath : basePath + '/'}api/products.json`,
+    )
       .then(res => {
         if (!res.ok) {
           throw new Error(`Network ${res.status}`);
