3bcc4f14fff5cd81c47fb3998168058b486cf3e1
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2026-01-30 14:22:28 +0200

    Change homepage

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <Router basename="/react_phone-catalog">
+  <Router basename="/">
     <App />
   </Router>,
 );
