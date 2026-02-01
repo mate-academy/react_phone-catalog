36f4c3a7014e0c1a6b673abed01286b0cea95fdd
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2026-02-01 22:48:42 +0200

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import App from './App';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 const basename =
   process.env.NODE_ENV === 'production' ? '/react_phone-catalog' : undefined;
 
