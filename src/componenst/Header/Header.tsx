b7175615b004de7a31f4db36810bc24c3fd5ad9a
Vlad Zhovtani <zovtanyy@gmail.com>
2026-02-16 21:03:43 +0200

    header syles added

--- src/componenst/Header/Header.tsx
+++ src/componenst/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
 import { useCart } from '../../context/CartContext';
 import { useFavourites } from '../../context/FavouritesContext';
@@ -10,20 +10,55 @@ const Header: React.FC = () => {
 
   return (
     <header className={styles.header}>
-      <div className={`container ${styles.header__inner}`}>
+      <div className={styles.header__wrapper}>
         <Link to="/" className={styles.header__logo}>
-          Gadget Catalog
+          <img src="/icons/Logo.svg" alt="Gadget Catalog" />
         </Link>
-        <nav className={styles.header__nav}>
-          <Link to="/">Home</Link>
-          <Link to="/products/phones">Phones</Link>
-          <Link to="/products/tablets">Tablets</Link>
-          <Link to="/products/accessories">Accessories</Link>
-          <Link to="/favourites">
-            Favourites {favourites.length > 0 && `(${favourites.length})`}
+        <div className={`container ${styles.header__inner}`}>
+          <nav className={styles.header__nav}>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/products/phones"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Phones
+            </NavLink>
+            <NavLink
+              to="/products/tablets"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Tablets
+            </NavLink>
+            <NavLink
+              to="/products/accessories"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Accessories
+            </NavLink>
+          </nav>
+        </div>
+        <div className={styles.header__actions}>
+          <Link to="/favourites" className={styles.header__action}>
+            <img src="/icons/Favourites (Heart Like).svg" alt="Favourites" />
+            {favourites.length > 0 && (
+              <span className={styles.header__badge}>{favourites.length}</span>
+            )}
           </Link>
-          <Link to="/cart">Cart {cart.length > 0 && `(${cart.length})`}</Link>
-        </nav>
+          <Link to="/cart" className={styles.header__action}>
+            <img src="/icons/Shopping bag (Cart).svg" alt="Cart" />
+            {cart.length > 0 && (
+              <span className={styles.header__badge}>{cart.length}</span>
+            )}
+          </Link>
+        </div>
+        <button className={styles.header__burger} aria-label="Open menu">
+          <img src="/icons/Menu.svg" alt="Menu" />
+        </button>
       </div>
     </header>
   );
