5a0b62c073e5a9df6617e063b76752df908ef118
Andriï Zakhartchenko <andriizakhartchenko@outlook.fr>
2026-02-18 11:28:53 +0100

    changed footer path link

--- src/shared/components/Footer/utils/footer_links.tsx
+++ src/shared/components/Footer/utils/footer_links.tsx
@@ -3,17 +3,17 @@ import { FooterType } from '../types/FooterType';
 export const footerLinks: FooterType[] = [
   {
     id: 0,
-    to: '/',
+    to: 'https://github.com/ZAKANDRE/react_phone-catalog',
     label: 'GITHUB',
   },
   {
     id: 1,
-    to: '/phones',
+    to: 'https://www.linkedin.com/in/andrii-zakhartchenko-66a3a12b4/',
     label: 'CONTACTS',
   },
   {
     id: 2,
-    to: '/tablets',
+    to: '/',
     label: 'RIGHTS',
   },
 ];
