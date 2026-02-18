c0e446c1b03e1a755ee6e294752e462a32d622e3
Vlad Zhovtani <zovtanyy@gmail.com>
2026-02-18 19:25:27 +0200

    swiper and product cart

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import styles from './HomePage.module.scss';
+import Slider from '../../componenst/Slider/Slider';
+
+const bannerSlides = [
+  { id: 'b-phones', image: '/img/banner-phones.png', link: '/phones' },
+  { id: 'b-tablets', image: '/img/banner-tablets.png', link: '/tablets' },
+  {
+    id: 'b-accessories',
+    image: '/img/banner-accessories.png',
+    link: '/accessories',
+  },
+];
 
 const HomePage: React.FC = () => (
   <section className={styles.homePage}>
-    <h2>Welcome to Gadget Catalog</h2>
-    <p>Browse the collection of phones, tablets and accessories.</p>
+    <div className={styles.homePage__banner}>
+      <h2 className={styles.homePage__banner__text}>
+        Welcome to Nice Gadgets store!
+      </h2>
+    </div>
+    <div className={styles.sliderRow}>
+      <Slider slides={bannerSlides} />
+    </div>
   </section>
 );
 
