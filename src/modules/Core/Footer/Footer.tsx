807b61683ec9b2af4d69c34792440b1b5a70c3f9
Vadym Vasyliev <protocol222333@gmail.com>
2026-02-23 19:52:08 +0200

    feat: add responsive Footer

--- src/modules/Core/Footer/Footer.tsx
+++ src/modules/Core/Footer/Footer.tsx
@@ -1,5 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ArrowUpIcon } from '../../../components/Icons/ArrowUpIcon';
+import styles from './Footer.module.scss';
+import LOGO from '../../../assets/icons/logo/Logo.svg';
 
 export const Footer = () => {
-  return <footer></footer>;
+  const backToTopHandler = () => {
+    window.scroll({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <Link to="/" className={styles.logo}>
+          <img src={LOGO} alt="Nice Gadjets logo" />
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <a
+                href="https://github.com/n7lots"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.navLink}
+              >
+                Github
+              </a>
+            </li>
+
+            <li className={styles.navItem}>
+              <Link to="/contacts" className={styles.navLink}>
+                Contacts
+              </Link>
+            </li>
+
+            <li className={styles.navItem}>
+              <Link to="/rights" className={styles.navLink}>
+                Rights
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className={styles.backToTop} onClick={backToTopHandler}>
+          <span className={styles.backToTopText}>Back to top</span>
+
+          <button className={styles.backToTopButton}>
+            <ArrowUpIcon />
+          </button>
+        </div>
+      </div>
+    </footer>
+  );
 };
