12048944906c6c639c2d907fba0d54da44e2b7fe
Serhii Dmytruk <dmitruksl92@gmail.com>
2026-02-26 11:20:20 +0200

    Fixed bugs #8: roll back to hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { App } from './App';
 import { AppProvider } from './utils/Context';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <AppProvider>
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router>
       <App />
     </Router>
   </AppProvider>,
