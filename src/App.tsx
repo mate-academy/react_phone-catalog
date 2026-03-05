253053687b456199e9b80c5d0ad70e87f0712897
Vouchyk Vova <nechayvova2005@gmail.com>
2026-03-05 23:52:29 +0000

    Add hover effect for capacity, available color and ProductCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { AppRouter } from './router/AppRouter';
+import './App.scss';
 
 const App = () => {
   return (
