6799ebad0676a3e4c84cf503f8d6a9393a4d77cd
Oleh Lukash <138055276+1umamaster@users.noreply.github.com>
2026-03-07 14:39:54 +0200

    Refactor productService to use async/await for fetch

--- src/modules/shared/services/productService.ts
+++ src/modules/shared/services/productService.ts
@@ -3,7 +3,12 @@ import { Category } from '../../../types/Category';
 const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export const getProducts = async () => {
-  const response = await fetch('./api/products.json');
+  const response = await fetch(`${import.meta.env.BASE_URL}api/products.json`);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch products');
+  }
+
   const data = await response.json();
 
   await wait(500);
@@ -11,12 +16,14 @@ export const getProducts = async () => {
   return data;
 };
 
-export const getCategories = (): Promise<Category[]> => {
-  return fetch(`${import.meta.env.BASE_URL}api/categories.json`).then(res => {
-    if (!res.ok) {
-      throw new Error('Failed to fetch categories');
-    }
+export const getCategories = async (): Promise<Category[]> => {
+  const response = await fetch(
+    `${import.meta.env.BASE_URL}api/categories.json`,
+  );
 
-    return res.json();
-  });
+  if (!response.ok) {
+    throw new Error('Failed to fetch categories');
+  }
+
+  return response.json();
 };
