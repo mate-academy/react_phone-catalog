f5f15fcaf04d7c3ab085c26de14e73b79233cbd2
Anastasia <nastyapugach24@gmail.com>
2026-03-09 05:09:17 +0100

    fix: base url only for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/react_phone-catalog/'
-})
+  base: command === 'build' ? '/react_phone-catalog/' : '/',
+}))
