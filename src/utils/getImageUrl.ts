b1f20755d5bed4e907c32d265cc2878fd8613260
Anastasia <nastyapugach24@gmail.com>
2026-03-09 06:17:02 +0100

    fix: base url slash in getImg

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -2,5 +2,7 @@ export const getImg = (path: string): string => {
   const base = import.meta.env.BASE_URL; // '/react_phone-catalog/'
   const cleanPath = path.startsWith('/') ? path.slice(1) : path;
 
-  return `${base}${cleanPath}`;
+  const baseWithSlash = base.endsWith('/') ? base : `${base}/`;
+
+  return `${baseWithSlash}${cleanPath}`;
 };
