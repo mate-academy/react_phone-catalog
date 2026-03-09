3b96cf0f7fe8198c2052a55a505d76e2fed6aadb
Anastasia <nastyapugach24@gmail.com>
2026-03-09 03:35:10 +0100

    added not found page

--- src/modules/NotFoundPage/NotFoundPage.tsx
+++ src/modules/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,21 @@
+import { Link } from 'react-router-dom';
+import styles from './NotFoundPage.module.scss';
+
 export const NotFoundPage = () => {
-  return <div>Page not found</div>;
+  return (
+    <div className={styles.page}>
+      <img
+        src="/img/page-not-found.png"
+        alt="Page not found"
+        className={styles.image}
+      />
+      <h1 className={styles.title}>Page not found</h1>
+      <p className={styles.text}>
+        The page you are looking for does not exist.
+      </p>
+      <Link to="/" className={styles.link}>
+        Go to Home page
+      </Link>
+    </div>
+  );
 };
