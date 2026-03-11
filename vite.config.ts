0e9babbdb140790d7ddac717db2d7ae8a14e3f89
Hanna Torianyk <an.mol4anova@gmail.com>
2026-03-11 11:39:28 +0200

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/react_phone-catalog/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
