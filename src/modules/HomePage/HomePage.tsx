d5e865206954938ed23075c771714d24655c36a3
Alexandr <AlexandrKsenz@gmail.com>
2026-03-11 19:39:52 +0100

    added categories section on homepage

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,8 +1,11 @@
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { Preview } from '../../components/layout/Preview';
 import { ProductColection } from '../../components/layout/ProductColection';
 import { PageTitle } from '../../components/ui/PageTitle';
+import { SectionTitle } from '../../components/ui/SectionTitle';
 import { ProductsContext } from '../../store/ProductsProvider';
+import { imageUrl } from '../../utils/imageUrl';
 import styles from './HomePage.module.scss';
 
 export const HomePage = () => {
@@ -37,13 +40,6 @@ export const HomePage = () => {
       .slice(0, 24);
   };
 
-  // const getHotPrices = products?.slice().sort((a, b) => {
-  //   const discountMin = a.fullPrice ? a.fullPrice - a.price : 0;
-  //   const discountMax = b.fullPrice ? b.fullPrice - b.price : 0;
-
-  //   return discountMax - discountMin;
-  // });
-
   return (
     <>
       <div className={styles.container}>
@@ -52,6 +48,44 @@ export const HomePage = () => {
       <Preview />
       <div className={styles.content}>
         <ProductColection title="Brand new models" products={getNewest()} />
+        <section className={styles.category}>
+          <SectionTitle>Shop by category</SectionTitle>
+          <div className={styles.category__content}>
+            <div className={styles.category__block}>
+              <Link to="#" className={styles.category__link}>
+                <img
+                  src={imageUrl('img/Phones.png')}
+                  alt=""
+                  className={styles.category__img}
+                />
+              </Link>
+              <h3 className={styles.category__title}>Mobile phones</h3>
+              <p className={styles.category__info}>95 models</p>
+            </div>
+            <div className={styles.category__block}>
+              <Link to="#" className={styles.category__link}>
+                <img
+                  src={imageUrl('img/Tablets.png')}
+                  alt=""
+                  className={styles.category__img}
+                />
+              </Link>
+              <h3 className={styles.category__title}>Tablets</h3>
+              <p className={styles.category__info}>24 models</p>
+            </div>
+            <div className={styles.category__block}>
+              <Link to="#" className={styles.category__link}>
+                <img
+                  src={imageUrl('img/Accessories.png')}
+                  alt=""
+                  className={styles.category__img}
+                />
+              </Link>
+              <h3 className={styles.category__title}>Accessories</h3>
+              <p className={styles.category__info}>3 models</p>
+            </div>
+          </div>
+        </section>
         <ProductColection title="Hot prices" products={getHotPrices()} />
       </div>
     </>
