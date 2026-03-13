6214d8ade539879199bf3688ad85db5081811e5d
Medvid Oleksandr <samaster010604@gmail.com>
2026-03-13 17:39:30 +0200

    Solution

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `
+          @use "sass:color";
           @import "styles/variables.scss";
           @import "styles/mixins.scss";
         `,
