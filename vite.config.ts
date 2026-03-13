c6bdcea3d6de2e02c99d622a330c41a286669b39
Medvid Oleksandr <samaster010604@gmail.com>
2026-03-13 17:43:32 +0200

    Solution

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/react_phone-catalog/',
   plugins: [react()],
   resolve: {
     alias: {
