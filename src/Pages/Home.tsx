85e1662d0626b8119849e35c5b3a863cd837b067
Yar14k <tabachuk1406@gmail.com>
2026-03-21 20:04:08 +0100

    Add solution

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,13 @@
 import Header from '../components/Header/Header';
+import Main from '../components/Main/Main';
 
 const Home: React.FC = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import './Main.scss';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <h1 className="main__title">Welcome to Nice Gadgets store!</h1>
+    </div>
+  );
+};
+
+export default Main;
