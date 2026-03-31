5e9b67e6ec764056c7c5fdd01e40a87fd764eb32
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2026-03-31 23:32:41 +0200

    clear

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'; // Імпортуємо модуль path
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react_phone-catalog/',
+  // base: '/react_phone-catalog/',
 
   resolve: {
     alias: {
