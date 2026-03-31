a8b7f8017b327b0b65ab4a197048fcb227b1b6ff
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2026-03-31 22:57:49 +0200

    add new path

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="vite/client" />
-
 declare module "*.css";
 declare module "*.scss";
 
@@ -12,3 +10,4 @@ declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
+
