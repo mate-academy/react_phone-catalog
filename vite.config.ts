940bf8e99c4f8a3c0ce9845e5c8880aeb9834da4
Heorhii Lytvynenko <104064704+heorhii-lytvynenko@users.noreply.github.com>
2026-03-31 12:38:08 +0300

    vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), svgr(),],
+  base: '/',
+  plugins: [react(), svgr()],
   css: {
     preprocessorOptions: {
       scss: {
@@ -16,14 +17,13 @@ export default defineConfig({
         @use "@/styles/tockens/other.scss" as *;
         @use "@/styles/animation/animation.scss" as *;
         @use "@/styles/keyframes/keyframes.scss" as *;
-        `
-
-      }
-    }
+        `,
+      },
+    },
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
-  }
-})
+  },
+});
