3af59227f94783aa71d647d33ed46f9f615ab3ce
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-04-02 00:22:18 +0300

    Add route components to `AppRouter` for main application pages

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,11 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
 import { MainLayout } from './modules/shared/MainLayout';
+import { HomePage } from './modules/HomePage';
+import { PhonesPage } from './modules/PhonesPage';
+import { TabletsPage } from './modules/TabletsPage';
+import { AccessoriesPage } from './modules/AccessoriesPage';
+import { ProductDetailsPage } from './modules/ProductDetailsPage';
+import { FavoritesPage } from './modules/FavoritesPage';
+import { CartPage } from './modules/CartPage';
+import { NotFoundPage } from './modules/NotFoundPage';
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/phones" element={<PhonesPage />} />
+        <Route path="/tablets" element={<TabletsPage />} />
+        <Route path="/accessories" element={<AccessoriesPage />} />
+        <Route path="/product/:productId" element={<ProductDetailsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
