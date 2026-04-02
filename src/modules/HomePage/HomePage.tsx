5b08497d5e0498c9f51fe9c4069d520dd5ac24b6
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-04-02 16:22:59 +0300

    Replace `PicturesSlider` with `WelcomeSlider` on `HomePage` and add visually hidden title for accessibility

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { PicturesSlider } from './components/PicturesSlider';
+import { WelcomeSlider } from './components/WelcomeSlider';
 import { ProductsSlider } from './components/ProductsSlider';
 import { ShopByCategory } from './components/ShopByCategory';
 import styles from './HomePage.module.scss';
@@ -6,7 +6,8 @@ import styles from './HomePage.module.scss';
 export const HomePage = () => {
   return (
     <div className={styles.page}>
-      <PicturesSlider />
+      <h1 className={styles.title}>Product Catalog</h1>
+      <WelcomeSlider />
       <ProductsSlider title="Hot prices" />
       <ShopByCategory />
       <ProductsSlider title="Brand new" />
