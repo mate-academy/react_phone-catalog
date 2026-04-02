718f646e4e6f45b94ccd8a4b90eb958a7c083956
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-04-02 16:12:45 +0300

    Refactor `HomePage`: wrap content in styled container and add `HomePage.module.scss`

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,15 +1,15 @@
 import { PicturesSlider } from './components/PicturesSlider';
 import { ProductsSlider } from './components/ProductsSlider';
 import { ShopByCategory } from './components/ShopByCategory';
+import styles from './HomePage.module.scss';
 
 export const HomePage = () => {
   return (
-    <>
-      <h1>Home Page</h1>
+    <div className={styles.page}>
       <PicturesSlider />
       <ProductsSlider title="Hot prices" />
       <ShopByCategory />
       <ProductsSlider title="Brand new" />
-    </>
+    </div>
   );
 };
