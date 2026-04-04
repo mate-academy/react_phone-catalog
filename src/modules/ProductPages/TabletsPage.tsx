9153e2f26a14de49069fbef4d02154f5cf92604b
vikaruda <rudaviktoria0@gmail.com>
2026-04-04 23:17:06 +0300

    Solution 0404

--- src/modules/ProductPages/TabletsPage.tsx
+++ src/modules/ProductPages/TabletsPage.tsx
@@ -18,11 +18,9 @@ export const TabletsPage = () => {
   // Ініціалізація з URL
   useEffect(() => {
     const sortParam = searchParams.get('sort') ?? 'newest';
-
     setSortBy(sortParam);
 
     const perPageParam = searchParams.get('perPage') ?? '4';
-
     if (perPageParam === 'all') {
       setItemsPerPage(Infinity); // показуємо всі
     } else {
@@ -30,7 +28,6 @@ export const TabletsPage = () => {
     }
 
     const pageParam = Number(searchParams.get('page')) || 1;
-
     setCurrentPage(pageParam);
   }, [searchParams]);
 
@@ -42,10 +39,7 @@ export const TabletsPage = () => {
     setTimeout(() => {
       fetch('./api/tablets.json')
         .then(res => {
-          if (!res.ok) {
-            throw new Error('Network response was not ok');
-          }
-
+          if (!res.ok) throw new Error('Network response was not ok');
           return res.json();
         })
         .then(data => setTablets(data))
@@ -64,15 +58,12 @@ export const TabletsPage = () => {
         new Date(b.releaseDate).getTime() - new Date(a.releaseDate).getTime()
       );
     }
-
     if (sortBy === 'alphabetically') {
       return a.name.localeCompare(b.name);
     }
-
     if (sortBy === 'cheapest') {
       return a.price - b.price;
     }
-
     return 0;
   });
 
@@ -89,30 +80,23 @@ export const TabletsPage = () => {
       : sortedTablets.slice(indexFirst, indexLast);
 
   const handlePageChange = (page: number) => {
-    if (page < 1 || page > totalPages) {
-      return;
-    }
+    if (page < 1 || page > totalPages) return;
 
     setSearchParams(params => {
       const newParams = new URLSearchParams(params);
-
       newParams.set('page', String(page));
-
       return newParams;
     });
   };
 
   const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const newSort = e.target.value;
-
     setSortBy(newSort);
 
     setSearchParams(params => {
       const newParams = new URLSearchParams(params);
-
       newParams.set('sort', newSort);
       newParams.set('page', '1'); // при зміні сортування скидаємо сторінку
-
       return newParams;
     });
   };
@@ -124,10 +108,8 @@ export const TabletsPage = () => {
 
     setSearchParams(params => {
       const newParams = new URLSearchParams(params);
-
       newParams.set('perPage', perPage);
       newParams.set('page', '1'); // при зміні кількості елементів скидаємо сторінку
-
       return newParams;
     });
   };
@@ -154,7 +136,7 @@ export const TabletsPage = () => {
     const maxVisible = 4;
 
     let start = Math.max(currentPage - 1, 1);
-    const end = Math.min(start + maxVisible - 1, totalPages);
+    let end = Math.min(start + maxVisible - 1, totalPages);
 
     if (end - start + 1 < maxVisible) {
       start = Math.max(end - maxVisible + 1, 1);
@@ -167,94 +149,89 @@ export const TabletsPage = () => {
     return pages;
   };
 
-  if (loading) {
-    return <Loader loading={true} />;
-  }
+  if (loading) return <Loader loading={true} />;
 
-  if (errorReload) {
-    {
-      return (
-        <div className="error">
-          <p>Error loading data, please try again.</p>
-          <button onClick={handleReload} className="reload-button">
-            Reload
-          </button>
-        </div>
-      );
-    }
-
+  if (errorReload)
     return (
-      <div className="gargets">
-        <div className="gargets__back-to-home">
-          <NavLink to="/" className="gargets__home-img" />
-          <img
-            src="../../../public/img/arrow.png"
-            className="gargets__arrow"
-          ></img>
-          <span className="gargets__back-home-h2">Tablets</span>
-        </div>
-        <h1 className="gargets__mobile-phones-h1">Tablets</h1>
-        <h3 className="gargets__count-models">{tablets.length} models</h3>
-
-        <div className="gargets__position-sorting">
-          <div className="gargets__sort-by">
-            <h3 className="gargets__sort-by-h3">Sort by</h3>
-            <select
-              className="gargets__sort-by-choose-value"
-              value={sortBy}
-              onChange={handleSortChange}
-            >
-              <option value="newest">Newest</option>
-              <option value="alphabetically">Alphabetically</option>
-              <option value="cheapest">Cheapest</option>
-            </select>
-          </div>
-
-          <div className="gargets__items-on-page">
-            <h3 className="gargets__items-on-page-h3">Items on page</h3>
-            <select
-              className="gargets__items-on-page-choose-item"
-              onChange={handleItemsPerPageChange}
-              value={itemsPerPage === Infinity ? 'all' : String(itemsPerPage)}
-            >
-              <option value="4">4</option>
-              <option value="8">8</option>
-              <option value="16">16</option>
-              <option value="all">All</option>
-            </select>
-          </div>
-        </div>
-
-        <ProductList items={currentTablets} />
-
-        {itemsPerPage !== Infinity && (
-          <div className="pagination">
-            <button
-              onClick={() => handlePageChange(currentPage - 1)}
-              disabled={currentPage === 1}
-            >
-              {'<'}
-            </button>
-
-            {getVisiblePages().map(page => (
-              <button
-                key={page}
-                onClick={() => handlePageChange(page)}
-                className={`page-btn ${currentPage === page ? 'active' : ''}`}
-              >
-                {page}
-              </button>
-            ))}
-
-            <button
-              onClick={() => handlePageChange(currentPage + 1)}
-              disabled={currentPage === totalPages}
-            >
-              {'>'}
-            </button>
-          </div>
-        )}
+      <div className="error">
+        <p>Error loading data, please try again.</p>
+        <button onClick={handleReload} className="reload-button">
+          Reload
+        </button>
       </div>
     );
-  }
+
+  return (
+    <div className="gargets">
+      <div className="gargets__back-to-home">
+        <NavLink to="/" className="gargets__home-img" />
+        <img
+          src="../../../public/img/arrow.png"
+          className="gargets__arrow"
+        ></img>
+        <span className="gargets__back-home-h2">Tablets</span>
+      </div>
+      <h1 className="gargets__mobile-phones-h1">Tablets</h1>
+      <h3 className="gargets__count-models">{tablets.length} models</h3>
+
+      <div className="gargets__position-sorting">
+        <div className="gargets__sort-by">
+          <h3 className="gargets__sort-by-h3">Sort by</h3>
+          <select
+            className="gargets__sort-by-choose-value"
+            value={sortBy}
+            onChange={handleSortChange}
+          >
+            <option value="newest">Newest</option>
+            <option value="alphabetically">Alphabetically</option>
+            <option value="cheapest">Cheapest</option>
+          </select>
+        </div>
+
+        <div className="gargets__items-on-page">
+          <h3 className="gargets__items-on-page-h3">Items on page</h3>
+          <select
+            className="gargets__items-on-page-choose-item"
+            onChange={handleItemsPerPageChange}
+            value={itemsPerPage === Infinity ? 'all' : String(itemsPerPage)}
+          >
+            <option value="4">4</option>
+            <option value="8">8</option>
+            <option value="16">16</option>
+            <option value="all">All</option>
+          </select>
+        </div>
+      </div>
+
+      <ProductList items={currentTablets} />
+
+      {itemsPerPage !== Infinity && (
+        <div className="pagination">
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            {'<'}
+          </button>
+
+          {getVisiblePages().map(page => (
+            <button
+              key={page}
+              onClick={() => handlePageChange(page)}
+              className={`page-btn ${currentPage === page ? 'active' : ''}`}
+            >
+              {page}
+            </button>
+          ))}
+
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            {'>'}
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
