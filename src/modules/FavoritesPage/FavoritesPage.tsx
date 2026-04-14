215f55aa1b0f683ae659b0352480f907566c81d6
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-04-14 22:51:34 +0300

    feat: add FavoritesPage with context and styling integration

--- src/modules/FavoritesPage/FavoritesPage.tsx
+++ src/modules/FavoritesPage/FavoritesPage.tsx
@@ -1,10 +1,16 @@
+import { useFavorites } from '@/contexts/FavoritesContext';
+import { Breadcrumbs } from '../shared/components/Breadcrumbs';
 import { ProductsList } from '../shared/components/ProductsList';
+import styles from './FavoritesPage.module.scss';
 
 export const FavoritesPage = () => {
+  const { favorites } = useFavorites();
+
   return (
-    <>
+    <div className={styles.page}>
+      <Breadcrumbs items={[{ label: 'Favorites' }]} />
       <h1>Favorites page</h1>
-      <ProductsList title="ProductsList (favorites)" />
-    </>
+      <ProductsList products={favorites} />
+    </div>
   );
 };
