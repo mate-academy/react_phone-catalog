9939ddbb5da86ba49e4192a4c7c319746bd53847
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-04-16 23:11:44 +0300

    feat: display empty state message on FavoritesPage

--- src/modules/FavoritesPage/FavoritesPage.tsx
+++ src/modules/FavoritesPage/FavoritesPage.tsx
@@ -10,7 +10,11 @@ export const FavoritesPage = () => {
     <div className={styles.page}>
       <Breadcrumbs items={[{ label: 'Favorites' }]} />
       <h1>Favorites page</h1>
-      <ProductsList products={favorites} />
+      {favorites.length === 0 ? (
+        <p className={styles.empty}>You have no favorites yet</p>
+      ) : (
+        <ProductsList products={favorites} />
+      )}
     </div>
   );
 };
