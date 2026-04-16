6a1739c9713340b8c3e7d81bb7ac5468282ce1ab
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-04-16 22:47:20 +0300

    feat: add Home link to NotFoundPage with styles

--- src/modules/NotFoundPage/NotFoundPage.tsx
+++ src/modules/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
 import styles from './NotFoundPage.module.scss';
 
 export const NotFoundPage = () => {
   return (
     <div className={styles.container}>
       <h1>Page not found</h1>
+      <Link to="/" className={styles.homeLink}>
+        Go to Home page
+      </Link>
     </div>
   );
 };
