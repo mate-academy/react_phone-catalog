e12e2cf8386906e5e6e8e548e3e92136225bed2c
Ivan Ilnytskyy <ilniskyi2602@gmail.com>
2026-04-17 21:58:02 +0300

    add task solution

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
-const API_BASE = `${import.meta.env.BASE_URL}/api/`;
+const API_BASE = `${import.meta.env.BASE_URL}`;
 
 export async function request<T>(path: string): Promise<T> {
-  const response = await fetch(`${API_BASE}${path}`);
+  const response = await fetch(`${API_BASE}api/${path}`);
 
   if (!response.ok) {
     throw new Error('Failed to load data');
