b401038768335b8e9a2b7f682e459d51f068d6dd
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2026-04-20 08:57:02 +0300

    Add hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ShopProvider } from './store/shop/ShopContext';
 import { ThemeProvider } from './store/theme/ThemeContext';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
+  <HashRouter>
     <ShopProvider>
       <ThemeProvider>
         <App />
       </ThemeProvider>
     </ShopProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
