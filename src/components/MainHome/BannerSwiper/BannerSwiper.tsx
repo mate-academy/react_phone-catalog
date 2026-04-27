cd4ec9aa6715ce6ba7d5c3ed99b5f863cb910374
Yar14k <tabachuk1406@gmail.com>
2026-04-27 09:48:08 +0200

    Add solution

--- src/components/MainHome/BannerSwiper/BannerSwiper.tsx
+++ src/components/MainHome/BannerSwiper/BannerSwiper.tsx
@@ -1,14 +1,17 @@
 import './BannerSwiper.scss';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide, SwiperClass } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 const MainSwiper = () => {
-  const swiperRef = useRef(null);
   const progressCircle = useRef<SVGSVGElement | null>(null);
   const progressContent = useRef<HTMLSpanElement | null>(null);
-  const onAutoplayTimeLeft = (s, time, progress) => {
+  const onAutoplayTimeLeft = (
+    _: SwiperClass,
+    time: number,
+    progress: number,
+  ) => {
     if (progressCircle.current && progressContent.current) {
       progressCircle.current.style.setProperty(
         '--progress',
@@ -20,61 +23,56 @@ const MainSwiper = () => {
 
   return (
     <div className="swiper__container">
-      <button
-        onClick={() => swiperRef.current?.slidePrev()}
-        className="custom-prev"
-      ></button>{' '}
-      <div className="main-swiper">
-        <div className="slide__content--1">
-          <h2 className="slide__title--1">
-            Now available in our store
-            <Link to="" className="slide__logo"></Link>
-          </h2>
-          <p className="slide__text--1">Be the first!</p>
-          <button className="slide__button">ORDER NOW</button>
-        </div>
-        <div className="slide__content--2">
-          {' '}
-          <h3 className="slide__title--2">iPhone 14 Pro</h3>
-          <p className="slide__text--2">Pro Beyond</p>
-          <Swiper
-            onSwiper={swiper => (swiperRef.current = swiper)}
-            spaceBetween={30}
-            centeredSlides={true}
-            autoplay={{
-              delay: 6000,
-              disableOnInteraction: false,
-            }}
-            pagination={{ clickable: true }}
-            modules={[Autoplay, Pagination]}
-            onAutoplayTimeLeft={onAutoplayTimeLeft}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <img
-                src="../../../public/img/category-phones--purple--otherSide.png"
-                className="slide__image"
-              />
+      <div className="swiper__content">
+        <button className="custom-prev" id="prev-button" />
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 6000,
+            disableOnInteraction: false,
+          }}
+          navigation={{
+            prevEl: '#prev-button',
+            nextEl: '#next-button',
+          }}
+          pagination={{
+            clickable: true,
+            el: '#main-slider-pagination',
+            bulletClass: 'swiper-bullet',
+            bulletActiveClass: 'swiper-bullet-active',
+          }}
+          modules={[Autoplay, Pagination, Navigation]}
+          onAutoplayTimeLeft={onAutoplayTimeLeft}
+          className="mySwiper"
+        >
+          {[
+            './img/category-phones--purple--otherSide.png',
+            './img/category-phones--gold.png',
+            './img/category-phones.webp',
+          ].map(slide => (
+            <SwiperSlide key={slide}>
+              <div className="main-swiper">
+                <div className="slide__content--1">
+                  <h2 className="slide__title--1">
+                    Now available in our store
+                    <Link to="" className="slide__logo"></Link>
+                  </h2>
+                  <p className="slide__text--1">Be the first!</p>
+                  <Link to="/phones/apple-iphone-14-pro-128gb-spaceblack" className="slide__button">ORDER NOW</Link>
+                </div>
+                <div className="slide__content--2">
+                  <h3 className="slide__title--2">iPhone 14 Pro</h3>
+                  <p className="slide__text--2">Pro Beyond</p>
+                  <img src={slide} className="slide__image" />
+                </div>
+              </div>
             </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="../../../public/img/category-phones--gold.png"
-                className="slide__image"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="../../../public/img/category-phones.webp"
-                className="slide__image"
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div>
+          ))}
+        </Swiper>
+        <button id="next-button" className="custom-next"></button>
       </div>
-      <button
-        onClick={() => swiperRef.current?.slideNext()}
-        className="custom-next"
-      ></button>
+      <div id="main-slider-pagination" className="pagination" />
     </div>
   );
 };
