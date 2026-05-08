56e6bee36be885971fc91d4abdeebe3975bf1418
Maksum Nemchenko <maksnemch2004@gmail.com>
2026-05-08 19:39:41 +0300

    dev

--- src/modules/shared/constants/Baners.ts
+++ src/modules/shared/constants/Baners.ts
@@ -1,5 +1,5 @@
 export const banners = [
-  '/img/banner-accessories.png',
-  '/img/banner-phones.png',
-  '/img/banner-tablets.png',
+  './img/banner-accessories.png',
+  './img/banner-phones.png',
+  './img/banner-tablets.png',
 ];
