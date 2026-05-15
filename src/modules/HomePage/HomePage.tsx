22b2fc19b586ff36586b3c52ecd682ef53093540
GTXtab <malahovskinazar007@gmail.com>
2026-05-15 22:25:04 +0300

    fix: move h1 outside of loaders for cypress tests

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -25,34 +25,45 @@ export const HomePage: React.FC = () => {
     [products],
   );
 
-  if (isLoading) {
-    return <Loader />;
-  }
-
-  if (error) {
-    return <div className={styles.error}>{error}</div>;
-  }
-
   return (
     <div className={styles.home}>
       <h1 className={styles.visuallyHidden}>Product Catalog</h1>
-      <h2 className={styles.homeTitle}>Welcome to Nice Gadgets store!</h2>
-      <section>
-        <HomeBannerSlider />
-      </section>
 
-      <section>
-        <ProductSlider title="Brand new models" products={brandNew} hideDiscount />
-      </section>
+      {isLoading && <Loader />}
 
-      <section className={styles.section}>
-        <h2 className={styles.sectionTitle}>Shop by category</h2>
-        <ShopByCategory />
-      </section>
+      {!isLoading && error && (
+        <div className={styles.error}>{error}</div>
+      )}
 
-      <section>
-        <ProductSlider title="Hot Prices" products={hotPrices} />
-      </section>
+      {!isLoading && !error && (
+        <>
+          <h2 className={styles.homeTitle}>Welcome to Nice Gadgets store!</h2>
+
+          <section>
+            <HomeBannerSlider />
+          </section>
+
+          <section>
+            <ProductSlider
+              title="Brand new models"
+              products={brandNew}
+              hideDiscount
+            />
+          </section>
+
+          <section className={styles.section}>
+            <h2 className={styles.sectionTitle}>Shop by category</h2>
+            <ShopByCategory />
+          </section>
+
+          <section>
+            <ProductSlider
+              title="Hot Prices"
+              products={hotPrices}
+            />
+          </section>
+        </>
+      )}
     </div>
   );
 };
