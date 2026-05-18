d53888532c0bd9d6bc5a6d23c4ae3c757aeef5ec
Your Name <u513509754@gmail.com>
2026-05-18 12:34:09 -0700

    solution

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/react_phone-catalog/' : '/',
+  base: command === 'build' ? '/react-phone-catalog/' : '/',
 }))
