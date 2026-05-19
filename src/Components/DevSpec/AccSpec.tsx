cab5cc07e00d9c1c01dcc5ea93aef7c59d5b5454
Your Name <u513509754@gmail.com>
2026-05-19 16:10:24 -0700

    solution

--- src/Components/DevSpec/AccSpec.tsx
+++ src/Components/DevSpec/AccSpec.tsx
@@ -71,9 +71,7 @@ export const AccSpec: React.FC = () => {
   const [, setErrorMessage] = useState(false);
   const [, setLoading] = useState(false);
   const [image, setImage] = useState<Image>(Image.first);
-  const [color, setColor] = useState<Color>(
-    accessorie?.colorsAvailable[0] as Color,
-  );
+  const [color, setColor] = useState(accessorie?.colorsAvailable[0] || '');
   const images = accessorie?.images || [];
   const path = accessorie?.id;
   // const capacitiesRaw = accessorie?.capacity ?? [];
