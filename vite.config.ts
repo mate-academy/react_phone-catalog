4da4db9df765af4745963e8c836aeb84021038fa
Your Name <u513509754@gmail.com>
2026-05-19 05:27:51 -0700

    solution

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  base: command === 'build' ? '/react-portfolio/' : '/',
 }))
