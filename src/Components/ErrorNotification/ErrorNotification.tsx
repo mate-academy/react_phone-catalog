c43179e26b3aa77f60d61e8d2cd339d74467cb61
Your Name <u513509754@gmail.com>
2026-05-21 06:41:59 -0700

    solution

--- src/Components/ErrorNotification/ErrorNotification.tsx
+++ src/Components/ErrorNotification/ErrorNotification.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Header } from '../Header/header';
+import { Footer } from '../Footer/Footer';
 
 export const ErrorNotification = () => {
   return (
-    <div className="error-notification">
-      <h1 className="error-notification__title">Page Not Found</h1>
-      <img src={'img/page-not-found.png'} alt="" />
-    </div>
+    <>
+      <Header cartItemsCount={0} favouritesCount={0} setMenuOpen={() => {}} />
+      <div className="error-notification">
+        <h1 className="error-notification__title">Page Not Found</h1>
+        <img src={'img/page-not-found.png'} alt="" />
+      </div>
+      <Footer />
+    </>
   );
 };
