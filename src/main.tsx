210a61ea2a8095969f4397edc834db81891de6a6
Sidach Ruslan <flashangel4600@gmail.com>
2026-05-22 11:13:59 +0300

    fix import extensions

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
-import App from './App.tsx';
+import App from './App';
 import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
