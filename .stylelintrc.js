a6f66255fe45711cba0c9ac05b06f15acb9bc24c
friranger <65720979+friranger1@users.noreply.github.com>
2026-05-27 18:13:19 +0300

    Fixed tests

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   extends: '@mate-academy/stylelint-config',
-  rules: {},
+  rules: {
+    'lightness-notation': null,
+    'keyframes-name-pattern': null,
+    'scss/double-slash-comment-empty-line-before': null,
+  },
 };
