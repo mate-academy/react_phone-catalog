0acb217eaf2a3da386c19c3f5090ef5a16607265
chlodav <153681219+chlodav@users.noreply.github.com>
2026-05-31 19:32:16 +0300

    add task solution

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
   base: '/react_phone-catalog/',
   publicDir: 'public',
   build: {
