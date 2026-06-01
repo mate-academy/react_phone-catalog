56d2613f5ede011190162fee7592694f91249117
shymkivvasyl <shymkiv.vasyl333@gmail.com>
2026-06-01 20:52:18 +0200

    fix: api base url for github pages

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,7 @@
-/* eslint-disable no-console */
 export function getData<T>(apiEndpoint: string): Promise<T> {
-  return fetch('/api/' + apiEndpoint + '.json')
+  return fetch(`${import.meta.env.BASE_URL}api/${apiEndpoint}.json`)
     .then(response => response.json())
-    .then(data => {
-      return data as T;
-    })
+    .then(data => data as T)
     .catch(() => {
       throw new Error('Error fetching data');
     });
