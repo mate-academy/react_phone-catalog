243382a7b10c857ba276fccdb854a51ffabfccf5
Vadym Kirilkov <tav0r0tuk@gmail.com>
2026-06-05 09:47:58 +0200

    chore: checkpoint from macbook for mac mini sync

--- src/modules/components/FavouritesPage/FavouritesPage.tsx
+++ src/modules/components/FavouritesPage/FavouritesPage.tsx
@@ -1,7 +1,38 @@
+/* eslint-disable import/extensions */
 /* eslint-disable prettier/prettier */
+/* eslint-disable max-len */
+
+import { useFavourites } from '@/modules/shared/utils/context/FavouritesContext';
+
+import styles from './FavouritesPage.module.scss';
+import { Breadcrumbs } from '@/modules/shared/components/Breadcrumbs';
+import { ProductCard } from '@/modules/shared/components/ProductCard';
+
+const {
+  favouritesPage,
+  favouritesTitle,
+  favouritesCount,
+  favouritesList,
+} = styles;
 
 export const FavouritesPage = () => {
+  const { favourites, favouritesCount: count, } = useFavourites();
+
   return (
-    <h3>Favourites Page</h3>
+    <div className={favouritesPage}>
+      <Breadcrumbs pageTitle="Favourites" />
+
+      <h1 className={favouritesTitle}>Favourites</h1>
+
+      <span className={favouritesCount}>
+        {`${count} ${count > 1 ? 'items' : 'item'}`}
+      </span>
+
+      <div className={favouritesList}>
+        {favourites.map(product => (
+          <ProductCard key={product.itemId} product={product} />
+        ))}
+      </div>
+    </div>
   );
 };
