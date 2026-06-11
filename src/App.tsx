df127dfa0b9b2811d9112ac1719300e32cf5e140
Yurii Drutsul <druculurij@gmail.com>
2026-06-11 10:53:33 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { HomePage } from './modules/HomePage/HomePage';
 import { PhonesPage } from './modules/PhonesPage/PhonesPage';
 import { TabletsPage } from './modules/TabletsPage/TabletsPage';
 import { AccessoriesPage } from './modules/AccessoriesPage/AccessoriesPage';
+import { NotFoundPage } from './modules/NotFoundPage/NotFoundPage';
 
 export const App = () => (
   <BrowserRouter>
@@ -17,7 +18,7 @@ export const App = () => (
       <Route path="/tablets" element={<TabletsPage />} />
       <Route path="/accessories" element={<AccessoriesPage />} />
 
-      <Route path="*" element={<HomePage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>
 );
