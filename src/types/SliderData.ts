179afb6c5fdb84bb5c8154d805d58bb225e9a860
Yevhen Serdiuk <yevhen.srdk@gmail.com>
2026-06-16 15:14:17 +0300

    change path for croppedImgUrl in SliderData.ts

--- src/types/SliderData.ts
+++ src/types/SliderData.ts
@@ -3,17 +3,17 @@ import { PromoImageData } from './PromoImgType';
 export const sliderData: PromoImageData[] = [
   {
     url: 'promo/Apple-iPhone-15-promo-banner-buy-now-scaled.jpg',
-    croppedImgUrl: '/promo/iphone-cropped.jpg',
+    croppedImgUrl: 'promo/iphone-cropped.jpg',
     alt: 'Iphone',
   },
   {
     url: 'promo/Banner-Samsung-Galaxy-S25-FE-Launch-Over-Desktop.webp',
-    croppedImgUrl: '/promo/cropped-sumsung.webp',
+    croppedImgUrl: 'promo/cropped-sumsung.webp',
     alt: 'Samsung',
   },
   {
     url: 'promo/Banner-web-Serie-RN15.jpg',
-    croppedImgUrl: '/promo/xiaomi-cropped.png',
+    croppedImgUrl: 'promo/xiaomi-cropped.png',
     alt: 'Xiaomi',
   },
 ];
