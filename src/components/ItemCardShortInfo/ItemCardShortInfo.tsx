e0be1f29413f1681dc065c820c90d813e886a4ad
Daniel <freelinex7@gmail.com>
2026-06-25 19:52:31 +0300

    Fixed Additional info container in ItemCardShortInfo.tsx

--- src/components/ItemCardShortInfo/ItemCardShortInfo.tsx
+++ src/components/ItemCardShortInfo/ItemCardShortInfo.tsx
@@ -123,100 +123,107 @@ export const ItemCardShortInfo = ({ productDetails, product }: Props) => {
             ))}
           </div>
         </div>
-        <div className={itemCardShortInfoStyles.AdditionalInfoContainer}>
-          <section className={itemCardShortInfoStyles.ColorSelectorSection}>
-            <ColorSelector
-              colors={productDetails.colorsAvailable}
-              selectedColor={productDetails.color}
-              handleColorChange={handleColorChange}
-            />
+        <div className={itemCardShortInfoStyles.AdditionalInfoWrapper}>
+          <div className={itemCardShortInfoStyles.AdditionalInfoContainer}>
+            <section className={itemCardShortInfoStyles.ColorSelectorSection}>
+              <ColorSelector
+                colors={productDetails.colorsAvailable}
+                selectedColor={productDetails.color}
+                handleColorChange={handleColorChange}
+              />
 
-            <span className={'font-small'}>
-              ID:{product.id.toString().padStart(6, '0')}
-            </span>
-          </section>
-          <div className={itemCardShortInfoStyles.HorizontalLine}></div>
-          <section className={itemCardShortInfoStyles.CapacitySection}>
-            <span
-              className={classNames(
-                'font-small',
-                itemCardShortInfoStyles.CapacitySubtitle,
-              )}
-            >
-              Select capacity
-            </span>
-            <div className={itemCardShortInfoStyles.CapacityButtons}>
-              {productDetails.capacityAvailable.map(capacity => (
-                <div
-                  key={capacity}
-                  className={itemCardShortInfoStyles.CapacityButtonWrapper}
-                >
-                  <SecondaryButton
-                    isSelected={capacity === productDetails.capacity}
-                    onClick={() => handleCapacityChange(capacity)}
-                  >
-                    {capacity}
-                  </SecondaryButton>
-                </div>
-              ))}
-            </div>
-          </section>
-          <div className={itemCardShortInfoStyles.HorizontalLine}></div>
-          <section className={itemCardShortInfoStyles.PriceAndButtons}>
-            <h2
-              className={classNames('font-h2', itemCardShortInfoStyles.Prices)}
-            >
+              <span className={'font-small'}>
+                ID:{product.id.toString().padStart(6, '0')}
+              </span>
+            </section>
+            <div className={itemCardShortInfoStyles.HorizontalLine}></div>
+            <section className={itemCardShortInfoStyles.CapacitySection}>
               <span
-                className={itemCardShortInfoStyles.NewPrice}
-              >{`$${isDiscountPresent ? productDetails.priceDiscount : productDetails.priceRegular}`}</span>
-              {isDiscountPresent && (
-                <span className={itemCardShortInfoStyles.OldPrice}>
-                  {`$${productDetails.priceRegular}`}
-                </span>
-              )}
-            </h2>
-            <div className={itemCardShortInfoStyles.Buttons}>
-              <AddToCartButton product={product} />
-              <FavoritesButton product={product} />
-            </div>
-            <div
-              className={classNames(
-                'font-small',
-                itemCardShortInfoStyles.Details,
-              )}
-            >
-              <p className={itemCardShortInfoStyles.DetailsInfo}>
-                <span className={itemCardShortInfoStyles.DetailTitle}>
-                  Screen
-                </span>
-                <span className={itemCardShortInfoStyles.DetailText}>
-                  {productDetails.screen}
-                </span>
-              </p>
-              <p className={itemCardShortInfoStyles.DetailsInfo}>
-                <span className={itemCardShortInfoStyles.DetailTitle}>
-                  Resolution
-                </span>
-                <span className={itemCardShortInfoStyles.DetailText}>
-                  {productDetails.resolution}
-                </span>
-              </p>
-              <p className={itemCardShortInfoStyles.DetailsInfo}>
-                <span className={itemCardShortInfoStyles.DetailTitle}>
-                  Processor
-                </span>
-                <span className={itemCardShortInfoStyles.DetailText}>
-                  {productDetails.processor}
-                </span>
-              </p>
-              <p className={itemCardShortInfoStyles.DetailsInfo}>
-                <span className={itemCardShortInfoStyles.DetailTitle}>RAM</span>
-                <span className={itemCardShortInfoStyles.DetailText}>
-                  {productDetails.ram}
-                </span>
-              </p>
-            </div>
-          </section>
+                className={classNames(
+                  'font-small',
+                  itemCardShortInfoStyles.CapacitySubtitle,
+                )}
+              >
+                Select capacity
+              </span>
+              <div className={itemCardShortInfoStyles.CapacityButtons}>
+                {productDetails.capacityAvailable.map(capacity => (
+                  <div
+                    key={capacity}
+                    className={itemCardShortInfoStyles.CapacityButtonWrapper}
+                  >
+                    <SecondaryButton
+                      isSelected={capacity === productDetails.capacity}
+                      onClick={() => handleCapacityChange(capacity)}
+                    >
+                      {capacity}
+                    </SecondaryButton>
+                  </div>
+                ))}
+              </div>
+            </section>
+            <div className={itemCardShortInfoStyles.HorizontalLine}></div>
+            <section className={itemCardShortInfoStyles.PriceAndButtons}>
+              <h2
+                className={classNames(
+                  'font-h2',
+                  itemCardShortInfoStyles.Prices,
+                )}
+              >
+                <span
+                  className={itemCardShortInfoStyles.NewPrice}
+                >{`$${isDiscountPresent ? productDetails.priceDiscount : productDetails.priceRegular}`}</span>
+                {isDiscountPresent && (
+                  <span className={itemCardShortInfoStyles.OldPrice}>
+                    {`$${productDetails.priceRegular}`}
+                  </span>
+                )}
+              </h2>
+              <div className={itemCardShortInfoStyles.Buttons}>
+                <AddToCartButton product={product} />
+                <FavoritesButton product={product} />
+              </div>
+              <div
+                className={classNames(
+                  'font-small',
+                  itemCardShortInfoStyles.Details,
+                )}
+              >
+                <p className={itemCardShortInfoStyles.DetailsInfo}>
+                  <span className={itemCardShortInfoStyles.DetailTitle}>
+                    Screen
+                  </span>
+                  <span className={itemCardShortInfoStyles.DetailText}>
+                    {productDetails.screen}
+                  </span>
+                </p>
+                <p className={itemCardShortInfoStyles.DetailsInfo}>
+                  <span className={itemCardShortInfoStyles.DetailTitle}>
+                    Resolution
+                  </span>
+                  <span className={itemCardShortInfoStyles.DetailText}>
+                    {productDetails.resolution}
+                  </span>
+                </p>
+                <p className={itemCardShortInfoStyles.DetailsInfo}>
+                  <span className={itemCardShortInfoStyles.DetailTitle}>
+                    Processor
+                  </span>
+                  <span className={itemCardShortInfoStyles.DetailText}>
+                    {productDetails.processor}
+                  </span>
+                </p>
+                <p className={itemCardShortInfoStyles.DetailsInfo}>
+                  <span className={itemCardShortInfoStyles.DetailTitle}>
+                    RAM
+                  </span>
+                  <span className={itemCardShortInfoStyles.DetailText}>
+                    {productDetails.ram}
+                  </span>
+                </p>
+              </div>
+            </section>
+          </div>
         </div>
       </div>
     </article>
