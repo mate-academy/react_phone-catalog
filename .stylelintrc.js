7e26c20ef58cb826e68aff10f9a83f9f7614ad8d
Daniel <freelinex7@gmail.com>
2026-06-25 16:35:03 +0300

    Clean up old slider images from public and commit workflow updates

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
-  extends: "@mate-academy/stylelint-config",
+  extends: '@mate-academy/stylelint-config',
   rules: {
-    "selector-pseudo-class-no-unknown": [true, {
-      ignorePseudoClasses: ["global"]
-    }]
-  }
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+  },
 };
