11551e9616a55b7eda5984d2105752334e6356e8
valerij <valerijkolpacov@gmail.com>
2026-06-27 16:27:58 +0300

    fix: add vercel.json for react router and revert base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        // Шлях має точно вказувати на твій файл з міксинами
-        additionalData: `@import "./src/styles/utils/_mixins.scss";`
-      }
-    }
-  }
-})
+        additionalData: `@import "./src/styles/utils/_mixins.scss";`,
+      },
+    },
+  },
+});
