25bada40260dc5cefbdd8cc8732ecf0668cbc40e
Valentyna <valya0322@ukr.net>
2026-07-01 16:02:22 +0300

    delet fix API URL paths for product data fetching

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,24 +1,24 @@
 // hooks/useProducts.ts
 import { useEffect, useState } from 'react';
 import { Product } from '../types/Product';
-import { apiUrl } from '../utils/api';
+// import { apiUrl } from '../utils/api';
 
 export const useProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
 
-  // useEffect(() => {
-  //   fetch(`${import.meta.env.BASE_URL}api/products.json`)
-  //     .then(res => res.json())
-  //     .then((data: Product[]) => setProducts(data));
-  // }, []);
-
   useEffect(() => {
-    // const url = new URL('api/products.json', import.meta.env.BASE_URL);
-
-    fetch(apiUrl('api/products.json'))
+    fetch(`${import.meta.env.BASE_URL}api/products.json`)
       .then(res => res.json())
       .then((data: Product[]) => setProducts(data));
   }, []);
+
+  // useEffect(() => {
+  //   // const url = new URL('api/products.json', import.meta.env.BASE_URL);
+
+  //   fetch(apiUrl('api/products.json'))
+  //     .then(res => res.json())
+  //     .then((data: Product[]) => setProducts(data));
+  // }, []);
 
   return { products };
 
