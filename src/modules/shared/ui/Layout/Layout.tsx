9a162ae6d4dae9d1a11034ddc5fcc400cec75943
Cássia <cassiarsqueiroz@gmail.com>
2026-07-03 18:36:00 -0300

    fix: reset scroll position on route change

--- src/modules/shared/ui/Layout/Layout.tsx
+++ src/modules/shared/ui/Layout/Layout.tsx
@@ -1,18 +1,27 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import styles from './Layout.module.scss';
 
-export const Layout = () => (
-  <div className={styles.layout}>
-    <Header />
+export const Layout = () => {
+  const { pathname } = useLocation();
 
-    <main className={styles.main}>
-      <div className={`${styles.content} container`}>
-        <Outlet />
-      </div>
-    </main>
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-    <Footer />
-  </div>
-);
+  return (
+    <div className={styles.layout}>
+      <Header />
+
+      <main className={styles.main}>
+        <div className={`${styles.content} container`}>
+          <Outlet />
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
