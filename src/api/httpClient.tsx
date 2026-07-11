33cc8dc77258afc4acf81ea730fb7058a2af4872
Serhii Shimko <shimkosergij2010@gmail.com>
2026-07-11 12:38:29 +0300

    resolve broken images and API requests on GitHub Pages

--- src/api/httpClient.tsx
+++ src/api/httpClient.tsx
@@ -1,10 +1,11 @@
 type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
-const BASE_URL = import.meta.env.BASE_URL + 'api';
+const BASE_URL = import.meta.env.BASE_URL.endsWith('/')
+  ? `${import.meta.env.BASE_URL}api`
+  : `${import.meta.env.BASE_URL}/api`;
 
 async function request<T>(url: string, method: Method = 'GET'): Promise<T> {
   const finishedUrl = BASE_URL + url;
-
   const res = await fetch(finishedUrl, { method: method });
 
   if (res.ok) {
