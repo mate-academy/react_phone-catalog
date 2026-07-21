c93c7cf500036469a2d568d41c2338348b3ebd5a
Mariia <proninamaria2002@gmail.com>
2026-07-21 15:48:33 +0300

    Second-try-port

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
+const redirect = new URLSearchParams(window.location.search).get('redirect');
+const routerBaseName = new URL(import.meta.env.BASE_URL, window.location.origin)
+  .pathname;
+
+if (redirect) {
+  window.history.replaceState(
+    null,
+    '',
+    `${import.meta.env.BASE_URL}${redirect}`,
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={routerBaseName}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
