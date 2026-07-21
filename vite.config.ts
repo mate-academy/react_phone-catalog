abb23d93394791a210784f5a6db773d119719d14
Igor Tinyayev <tinyayev.ca@gmail.com>
2026-07-21 11:05:53 -0600

    react_phone-catalog

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/react_phone-catalog/' : '/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['import', 'legacy-js-api'],
+      },
+    },
+  },
   plugins: [react()],
 })
